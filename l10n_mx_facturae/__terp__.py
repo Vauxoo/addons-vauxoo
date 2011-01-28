edc6cfbc4047becaa91da45baf013afce207b83d
moylop260 <moylop260@moylop260-hpi7>
2011-01-28 11:09:57 -0600

    [REV] Modify file __terp__.py: Chage license and add description depends

--- l10n_mx_facturae/__terp__.py
+++ l10n_mx_facturae/__terp__.py
@@ -1,17 +1,24 @@
 # -*- encoding: utf-8 -*-
 {
-    "name" : "Creacion de Factura Electronica para Mexico",
+    "name" : "Creacion de Factura Electronica para Mexico (CFD)",
     "version" : "1.0",
     "author" : "moylop260@hotmail.com",
     "category" : "Localisation/Accounting",
     "description" : """This module creates e-invoice files from invoices with standard of Mexican SAT.
+Requires the following programs:
+  xsltproc
+    Ubuntu insall with:
+        sudo apt-get install xsltproc
+  
+  openssl
+      Ubuntu insall with:
+        sudo apt-get install openssl
     """,
-    "website" : "http://moylop260.wordpress.com/",
-    "license" : "GPL-2",
+    "website" : "http://moylop.blogspot.com/",
+    "license" : "AGPL-3",
     "depends" : ["account", "base_vat", "document", 
-            "sale",#no depende de sale directamente, pero marca error en algunas versiones
+            "sale",#no depende de "sale" directamente, pero marca error en algunas versiones
         ],
-    "depends_app": ["xslproc", "openssl"],#Esta variable no la procesa openerp, solo la pongo, como informativa.
     "init_xml" : [],
     "demo_xml" : [],
     "update_xml" : [
