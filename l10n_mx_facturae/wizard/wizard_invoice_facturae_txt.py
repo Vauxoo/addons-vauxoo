ce64d54ba177af99d5b0c561acb33011314df73d
moylop260 <moylop260@moylop260-hpi7>
2011-01-28 11:11:09 -0600

    [REV] Modify licence code

--- l10n_mx_facturae/wizard/wizard_invoice_facturae_txt.py
+++ l10n_mx_facturae/wizard/wizard_invoice_facturae_txt.py
@@ -1,24 +1,27 @@
 # -*- encoding: utf-8 -*-
-################################################
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
 #
-# OpenERP, Open Source Management Solution
-# Copyright (c) 2010 moylop260 (moylop260@hotmail.com) All Rights Reserved.
-# info <moylop260@hotmail.com>
-# 
-# $Id$
+#    Copyright (c) 2010 moylop260 - http://moylop.blogspot.com/
+#    All Rights Reserved.
+#    info moylop260 (moylop260@hotmail.com)
+############################################################################
+#    Coded by: moylop260 (moylop260@hotmail.com)
+#    Finance by: Grupo de empresas varias
+############################################################################
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see http://www.gnu.org/licenses/.
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
