b3dbdc2cb37794eefb774285a0a19e3f3bb448bc
openerp <openerp@cl-t076-283cl>
2011-07-23 21:34:35 -0400

    [ADD] Module to correct fetchmail

---
+++ fetchmail_document/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+{
+    "name" : "Fetchmail Corrections",
+    "version" : "0.1",
+    "depends" : ["fetchmail","document",],
+    "author" : "Vauxoo",
+    "description" : """
+    This module correct some behaviours on Document and Fetchmail modules combinated.
+                    """,
+    "website" : "http://vauxoo.com",
+    "category" : "Tools",
+    "init_xml" : [
+    ],
+    "demo_xml" : [
+    ],
+    "update_xml" : [
+    ],
+    "active": False,
+    "installable": True,
+}
