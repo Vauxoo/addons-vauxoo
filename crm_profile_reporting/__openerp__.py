53e0cc3d01ebec99e9f4ae9379f86fbfb95017fd
Miguel Delgado <miguel.delgado07@gmail.com>
2011-09-05 18:00:00 -0430

    [ADD] se agrego el modulo para imprimir reportes de crm_profile

---
+++ crm_profile_reporting/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+{
+    "name" : "Opening balance Update",
+    "version" : "0.1",
+    "depends" : ["base",],
+    "author" : "",
+    "description" : """
+    What do this module:
+                    """,
+    "website" : "http://openerp.netquatro.com",
+    "category" : "Generic Modules/Accounting",
+    "init_xml" : [    ],
+    "demo_xml" : [    ],
+    "update_xml" : ["report/profiling_rml.xml"],
+    "active": False,
+    "installable": True,
+}
