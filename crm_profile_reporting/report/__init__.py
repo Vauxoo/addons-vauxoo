82140267c3a842bfc7c46c8e70fa7d0fc64d5bc0
Miguel Delgado <miguel.delgado07@gmail.com>
2011-09-05 18:17:10 -0430

    [MOD] se modificaron los archivos que contenia este branch para colocarlos de acuerdo a los guide lines

--- crm_profile_reporting/report/__init__.py
+++ crm_profile_reporting/report/__init__.py
@@ -1,2 +1,2 @@
 # -*- encoding: utf-8 -*-
-import report
+import crm_profile_report
---
+++ crm_profile_reporting/report/crm_profile_report.py
@@ -0,0 +1,39 @@
+# -*- encoding: utf-8 -*-
+import time
+import pooler
+from report import report_sxw
+from tools.translate import _
+
+class crm_report_profile(report_sxw.rml_parse):
+  """
+  Description about crm_report_profile
+  """
+  
+  def __init__(self, cr, user, name, context):
+    """
+    Initlize a report parser, add custome methods to localcontext 
+    @param cr: cursor to database
+    @param user: id of current user
+    @param name: name of the reports it self
+    @param context: context arguments, like lang, time zone 
+    """
+    super(crm_report_profile, self).__init__(cr, user, name, context=context)
+    self.localcontext.update({
+      'time': time,
+      'method':selfself.method_name,
+    })
+  
+  def self.method_name(self, obj):
+    """
+    Custom method that process obj and return required data to report
+    @param obj: parameter to method
+    """
+    
+    
+    
+report_sxw.report_sxw(
+  'report.crm.profile.reporting',
+  'res.partner',
+  "addons/crm_profile_reporting/report/report_profile.rml",
+  parser=crm_report_profile
+)
