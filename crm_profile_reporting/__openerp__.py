62e24cf57497c2088c5f4c34be781af5c797a590
Nhomar Hernandez nhomar@vauxoo.com <>
2011-09-05 18:52:50 -0430

    [FIX] Data minimal, and some fixes

--- crm_profile_reporting/__openerp__.py
+++ crm_profile_reporting/__openerp__.py
@@ -1,17 +1,19 @@
 # -*- encoding: utf-8 -*-
 {
-    "name" : "Opening balance Update",
+    "name" : "Crm Profiling Report",
     "version" : "0.1",
     "depends" : ["base","crm_profiling"],
     "author" : "",
     "description" : """
     What do this module:
                     """,
-    "website" : "http://openerp.netquatro.com",
+    "website" : "http://vauxoo.com",
     "category" : "Generic Modules/Accounting",
     "init_xml" : [    ],
     "demo_xml" : [    ],
-    "update_xml" : ["crm_profile_reporting_report.xml"],
+    "update_xml" : [
+    "crm_profile_reporting_report.xml",
+    "data/crm_profiling_data.xml"],
     "active": False,
     "installable": True,
 }
