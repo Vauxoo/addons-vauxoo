2a8d4235346c57a1475d95bd16ea1db16a54faa6
Nhomar nhomar@vauxoo.com <>
2011-09-13 20:24:21 -0430

    [ADD] Multicompany Invoice Report

---
+++ invoice_report_multicompany/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+{
+    "name" : "Invoice report for AMD Consorcium",
+    "version" : "0.1",
+    "depends" : ["account"],
+    "author" : "Vauxoo",
+    "description" : """
+    What do this module:
+    Just the quotation format.
+                    """,
+    "website" : "http://vauxoo.com",
+    "category" : "Generic Modules/Account",
+    "init_xml" : [
+    ],
+    "demo_xml" : [
+    ],
+    "update_xml" : [
+    "invoice_report.xml",
+    "invoice_view.xml"
+    ],
+    "active": False,
+    "installable": True,
+}
