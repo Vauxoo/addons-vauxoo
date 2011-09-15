67732e9fd99a780eaf480873dcd0c7bcf458703d
miguel@openerp.com.ve <>
2011-09-15 09:21:03 -0430

    [ADD] wizar to print report multicompany

---
+++ invoice_multicompany_report/wizard/account_invoice_multicompany.py
@@ -0,0 +1,35 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution    
+#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). All Rights Reserved
+#    author.name@company.com
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import fields, osv
+
+class print_account_invoice_report(osv.osv_memory):
+  
+  _name = "print.account.invoice.report"
+  """
+  OpenERP Wizard : print.account.invoice.report
+  """
+
+
+print_account_invoice_report()
+
+
