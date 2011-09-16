dbed941c0d5393445b6d45df4196af02b9671d46
Israel Fermín Montilla <ferminster@gmail.com>
2011-09-16 12:47:03 -0430

    [REF] changed the import statement on the __init__ file for the report directory

--- m321_reports/report/__init__.py
+++ m321_reports/report/__init__.py
@@ -22,4 +22,4 @@
 #    You should have received a copy of the GNU General Public License     #
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>. #
 ############################################################################
-import account_invoice_multicompany_report
+import report_company1
