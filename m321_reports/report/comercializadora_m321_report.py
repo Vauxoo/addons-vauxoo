de528ce400cde87e3dd79121fb3c03fda7e1c8db
Miguel Delgado <miguel.delgado07@gmail.com>
2011-09-19 14:44:03 -0430

    [ADD] add company reports

--- m321_reports/report/comercializadora_m321_report.py
+++ m321_reports/report/comercializadora_m321_report.py
@@ -22,7 +22,22 @@
 #    You should have received a copy of the GNU General Public License     #
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>. #
 ############################################################################
-import amd_computadoras_report
-import amd_computer_shop_report
-import computacion_activa_report
-import comercializadora_m321_report
+import time
+import pooler
+from report import report_sxw
+from tools.translate import _
+
+class cm321_report(report_sxw.rml_parse):
+    def __init__(self, cr, uid, name, context):
+        super(cm321_report, self).__init__(cr, uid, name, context=context)
+        self.localcontext.update({  })
+
+report_sxw.report_sxw(
+  'report.cm321.report',
+  'account.invoice',
+  'addons/m321_reports/report/comercializadora_m321_report.rml',
+  parser=cm321_report
+)
+  # 1 addons/nombre del modulo/carpeta(report)/nombre del archivo rml
+  # 2 A modo didactico vamos a poner que el modulo al que le vamos a poner el reporte es a res.partner
+  #   pero podria ser cualquier modulo.
