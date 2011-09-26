6ba8bdbd9365e2730443da6a3ab0917c086b1b42
Nhomar nhomar@vauxoo.com <>
2011-09-25 22:08:33 -0430

    [ADD] Just Adding openacademy module

---
+++ openacademy/__openerp__.py
@@ -0,0 +1,30 @@
+# -*- encoding: utf-8 -*-
+{
+    "name" : "OpenAcademy Module",
+    "version" : "0.1",
+    "depends" :["base",],
+    "author" : "Vauxoo",
+    "description" : """
+    What it does!
+                    """,
+    "website" : "http://vauxoo.com",
+    "category" : "Generic Modules",
+    "init_xml" : [
+    ],
+    "demo_xml" : [
+    "demo/demo_partners.xml"
+    ],
+    "update_xml" : [
+    "openacademy_view.xml",
+    "res_partner_view.xml",
+    "data/openacademy_data.xml",
+    "workflow/openacademy_workflow.xml",
+    "wizard/create_attendee_wizard.xml",
+    "report_openacademy.xml",
+    "openacademy_board.xml",
+    "security/res_groups.xml",
+    "security/ir.model.access.csv",
+    ],
+    "active": False,
+    "installable": True,
+}
