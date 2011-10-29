c7485901b84f16389e4c57f5a2cc8e07be926bcd
moylop260 <moylop260@vauxoo.com>
2011-10-29 15:29:24 -0500

    [IMP] [l10n_mx_facturae_cbb] Remove functions innecesary

--- l10n_mx_facturae_cbb/invoice.py
+++ l10n_mx_facturae_cbb/invoice.py
@@ -38,39 +38,6 @@ msg2= "Contacte a su administrador y/o a moylop260@hotmail.com"
 class account_invoice(osv.osv):
     _inherit = 'account.invoice'
     
-    def _get_invoice_sequence(self, cr, uid, ids, field_names=None, arg=False, context={}):
-        if not context:
-            context = {}
-        res = {}
-        for invoice in self.browse(cr, uid, ids):
-            sequence_id = False
-            company = invoice.company_id
-            while True:
-                if invoice.type == 'out_invoice':
-                    if company._columns.has_key('invoice_out_sequence_id'):
-                        sequence_id = company.invoice_out_sequence_id
-                elif invoice.type == 'out_refund':
-                    if company._columns.has_key('invoice_out_refund_sequence_id'):
-                        sequence_id = company.invoice_out_refund_sequence_id
-                company = company.parent_id
-                if sequence_id or not company:
-                    break
-            if not sequence_id:
-                if invoice.journal_id._columns.has_key('invoice_sequence_id') and invoice.journal_id.invoice_sequence_id:
-                    sequence_id = invoice.journal_id.invoice_sequence_id
-                elif invoice.journal_id._columns.has_key('sequence_id') and invoice.journal_id.sequence_id:
-                    sequence_id = invoice.journal_id.sequence_id
-            sequence_id = sequence_id and sequence_id.id or False
-            if not sequence_id:
-                sequence_str = 'account.invoice.' + invoice.type
-                test = 'code=%s'
-                cr.execute('SELECT id FROM ir_sequence WHERE '+test+' AND active=%s LIMIT 1', (sequence_str, True))
-                res2 = cr.dictfetchone()
-                sequence_id = res2 and res2['id'] or False
-            res[invoice.id] = sequence_id
-        return res
-    
-        
     def _get_fname_invoice(self, cr, uid, ids, field_names=None, arg=False, context={}):
         if not context:
             context = {}
@@ -100,50 +67,13 @@ class account_invoice(osv.osv):
     _columns = {
         ##Extract date_invoice from original, but add datetime
         #'date_invoice': fields.datetime('Date Invoiced', states={'open':[('readonly',True)],'close':[('readonly',True)]}, help="Keep empty to use the current date"),
-        'invoice_sequence_id': fields.function(_get_invoice_sequence, method=True, type='many2one', relation='ir.sequence', string='Invoice Sequence', store=True),
+        #'invoice_sequence_id': fields.function(_get_invoice_sequence, method=True, type='many2one', relation='ir.sequence', string='Invoice Sequence', store=True),
         'fname_invoice':  fields.function(_get_fname_invoice, method=True, type='char', size='26', string='File Name Invoice'),
     }
     
     _defaults = {
         #'date_invoice': lambda *a: time.strftime('%Y-%m-%d %H:%M:%S'),
     }
-    
-    def action_number(self, cr, uid, ids, *args):
-        invoice_id__sequence_id = self._get_invoice_sequence(cr, uid, ids)#Linea agregada
-        #Sustituye a la funcion original, es el mismo codigo, solo le agrega unas lineas, y no hacer SUPER
-        cr.execute('SELECT id, type, number, move_id, reference ' \
-                'FROM account_invoice ' \
-                'WHERE id IN ('+','.join(map(str,ids))+')')
-        obj_inv = self.browse(cr, uid, ids)[0]
-        for (id, invtype, number, move_id, reference) in cr.fetchall():
-            if not number:
-                tmp_context = {
-                    'fiscalyear_id' : obj_inv.period_id and obj_inv.period_id.fiscalyear_id and obj_inv.period_id.fiscalyear_id.id or False,
-                }
-                sid = invoice_id__sequence_id[id]
-                if sid:
-                    number = self.pool.get('ir.sequence').get_id(cr, uid, sid, 'id=%s', context=tmp_context)
-                if not number:
-                    raise osv.except_osv('Warning !', 'No hay una secuencia de folios, definida !')
-
-                if invtype in ('in_invoice', 'in_refund'):
-                    ref = reference
-                else:
-                    ref = self._convert_ref(cr, uid, number)
-                cr.execute('UPDATE account_invoice SET number=%s ' \
-                        'WHERE id=%s', (number, id))
-                cr.execute('UPDATE account_move SET ref=%s ' \
-                        'WHERE id=%s AND (ref is null OR ref = \'\')',
-                        (ref, move_id))
-                cr.execute('UPDATE account_move_line SET ref=%s ' \
-                        'WHERE move_id=%s AND (ref is null OR ref = \'\')',
-                        (ref, move_id))
-                cr.execute('UPDATE account_analytic_line SET ref=%s ' \
-                        'FROM account_move_line ' \
-                        'WHERE account_move_line.move_id = %s ' \
-                            'AND account_analytic_line.move_id = account_move_line.id',
-                            (ref, move_id))
-        return True
     
     def create_report(self, cr, uid, res_ids, report_name=False, file_name=False):
         if not report_name or not res_ids:
