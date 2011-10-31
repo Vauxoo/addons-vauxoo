1a3038d63b6a7c647108631d8b6f03989d86487c
moylop260 <moylop260@vauxoo.com>
2011-10-30 18:42:03 -0600

    [REF] [l10n_mx_facturae_lib] Remove comment & revert comment of try

--- l10n_mx_facturae_lib/facturae_lib.py
+++ l10n_mx_facturae_lib/facturae_lib.py
@@ -26,46 +26,17 @@
 
 from osv import osv
 from osv import fields
-from tools.translate import _
 import tools
+
 import os
 import time
-import release
 import tempfile
 import base64
 
 class facturae_certificate_library(osv.osv):
     _name = 'facturae.certificate.library'
     _auto = False
-    """
-    def set_global_fname_der(self, cer_der_b64=None, key_der_b64=None, password_der=None):
-        self._fname_cer_der = self.binary2file(None, None, [], cer_der_b64, file_prefix='openerp__' + (False or '') + '__ssl__', file_suffix='.der.cer')
-        self._fname_key_der = self.binary2file(None, None, [], key_der_b64, file_prefix='openerp__' + (False or '') + '__ssl__', file_suffix='.der.key')
-        password_der_b64 = base64.encodestring(password_der)
-        self._fname_password_der = self.binary2file(None, None, [], password_der_b64, file_prefix='openerp__' + (False or '') + '__ssl__', file_suffix='.der.txt')
     
-    def delete_global_fname_der(self):
-        os.unlink( self._fname_cer_der )
-        os.unlink( self._fname_key_der )
-        os.unlink( self._fname_password_der )
-    
-    def set_global_fname_pem(self, cer_pem_b64=None, key_pem_b64=None):
-        self._fname_cer_pem = self.binary2file(None, None, [], cer_pem_b64, file_prefix='openerp__' + (False or '') + '__ssl__', file_suffix='.pem.cer')
-        self._fname_key_pem = self.binary2file(None, None, [], key_pem_b64, file_prefix='openerp__' + (False or '') + '__ssl__', file_suffix='.pem.key')
-    
-    def delete_global_fname_pem(self):
-        os.unlink( self._fname_cer_pem )
-        os.unlink( self._fname_key_pem )
-    
-    def set_global_fname_temp(self, fname_temp=None):
-        if not fname_temp:
-            self._fname_temp = self.binary2file(None, None, [], '', file_prefix='openerp__' + (False or '') + '__ssl__', file_suffix='.temp.txt')
-        else:
-            self._fname_temp = fname_temp
-    
-    def delete_global_fname_temp(self):
-        os.unlink( self._fname_temp )
-    """
     def b64str_to_tempfile(self, b64_str="", file_suffix="", file_prefix=""):
         (fileno, fname) = tempfile.mkstemp(file_suffix, file_prefix)
         f = open( fname, 'wb' )
@@ -87,12 +58,10 @@ class facturae_certificate_library(osv.osv):
         cont = 1
         while True:
             time.sleep( seconds_delay )
-            if True:
-            #try:
-                #fdata = open( fname, "r").read()
+            try:
                 fdata = file_obj.read()
-            #except:
-                #pass
+            except:
+                pass
             if fdata or max_attempt < cont:
                 break
             cont += 1
@@ -153,103 +122,15 @@ class facturae_certificate_library(osv.osv):
         @params: list [noout serial startdate enddate subject issuer dates]
         @type: str DER or PEM
         """
-        #params = "-startdate -enddate"
-        #params.split(' ')
         cmd_params = ' -'.join(params)
         cmd_params = cmd_params and '-' + cmd_params or ''
-        #print "cmd_params",cmd_params
-        #cmd = "openssl x509 -inform %s -noout %s -in %s -out %s"%(type, cmd_params, fname, fname_out)
         cmd = "openssl x509 -inform %s -in %s -noout %s -out %s"%( type, fname, cmd_params, fname_out )
-        #print "cmd",cmd
         args = tuple( cmd.split(' ') )
         input, output = tools.exec_command_pipe(*args)
-        #result = output.read()
         result = self._read_file_attempts(output)
         input.close()
         output.close()
         return result
-        
-    ##############TODAS ESTAS FUNCIONES QUEDARAN EN DESUSO###
-    def _____get_serial(self, fname, fname_out=None, type='DER'):
-        """
-        @type: DER or PEM
-        """
-        type = type and type.upper() or ''
-        cmd = "openssl x509 -inform %s -in %s -serial -noout -out %s"%(type, fname, fname_out)
-        args = tuple( cmd.split(' ') )
-        input, output = tools.exec_command_pipe(*args)
-        #no_certificado = self._read_file_attempts(fname_out, max_attempt=6, seconds_delay=0.5)
-        #result = output.read()
-        result = self._read_file_attempts(output)
-        result = result.replace('serial=', '').replace('33', 'B').replace('3', '').replace('B', '3').replace(' ', '').replace('\r', '').replace('\n', '').replace('\r\n', '')
-        input.close()
-        output.close()
-        return result
-    def _generate_pem_fname(self, fname, type='cer', password=None):
-        file_b64 = base64.encodestring( open(fname, "r").read() )
-        pem_b64 = self._generate_pem_fname(cr, uid, ids, file_b64)
-        return pem_b64
-    
-    def _generate_pem_b64(self, cr, uid, ids, file_b64, type='cer', password=None):
-        fname = self.binary2file(cr, uid, [], file_b64, file_prefix="openerp__", file_suffix="."+type)
-        
-        (fileno_pem, fname_pem) = tempfile.mkstemp('.'+type+'.pem', 'openerp_' + (type or '') + '__facturae__' )
-        os.close(fileno_pem)
-        pem = ''
-        if type == 'cer':
-            cmd = 'openssl x509 -inform DER -in %s -outform PEM -pubkey -out %s'%(fname, fname_pem)
-            args = tuple( cmd.split(' ') )
-            input, output = tools.exec_command_pipe(*args)
-            pem = self._read_file_attempts(output)
-            input.close()
-            output.close()
-        elif type == 'key':
-            (fileno_password, fname_password) = tempfile.mkstemp('.txt', 'openerp_' + (False or '') + '__facturae__' )
-            os.close(fileno_password)
-            open(fname_password, "w").write( password )
-            
-            cmd = 'openssl pkcs8 -inform DER -in %s -passin file:%s -out %s'%(fname, fname_password, fname_pem)
-            args = tuple(cmd.split(' '))
-            input, output = tools.exec_command_pipe(*args)
-            pem = self._read_file_attempts(output)
-            input.close()
-            output.close()
-            os.unlink(fname_password)
-        pem_b64 = base64.encodestring( pem or '') or False
-        os.unlink(fname_pem)
-        os.unlink(fname)
-        return pem_b64
-    
-    def _________get_serial(self, file_b64, pem=True):
-        fname = self.binary2file(False, False, [], file_b64, file_prefix="openerp__", file_suffix=".pem")
-        (fileno_tmp, fname_tmp) = tempfile.mkstemp('.txt', 'openerp_' + ('serial' or '') + '__facturae__')
-        os.close(fileno_tmp)
-        cmd = "openssl x509 -in %s -serial -noout -out %s"%(fname, fname_tmp)
-        args = tuple( cmd.split(' ') )
-        input, output = tools.exec_command_pipe(*args)
-        #no_certificado = self._read_file_attempts(fname_tmp, max_attempt=6, seconds_delay=0.5)
-        #no_certificado_hex = output.read()
-        no_certificado_hex = self._read_file_attempts(output)
-        no_certificado = no_certificado_hex.replace('serial=', '').replace('33', 'B').replace('3', '').replace('B', '3').replace(' ', '').replace('\r', '').replace('\n', '').replace('\r\n', '')
-        input.close()
-        output.close()
-        os.unlink(fname)
-        os.unlink(fname_tmp)
-        return no_certificado
-    
-    def _get_startdate_enddate(self, file_b64, pem=False):
-        fname = self.binary2file(False, False, [], file_b64, file_prefix="openerp__", file_suffix=".cer")
-        (fileno_tmp, fname_tmp) = tempfile.mkstemp('.txt', 'openerp_' + ('serial' or '') + '__facturae__')
-        os.close(fileno_tmp)
-        cmd = "openssl x509 -inform DER -noout -startdate -enddate -in %s -out %s"%( fname, fname_tmp)
-        args = tuple( cmd.split(' ') )
-        input, output = tools.exec_command_pipe(*args)
-        res = self._read_file_attempts(output)
-        input.close()
-        output.close()
-        os.unlink(fname)
-        os.unlink(fname_tmp)
-        return res
 facturae_certificate_library()
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
