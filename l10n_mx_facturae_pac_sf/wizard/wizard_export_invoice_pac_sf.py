51dc3c0653287885e5172f3645506ec0b3d4eae1
moylop260 <moylop260@vauxoo.com>
2011-10-30 23:44:15 -0600

    [FIX] [l10n_mx_facturae_pac_sf] Add validate when status is None

--- l10n_mx_facturae_pac_sf/wizard/wizard_export_invoice_pac_sf.py
+++ l10n_mx_facturae_pac_sf/wizard/wizard_export_invoice_pac_sf.py
@@ -153,8 +153,9 @@ def _upload_ws_file(self, cr, uid, data, context={}):
                 wsdl_client.soapproxy.config.debug = 0
                 wsdl_client.soapproxy.config.dict_encoding='UTF-8'
                 resultado = wsdl_client.timbrar(*params)
-                msg = resultado['resultados']['mensaje']
-                status = resultado['resultados']['status']
+                
+                msg = resultado['resultados'] and resultado['resultados']['mensaje'] or ''
+                status = resultado['resultados'] and resultado['resultados']['status'] or ''
                 if status == '200' or status == '307':
                     fecha_timbrado = resultado['resultados']['fechaTimbrado'] or False
                     fecha_timbrado = fecha_timbrado and time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(fecha_timbrado[:19], '%Y-%m-%dT%H:%M:%S')) or False
@@ -174,6 +175,10 @@ def _upload_ws_file(self, cr, uid, data, context={}):
                     #open("D:\\cfdi.xml", "wb").write( base64.decodestring( resultado['resultados']['cfdiTimbrado'] or '' ) )
                 elif status == '500':#documento no es un cfd version 2, probablemente ya es un CFD version 3
                     msg = "Probablemente el archivo XML ya ha sido timbrado previamente y no es necesario volverlo a subir.\nO puede ser que el formato del archivo, no es el correcto.\nPor favor, visualice el archivo para corroborarlo y seguir con el siguiente paso o comuniquese con su administrador del sistema."
+                else:
+                    msg += '\n\n\n\n\n' + resultado['mensaje']
+                    if not status:
+                        status = 'parent_' + resultado['status']
     else:
         msg = 'No se encontro informacion del webservices del PAC, verifique que la configuración del PAC sea correcta'
     return {'file': data['form']['file'], 'msg': msg}
