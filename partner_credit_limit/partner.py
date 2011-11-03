67d08bd57f624aef242a674490c8b33513bd80ad
Isaac Lopez <isaac@vauxoo.com>
2011-11-03 15:05:03 -0600

    [ADD][account_aged_partner_balance_vw][partner_credit_limit] 2 modules added

---
+++ partner_credit_limit/partner.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). All Rights Reserved
+#    d$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import time
+import netsvc
+from osv import fields, osv
+from mx import DateTime
+from tools import config
+from tools.translate import _
+
+class res_partner(osv.osv):
+    _inherit = 'res.partner'
+    _columns = {
+        'over_credit':fields.boolean('Allow Over Credit?', required=False),
+        'maturity_over_credit':fields.boolean('Allow Maturity Over Credit?', required=False),
+        'credit_maturity_limit': fields.float(string='Credit Maturity Limit'),
+    }
+res_partner()
+
