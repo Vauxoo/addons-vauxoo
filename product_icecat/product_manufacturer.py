c2a00268166fd898fbfedddcb2c6a217d94a1e14
Israel Fermín Montilla <israel@openerp.com.ve>
2011-11-07 16:37:05 -0430

    [ADD] added missing product_icecat module

---
+++ product_icecat/product_manufacturer.py
@@ -0,0 +1,51 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (c) 2011 Zikzakmedia S.L. (http://zikzakmedia.com) All Rights Reserved.
+#                       Raimon Esteve <resteve@zikzakmedia.com>
+#    $Id$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import osv
+from osv import fields
+from tools.translate import _
+
+class product_product(osv.osv):
+    _inherit = 'product.product'
+    _columns = {
+        'manufacturer_pname' : fields.char('Manufacturer Product Name', size=128),
+    }
+product_product()
+
+class product_attribute(osv.osv):
+    _inherit = 'product.manufacturer.attribute'
+
+    _columns = {
+        'name': fields.char('Attribute', size=128, required=True),
+        'value' : fields.char('Value', size=128),
+        'icecat_category':fields.char('Icecat Category', size=64),
+        'sequence': fields.integer('Sequence'),
+    }
+
+    _order = 'sequence, id'
+
+    _defaults = {
+        'sequence': lambda *a: 10,
+    }
+
+product_attribute()
