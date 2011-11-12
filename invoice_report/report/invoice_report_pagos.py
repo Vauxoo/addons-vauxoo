8d2aa3670faf8d9ee83da1a0772001d7de64ca9e
Isaac Lopez <isaac@vauxoo.com>
2011-11-12 15:25:28 -0600

    [IMP][invoice_report] report 2 calculate balance final total. Add horizontal lines on first and last row (rml only)

--- invoice_report/report/invoice_report_pagos.py
+++ invoice_report/report/invoice_report_pagos.py
@@ -40,11 +40,17 @@ class invoice_report_pagos(report_sxw.rml_parse):
                 'get_invoice_payment': self._get_invoice_payment,
                 'get_line_payment': self._get_line_payment,
                 'get_total_pago': self._get_total_pago,
+                'get_total_final_pagado': self._get_total_final_pagado,
 
             })
 
+    def _get_total_final_pagado(self):
+        print 'dentro del total final pagado y devuelve',self.total_final_pagado
+        return self.total_final_pagado
+
     def _get_total_pago(self):
         print 'el get_total_pago es --------------------------------',self.tot_pago
+        self.total_final_pagado += self.tot_pago
         return self.tot_pago
 
 
@@ -89,6 +95,7 @@ class invoice_report_pagos(report_sxw.rml_parse):
                         inner join account_invoice d
                             on d.move_id=c.move_id
                     where a.id=%s
+                    and d.state not in ('cancel','proforma','proforma2')
                 """%( pay_id )
         self.cr.execute( subq )
         invoice_ids = [ inv_id[0] for inv_id in self.cr.fetchall() ]
@@ -109,7 +116,7 @@ class invoice_report_pagos(report_sxw.rml_parse):
         #~ print 'lo browse de invoice es',inv_brw.internal_number
         self.vou_brw=vou_brw
         print 'el vou_brw es',vou_brw
-        self.tot_pago=0
+        self.total_final_pagado=0
         return vou_brw
 
 report_sxw.report_sxw('report.invoice.report.pagos', 'res.partner','addons/invoice_report/report/payment_report2.rml', parser=invoice_report_pagos)
