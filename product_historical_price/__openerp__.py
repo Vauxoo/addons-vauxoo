a552ee6dc140a97c0bf49f33458dfb33cc19c4dc
Gabriela (Vauxoo) <gabrielaquilarque97@gmail.com>
2012-01-02 16:50:56 -0430

    [ADD] Added new module product_historical_price. [ADD] Added new module inventory_stock_report.

---
+++ product_historical_price/__openerp__.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#    Copyright (C) OpenERP Venezuela (<http://openerp.com.ve>).
+#    All Rights Reserved
+###############Credits######################################################
+#    Coded by: Vauxoo C.A.           
+#    Planified by: Nhomar Hernandez
+#    Audited by: Vauxoo C.A.
+#############################################################################
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+{
+    "name" : "Product Historical Price",
+    "version" : "0.2",
+    "depends" : ["product","decimal_precision"],
+    "author" : "Vauxoo",
+    "description" : """
+    What do this module:
+    This module gets the historical price of a product
+                    """,
+    "website" : "http://Vauxoo.com",
+    "category" : "Generic Modules/Product",
+    "init_xml" : [],
+    "update_xml" : ["view/product_view.xml",
+                    "data/product_data.xml",
+#                    "security/groups.xml",
+                    "security/ir.model.access.csv",
+                    ],
+    "active": False,
+    "installable": True,
+}
---
+++ product_historical_price/product.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#    Copyright (C) OpenERP Venezuela (<http://openerp.com.ve>).
+#    All Rights Reserved
+###############Credits######################################################
+#    Coded by: Vauxoo C.A.           
+#    Planified by: Nhomar Hernandez
+#    Audited by: Vauxoo C.A.
+#############################################################################
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+from osv import osv
+from osv import fields
+from tools.translate import _
+import decimal_precision as dp
+import pooler
+import time
+import math
+
+class product_historical(osv.osv):
+    """
+    product_historical
+    """
+    def _get_historical_price(self, cr, uid, ids, field_name, field_value, arg, context={}):
+        res = {}
+        product_hist = self.pool.get('product.historic.price')
+        for id in ids:
+            if self.browse(cr, uid, id).list_price != self.browse(cr, uid, id).list_price_historical:
+                res[id] = self.browse(cr, uid, id).list_price
+                product_hist.create(cr, uid, {
+                    'product_id' : id,
+                    'name': time.strftime('%Y-%m-%d %H:%M:%S'),
+                    'price': self.browse(cr, uid, id).list_price,
+               },context)
+        return res
+    
+    def _get_historical_cost(self, cr, uid, ids, field_name, field_value, arg, context={}):
+        res = {}
+        product_hist = self.pool.get('product.historic.cost')
+        for id in ids:
+            if self.browse(cr, uid, id).standard_price != self.browse(cr, uid, id).cost_historical:
+                res[id] = self.browse(cr, uid, id).standard_price
+                product_hist.create(cr, uid, {
+                    'product_id' : id,
+                    'name': time.strftime('%Y-%m-%d %H:%M:%S'),
+                    'price': self.browse(cr, uid, id).standard_price,
+               },context)
+        return res
+    
+    _inherit = 'product.product'
+    _columns = {
+        'list_price_historical': fields.function(_get_historical_price, method=True, string='Latest Price', type='float',digits_compute= dp.get_precision('List_Price_Historical'),
+            store={'product.product': (lambda self, cr, uid, ids, c={}: ids, ['list_price'], 50),}, 
+            help="Latest Recorded Historical Value"),
+        'cost_historical': fields.function(_get_historical_cost, method=True, string=' Latest Cost', type='float',digits_compute= dp.get_precision('Cost_Historical'),
+            store={'product.product': (lambda self, cr, uid, ids, c={}: ids, ['standard_price'], 50),}, 
+            help="Latest Recorded Historical Cost"),
+        'list_price_historical_ids': fields.one2many('product.historic','product_id','Historical Prices'),
+        'cost_historical_ids': fields.one2many('product.historic.cost','product_id','Historical Prices'),
+        
+    }
+product_historical()
+
+class product_historic_price(osv.osv):
+    _order= "name desc"
+    _name = "product.historic.price"
+    _description = "Historical Price List"
+
+    _columns = {
+        'product_id': fields.many2one('product.product',string='Product related to this Price', required=True),
+        'name': fields.datetime(string='Date',  required=True),
+        'price': fields.float(string='Price',digits_compute= dp.get_precision('Price')),
+        'product_uom': fields.many2one('product.uom', string="Supplier UoM", help="Choose here the Unit of Measure in which the prices and quantities are expressed below.")
+        
+    }
+    _defaults = { 'name': lambda *a: time.strftime('%Y-%m-%d %H:%M:%S'),
+    }
+
+product_historic_price()
+
+class product_historic_cost(osv.osv):
+    _order= "name desc" 
+    _name = "product.historic.cost"
+    _description = "Historical Price List"
+
+    _columns = {
+        'product_id': fields.many2one('product.product', string='Product related to this Cost', required=True),
+        'name': fields.datetime(string='Date', required=True),
+        'price': fields.float(string='Cost', digits_compute= dp.get_precision('Price2')),
+        'product_uom': fields.many2one('product.uom', string="Supplier UoM", help="Choose here the Unit of Measure in which the prices and quantities are expressed below.")
+        
+    }
+    _defaults = { 'name': lambda *a: time.strftime('%Y-%m-%d %H:%M:%S'),
+    }
+
+product_historic_cost()
