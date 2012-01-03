c93d7f45af93c5f4b6f8bf823befa3ffa6c67438
Miguel Delgado <miguel.delgado07@gmail.com>
2012-01-03 14:14:05 -0430

    [ADD] migration of module sale_uom_group to version 6.0

---
+++ sale_uom_group/__openerp__.py
@@ -0,0 +1,52 @@
+# -*- encoding: utf-8 -*-
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#    Copyright (C) OpenERP Venezuela (<http://openerp.com.ve>).
+#    All Rights Reserved
+###############Credits######################################################
+#    Coded by: Javier Duran <javieredm@gmail.com>,
+#    Planified by: Nhomar Hernandez
+#    Finance by: Helados Gilda, C.A. http://heladosgilda.com.ve
+#    Audited by: Humberto Arocha humberto@openerp.com.ve
+#############################################################################
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##############################################################################
+
+{
+    "name" : "salem_uom_group"                                                                              ,
+    "version" : "0.1"                                                                                       ,
+    "depends" : ["sale"]                                                                                    ,
+    "author" : "Openerp Venzuela"                                                                           ,
+    "description" : """
+    What do this module:
+    Add multi product uom sum of order line.
+                    """                                                                                     ,
+    "website" : "http://openerp.com.ve"                                                                     ,
+    "category" : "Generic Modules/MRP"                                                                      ,
+    "init_xml" : [
+    ]                                                                                                       ,
+    "demo_xml" : [
+    ]                                                                                                       ,
+    "update_xml" : [
+        "security/groups.xml"                                                                               ,
+        "security/ir.model.access.csv"                                                                      ,
+        "view/sale_uom_group_view.xml"                                                                      ,
+
+    ]                                                                                                       ,
+    "active": False                                                                                         ,
+    "installable": True                                                                                     ,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
