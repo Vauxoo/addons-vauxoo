39f7e9dac5a5147d2a0f82883df5ad4b9687eaca
javier@vauxoo.com <>
2012-01-20 20:50:12 -0430

    [ADD] se agrega el modulo bank_iva_report

---
+++ bank_iva_report/__openerp__.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#    Copyright (C) OpenERP Venezuela (<http://openerp.com.ve>).
+#    All Rights Reserved
+###############Credits######################################################
+#    Coded by: Humberto Arocha           <humberto@openerp.com.ve>
+#              Angelica Barrios          <angélicaisabelb@gmail.com>
+#              María Gabriela Quilarque  <gabrielaquilarque97@gmail.com>
+#              Javier Duran              <javier@vauxoo.com>             
+#    Planified by: Nhomar Hernande
+#    Finance by: Helados Gilda, C.A. http://heladosgilda.com.ve
+#    Audited by: Humberto Arocha humberto@openerp.com.ve
+#############################################################################
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+###  
+### res_company --> account_management ver si se puede eliminar esta dependencia ####
+### retencion_iva, base_vat_ve  ver si se puede eliminar esta dependencia
+
+{
+    "name" : "Voucher Paid support report",
+    "version" : "0.2",
+    "author" : "Vauxoo",
+    "website" : "http://vauxoo.com",
+    "category": 'Generic Modules/Accounting',
+    "description": """
+        Este modulo agrega al reporte del soporte de pago detallado las retenciones del Impuesto al valor agregado (IVA)
+
+    """,
+    'init_xml': [],
+    "depends" : ["l10n_ve_withholding_iva", "bank_management"],
+    'update_xml': [
+        'bank_iva_report.xml',
+    ],
+    'demo_xml': [],
+    'test': [],
+    'installable': True,
+    'active': False,
+    'external_dependencies': {},
+}
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
