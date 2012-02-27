b116e9a2045a1ed35f85a786639cc8602cc53b25
Luis E. Escobar V. <vxkikevx@gmail.com>
2012-02-27 11:33:00 -0430

    [IMP] Add module crm_cost_issue that add cost field.

---
+++ crm_cost_issue/__openerp__.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#    Copyright (C) OpenERP Venezuela (<http://openerp.com.ve>).
+#    All Rights Reserved
+###############Credits######################################################
+#    Coded by: Luis Escobar                <luis@vauxoo.com>
+#              María Gabriela Quilarque    <gabriela@openerp.com.ve>
+#    Planified by: Nhomar Hernandez        <nhomar@openerp.com.ve>
+#    Finance by: Helados Gilda, C.A. http://heladosgilda.com.ve
+#    Audited by: María Gabriela Quilarque  <gabriela@openerp.com.ve>
+#############################################################################
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+###  
+
+{
+    "name" : "CRM Cost Issue",
+    "version" : "0.2",
+    "author" : "Vauxoo",
+    "website" : "http://vauxoo.com",
+    "category": 'Generic Modules/Accounting',
+    "description": """
+        Este modulo agrega el costo de una insidencia segun las horas estimadas.
+
+        Para definir el costo de la Hora, hay que crear un producto de tipo servicio y asociarlo al usuario para que asi pueda generar el costo.
+
+    """,
+    'init_xml': [],
+    "depends" : ["base", "crm", "project", "project_issue", "product"],
+    'update_xml': [
+        'view/project_task_view.xml'
+    ],
+    'demo_xml': [],
+    'test': [],
+    'installable': True,
+    'active': False,
+    'external_dependencies': {},
+}
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
