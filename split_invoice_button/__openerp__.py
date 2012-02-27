87155f28419725f4c502635bfc6b7b9c0ee9a0fd
Tulio Ruiz <tulio@vauxoo.com>
2012-02-27 15:21:54 -0430

    [FIX] Arreglada la descripción del componente

--- split_invoice_button/__openerp__.py
+++ split_invoice_button/__openerp__.py
@@ -26,32 +26,11 @@
 {
     "name" : "Split Invoice Button",
     "version" : "0.1",
-    "depends" : ["account"],
+    "depends" : ["l10n_ve_split_invoice"],
     "author" : "Vauxoo",
     "description" : """
-For legal reasons in Venezuela we need just ONE invoice per page, with this module depending on your company configuration you will stablish the number of lines per invoice, with this you will be able of:
- 
- 1.-Add module for establishing the number of lines per invoice
- 2.-Split Invoice according number of lines per invoice once you confirm it.
-
--------Testing Instructions--------
-Once you installed the module:
-
-1.- Go to Administration - Companies, open one of your companies (if more than one) and 
-    open the Configuration page.
-2.- Set a number of lines per invoice on the corresponding field.
-
-3.- Go to Account - Clients - Client Invoices and create a new invoice with more lines 
-    than you previously specified on the company configuration
-
-4.- Validate the invoice document
-
-5.- Click Search Asociated button
-
-The document should be now splited on N invoices with the number of lines that you specified
-
-This button allows the user view all generated invoices
-
+For legal reasons in Venezuela we need just ONE invoice per page,
+so this button allows the user view all generated invoices with the l10n_ve_split_invoice module
  """     ,
     "website" : "http://vauxoo.com",
     "category" : "Localization",
