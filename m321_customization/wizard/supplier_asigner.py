bbdee0e8fd3fe6147b2c119ac1cca150fc943921
Vauxoo <info@vauxoo.com>
2012-03-12 18:45:04 -0430

    [IMP] Added Validations in the wizard to assign suppliers

--- m321_customization/wizard/supplier_asigner.py
+++ m321_customization/wizard/supplier_asigner.py
@@ -49,13 +49,18 @@ class suppliers_assigner(osv.osv_memory):
         company_id = self.pool.get('res.users').browse(cr, uid, uid).company_id.id
         product_supp_obj = self.pool.get('product.supplierinfo')
         purchase_ids = purchase_obj.search(cr,uid,[],context=context)
-        for po in purchase_obj.browse(cr, uid, purchase_ids, context = context):
-            partner_id = po.partner_id.id
-            for line in po.order_line:
-                product_id = line.product_id.id
-                if not product_supp_obj.search(cr, uid, [('product_id', '=', product_id), ('name', '=', partner_id)]):
-                    product_obj.write(cr,uid,[product_id],{'seller_ids':[(0,0,{'name': partner_id, 'min_qty': 1.0, 'delay': 1, 'sequence': 10, 'product_id': product_id, 'company_id': company_id, 'product_uom': line.product_id.uom_id.id })]})
-            
+        wz_brw = self.browse(cr,uid,ids and ids[0],context=context)
+        if wz_brw.sure and wz_brw.are_sure:
+            for po in purchase_obj.browse(cr, uid, purchase_ids, context = context):
+                partner_id = po.partner_id.id
+                for line in po.order_line:
+                    product_id = line.product_id.id
+                    if not product_supp_obj.search(cr, uid, [('product_id', '=', product_id), ('name', '=', partner_id)]) and product_id:
+                        print "line.product_id.id",line.product_id.name
+                        print "line.product_id.uom_id.id",line and line.product_id and line.product_id.uom_id and line.product_id.uom_id.id
+                        product_obj.write(cr,uid,[product_id],{'seller_ids':[(0,0,{'name': partner_id, 'min_qty': 1.0, 'delay': 1, 'sequence': 10, 'product_id': product_id, 'company_id': company_id, 'product_uom': line and line.product_id and line.product_id.uom_id and line.product_id.uom_id.id})]})
+        else:
+            raise osv.except_osv(_('Processing Error'), _('Must select the 2 options to make sure the operation'))
 
         return {'type': 'ir.actions.act_window_close'}
 
