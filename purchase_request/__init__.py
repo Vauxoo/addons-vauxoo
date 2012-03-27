935956261e2fe8c6b26f3b4e40a6d62574819c78
Nhomar Hernandez nhomar@vauxoo.com <>
2012-03-26 19:38:49 -0430

    [IMP] Added custom report field.

--- purchase_request/__init__.py
+++ purchase_request/__init__.py
@@ -25,4 +25,5 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ##############################################################################
+import res_company
 import report
