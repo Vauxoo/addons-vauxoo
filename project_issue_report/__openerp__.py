bc6aa950ec10bed464515637c7e97d7d988b520a
Tulio Ruiz <tulio@vauxoo.com>
2012-03-30 15:06:02 -0430

    [ADD] Added issue report

---
+++ project_issue_report/__openerp__.py
@@ -0,0 +1,45 @@
+# -*- encoding: utf-8 -*-
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#    Copyright (C) OpenERP Venezuela (<http://openerp.com.ve>).
+#    All Rights Reserved
+###############Credits######################################################
+#    Coded by: María Gabriela Quilarque <gabriela@vauxoo.com>
+#    Planified by: Nhomar Hernandez
+#    Finance by: Helados Gilda, C.A. http://heladosgilda.com.ve
+#    Audited by: Humberto Arocha humberto@openerp.com.ve
+#############################################################################
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##############################################################################
+
+{
+	"name" : "Project Issue Report",
+	"version" : "0.1",
+	"author" : "Vauxoo",
+	"category" : "Generic Modules/Others",
+	"website": "http://wiki.openerp.org.ve/",
+    "description": """
+    
+    Modulo que realiza la personalizacion del reporte de incidencias 
+    
+    """,
+	"depends" : ["base", "project"],
+	"init_xml" : [],
+	"update_xml" : [
+		"project_issue_report.xml",
+
+	],
+	"active": False,
+	"installable": True
+}
