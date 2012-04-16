ef96d3d967498e8c588791c1c5533a4b28ac1820
Vauxoo <info@vauxoo.com>
2012-04-16 10:39:45 -0430

    [FIX] Added user field.

--- report_move_voucher/report/account.py
+++ report_move_voucher/report/account.py
@@ -27,86 +27,27 @@
 import time
 from report import report_sxw
 
-class invoice_report(report_sxw.rml_parse):
+class move_voucher_report(report_sxw.rml_parse):
     def __init__(self, cr, uid, name, context):
-        super(invoice_report, self).__init__(cr, uid, name, context=context)
+        super(move_voucher_report, self).__init__(cr, uid, name, context=context)
         self.localcontext.update({
             'time': time,
-            'get_company_addr': self._get_company_addr,
-            'get_fiscal_addr': self._get_fiscal_addr,
-            'get_contact_addr': self._get_contact_addr,
-            'get_tipo':self.get_tipo,
+            'get_user_create': self._get_user_create,
         })
 
+    def _get_user_create(self, move_id):
+        info = self.cr.execute("SELECT create_uid FROM account_move WHERE id = %s" % move_id )
+        res = [x[0] for x in self.cr.fetchall()][0]
 
-    def _get_company_addr(self):
-        company_obj = self.pool.get('res.company')
-        company_ids = company_obj.search(self.cr,self.uid,[])
-        company = company_obj.browse(self.cr, self.uid, company_ids[0])
-        addr_com = self._get_partner_addr(company.partner_id.id)
-        return addr_com
-
-
-    #metodo que retorna la direccion fiscal si es de tipo invoice 
-    def _get_fiscal_addr(self, idp=None):
-        if not idp:
-            return []
-        addr_obj = self.pool.get('res.partner.address')
-        res = 'NO HAY DIRECCION FISCAL DEFINIDA'
-        addr_ids = addr_obj.search(self.cr,self.uid,[('partner_id','=',idp), ('type','=','invoice')])
-        addr_inv={}
-        lista=""
-
-        if addr_ids: 
-            addr = addr_obj.browse(self.cr,self.uid, addr_ids[0])
-            lista=(addr.street and ('%s, '%(addr.street)) or '')+' '+(addr.street2 and ('%s - '%(addr.street2)) or '')+' '+(addr.zipcode_id and ('C.P. %s - '%(addr.zipcode_id.name)) or '')+ ' '+(addr.municipality_id and ('%s - '%(addr.municipality_id.name)) or '')+ ' '+(addr.parish_id and ('%s - '%(addr.parish_id.name)) or '')+ ' '+(addr.city_id and ('%s - '%(addr.city_id.name)) or '')+ ' '+(addr.state_id and ('%s - '%(addr.state_id.name)) or '')+' '+(addr.country_id and ('%s '%(addr.country_id.name)) or '')+ '\n '+ (addr.phone and (' TELF.: %s '%(addr.phone)) or '') + (addr.fax and (' FAX: %s '%(addr.fax)) or '') +(addr.mobile and (' CEL.: %s '%(addr.mobile)) or '') + (addr.email and (' EMAIL: %s '%(addr.email)) or '') 
-
-        if lista:
-            respuesta=lista
-        else:
-            respuesta=res
-        return respuesta 
-
-    #metodo que retorna la direccion de contacto si es de tipo contact 
-    def _get_contact_addr(self, idp=None):
-        if not idp:
-            return []
-        addr_obj = self.pool.get('res.partner.address')
-        res = ''
-        addr_ids = addr_obj.search(self.cr,self.uid,[('partner_id','=',idp), ('type','=','contact')])
-        addr_inv={}
-        lista=""
-
-        if addr_ids: 
-            addr = addr_obj.browse(self.cr,self.uid, addr_ids[0])
-            if addr.name:
-                lista=('CONTACTO: %s, '%addr.name) + (addr.street and ('%s, '%(addr.street)) or '')+' '+(addr.street2 and ('%s - '%(addr.street2)) or '')+' '+(addr.zipcode_id and ('C.P. %s - '%(addr.zipcode_id.name)) or '')+ ' '+(addr.municipality_id and ('%s - '%(addr.municipality_id.name)) or '')+ ' '+(addr.parish_id and ('%s - '%(addr.parish_id.name)) or '')+ ' '+(addr.city_id and ('%s - '%(addr.city_id.name)) or '')+ ' '+(addr.state_id and ('%s - '%(addr.state_id.name)) or '')+' '+(addr.country_id and ('%s '%(addr.country_id.name)) or '')+ '\n '+ (addr.phone and (' TELF.: %s '%(addr.phone)) or '') + (addr.fax and (' FAX: %s '%(addr.fax)) or '') +(addr.mobile and (' CEL.: %s '%(addr.mobile)) or '') + (addr.email and (' EMAIL: %s '%(addr.email)) or '') 
-
-        if lista:
-            respuesta=lista
-        else:
-            respuesta=res
-        return respuesta 
-
-
-    def get_tipo(self, idp=None):
-        if not idp:
-            return []
-        print idp
-        res=" "
-        if idp == 'in_invoice' or idp == 'in_refund':
-            res="Compra"
-        elif idp == 'out_invoice' or idp == 'out_refund':
-            res="Venta"     
-            
-        return res 
-
+        user_obj = self.cr.execute("SELECT name FROM res_users WHERE id = %s" %res)
+        user_name = [x[0] for x in self.cr.fetchall()][0]
+        return user_name
 
 report_sxw.report_sxw( #nuevo
-    'report.account.invoice_vauxoo',
-    'account.invoice',
-    'addons/invoice_report/report/invoice.rml',
-    parser=invoice_report,
+    'report.account.move_vauxoo',
+    'account.move',
+    'addons/report_move_voucher/report/move_voucher.rml',
+    parser=move_voucher_report,
     header = False 
 )
 
