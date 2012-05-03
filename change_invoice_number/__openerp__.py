666682d67eacfff2cc4422a233fc87616b961abf
Jose Antonio <jose@vauxoo.com>
2012-05-03 11:37:48 -0430

    [ADD] Module change_invoice_number added, This module allows number invoice change after this is validate

---
+++ change_invoice_number/__openerp__.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#    Copyright (C) Vauxoo (<http://vauxoo.com>).
+#    All Rights Reserved
+###############Credits######################################################
+#    Coded by: Vauxoo C.A.           
+#    Planified by: Nhomar Hernandez
+#    Audited by: Vauxoo C.A.
+#############################################################################
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+{
+    "name" : "Cost assigned in the account move",
+    "version" : "0.1",
+    "depends" : ["account",'account_cancel'],
+    "author" : "Vauxoo",
+    "description" : """
+    This module allows you to modify the control number of the invoice and the internal number is 
+    """,
+    "website" : "http://vauxoo.com",
+    "category" : "Generic Modules",
+    "init_xml" : [],
+    "demo_xml" : [],
+    "test": [ ],
+    "update_xml" : [
+    'wizard/change_number_view.xml',
+    'view/invoice_view.xml'
+    ],
+    "active": False,
+    "installable": True,
+}
