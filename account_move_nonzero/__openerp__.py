c6fcad9ef9ce37d0669dffb09e0b3abf0e2f5d82
javier@vauxoo.com <>
2012-05-04 13:05:59 -0430

    [ADD] Se incorpora modulo para la validación de apuntes con saldo diferente de cero

---
+++ account_move_nonzero/__openerp__.py
@@ -0,0 +1,43 @@
+# -*- encoding: utf-8 -*-
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#    Copyright (C) OpenERP Venezuela (<http://openerp.com.ve>).
+#    All Rights Reserved
+###############Credits######################################################
+#    Coded by: javier@vauxoo.com
+#############################################################################
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##############################################################################
+
+{
+    "name" : "Account Move Nonzero",
+    "version" : "0.1",
+    "depends" : ["account"],
+    "author" : "Vauxoo",
+    "description" : """
+    Check that the entry balance is greater than zero
+ """     ,
+    "website" : "http://vauxoo.com",
+    "category" : "Localization",
+    "init_xml" : [
+    ],
+    "demo_xml" : [
+    ],
+    "update_xml" : [
+    ],
+    "active": False,
+    "installable": True,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
