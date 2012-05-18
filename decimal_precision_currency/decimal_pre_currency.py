bc1885d20726487063090ddc75d9d0dc710ba168
Vauxoo <info@vauxoo.com>
2012-05-18 16:07:26 -0430

    [ADD] Added new module decimal_precision currency, add decimal  precision format to Rate Currency.

---
+++ decimal_precision_currency/decimal_pre_currency.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#    Copyright (C) OpenERP Venezuela (<http://openerp.com.ve>).
+#    All Rights Reserved
+###############Credits######################################################
+#    Coded by: Maria Gabriela Quilarque  <gabriela@openerp.com.ve>
+#    Planified by: Nhomar Hernandez <nhomar@vauxoo.com>
+#    Audited by: Maria Gabriela Quilarque  <gabriela@openerp.com.ve>
+#############################################################################
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##############################################################################
+from osv import osv
+from osv import fields
+from tools.translate import _
+from tools import config
+import time
+import datetime
+import decimal_precision as dp
+
+class res_currency_rate(osv.osv):
+
+    _inherit = "res.currency.rate"
+    _columns= {
+        'rate': fields.float('Rate', digits_compute= dp.get_precision('Currency'), required=True, help='The rate of the currency to the currency of rate 1'),
+    }
+
+res_currency_rate()
+
+
+
+
+
+
