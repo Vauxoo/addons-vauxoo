71ba7823ab903ac1f1aecbfb770b0f5e4bba8770
Miguel Delgado <miguel.delgado07@gmail.com>
2012-05-23 15:31:14 -0430

    [IMP] se arreglo el reporte de orden de venta para su mejor funcionamiento con el fin de que no se produzcan errores por la falta de algun campo

--- vauxoo_reports/sale_order_report/report/vauxoo.py
+++ vauxoo_reports/sale_order_report/report/vauxoo.py
@@ -31,7 +31,51 @@ class sale_vauxoo_report(report_sxw.rml_parse):
         self.localcontext.update({
             'time': time,
             'hello': self._hello,
+            'get_addr': self._get_addr,
+            'get_delay': self._get_delay,
+            'get_rif': self._get_rif,
+            
         })
+    
+    def _get_delay(self, obj):
+        
+        aux=[]
+        
+        for aux2 in obj.order_line:
+            if aux2.delay > 0:
+                aux.append(True)
+            else:
+                aux.append(False)
+        print any(aux)
+        return any(aux)
+         
+    def _get_rif(self,obj):
+        rif = obj.user_id.company_id.partner_id.vat[2]+'-'+obj.user_id.company_id.partner_id.vat[3:]
+        print 'rif',rif
+        return rif 
+        
+    
+    
+    def _get_addr(self, idp=None,type_r=None):
+        if not idp:
+            return []
+        print type_r
+        addr_obj = self.pool.get('res.partner.address')
+        res = ''
+        addr_ids = addr_obj.search(self.cr,self.uid,[('partner_id','=',idp),('type','=',type_r)])
+        addr_inv={}
+        lista=""
+
+        if addr_ids: 
+            addr = addr_obj.browse(self.cr,self.uid, addr_ids[0])
+            lista=(hasattr(addr,'street') and addr.street and ('%s'%(addr.street)) or '')+(hasattr(addr,'street2') and addr.street2 and (', %s'%(addr.street2)) or '')+(hasattr(addr,'zipcode_id') and addr.zipcode_id and (',C.P: %s'%(addr.zipcode_id.name)) or '')+(hasattr(addr,'municipality_id') and addr.municipality_id and (', %s'%(addr.municipality_id.name)) or '')+(hasattr(addr,'parish_id') and addr.parish_id and (', %s'%(addr.parish_id.name)) or '')+(hasattr(addr,'city_id') and addr.city_id and (', %s'%(addr.city_id.name)) or '')+(addr.state_id and (', %s'%(hasattr(addr,'state_id') and addr.state_id.name)) or '')+(hasattr(addr,'country_id') and addr.country_id and (', %s'%(addr.country_id.name)) or '')+(hasattr(addr,'phone') and addr.phone and (',TELF.: %s'%(addr.phone)) or '') + (hasattr(addr,'fax') and  addr.fax and (',FAX: %s'%(addr.fax)) or '') +(hasattr(addr,'mobile') and addr.mobile and (',CEL.: %s'%(addr.mobile)) or '') 
+        if lista:
+            respuesta=lista
+        else:
+            respuesta=res
+        return respuesta     
+    
+    
     def _hello(self,p):
         print "estoy en hello"
         return "Hello World %s"
