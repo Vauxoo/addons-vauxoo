1b24bb73224ead6af797f20106640adc9da169fc
Isaac Lopez <isaac@vauxoo.com>
2012-06-25 12:09:11 -0500

    [IMP][l10n_mx_facturae_22] Dependence to module partner_bank_last_digits added

--- l10n_mx_facturae_22/__openerp__.py
+++ l10n_mx_facturae_22/__openerp__.py
@@ -34,7 +34,7 @@
     """,
     "website" : "www.vauxoo.com",
     "license" : "AGPL-3",
-    "depends" : ["l10n_mx_facturae",
+    "depends" : ["l10n_mx_facturae","partner_bank_last_digits",
         ],
     "init_xml" : [],
     "demo_xml" : [],
--- l10n_mx_facturae_22/invoice.py
+++ l10n_mx_facturae_22/invoice.py
@@ -67,7 +67,7 @@ class account_invoice(osv.osv):
             invoice_data_parents[0]['Comprobante']['version'] = '2.2'
             invoice_data_parents[0]['Comprobante']['TipoCambio'] = invoice.rate or 1
             invoice_data_parents[0]['Comprobante']['Moneda'] = invoice.currency_id.name or ''
-            invoice_data_parents[0]['Comprobante']['NumCtaPago'] = invoice.acc_payment.acc_number or 'No identificado'
+            invoice_data_parents[0]['Comprobante']['NumCtaPago'] = invoice.acc_payment.last_acc_number or 'No identificado'
             invoice_data_parents[0]['Comprobante']['metodoDePago'] = invoice.pay_method_id.name or 'No identificado'
             invoice_data_parents[0]['Comprobante']['Emisor']['RegimenFiscal'] = {'Regimen':invoice.company_id.partner_id.regimen_fiscal_id.name or ''}
             invoice_data_parents[0]['Comprobante']['LugarExpedicion'] = address
