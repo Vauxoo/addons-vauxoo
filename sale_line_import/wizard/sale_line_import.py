4a689a107f840ad29b3a3004cc3bc59516d4dd26
hearthebreaker@hotmail.com <>
2012-06-28 12:11:09 -0500

    [IMP][SALE_LINE_IMPORT] added fuction send_lines

--- sale_line_import/wizard/sale_line_import.py
+++ sale_line_import/wizard/sale_line_import.py
@@ -2,6 +2,7 @@ import time
 
 from osv import osv, fields
 from tools.translate import _
+import base64
 
 
 
@@ -10,5 +11,12 @@ class wizard_import(osv.osv_memory):
     _columns={
         'name' : fields.binary('File')
     }
-
+    
+    def send_lines(self,cr,uid,ids,context={}):
+        form = self.read(cr,uid,ids,[])
+        fdata = base64.decodestring( form[0]['name'] )
+        print fdata
+        return True
+    
+    
 wizard_import()
