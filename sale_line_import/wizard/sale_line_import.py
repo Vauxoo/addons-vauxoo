bd360fdd6e979feb7132b0da6a1c70524b979c22
hearthebreaker@hotmail.com <>
2012-06-28 17:15:55 -0500

    [IMP][SALE_IMPORT_SOL] added onchange to function

--- sale_line_import/wizard/sale_line_import.py
+++ sale_line_import/wizard/sale_line_import.py
@@ -4,6 +4,8 @@ from osv import osv, fields
 from tools.translate import _
 import base64
 
+import csv
+import cStringIO
 
 
 class wizard_import(osv.osv_memory):
@@ -13,9 +15,30 @@ class wizard_import(osv.osv_memory):
     }
     
     def send_lines(self,cr,uid,ids,context={}):
+        print context
+        order_id=context.get('active_id',False)
+        order=self.pool.get('sale.order').browse(cr,uid,order_id)
         form = self.read(cr,uid,ids,[])
         fdata = base64.decodestring( form[0]['name'] )
-        print fdata
+        input=cStringIO.StringIO(fdata)
+        input.seek(0)
+        data = list(csv.reader(input, quotechar='"' or '"', delimiter=','))
+        data[0].append('order_id.id')
+        list_prod=data[0].index('product_id')
+        datas=[]
+        for dat in data[1:]:
+            prod_name=dat[list_prod]
+            prod_id=self.pool.get('product.product').search(cr,uid,[('name','=',prod_name)],limit=1)
+            
+            lines=self.pool.get('sale.order.line').product_id_change(cr, uid, [], order.pricelist_id.id,prod_id[0],
+                                        qty=0,uom=False, qty_uos=0, uos=False, name='', partner_id=order.partner_id.id,
+                                        lang=False, update_tax=True, date_order=False, packaging=False, fiscal_position=False, flag=False,).get('value',False)
+            print lines.keys()
+            print lines.values()
+            dat.append(order_id)
+            datas.append(dat)
+        print datas,'imprimo datas'
+#        self.pool.get('sale.order.line').import_data(cr, uid, data[0], datas, 'init', '')
         return True
     
     
