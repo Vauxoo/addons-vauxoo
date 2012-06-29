8a69212938b68e025b9e87ad7f184d9d45481c8b
Jose Antonio <jose@vauxoo.com>
2012-06-28 21:27:41 -0430

    [IMP] Added structure of field

---
+++ issue_load/workflow/__openerp__.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#    Copyright (C) Vauxoo (<http://vauxoo.com>).
+#    All Rights Reserved
+###############Credits######################################################
+#    Coded by: Vauxoo C.A.           
+#    Planified by: Nhomar Hernandez
+#    Audited by: Vauxoo C.A.
+#############################################################################
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+{
+    "name" : "Load incidences from xls fiel",
+    "version" : "0.1",
+    "depends" : ["project",'project_issue'],
+    "author" : "Vauxoo",
+    "description" : """
+    Load incidences from xls
+    You need install python library xlrd and only load xls field
+    """,
+    "website" : "http://vauxoo.com",
+    "category" : "Generic Modules",
+    "init_xml" : [],
+    "demo_xml" : [],
+    "test": [ ],
+    "update_xml" : [
+    'wizard/load_issue_view.xml'
+    ],
+    "active": False,
+    "installable": True,
+}
