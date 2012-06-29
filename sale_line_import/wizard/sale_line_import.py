f50a92c354b245c2bae62b5a392129652fc0bb6c
hearthebreaker@hotmail.com <>
2012-06-29 12:51:25 -0500

    [IMP][SALE_IMPORT_SOL] add list in fuction

--- sale_line_import/wizard/sale_line_import.py
+++ sale_line_import/wizard/sale_line_import.py
@@ -13,6 +13,9 @@ class wizard_import(osv.osv_memory):
     _columns={
         'name' : fields.binary('File')
     }
+    def send_error(self,cr,uid,ids,context={}):
+        print 'error'
+        return {}
     
     def send_lines(self,cr,uid,ids,context={}):
         print context
@@ -25,21 +28,33 @@ class wizard_import(osv.osv_memory):
         data = list(csv.reader(input, quotechar='"' or '"', delimiter=','))
         data[0].append('order_id.id')
         list_prod=data[0].index('product_id')
-        datas=[]
+
         for dat in data[1:]:
+            datas=[]
+            data2=list(data[0])
             dat.append(order_id)
-            print data[0]
-            print dat
             prod_name=dat[list_prod]
             prod_id=self.pool.get('product.product').search(cr,uid,[('name','=',prod_name)],limit=1)
-            
-            lines=self.pool.get('sale.order.line').product_id_change(cr, uid, [], order.pricelist_id.id,prod_id[0],
+
+            lines=prod_id and self.pool.get('sale.order.line').product_id_change(cr, uid, [], order.pricelist_id.id,prod_id[0],
                                         qty=0,uom=False, qty_uos=0, uos=False, name='', partner_id=order.partner_id.id,
-                                        lang=False, update_tax=True, date_order=False, packaging=False, fiscal_position=False, flag=False,).get('value',False)
+                                        lang=False, update_tax=True, date_order=False, packaging=False, fiscal_position=False, flag=False,).get('value',False) or self.send_error(cr,uid,ids,context={})
+                                        
             for lin in range(len(lines.keys())):
                 if lines.keys()[lin] not in data[0]:
-                    print lines.keys()[lin]
+                    if lines.keys()[lin] in ('tax_id','product_uom','product_packaging'):
+                        field_val=str(lines.keys()[lin])
+                        field_val=field_val+'.id'
+                        data2.append(field_val)
+                        vals_many =str( lines[lines.keys()[lin]] ).replace('[','').replace(']','').replace('False','')
+                        dat.append( vals_many )
+                    else:
+                        data2.append(lines.keys()[lin])
+                        dat.append(lines[lines.keys()[lin]])
             datas.append(dat)
-        self.pool.get('sale.order.line').import_data(cr, uid, data[0], datas, 'init', '')
+            print data2
+            print datas
+            lines and self.pool.get('sale.order.line').import_data(cr, uid, data2, datas, 'init', '') or False
+            data2=[]
         return True
 wizard_import()
