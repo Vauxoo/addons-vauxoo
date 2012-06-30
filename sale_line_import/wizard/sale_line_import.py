b305ada0e14d954bc1a6e3b7692fe41f6a76026a
hearthebreaker@hotmail.com <>
2012-06-29 19:09:00 -0500

    [IMP][sale_import_line] add validate in formdata

--- sale_line_import/wizard/sale_line_import.py
+++ sale_line_import/wizard/sale_line_import.py
@@ -40,17 +40,12 @@ class wizard_import(osv.osv_memory):
         'name' : fields.binary('File'),
         'msg' : fields.text('Mensajes',readonly=True)
     }
-    def send_error(self,cr,uid,ids,context={}):
-        
-#        self.write(cr,uid,ids,{'msg':'No se encontro Referencia %s'%(context.get('ref',False))})
-        return {}
-    
     def send_lines(self,cr,uid,ids,context={}):
         print context
         order_id=context.get('active_id',False)
         order=self.pool.get('sale.order').browse(cr,uid,order_id)
         form = self.read(cr,uid,ids,[])
-        fdata = base64.decodestring( form[0]['name'] )
+        fdata = form and base64.decodestring( form[0]['name'] ) or False
         input=cStringIO.StringIO(fdata)
         input.seek(0)
         data = list(csv.reader(input, quotechar='"' or '"', delimiter=','))
@@ -67,7 +62,7 @@ class wizard_import(osv.osv_memory):
             lines=prod_id and self.pool.get('sale.order.line').product_id_change(cr, uid, [], order.pricelist_id.id,prod_id[0],
                                         qty=0,uom=False, qty_uos=0, uos=False, name='', partner_id=order.partner_id.id,
                                         lang=False, update_tax=True, date_order=False, packaging=False, fiscal_position=False, flag=False,).get('value',False) or {}
-            if not lines: msg+='No se encontro Referencia %s \n'% (prod_name)
+            if not lines: msg+='No Se Encontro Referencia: %s \n'% (prod_name)
             for lin in range(len(lines.keys())):
                 if lines.keys()[lin] not in data[0]:
                     if lines.keys()[lin] in ('tax_id','product_uom','product_packaging'):
