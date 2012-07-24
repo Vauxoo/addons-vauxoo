03cabdee38c7db20f1049e60ae14dcfe1c600111
Luis Torres <luis_t@vauxoo.com>
2012-07-24 18:49:34 -0500

    [DEL]Delete modul partner_description_invoice

--- partner_description_invoice/partner.py
+++
@@ -1,9 +0,0 @@
-from osv import osv
-from osv import fields
-
-class res_partner(osv.osv):
-    _inherit = 'res.partner'
-    _columns = {
-        'description_invoice': fields.text("Description Invoice")
-    }
-res_partner()
