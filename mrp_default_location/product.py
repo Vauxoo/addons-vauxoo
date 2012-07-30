73dcc05fcdd7367d16231c12a7090e136733dad4
Julio Serna <julio@vauxoo.com>
2012-07-30 17:11:47 -0500

    [INIT] added module mrp_default_location

---
+++ mrp_default_location/product.py
@@ -0,0 +1,44 @@
+# -*- encoding: utf-8 -*-
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#
+#    Copyright (c) 2012 Vauxoo - http://www.vauxoo.com
+#    All Rights Reserved.
+#    info@vauxoo.com
+############################################################################
+#    Coded by: julio (julio@vauxoo.com)
+############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import time
+from osv import osv, fields
+from tools.translate import _
+from tools import config
+import base64
+import csv
+import cStringIO
+import tools
+
+
+class product_category(osv.osv):
+    _inherit='product.category'
+    _columns = {
+        'location_src_id' : fields.many2one('stock.location','Raw Materials Location'),
+        'location_dest_id' : fields.many2one('stock.location','Finished Products Location'),
+    }
+        
+product_category()
