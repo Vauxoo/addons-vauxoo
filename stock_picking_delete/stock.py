a9b9fc5a89e6fde805b248c7adfdb79f1f8374ff
Luis Torres <luis_t@vauxoo.com>
2012-08-06 18:40:35 -0500

    [ADD][stock_picking_delete]Add validation that state = draft for delete record

---
+++ stock_picking_delete/stock.py
@@ -0,0 +1,41 @@
+# -*- encoding: utf-8 -*-
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#
+#    Copyright (c) 2010 Vauxoo - http://www.vauxoo.com/
+#    All Rights Reserved.
+#    info Vauxoo (info@vauxoo.com)
+############################################################################
+#    Coded by: Luis Torres (luis_t@vauxoo.com)
+############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from osv import osv, fields
+from tools.translate import _
+
+class stock_picking(osv.osv):
+    _inherit = 'stock.picking'
+    
+    def unlink(self, cr, uid, ids, context=None):
+        stock_picking = self.read(cr, uid, ids, ['state'], context=context)
+        unlink_ids = []
+        for s in stock_picking:
+            if s['state'] in ['draft']:
+                unlink_ids.append(s['id'])
+            else:
+                raise osv.except_osv(_('Invalid action !'), _('Stocks can be removed only in draft.'))
+
+        return osv.osv.unlink(self, cr, uid, unlink_ids, context=context)
