2ed6e316453e1411700e3f4d9039b7277bcaea27
Luis Torres <luis_t@vauxoo.com>
2012-08-06 10:39:03 -0500

    [ADD][stock_picking_cancel]Add button to cancel and reactiv the picking

---
+++ stock_picking_cancel/stock.py
@@ -0,0 +1,48 @@
+# -*- encoding: utf-8 -*-
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#
+#    Copyright (c) 2010 Vauxoo - http://www.vauxoo.com/
+#    All Rights Reserved.
+#    info Vauxoo (info@vauxoo.com)
+############################################################################
+#    Coded by: Luis Torres (luis_t@vauxoo.com)
+############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from osv import osv, fields
+
+class stock_picking(osv.osv):
+    _inherit = 'stock.picking'
+    
+    def button_draft(self, cr, uid, ids, context=None):
+        print 'holaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+        if not len(ids):
+            return False
+        cr.execute('select id from sale_order_line where order_id IN %s and state=%s', (tuple(ids), 'cancel'))
+        line_ids = map(lambda x: x[0], cr.fetchall())
+        self.write(cr, uid, ids, {'state': 'draft', 'invoice_ids': [], 'shipped': 0})
+        self.pool.get('sale.order.line').write(cr, uid, line_ids, {'invoiced': False, 'state': 'draft', 'invoice_lines': [(6, 0, [])]})
+        wf_service = netsvc.LocalService("workflow")
+        for inv_id in ids:
+            # Deleting the existing instance of workflow for SO
+            wf_service.trg_delete(uid, 'sale.order', inv_id, cr)
+            wf_service.trg_create(uid, 'sale.order', inv_id, cr)
+        for (id,name) in self.name_get(cr, uid, ids):
+            message = _("The sales order '%s' has been set in draft state.") %(name,)
+            self.log(cr, uid, id, message)
+        return True
+stock_picking()
