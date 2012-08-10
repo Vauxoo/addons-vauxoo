15a634ecd82837f2d3377e79b9db8f0f1b9c2503
Luis Torres <luis_t@vauxoo.com>
2012-08-10 17:58:02 -0500

    [FIX][invoice_sale_ref]Change inherit to osv and delete a depends

--- invoice_sale_ref/__openerp__.py
+++ invoice_sale_ref/__openerp__.py
@@ -34,7 +34,7 @@
     """,
     "website" : "http://www.vauxoo.com/",
     "license" : "AGPL-3",
-    "depends" : ["sale","account","stock"
+    "depends" : ["sale","stock"
         ],
     "init_xml" : [],
     "demo_xml" : [],
