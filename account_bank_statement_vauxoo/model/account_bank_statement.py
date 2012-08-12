9a8f35d873b08483894a342c4685cdc0857cf080
jose@vauxoo.com <>
2012-08-12 19:23:17 -0430

    [IMP] Work with accoun move line in the invoices

--- account_bank_statement_vauxoo/model/account_bank_statement.py
+++ account_bank_statement_vauxoo/model/account_bank_statement.py
@@ -315,12 +315,18 @@ class bank_statement_imported_lines(osv.osv):
             counterpart_id = bsil_brw.counterpart_id
             for aml in bsil_brw.aml_ids:
                 if aml.account_id == counterpart_id:
-                    debit += aml.debit
+                    debit += aml.debit or aml.credit
             for inv in bsil_brw.invoice_ids:
                 if inv.account_id == counterpart_id:
                     amt_unt += inv.amount_total
+            for amls in bsil_brw.acc_move_line_ids:
+                if amls.account_id == counterpart_id:
+                    amt_unt+=amls[aml.debit and 'credit' or 'debit']
+                
             res[i]=debit-amt_unt
         return res
+    
+    
     _columns = {
         'name':fields.char('Description', size=255, required=True, readonly=False),
         'date': fields.date('Date', required=True),
@@ -362,15 +368,7 @@ class bank_statement_imported_lines(osv.osv):
 
 
 
-    def parser_generator(self,aml_due,abs):
-        
-        
-        for i in aml_due:
-            if i.date_maturity <= abs.date:
-                return i.debit or i.credit
-        
-        return False
-
+   
     def prepare(self, cr, uid, ids, context=None):
         if context is None:
             context={}
@@ -392,59 +390,36 @@ class bank_statement_imported_lines(osv.osv):
                         aml = line
             if aml:
                 
-                invoice_ids = [i.id for i in abs_brw.invoice_ids if i.state=='open' ]
+                aml_ids = [line.id for i in abs_brw.invoice_ids if i.state=='open' \
+                                   for line in i.move_id.line_id if i.account_id.id == line.account_id.id]
                 
-                invoice_ids = invoice_obj.search(cr,uid,[('id','in',invoice_ids)],order='date_invoice asc,amount_total asc',context=context)
-                
-                aml_ids = [i.id for i in abs_brw.acc_move_line_ids ]
+                aml_ids+=[i.id for i in abs_brw.acc_move_line_ids ]
                 
                 aml_ids = aml_ids and account_move_line_obj.search(cr,uid,[('id','in',aml_ids)],order='date asc',context=context)
+                
                 
                 
                 if aml.debit or aml.credit and  not context.get('cancel',False):
                     
                     total = aml.debit or aml.credit
-                    for invoice in invoice_ids and invoice_obj.browse(cr,uid,invoice_ids,context=context):
-                        
-                        aml_due = invoice.date_due and [ i.id for i in invoice.move_id.line_id if invoice.account_id.id == i.account_id.id and i.date_maturity ] or False
-                        aml_due = aml_due and account_move_line_obj.search(cr,uid,[('id','in',aml_due)], order='date_maturity asc') or False
-                        aml_due = aml_due and account_move_line_obj.browse(cr,uid,aml_due,context=context)
-                        
-                        #print 'aml_due',aml_due.credit 
-                        #print 'aml_due debit',aml_due.debit 
-                        if total > invoice.residual:
-                            total = total - (aml_due and self.parser_generator(aml_due,abs_brw) or invoice.residual ) 
-                            res.append((account_move_line_obj.copy(cr,uid,aml.id,{'%s'%(aml.debit > 0 and 'debit' or aml.credit > 0 and 'credit'):(aml_due and self.parser_generator(aml_due,abs_brw) or invoice.residual )}),
-                                        invoice,aml,account_move_line_obj.search(cr,uid,[('invoice','=',invoice.id),('account_id','=',invoice.account_id.id)])))
-                        
-                        elif total > 0 and (aml_due and self.parser_generator(aml_due,abs_brw) or invoice.residual ) >= total:
-                            res.append((account_move_line_obj.copy(cr,uid,aml.id,{'%s'%(aml.debit > 0 and 'debit' or aml.credit > 0 and 'credit'):total}),
-                                        invoice,aml,account_move_line_obj.search(cr,uid,[('invoice','=',invoice.id),('account_id','=',invoice.account_id.id)])))
-                            total = 0
-                        
-                        elif total<=0:
-                            break
-                        #TODO Revisar que hacer cuando no cuadra el total
-                        #elif total == 0:
-                            #self.write(cr,uid,ids,{'invoice_ids':[(3,invoice.id)]},context=context)
-                
                     for aml_id in account_move_line_obj.browse(cr,uid,aml_ids,context=context):
                         
-                        if total > aml_id[aml.debit and 'credit' or 'debit']:
-                            total = total - aml_id[aml.debit and 'credit' or 'debit']
-                            res.append((account_move_line_obj.copy(cr,uid,aml.id,{'%s'%(aml.debit > 0 and 'debit' or aml.credit > 0 and 'credit'):aml_id[aml.debit and 'credit' or 'debit']}),
-                                        False,False,aml_id.id))
+                        if aml_id.date_maturity and aml_id.date_maturity < abs_brw.date:
                         
-                        elif total > 0 and aml_id[aml.debit and 'credit' or 'debit'] >= total:
-                            res.append((account_move_line_obj.copy(cr,uid,aml.id,{'%s'%(aml.debit > 0 and 'debit' or aml.credit > 0 and 'credit'):total}),
-                                        False,False,aml_id.id))
-                            total = 0
+                            if total > aml_id[aml.debit and 'credit' or 'debit']:
+                                total = total - aml_id[aml.debit and 'credit' or 'debit']
+                                res.append((account_move_line_obj.copy(cr,uid,aml.id,{'%s'%(aml.debit > 0 and 'debit' or aml.credit > 0 and 'credit'):aml_id[aml.debit and 'credit' or 'debit']}),
+                                            aml_id.id))
+                            
+                            elif total > 0 and aml_id[aml.debit and 'credit' or 'debit'] >= total:
+                                res.append((account_move_line_obj.copy(cr,uid,aml.id,{'%s'%(aml.debit > 0 and 'debit' or aml.credit > 0 and 'credit'):total}),
+                                            aml_id.id))
+                                total = 0
+                            
+                            elif total<=0:
+                                break
                         
-                        elif total<=0:
-                            break
                         
-                
-                
                 if total > 0:
                     account_move_line_obj.copy(cr,uid,aml.id,{'%s'%(aml.debit > 0 and 'debit' or aml.credit > 0 and 'credit'):total})
                 
@@ -522,15 +497,8 @@ class bank_statement_imported_lines(osv.osv):
             if res:
                 for reconcile in res:
                     recon = []
-                    # reconcile is tuple (id move_line, browse invoice, aml generate in prepare)
-                    #recon+=[i.id for i in reconcile[1].move_id.line_id if i.account_id and i.account_id.id == reconcile[2].id  ]
                     recon.append(reconcile[0])
-                    recon+=reconcile[3]
-                    print 'recon',recon 
-                    print 'credit1',account_move_line_obj.browse(cr,uid,recon[0],context=context).state
-                    account_move_line_obj.write(cr,uid,[recon[0],recon[1]],{'state':'valid'},context=context)
-                    print 'debit2',account_move_line_obj.browse(cr,uid,recon[1],context=context)['state']
-                    #print 'invo',account_move_line_obj.browse(cr,uid,recon[1],context=context).invoice_id
+                    recon.append(reconcile[1])
                     account_move_line_obj.reconcile_partial(cr, uid, recon, 'manual', context=context)
                 if abs_brw.balance <= 0.0:
                     self.button_validate(cr, uid, ids, context=context)
