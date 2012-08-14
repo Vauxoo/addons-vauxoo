bcad009d106e7272c991cf8dffdd17ed52279a38
jose@vauxoo.com <>
2012-08-13 19:44:30 -0500

    [IMP] Remove move_id in the search

--- account_bank_statement_vauxoo/model/account_bank_statement.py
+++ account_bank_statement_vauxoo/model/account_bank_statement.py
@@ -438,9 +438,9 @@ class bank_statement_imported_lines(osv.osv):
                     else:
                         res.append(account_move_line_obj.search(cr,uid,[('invoice','in',invoice_ids),
                                                                     ('account_id','=',invoice.account_id.id),
-                                                                    ('move_id','=',False)
                                                                     ])) 
                     res.append('%s'%(aml.debit > 0 and 'debit' or aml.credit > 0 and 'credit'))
+                    print 'res',res
                     break
                 
                 res and [res[0].append(i.id) for i in abs_brw.acc_move_line_ids ] 
