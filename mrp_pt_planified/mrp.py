5e67dda84288defb3ceb3f9b91e8432bd8bc4c55
Julio Serna <julio@vauxoo.com>
2012-09-14 17:13:11 -0500

    [IMP][mrp_pt_planified] added field uom in o2m of product finished

--- mrp_pt_planified/mrp.py
+++ mrp_pt_planified/mrp.py
@@ -29,21 +29,32 @@ class mrp_production(osv.osv):
     _inherit='mrp.production'
     
     _columns = {
-        'pt_planified_ids' : fields.one2many('mrp.pt.planified','production_id','Products Planified'),
+        'pt_planified_ids' : fields.one2many('mrp.pt.planified','production_id','Products Finished Good Planified'),
     }
     
     def action_compute(self, cr, uid, ids, properties=[], context=None):
         mrp_pt = self.pool.get('mrp.pt.planified')
+        bom_obj = self.pool.get('mrp.bom')
         for production in self.browse(cr,uid,ids,context=context):
             
             mrp_pt.unlink(cr,uid,map(lambda x:x.id, production.pt_planified_ids ))
             
-            val = {
-                'product_id' : production.bom_id and production.bom_id.product_id.id or False,
-                'quantity' : production.bom_id and production.bom_id.product_qty or 0.0,
-                'production_id' : production.id
-            }
-            mrp_pt.create(cr,uid,val)
+            bom_point = production.bom_id
+            bom_id = production.bom_id.id
+            if not bom_point:
+                bom_id = bom_obj._bom_find(cr, uid, production.product_id.id, production.product_uom.id, properties)
+                    
+            if not bom_id:
+                raise osv.except_osv(_('Error'), _("Couldn't find a bill of material for this product."))
+            
+            for pro in bom_obj.browse(cr,uid,[bom_id]):
+                val = {
+                    'product_id' : pro.product_id and pro.product_id.id or False,
+                    'quantity' : pro.product_qty,
+                    'product_uom' : pro.product_uom.id,
+                    'production_id' : production.id
+                }
+                mrp_pt.create(cr,uid,val)
         res = super(mrp_production, self).action_compute(cr,uid,ids,properties=properties,context=context)
         return res
 mrp_production()
@@ -55,7 +66,8 @@ class mrp_pt_planified(osv.osv):
     _columns = {
         'product_id' : fields.many2one('product.product','Product'),
         'quantity' : fields.float('quantity'),
-        'production_id' : fields.many2one('mrp.production','production')
+        'production_id' : fields.many2one('mrp.production','production'),
+        'product_uom' : fields.many2one('product.uom','UoM')
     }
     
 mrp_pt_planified()
