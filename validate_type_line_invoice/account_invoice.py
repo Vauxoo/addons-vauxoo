99d01b20440ddce5bcf9b0fda1a0cf3045182792
Luis Torres <luis_t@vauxoo.com>
2012-09-14 16:24:31 -0500

    [ADD][validate_type_line_invoice]Add validation in lines to invoice, for not validate the invoice if type the account is 'payable' or 'receivable'

---
+++ validate_type_line_invoice/account_invoice.py
@@ -0,0 +1,48 @@
+# -*- encoding: utf-8 -*-
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#
+#    Copyright (c) 2010 Vauxoo - http://www.vauxoo.com/
+#    All Rights Reserved.
+#    info Vauxoo (info@vauxoo.com)
+############################################################################
+#    Coded by: Luis Torres (luis_t@vauxoo.com)
+############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from osv import osv, fields
+from tools.translate import _
+
+class account_invoice(osv.osv):
+    _inherit = 'account.invoice'
+    
+    def action_move_create(self, cr, uid, ids, context=None):
+        invoice_line_obj=self.pool.get('account.invoice.line')
+        account_obj=self.pool.get('account.account')
+        lines = invoice_line_obj.search(cr,uid,[('invoice_id','=',ids)])
+        ban=0
+        for line in lines:
+            id_account=invoice_line_obj.browse(cr,uid,line).account_id.id
+            type_line=account_obj.browse(cr,uid,id_account).type
+            if type_line == 'receivable' or type_line == 'payable':
+                ban=ban+1
+        if ban==0:
+            res =  super(account_invoice, self).action_move_create(cr, uid, ids)
+        else:
+            raise osv.except_osv(_('Error'), _("Type of account in line's most be differt to 'receivable' and 'payable'"))
+        return {}
+
+account_invoice()
