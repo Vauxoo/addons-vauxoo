9cc965bb1dd1aa6f68d36630fefe4064ac8e4319
Julio Serna <julio@vauxoo.com>
2012-09-15 14:50:28 -0500

    [IMP][mrp_variation] added function of calculate variation

--- mrp_variation/mrp_variation.py
+++ mrp_variation/mrp_variation.py
@@ -47,3 +47,73 @@ class mrp_variation(osv.osv):
     
 mrp_variation()
 
+class mrp_production(osv.osv):
+    _inherit='mrp.production'
+    
+    def action_finish(self,cr,uid,ids,context={}):
+        res = super(mrp_production, self).action_finish(cr,uid,ids,context=context)
+        self.create_variation(cr,uid,ids,context=context)
+        return res
+    
+    def create_consume_real(self,cr,uid,ids,context={}):
+        product_product=self.pool.get('product.product')
+        for production in self.browse(cr,uid,ids,context=context):
+            cr.execute("""
+                    SELECT sm.product_uom,sm.product_id,sum(COALESCE(sm.product_qty,0)) AS product_qty
+                        FROM mrp_production_move_ids mpmi JOIN stock_move sm
+                        ON sm.id=mpmi.move_id
+                    WHERE mpmi.production_id=%s
+                    AND sm.state='done'
+                    GROUP BY sm.product_id,sm.product_uom 
+                    """,(production.id,))
+            dat = cr.dictfetchall()
+            res_real={}
+            for lin in dat:
+                res_real.setdefault(lin['product_id'],0)
+                product=product_product.browse(cr,uid,lin['product_id'],context=context)
+                qty_uom_convert=self.pool.get('product.uom')._compute_qty(cr, uid, lin['product_uom'], lin['product_qty'], to_uom_id=product.uom_id.id)
+                res_real[lin['product_id']]+=qty_uom_convert
+        return res_real
+    
+    def create_consume_planned(self,cr,uid,ids,context={}):
+        product_product=self.pool.get('product.product')
+        for production in self.browse(cr,uid,ids,context=context):
+            cr.execute("""
+                    SELECT product_id,sum(COALESCE(product_qty,0)) AS product_qty,product_uom
+                        FROM mrp_production_product_line
+                    WHERE production_id=%s
+                    GROUP BY product_id,product_uom
+                    """,(production.id,))
+            dat = cr.dictfetchall()
+            res_planned={}
+            for lin in dat:
+                res_planned.setdefault(lin['product_id'],0)
+                product=product_product.browse(cr,uid,lin['product_id'],context=context)
+                qty_uom_convert=self.pool.get('product.uom')._compute_qty(cr, uid, lin['product_uom'], lin['product_qty'], to_uom_id=product.uom_id.id)
+                res_planned[lin['product_id']]+=qty_uom_convert
+        return res_planned
+    
+    def create_variation(self,cr,uid,ids,context={}):
+        prod_variation = self.pool.get('mrp.variation')
+        prod_product = self.pool.get('product.product')
+        for production in self.browse(cr,uid,ids,context=context):
+            prod_variation.unlink(cr,uid,map(lambda x:x.id, production.variation_ids))
+            real=self.create_consume_real(cr,uid,ids,context=context)
+            planned=self.create_consume_planned(cr,uid,ids,context=context)
+            lista=[]
+            lista.extend(real.keys())
+            lista.extend(planned.keys())
+            lista=list(set(lista))
+            res_diff = dict( planned )
+            for product_id in lista:
+                res_diff[product_id]-=real.get(product_id, 0)
+            for val_diff in res_diff.items():
+                prod_variation.create(cr,uid,{'product_id':val_diff[0],
+                    'quantity':(val_diff[1])*-1,
+                    'product_uom':prod_product.browse(cr,uid,val_diff[0]).uom_id.id,
+                    'production_id':production.id
+                    })
+        return True
+
+mrp_production()
+
