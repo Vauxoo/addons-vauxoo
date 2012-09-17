aefd4ef7f4c477a7a3dfad7101dfc419985bcb49
Luis Torres <luis_t@vauxoo.com>
2012-09-17 17:52:49 -0500

    [ADD]Add domain for acc_payment & pay_method_id

--- l10n_mx_purchase_payment_method/purchase.py
+++ l10n_mx_purchase_payment_method/purchase.py
@@ -29,6 +29,19 @@ from osv import osv, fields
 class purchase_order(osv.osv):
     _inherit = 'purchase.order'
     
+    def onchange_partner_id(self, cr, uid, ids, partner_id):
+        partner = self.pool.get('res.partner')
+        res=super(purchase_order, self).onchange_partner_id(cr, uid, ids, partner_id)
+        if partner_id:
+            partner_bank_obj=self.pool.get('res.partner.bank')
+            bank_partner_id=partner_bank_obj.search(cr, uid, [('partner_id','=',partner_id)])
+            pay_method_id=partner.browse(cr,uid,partner_id).pay_method_id.id
+            if bank_partner_id:
+                res['value'].update({'acc_payment': bank_partner_id[0] or False})
+            if pay_method_id:
+                res['value'].update({'pay_method_id':pay_method_id or False})
+        return res
+        
     _columns = {
         'acc_payment': fields.many2one ('res.partner.bank', 'Numero de cuenta', readonly=True, states={'draft':[('readonly',False)]},help = 'Es la cuenta con la que el cliente pagará la factura, si no se sabe con cual cuenta se va pagar dejarlo vacío y en el xml aparecerá "No identificado".'),
         'pay_method_id': fields.many2one('pay.method', 'Metodo de Pago', readonly=True, states={'draft':[('readonly',False)]}, help = 'Indica la forma en que se pagó o se pagará la factura, donde las opciones pueden ser: cheque, transferencia bancaria, depósito en cuenta bancaria, tarjeta de crédito, efectivo etc. Si no se sabe como va ser pagada la factura, dejarlo vacío y en el xml aparecerá “No identificado”.'),
