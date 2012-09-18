af91ae0909c4272917aadba3d2f678994fcbd379
Julio Serna <julio@vauxoo.com>
2012-09-18 17:44:27 -0500

    [IMP][mrp_account_variation] create function to get  accounst and journal

--- mrp_account_variation/mrp_account_variation.py
+++ mrp_account_variation/mrp_account_variation.py
@@ -41,20 +41,17 @@ class mrp_production(osv.osv):
         account_moves = []
         for production in self.browse(cr,uid,ids,context=context):
             for prod_variation in production.variation_ids:
-                if prod_variation.quantity>0:
-                    account_moves += [(prod_variation.product_id.categ_id.property_stock_journal.id, self._create_account_variation_move_line(cr,uid,prod_variation,prod_variation.product_id.property_stock_production.valuation_in_account_id.id,\
-                            prod_variation.product_id.property_stock_production.variation_in_account_id.id,prod_variation.cost_variation))]
-                if prod_variation.quantity<0:
-                    account_moves += [(prod_variation.product_id.categ_id.property_stock_journal.id, self._create_account_variation_move_line(cr,uid,prod_variation,prod_variation.product_id.property_stock_production.variation_in_account_id.id,\
-                            prod_variation.product_id.property_stock_production.valuation_in_account_id.id,(prod_variation.cost_variation*-1)))]
-                            
+                context['type'] = 'consumed'
+                if prod_variation.product_id and prod_variation.product_id.valuation == 'real_time':
+                    j_id, src_acc, dest_acc, reference_amount = self._get_journal_accounts(cr,uid,prod_variation,context=context)
+                    account_moves += [(j_id, self._create_account_variation_move_line(cr,uid,prod_variation,src_acc,dest_acc,reference_amount))]
+
             for prod_variation in production.variation_finished_product_ids:
-                if prod_variation.quantity>0:
-                    account_moves += [(prod_variation.product_id.categ_id.property_stock_journal.id, self._create_account_variation_move_line(cr,uid,prod_variation,prod_variation.product_id.property_stock_production.valuation_in_account_id.id,\
-                            prod_variation.product_id.property_stock_production.variation_in_account_id.id,prod_variation.cost_variation))]
-                if prod_variation.quantity<0:
-                    account_moves += [(prod_variation.product_id.categ_id.property_stock_journal.id, self._create_account_variation_move_line(cr,uid,prod_variation,prod_variation.product_id.property_stock_production.variation_in_account_id.id,\
-                            prod_variation.product_id.property_stock_production.valuation_in_account_id.id,(prod_variation.cost_variation*-1)))]
+                context['type'] = 'produced'
+                if prod_variation.product_id and prod_variation.product_id.valuation == 'real_time':
+                    j_id, src_acc, dest_acc, reference_amount = self._get_journal_accounts(cr,uid,prod_variation,context=context)
+                    account_moves += [(j_id, self._create_account_variation_move_line(cr,uid,prod_variation,src_acc,dest_acc,reference_amount))]
+                    
             if account_moves:
                 for j_id,move_lines in account_moves:
                     move_obj.create(cr, uid,
@@ -66,7 +63,54 @@ class mrp_production(osv.osv):
                     
         return True
     
-
+    def _get_journal_accounts(self,cr,uid,product,context={}):
+        if not context:
+            context = {}
+        
+        src_acc = False
+        dest_acc = False
+        
+        if context.get('type',False) == 'consumed':
+            if product.quantity > 0:
+                if product.product_id.property_stock_production.valuation_in_account_id:
+                    src_acc = product.product_id.property_stock_production.valuation_in_account_id.id
+                if product.product_id.property_stock_production.variation_in_account_id: 
+                    dest_acc = product.product_id.property_stock_production.variation_in_account_id.id
+                reference_amount = product.cost_variation
+                
+            if product.quantity < 0:
+                if product.product_id.property_stock_production.variation_in_account_id:
+                    src_acc = product.product_id.property_stock_production.variation_in_account_id.id
+                if product.product_id.property_stock_production.valuation_in_account_id:
+                    dest_acc = product.product_id.property_stock_production.valuation_in_account_id.id
+                reference_amount = product.cost_variation*-1
+                
+        if context.get('type',False) == 'produced':
+            if product.quantity > 0:
+                if product.product_id.property_stock_production.valuation_out_account_id:
+                    src_acc = product.product_id.property_stock_production.valuation_out_account_id.id
+                if product.product_id.property_stock_production.variation_out_account_id:
+                    dest_acc = product.product_id.property_stock_production.variation_out_account_id.id
+                reference_amount = product.cost_variation
+            if product.quantity < 0:
+                if product.product_id.property_stock_production.variation_out_account_id:
+                    src_acc = product.product_id.property_stock_production.variation_out_account_id.id
+                if product.product_id.property_stock_production.valuation_out_account_id:
+                    dest_acc = product.product_id.property_stock_production.valuation_out_account_id.id
+                reference_amount = product.cost_variation*-1
+            
+        journal_id = product.product_id.categ_id.property_stock_journal.id
+        
+        if not src_acc or not dest_acc:
+            raise osv.except_osv(_('Error!'),  _('There is no account defined for this location: "%s" ') % \
+                                    (product.product_id.property_stock_production.name,))
+                                    
+        if not journal_id:
+            raise osv.except_osv(_('Error!'), _('There is no journal defined on the product category: "%s" (id: %d)') % \
+                                    (product.product_id.categ_id.name, product.product_id.categ_id.id,))
+                                    
+        return journal_id, src_acc, dest_acc, reference_amount
+        
     def _create_account_variation_move_line(self, cr, uid, prod_variation, src_account_id, dest_account_id, reference_amount, context=None):
         debit_line_vals = {
                     'name': prod_variation.product_id.name,
