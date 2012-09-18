f48d872280f114f73f981f357533d8db405c150f
Luis Escobar <luiseev@mint>
2012-09-18 16:11:33 -0430

    [IMP] created parser check note report.

---
+++ m321_reports/report/picking.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import time
+from report import report_sxw
+from osv import osv
+import pooler
+
+class packing_list_report(report_sxw.rml_parse):
+    def __init__(self, cr, uid, name, context=None):
+        if context is None:
+            context = {}
+        super(packing_list_report, self).__init__(cr, uid, name, context=context)
+        self.localcontext.update({
+            'time': time,
+            'get_qtytotal':self._get_qtytotal,
+            'get_name':self._get_name,
+        })
+    def _get_qtytotal(self,move_lines):
+        total = 0.0
+        uom = move_lines[0].product_uom.name
+        for move in move_lines:
+            total+=move.product_qty
+        return {'quantity':total,'uom':uom}
+    
+    def _get_name(self,product):
+        print "hola"
+        prod = "ff"  #product.name_get([product.id])[0][1]
+        print "prod", prod
+        return prod
+
+report_sxw.report_sxw('report.m321_reports.packing_list_report',
+                      'stock.picking',
+                      'addons/m321_reports/report/picking.rml',
+                      parser=packing_list_report)
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
