46833721afcf0b909d3bb910a22c4a61d1aa6760
Luis Torres <luis_t@vauxoo.com>
2012-09-21 19:02:37 -0500

    [ADD]Inherit the class procurement_order function make_mo

---
+++ procurement_location/procurement.py
@@ -0,0 +1,34 @@
+# -*- encoding: utf-8 -*-
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#
+#    Copyright (c) 2012 Vauxoo - http://www.vauxoo.com
+#    All Rights Reserved.
+#    info@vauxoo.com
+############################################################################
+#    Coded by: Luis Torres (luis_t@vauxoo.com)
+############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from osv import osv, fields
+
+class procurement_order(osv.osv):
+    _inherit = 'procurement.order'
+    
+    def make_mo(self, cr, uid, ids, context=None):
+        res = super(procurement_order,self).make_mo(cr, uid, ids, context=context)
+        print 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',res
+        return res
