398237256c3f20f91ebbda96b9618e768b792831
Julio Serna <julio@vauxoo.com>
2012-09-24 16:22:02 -0500

    [IMP][mrp_request_return] create picking of same location

--- mrp_request_return/wizard/mrp_request_return.py
+++ mrp_request_return/wizard/mrp_request_return.py
@@ -31,9 +31,24 @@ from tools.translate import _
 class mrp_request_return(osv.osv_memory):
     _name='mrp.request.return'
     _columns={
-        're_line_ids' : fields.one2many('mrp.request.return.line','wizard_id','Acreation')
+        're_line_ids' : fields.one2many('mrp.request.return.line','wizard_id','Acreation'),
+        'type' : fields.selection([('request','Request'),('return','Return')], 'Type', required=True)
     }
-
+    def action_request_return(self, cr, uid, ids, context={}):
+        stock_picking = self.pool.get('stock.picking')
+        mrp_prouction = self.pool.get('mrp.production')
+        
+        mrp_ids = context.get('active_ids', [])
+        production = self.pool.get('mrp.production').browse(cr, uid, mrp_ids, context=context)[0]
+        
+        for wizard_moves  in self.browse(cr, uid, ids, context=context):
+            pick_id = mrp_prouction._make_production_internal_shipment(cr, uid, production, context=context)
+            for wiz_move in wizard_moves.re_line_ids:
+                if wiz_move.product_qty > 0.0:
+                    shipment_move_id = mrp_prouction._make_production_internal_shipment_line(cr, uid, wiz_move, pick_id, False)
+            
+        return True
+    
     def default_get(self, cr, uid, fields, context=None):
         if context is None: context = {}
         res = super(mrp_request_return, self).default_get(cr, uid, fields, context=context)
@@ -44,15 +59,20 @@ class mrp_request_return(osv.osv_memory):
         mrp_id, = mrp_ids
         if 're_line_ids' in fields:
             mrp = self.pool.get('mrp.production').browse(cr, uid, mrp_id, context=context)
-            moves = [self._partial_move_for(cr, uid, m) for m in mrp.product_lines]
+            moves = [self._partial_move_for(cr, uid, m, mrp) for m in mrp.product_lines]
             res.update(re_line_ids=moves)
         return res
 
-    def _partial_move_for(self, cr, uid, move):
+    def _partial_move_for(self, cr, uid, move, production):
         partial_move = {
             'product_id' : move.product_id.id,
-            'quantity' : 0,
+            'product_qty' : 0.0,
             'product_uom' : move.product_uom.id,
+            'product_uos_qty': move.product_uos and move.product_uos_qty or False,
+            'product_uos': move.product_uos and move.product_uos.id or False,
+            'location_id' : production.location_src_id.id,
+            'location_dest_id' : production.location_src_id.id,
+            'production_id' : move.production_id.id
         }
         return partial_move
 mrp_request_return()
@@ -63,11 +83,14 @@ class mrp_request_return_line(osv.osv_memory):
     _rec_name = 'product_id'
     _columns = {
         'product_id' : fields.many2one('product.product', string="Product", required=True),
-        'quantity' : fields.float("Quantity", digits_compute=dp.get_precision('Product UoM'), required=True),
+        'product_qty' : fields.float("Quantity", digits_compute=dp.get_precision('Product UoM'), required=True),
         'product_uom': fields.many2one('product.uom', 'Unit of Measure', required=True,),
         'location_id': fields.many2one('stock.location', 'Location', required=True),
         'location_dest_id': fields.many2one('stock.location', 'Dest. Location', required=True),
         'move_id' : fields.many2one('stock.move', "Move"),
+        'production_id' :fields.many2one('mrp.production','Production'),
+        'product_uos': fields.many2one('product.uom', 'Product UOS'),
+        'product_uos_qty' : fields.float('Quantity UoS'),
         'wizard_id' : fields.many2one('mrp.request.return', string="Wizard"),
     }
 
