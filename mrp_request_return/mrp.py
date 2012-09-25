9217c27be6023161aada51db70327c996f61b5cb
Julio Serna <julio@vauxoo.com>
2012-09-24 22:24:28 -0500

    [IMP][mrp_request_return] created function that create picking

--- mrp_request_return/mrp.py
+++ mrp_request_return/mrp.py
@@ -32,6 +32,34 @@ class mrp_production(osv.osv):
     _columns = {
         'picking_ids' : fields.one2many('stock.picking', 'production_id', 'Picking')
     }
+
+    def _make_production_internal_shipment2(self, cr, uid, production, context=None):
+        ir_sequence = self.pool.get('ir.sequence')
+        stock_picking = self.pool.get('stock.picking')
+        routing_loc = None
+        pick_type = 'internal'
+        address_id = False
+        
+        # Take routing address as a Shipment Address.
+        # If usage of routing location is a internal, make outgoing shipment otherwise internal shipment
+        if production.bom_id.routing_id and production.bom_id.routing_id.location_id:
+            routing_loc = production.bom_id.routing_id.location_id
+            if routing_loc.usage <> 'internal':
+                pick_type = 'out'
+            address_id = routing_loc.address_id and routing_loc.address_id.id or False
+
+        # Take next Sequence number of shipment base on type
+        pick_name = ir_sequence.get(cr, uid, 'stock.picking.' + pick_type)
+
+        picking_id = stock_picking.create(cr, uid, {
+            'name': pick_name+'-'+context.get('type',False),
+            'origin': (production.origin or '').split(':')[0] + ':' + production.name,
+            'type': pick_type,
+            'state': 'draft',
+            'address_id': address_id,
+            'company_id': production.company_id.id,
+        })
+        return picking_id
 mrp_production()
 
 class stock_picking(osv.osv):
--- mrp_request_return/wizard/mrp_request_return.py
+++ mrp_request_return/wizard/mrp_request_return.py
@@ -44,7 +44,8 @@ class mrp_request_return(osv.osv_memory):
         
         for wizard_moves in self.browse(cr, uid, ids, context=context):
             if wizard_moves.type == 'request':
-                pick_id = mrp_production._make_production_internal_shipment(cr, uid, production, context=context)
+                context['type'] = wizard_moves.type
+                pick_id = mrp_production._make_production_internal_shipment2(cr, uid, production, context=context)
                 stock_picking.write(cr, uid, pick_id, {'state':'draft', 'production_id':production.id})
                 for wiz_move in wizard_moves.re_line_ids:
                     if wiz_move.product_qty > 0.0:
@@ -52,7 +53,8 @@ class mrp_request_return(osv.osv_memory):
                         mrp_production._make_production_consume_line(cr, uid, wiz_move, False )
                             
             if wizard_moves.type == 'return':
-                pick_id_return = mrp_production._make_production_internal_shipment(cr, uid, production, context=context)
+                context['type'] = wizard_moves.type
+                pick_id_return = mrp_production._make_production_internal_shipment2(cr, uid, production, context=context)
                 stock_picking.write(cr, uid, pick_id_return, {'state':'draft', 'auto_picking':False, 'production_id':production.id})
                 for wiz_move2 in wizard_moves.re_line_ids:
                     if wiz_move2.product_qty > 0.0:
