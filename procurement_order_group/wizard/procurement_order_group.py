4ea9b04978c59001fac67fb92f907677a5b2a3fe
Julio Serna <julio@vauxoo.com>
2012-10-11 17:19:09 -0500

    [INIT] added module procurement_order_group

---
+++ procurement_order_group/wizard/procurement_order_group.py
@@ -0,0 +1,56 @@
+# -*- encoding: utf-8 -*-
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#
+#    Copyright (c) 2012 Vauxoo - http://www.vauxoo.com
+#    All Rights Reserved.
+#    info@vauxoo.com
+############################################################################
+#    Coded by: julio (julio@vauxoo.com)
+############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+import time
+from osv import osv, fields
+import decimal_precision as dp
+from tools.translate import _
+
+class procurement_order_group(osv.osv_memory):
+    _name='procurement.order.group'
+    
+    def procurement_group(self, cr, uid, ids, context=None):
+        print context,'imprimo context'
+        return {}
+    
+procurement_order_group()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
