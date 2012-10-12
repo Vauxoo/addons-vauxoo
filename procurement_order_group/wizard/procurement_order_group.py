462c34580b08c24c41dfcc687c315e24aee938ed
Julio Serna <julio@vauxoo.com>
2012-10-12 10:53:46 -0500

    [IMP][procurement_order_group] maked dummy wizard

--- procurement_order_group/wizard/procurement_order_group.py
+++ procurement_order_group/wizard/procurement_order_group.py
@@ -23,11 +23,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-import time
 from osv import osv, fields
-import decimal_precision as dp
-from tools.translate import _
-import netsvc
 
 class procurement_order_group(osv.osv_memory):
     _name='procurement.order.group'
@@ -37,37 +33,7 @@ class procurement_order_group(osv.osv_memory):
         if context is None:
             context = {}
         procurement_ids = context.get('active_ids', [])
-        res_product = {}
-        res_location = {}
-        res_method = {}
-        res_name = []
-        res_origin = []
-        
-        for procurement in procurement_order.browse(cr, uid, procurement_ids):
-            res_product.setdefault(procurement.product_id.id, 0)
-            res_product[procurement.product_id.id] += self.pool.get('product.uom')._compute_qty(cr, uid, procurement.product_uom.id, procurement.product_qty, to_uom_id=procurement.product_id.uom_id.id)
-            res_location.setdefault(procurement.location_id.id, 1)
-            res_method.setdefault(procurement.procure_method, 1)
-            res_name.append(procurement.name)
-            res_origin.append(procurement.origin)
-            product_uom = procurement.product_id.uom_id.id
-        if len(res_product) > 1 or len(res_location) > 1 or len(res_method) > 1:
-            print 'imprimo len mayor'
-        else:
-            procure_name = ','.join( map(str, res_name) )
-            procure_origin = ','.join( map(str, res_origin) )
-            procurement_order.create(cr, uid, {
-                'name' : procure_name,
-                'origin' : procure_origin,
-                'product_id' : res_product.items()[0][0],
-                'location_id' : res_location.keys()[0],
-                'product_qty' : res_product.items()[0][1],
-                'product_uom' : product_uom,
-                'procure_method' : res_method.keys()[0]
-            })
-        for proc in procurement_ids:
-            wf_service = netsvc.LocalService("workflow")
-            wf_service.trg_validate(uid, 'procurement.order', proc, 'button_cancel', cr)
+        procurement_order.do_group(cr, uid, procurement_ids, context=context)
         return {}
     
 procurement_order_group()
