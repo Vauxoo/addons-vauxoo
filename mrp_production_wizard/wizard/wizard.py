1640d2fc4c3e23cc10759b12b472c2164d4d1e56
fernando_ld@vauxoo.com <>
2012-10-19 13:47:51 -0500

    [IMP]Added functionality so it pulls the total quantity available of product on the wizard qty field.

--- mrp_production_wizard/wizard/wizard.py
+++ mrp_production_wizard/wizard/wizard.py
@@ -73,7 +73,11 @@ class wizard_data(osv.osv_memory):
             product_product_data = product_product_obj.browse(cr, uid, new_product_id, context=None)
             if product_product_data:
                 for line in product_product_data:
-                    val = {'name' : line.name, 'product_uom' : line.uom_id.id, 'product_qty' : 1}
+                    val = {
+                        'name' : line.name, 
+                        'product_uom' : line.uom_id.id,
+                        'product_qty' : line.qty_available,
+                    }
                     domain_uom = {'product_uom':[('category_id', '=', line.uom_id.category_id.id)]}
                     return {'value': val, 'domain': domain_uom}
         return {}
