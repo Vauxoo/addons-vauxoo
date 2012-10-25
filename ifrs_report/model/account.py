12f089238c405a6f46e7a32e4f70f0aa250696b4
Humberto Arocha <humbertoarocha@gmail.com>
2012-10-25 12:19:09 -0430

    [ADD] Added new method to model account.period to get previous periods

--- ifrs_report/model/account.py
+++ ifrs_report/model/account.py
@@ -7,7 +7,7 @@
 ###############Credits######################################################
 #    Coded by: Katherine Zaoral <katherine.zaoral@vauxoo.com>
 #    Coded by: Yanina Aular <yanina.aular@vauxoo.com>
-#    Planified by: Humberto Arocha <hbto@vauxoo.com>           
+#    Planified by: Humberto Arocha <hbto@vauxoo.com>
 #    Audited by: Nhomar Hernandez <nhomar@vauxoo.com>
 #############################################################################
 #    This program is free software: you can redistribute it and/or modify
@@ -24,5 +24,21 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-import account
-import ifrs
+from osv import osv
+from osv import fields
+
+class account_period(osv.osv):
+    _inherit='account.period'
+    
+    def previous(self, cr, uid, id, step, context=None):
+        if context is None: context = {}
+        period = self.pool.get('account.period').browse(cr,uid,id,context=context)
+        
+        #~ TODO: Take into account previous fiscalyear or just the fiscalyear of the period
+        ids = self.search(cr, uid, [('date_stop','<=',period.date_start),('special','=',False)])
+        if not ids:
+            self.search(cr, uid, [('date_stop','<=',period.date_start),('special','=',True)])
+        if len(ids)>=step:
+            return ids[-step]
+        return False
+account_period()
