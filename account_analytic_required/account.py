d23174e130edd7bf9c8b7c1b496a3ee6ec08b5b8
Luis Tores <luis_t@vauxoo.com>
2012-10-26 16:51:26 -0500

    [ADD][account_analytic_required]Add field analytic_required, for when is select required an account analytic to the validate an account.move

---
+++ account_analytic_required/account.py
@@ -0,0 +1,57 @@
+# -*- encoding: utf-8 -*-
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#
+#    Copyright (c) 2010 Vauxoo - http://www.vauxoo.com/
+#    All Rights Reserved.
+#    info Vauxoo (info@vauxoo.com)
+############################################################################
+#    Coded by: Luis Torres (luis_t@vauxoo.com)
+############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from osv import fields, osv
+from tools.translate import _
+
+class account_account(osv.osv):
+    _inherit='account.account'
+    
+    _columns = {
+        'analytic_required': fields.boolean('Analytic Required'),
+    }
+    
+account_account()
+
+
+class account_move(osv.osv):
+    _inherit='account.move'
+    
+    def button_validate(self, cursor, user, ids, context=None):
+        account_move_obj=self.pool.get('account.move.line')
+        account_obj=self.pool.get('account.account')
+        moves=account_move_obj.search(cursor, user, [('move_id', 'in', ids)])
+        if moves:
+            for move_id in moves:
+                move=account_move_obj.browse(cursor, user, move_id)
+                name_move=move.name or ''
+                analytic_st=move.account_id and move.account_id.analytic_required or False
+                if analytic_st is True:
+                    account_move_id=move.account_id and move.account_id.id or False
+                    analytic_acc_move=move.analytic_account_id and move.analytic_account_id.id or False
+                    if analytic_acc_move is False:
+                        raise osv.except_osv(_('Error'), _('Need add analytic account in move whit name '+ name_move + '.'))
+        res=super(account_move, self).button_validate(cursor, user, ids, context=context)
+        return res
