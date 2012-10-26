30bfdd56b90c94366cdb0838b49521b865146faa
fernando_ld@vauxoo.com <>
2012-10-26 10:48:55 -0500

    [IMP] Removed not needed iteration of list.

--- mrp_scheduled_onchange_product/mrp.py
+++ mrp_scheduled_onchange_product/mrp.py
@@ -37,14 +37,12 @@ class mrp_production_product_line(osv.osv):
     
     def onchange_product_scheduled_line(self, cr, uid, ids, product_id):
         if product_id:
-            new_product_id = [product_id]
             product_product_obj = self.pool.get('product.product')
-            product_product_data = product_product_obj.browse(cr, uid, new_product_id, context=None)
+            product_product_data = product_product_obj.browse(cr, uid, product_id, context=None)
             if product_product_data:
-                for line in product_product_data:
-                    val = {'name' : line.name, 'product_uom' : line.uom_id.id, 'product_qty' : 1}
-                    domain_uom = {'product_uom':[('category_id', '=', line.uom_id.category_id.id)]}
-                    return {'value': val, 'domain': domain_uom}
+                val = {'name' : product_product_data.name, 'product_uom' : product_product_data.uom_id.id, 'product_qty' : 1}
+                domain_uom = {'product_uom':[('category_id', '=', product_product_data.uom_id.category_id.id)]}
+                return {'value': val, 'domain': domain_uom}
         return {}
 
 mrp_production_product_line()
