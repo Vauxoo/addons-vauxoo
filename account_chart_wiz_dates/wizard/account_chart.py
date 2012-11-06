56d5f3bbd5e98b69035f5e49b1edaf6e5ba984c5
Luis Tores <luis_t@vauxoo.com>
2012-11-05 18:41:46 -0600

    [ADD][account_chart_wiz_dates]Add fields for filter by date in wizard the account_chart

---
+++ account_chart_wiz_dates/wizard/account_chart.py
@@ -0,0 +1,64 @@
+# -*- encoding: utf-8 -*-
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#
+#    Copyright (c) 2010 Vauxoo - http://www.vauxoo.com/
+#    All Rights Reserved.
+#    info Vauxoo (info@vauxoo.com)
+############################################################################
+#    Coded by: Luis Torres (luis_t@vauxoo.com)
+############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from osv import fields, osv
+import time
+
+class account_chart(osv.osv_memory):
+    _inherit= "account.chart"
+    
+    _columns={
+        'filter':fields.selection([ ('filter_no','Unfiltered'), ('periods','Periods'), ('dates','Dates')],'Filter',),
+        'initial_date': fields.date('Initial date',),
+        'end_date': fields.date('End date',),
+        }
+    
+    _defaults = {
+        'filter': 'filter_no',
+        }
+        
+    def onchange_fiscalyear(self, cr, uid, ids, fiscalyear_id=False, val_filter=False, context=None):
+        res={}
+        if val_filter=='periods':
+            res=super(account_chart, self).onchange_fiscalyear(cr, uid, ids, fiscalyear_id=fiscalyear_id, context=context)
+        return res
+        
+    def onchange_val_filter(self, cr, uid, ids, val_filter=False, fiscalyear_id=False, context=None):
+        res = {}
+        if val_filter=='dates':
+            res['value'] = {'period_from': False, 'period_to': False, 'initial_date': time.strftime('%Y-01-01'), 'end_date': time.strftime('%Y-%m-%d')}
+        elif val_filter=='periods':
+            res=super(account_chart, self).onchange_fiscalyear(cr, uid, ids, fiscalyear_id=fiscalyear_id, context=context)
+            res['value']['initial_date']= False
+            res['value']['end_date']= False
+        elif val_filter=='filter_no':
+            res['value'] = {'period_from': False, 'period_to': False, 'initial_date': False, 'end_date': False}
+        return res
+    
+    def account_chart_open_window(self, cr, uid, ids, context=None):
+        print 'ids', self.browse(cr, uid, ids)
+        res=super(account_chart, self).account_chart_open_window(cr, uid, ids, context=context)
+        print 'res', res
+        return res
