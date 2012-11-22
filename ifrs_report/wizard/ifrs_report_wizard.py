86529136caec9326a890dbcc179c8cf187a4a6d6
Katherine Zaoral (Vauxoo) <katherine.zaoral@vauxoo.com>
2012-11-22 12:29:01 -0430

    [ADD][REF] wizard: - campos nuevos: company, fiscalyear, type (requiered) y target_move. - esconder reportes de la banda derecha - manejo de todos los reportes a traves del wizard - borrado de comentarios y secciones viejas de codigo

--- ifrs_report/wizard/ifrs_report_wizard.py
+++ ifrs_report/wizard/ifrs_report_wizard.py
@@ -32,7 +32,16 @@ class ifrs_report_wizard(osv.osv_memory):
     _name = 'ifrs.report.wizard'
     _description = 'IFRS Report'
     _columns = {
-       'period': fields.many2one('account.period', 'Force period', help='Fiscal period to assign to the invoice. Keep empty to use the period of the current date.'),
+        'period': fields.many2one('account.period', 'Force period', help='Fiscal period to assign to the invoice. Keep empty to use the period of the current date.'),
+        'fiscalyear_id' : fields.many2one('account.fiscalyear', 'Fiscal Year' ),
+        'company_id' : fields.many2one('res.company', string='Company', ondelete='cascade', required = True ),
+        'type': fields.selection( [
+            ('ifrs','Two Columns'),
+            ('ifrs_12', 'Twelve Columns')],
+            string='Type', help='Number of columns to display', required=True ),
+        'target_move': fields.selection([('posted', 'All Posted Entries'),
+                                        ('all', 'All Entries'),
+                                        ], 'Target Moves'),
     }
 
     def _get_period(self, cr, uid, context={}):
@@ -49,28 +58,24 @@ class ifrs_report_wizard(osv.osv_memory):
     def print_report(self, cr, uid, ids, context={}):
         datas = {'ids': context.get('active_ids', [])}
         wizard_ifrs = self.browse(cr, uid, ids, context=context)[0]
-        datas['period'] = wizard_ifrs.period.id
+
+        datas['type'] = wizard_ifrs.type
+        datas['ifrs_title'] = context['title']
+        datas['company'] = wizard_ifrs.company_id.id
+
+        if datas['type'] == 'ifrs_12':
+            datas['period'] = 'All Periods'
+        else:
+            datas['period'] = wizard_ifrs.period.id or self._get_period(self, cr, uid)
+
+        datas['fiscalyear'] = wizard_ifrs.fiscalyear_id.id or wizard_ifrs.period.fiscalyear_id.id
+
         return {
             'type': 'ir.actions.report.xml',
-            'report_name': 'ifrs',
+            'report_name': datas['type'],
             'datas' : datas
        }
 
 ifrs_report_wizard()
-
-
-#~ class cfd_print(wizard.interface):
-        #~ states = {
-        #~ 'init': {
-            #~ 'actions': [],
-            #~ 'result': {
-                #~ 'type': 'print',
-                #~ 'report': 'ifrs_report',
-                #~ 'state': 'end',
-            #~ }
-        #~ },
-    #~ }
-#~ cfd_print("cfd.wizard.cfd.print")
-
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
