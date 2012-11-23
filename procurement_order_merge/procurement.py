188b12b46fb44c6a93753e6268c4cec9a18435bb
fernando_ld@vauxoo.com <>
2012-11-22 18:50:03 -0600

    [REM] Droped code for debugging and comments

--- procurement_order_merge/procurement.py
+++ procurement_order_merge/procurement.py
@@ -6,7 +6,7 @@
 #    All Rights Reserved.
 #    info@vauxoo.com
 ############################################################################
-#    Coded by: julio (julio@vauxoo.com)
+#    Coded by: fernandoL (fernando_ld@vauxoo.com)
 ############################################################################
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -50,18 +50,7 @@ class procurement_order(osv.osv):
             list_key.sort()
             return tuple(list_key)
         
-        procurement_order = self.pool.get('procurement.order')
-        
-        res_product = {}
-        res_location = {}
-        res_method = {}
-        res_products = {}
-        res_name = {}
-        res_origin = {}
-        product_uom = {}
-        proc_ids_to_cancel = {}
         new_orders = {}
-        ok = False
         
         for procurement in self.browse(cr, uid, ids):
             order_key = make_key(procurement, ('product_id', 'location_id', 'procure_method'))
@@ -79,90 +68,29 @@ class procurement_order(osv.osv):
                     'procure_method' : procurement.procure_method
                 })
             else:
-                #if procurement.date_order < order_infos['date_order']:
-                #    order_infos['date_order'] = procurement.date_order
-                #if procurement.notes:
-                #    order_infos['notes'] = (order_infos['notes'] or '') + ('\n%s' % (porder.notes,))
                 if procurement.name:
                     order_infos['name'] = (order_infos['name'] or '') + ',' + procurement.name
                 if procurement.origin:
                     order_infos['origin'] = (order_infos['origin'] or '') + ',' + procurement.origin
                 if procurement.product_qty:
                     order_infos['product_qty'] = (order_infos['product_qty'] or 0) + self.pool.get('product.uom')._compute_qty(cr, uid, procurement.product_uom.id, procurement.product_qty, to_uom_id=procurement.product_id.uom_id.id)
-                    ###########
-            print new_order,"<- new_order final de for\n"
             
-            
-            #########
         allorders = []
         orders_info = {}
         for order_key, (order_data, old_ids) in new_orders.iteritems():
         # skip merges with only one order
-            print order_key, "<- order key"#
-            print order_data, old_ids, "<- order data y old ids\n"#
             if len(old_ids) < 2:
                 allorders += (old_ids or [])
                 continue
 
-            # cleanup order line data
-            #for key, value in order_data['order_line'].iteritems():
-            #    del value['uom_factor']
-            #    value.update(dict(key))
-            #order_data['order_line'] = [(0, 0, value) for value in order_data['order_line'].itervalues()]
-
-            # create the new order
+            # create the new procurement order
             neworder_id = self.create(cr, uid, order_data)
             orders_info.update({neworder_id: old_ids})
             allorders.append(neworder_id)
 
-            # make triggers pointing to the old orders point to the new order
             for old_id in old_ids:
                 wf_service = netsvc.LocalService("workflow")
                 wf_service.trg_validate(uid, 'procurement.order', old_id, 'button_cancel', cr)
-                #wf_service.trg_redirect(uid, 'purchase.order', old_id, neworder_id, cr)
-                #wf_service.trg_validate(uid, 'purchase.order', old_id, 'purchase_cancel', cr)
             
-            ###########
-            """
-            if procurement.state <> 'draft':
-                ok = True
-                 
-            res_product.setdefault(procurement.product_id.id, 0)
-            res_product[procurement.product_id.id] += 1
-            res_products.setdefault(procurement.product_id.id, 0)
-            res_products[procurement.product_id.id] += self.pool.get('product.uom')._compute_qty(cr, uid, procurement.product_uom.id, procurement.product_qty, to_uom_id=procurement.product_id.uom_id.id)
-            res_location.setdefault(procurement.product_id.id, procurement.location_id.id)
-            res_method.setdefault(procurement.product_id.id, procurement.procure_method)
-            res_name.setdefault(procurement.product_id.id, [])
-            res_name[procurement.product_id.id].append(procurement.name)
-            res_origin.setdefault(procurement.product_id.id, [])
-            res_origin[procurement.product_id.id].append(procurement.origin)
-            product_uom.setdefault(procurement.product_id.id, procurement.product_id.uom_id.id)
-            proc_ids_to_cancel.setdefault(procurement.product_id.id, [])
-            proc_ids_to_cancel[procurement.product_id.id].append(procurement.id)
-            
-        if ok == True:
-            raise osv.except_osv(_('Error'), _('Procurements must be in state draft') )
-            
-        for lin in res_products:
-            if res_product[lin] > 1:
-                print lin,'impirmo lins'
-                print proc_ids_to_cancel, "<- proc_ids_to_cancel"
-                print res_origin, "<- res_origin"
-                procure_name = ','.join( map(str, res_name[lin]) )
-                procure_origin = ','.join( map(str, res_origin[lin]) )
-                procurement_order.create(cr, uid, {
-                    'name' : procure_name,
-                    'origin' : procure_origin,
-                    'product_id' : lin,
-                    'location_id' : res_location[lin],
-                    'product_qty' : res_products[lin],
-                    'product_uom' : product_uom[lin],
-                    'procure_method' : res_method[lin]
-                })
-                for proc in proc_ids_to_cancel[lin]:
-                    wf_service = netsvc.LocalService("workflow")
-                    wf_service.trg_validate(uid, 'procurement.order', proc, 'button_cancel', cr)
-    """
         return True
 procurement_order()
