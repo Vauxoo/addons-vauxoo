3512cd3e5baa0df4510097970a628a7a8be8cf54
fernando_ld@vauxoo.com <>
2012-11-23 17:15:31 -0600

    [REM] Cleaned code of comments and unused variables

--- mrp_jit_extended/wizard/procurement_order_merge.py
+++ mrp_jit_extended/wizard/procurement_order_merge.py
@@ -28,47 +28,21 @@ from tools.translate import _
 
 class procurement_order_merge_jit_extended(osv.osv_memory):
     _name = 'procurement.order.merge.jit.extended'
-    
+
     def procurement_merge_jit(self, cr, uid, ids, context=None):
         procurement_order = self.pool.get('procurement.order')
         mrp_production_pool = self.pool.get('mrp.production')
         if context is None:
             context = {}
         production_ids = context.get('active_ids', [])
-        procurement_product_list = []
-        procurement_product_records = []
         procurement_ids = []
-        procurement_ids2 = []
-        product_procurement_dict = {}
         for production_id in production_ids:
-            print production_id, "production id?"
             production_data = mrp_production_pool.browse(cr, uid, production_id, context=context)
             for line in production_data.procurement_ids:
-                #    line.state == 'draft':
-                #    print line.id, "id del procurement"
-                #    print line.state, "state"
-                #    print line.product_id.name, "producto\n"
-                if (line.product_id.id not in procurement_product_list and line.state == 'draft'):
-                    procurement_product_list.append(line.product_id.id)
-                    print "producto diferente agregado"
                 if line.state == 'draft':
-                    #procurement_product_records.append(line)
-                    #procurement_ids.append(line.id)
-                    #product_procurement_dict.update({line.product_id.id : procurement_ids.append(line.id)})
-                    #if product_procurement_dict.get(line.product_id.id) == None:
-                        #product_procurement_dict[line.product_id.id] = []
-                    product_procurement_dict.setdefault(line.product_id.id, [])
-                    product_procurement_dict[line.product_id.id].append(line.id)
-                    procurement_ids2.append(line.id)
-        print procurement_product_list, "lista de productos ke aparecen"
-        print product_procurement_dict, "diccionario de products y procurements"
-        
-        for prod_proc_ids in product_procurement_dict:
-            procurements_ids = product_procurement_dict.get(prod_proc_ids)
-            print procurements_ids, " ids para mandar"
-            
-        print procurement_ids2, "ids completos a mandar\n"
-        procurement_order.do_merge(cr, uid, procurement_ids2, context=context)
+                    procurement_ids.append(line.id)
+
+        procurement_order.do_merge(cr, uid, procurement_ids, context=context)
         return {}
-    
+
 procurement_order_merge_jit_extended()
