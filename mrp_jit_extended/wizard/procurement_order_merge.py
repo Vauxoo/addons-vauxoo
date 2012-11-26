0f8cae13c6f0a2a51f5a817b82453c9b86ebfd95
fernando_ld@vauxoo.com <>
2012-11-26 11:42:35 -0600

    [IMP] Wizard is now recursive.

--- mrp_jit_extended/wizard/procurement_order_merge.py
+++ mrp_jit_extended/wizard/procurement_order_merge.py
@@ -29,12 +29,19 @@ from tools.translate import _
 class procurement_order_merge_jit_extended(osv.osv_memory):
     _name = 'procurement.order.merge.jit.extended'
 
-    def procurement_merge_jit(self, cr, uid, ids, context=None):
+    def procurement_merge_jit(self, cr, uid, ids, context=None, rec_ids=None):
         procurement_order = self.pool.get('procurement.order')
         mrp_production_pool = self.pool.get('mrp.production')
+        print rec_ids,"rec ids"
         if context is None:
             context = {}
-        production_ids = context.get('active_ids', [])
+        if rec_ids is None:
+            print "entre if"
+            production_ids = context.get('active_ids', [])
+        else:
+            print "entre else"
+            production_ids = rec_ids
+        print production_ids, "productions ids"
         procurement_ids = []
         for production_id in production_ids:
             production_data = mrp_production_pool.browse(cr, uid, production_id, context=context)
@@ -42,7 +49,11 @@ class procurement_order_merge_jit_extended(osv.osv_memory):
                 if line.state == 'draft':
                     procurement_ids.append(line.id)
 
-        procurement_order.do_merge(cr, uid, procurement_ids, context=context)
+        print procurement_ids, " procurements ids, porke no entra?"
+        new_prods = procurement_order.do_merge(cr, uid, procurement_ids, context=context)
+        print new_prods, "nuevas producciones ya en wizard de productions"
+        if new_prods:
+            self.procurement_merge_jit(cr, uid, ids, context, new_prods)
         return {}
 
 procurement_order_merge_jit_extended()
