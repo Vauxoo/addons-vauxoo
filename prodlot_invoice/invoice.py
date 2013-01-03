af43c0485aa606ca2c90d6eb6aca843677fb0b28
Juan Carlos Funes <openerp1@hotmail.com>
2013-01-03 12:00:59 -0600

    [IMP][prodlot_invoice]add module prodlot_invoice

---
+++ prodlot_invoice/invoice.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#    Copyright (C) Vauxoo (<http://vauxoo.com>).
+#    All Rights Reserved
+###############Credits######################################################
+#    Coded by: Juan Carlos Funes(juan@vauxoo.com)
+#############################################################################
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+from osv import fields, osv, orm
+from tools.translate import _
+
+class account_invoice_line(osv.osv):
+    _inherit = "account.invoice.line"
+
+    _columns = {
+        'prod_lot': fields.many2one('stock.production.lot', 'Prod_lot'),
+}
+
+account_invoice_line()
