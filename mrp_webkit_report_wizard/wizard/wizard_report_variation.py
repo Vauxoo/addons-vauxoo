a6675de70bb6461335288f2124adf96f6fb74b14
fernando_ld@vauxoo.com <>
2013-01-24 15:10:46 -0600

    [IMP] Subproduction ids obtained

--- mrp_webkit_report_wizard/wizard/wizard_report_variation.py
+++ mrp_webkit_report_wizard/wizard/wizard_report_variation.py
@@ -97,6 +97,11 @@ class wizard_report_variation(osv.osv_memory):
             user = self.pool.get('res.users').browse(cr, uid, uid, context=context)
             company_id = context.get('company_id', user.company_id.id)
             prod_ids = tuple(data.get('product_ids'))
+            mrp_obj = self.pool.get('mrp.production')
+            production_ids = mrp_obj.search(cr, uid , [('state', 'not in', ('draft', 'cancel')), \
+            ('product_id', 'in', data.get('product_ids')), ('date_planned', '>', data.get('date_start')), \
+            ('date_planned', '<', data.get('date_finished')), ('state', 'not in', ('draft', 'cancel')), ('company_id', '=', company_id)])
+
             #obtain data for variation in consumed products
             cr.execute("""
 SELECT product_id, sum(quantity), sum(standard_price * quantity) FROM mrp_variation
@@ -104,17 +109,10 @@ INNER JOIN product_product
    ON product_product.id = mrp_variation.product_id
 INNER JOIN product_template
    ON product_template.id = product_product.product_tmpl_id
-WHERE production_id IN (
-
- SELECT id FROM mrp_production AS mp
- WHERE mp.date_planned > %s
-  AND mp.date_planned < %s
-  AND mp.product_id in %s
-  AND state NOT IN ('cancel' ,'draft')
-  AND company_id = %s
- )
+WHERE production_id IN 
+%s
 GROUP BY product_id
-            """, (data.get('date_start'), data.get('date_finished'), prod_ids, company_id))
+            """, (tuple(production_ids),))
             records = cr.fetchall()
             if not records:
                 raise osv.except_osv(_('Advice'), _('There is no production orders for the products you selected in the range of dates you specified.'))
@@ -130,23 +128,27 @@ INNER JOIN product_product
    ON product_product.id = mrp_variation_finished_product.product_id
 INNER JOIN product_template
    ON product_template.id = product_product.product_tmpl_id
-WHERE production_id IN (
-
- SELECT id FROM mrp_production AS mp
- WHERE mp.date_planned > %s
-  AND mp.date_planned < %s
-  AND mp.product_id in %s
-  AND state NOT IN ('cancel' ,'draft')
-  AND company_id = %s
- )
+WHERE production_id IN 
+%s
 GROUP BY product_id
-            """, (data.get('date_start'), data.get('date_finished'), prod_ids, company_id))
+            """, (tuple(production_ids),))
             records2 = cr.fetchall()
             finished_variation = []
             if records2:
                 for line in records2:
                     finished_data = self.pool.get('product.product').browse(cr, uid, line[0], context=context)
                     finished_variation.append((finished_data.name, line[1], finished_data.uom_id.name, line[2]))
+                    
+            #obtain ids of the products of children productions.
+            mrp_data = mrp_obj.browse(cr, uid, production_ids, context=context)
+            child_prod_ids = {}
+            for mrp in mrp_data:
+                if mrp.subproduction_ids:
+                    for subp in mrp.subproduction_ids:
+                        child_prod_ids.setdefault(subp.product_id.id, subp.product_id.id)
+            print child_prod_ids.values(), "child"
+            
+            mandaerror.sss
 
             datas = {
                 'ids': ids,
@@ -162,4 +164,8 @@ GROUP BY product_id
                 'report_name': 'webkitmrp.production_variation_group',
                 'datas': datas,
             }
+
+    def generate_datas_dict():
+        return report_datas
+    
 wizard_report_variation()
