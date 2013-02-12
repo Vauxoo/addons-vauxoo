7e1695ade76d1d751cc51bab38e053147ae377f7
Julio Serna <julio@vauxoo.com>
2013-02-12 16:54:10 -0600

    [IMP] added depends to account

--- l10n_mx_res_partner_bank_currency/__openerp__.py
+++ l10n_mx_res_partner_bank_currency/__openerp__.py
@@ -34,7 +34,7 @@
     """,
     "website" : "www.vauxoo.com",
     "license" : "AGPL-3",
-    "depends" : ["base"],
+    "depends" : ["account"],
     "init_xml" : [],
     "demo_xml" : [],
     "update_xml" : ["res_bank_view.xml",],
