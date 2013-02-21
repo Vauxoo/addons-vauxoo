c8b3421e97ee96859ff2a2e2d3e795a0e9601f27
Isaac Lopez <isaako34@hotmail.com>
2013-02-21 17:19:49 -0600

    [ADD][l10n_mx_cities] Agrega archivos init y openerp

---
+++ l10n_mx_cities/__openerp__.py
@@ -0,0 +1,47 @@
+# -*- encoding: utf-8 -*-
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#    Copyright (C) Vauxoo (<http://www.vauxoo.com>).
+#    All Rights Reserved
+#    Info Vauxoo (info@vauxoo.com)
+###############Credits######################################################
+#    Coded by: nhomar@vauxoo.com,
+#    Planified by: Nhomar Hernandez
+#    Finance by: 
+#    Audited by: Alejandro Negrin anegrin@vauxoo.com
+#############################################################################
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##############################################################################
+
+{
+    "name" : "Mexican Cities",
+    "version" : "0.1",
+    "depends" : ["base"],
+    "author" : "Vauxoo",
+    "description" : """
+    What do this module:
+    Load all Mexican States
+                    """,
+    "website" : "http://www.vauxoo.com",
+    "category" : "Generic Modules",
+    "init_xml" : [
+    ],
+    "demo_xml" : [
+    ],
+    "update_xml" : [
+        "data/l10n_mx_cities.xml",
+    ],
+    "active": False,
+    "installable": True,
+}
