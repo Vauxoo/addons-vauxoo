103afdd12027561d2b35b8d8f10f1dfe9abd7f36
Luis Tores <luis_t@vauxoo.com>
2013-03-01 15:46:31 -0600

    [REF][l10n_mx_facturae_pac_sf]Add word openerp in import osv & fields

--- l10n_mx_facturae_pac_sf/invoice.py
+++ l10n_mx_facturae_pac_sf/invoice.py
@@ -24,8 +24,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from osv import osv
-from osv import fields
+from openerp.osv import osv, fields
 import base64
 import xml.dom.minidom
 
