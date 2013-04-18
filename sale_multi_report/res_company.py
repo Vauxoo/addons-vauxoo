36667e22b506de69e9d91eb3cae9827f7bc65010
Julio Serna <julio@vauxoo.com>
2013-04-18 18:13:21 -0500

    [V7-MIGRATION]

--- sale_multi_report/res_company.py
+++ sale_multi_report/res_company.py
@@ -23,12 +23,12 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>. #
 ############################################################################
 
-from osv import osv
-from osv import fields
-from tools.translate import _
+from openerp.osv import fields, osv
+from openerp.tools.translate import _
 
 
-class invoice_multicompany_report(osv.osv):
+
+class invoice_multicompany_report(osv.Model):
     """
     invoice_multicompany_report
     """
@@ -37,4 +37,3 @@ class invoice_multicompany_report(osv.osv):
     _columns = {
         'sale_report_id': fields.many2one('ir.actions.report.xml', 'Sale Order Report', required=False, domain="[('model','=','sale.order')]"),
     }
-invoice_multicompany_report()
