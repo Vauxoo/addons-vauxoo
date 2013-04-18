7f2fe56750a1905420b04e38dca7c096c91de8f2
Julio Serna <julio@vauxoo.com>
2013-04-18 17:48:38 -0500

    [AUTOPEP8]

--- purchase_supplier/purchase.py
+++ purchase_supplier/purchase.py
@@ -32,21 +32,26 @@ from mx import DateTime
 from tools import config
 from tools.translate import _
 
+
 class purchase_order(osv.osv):
     _inherit = "purchase.order"
-    
-    def wkf_confirm_order(self, cr, uid, ids, context = None):
+
+    def wkf_confirm_order(self, cr, uid, ids, context=None):
         product_supp_obj = self.pool.get('product.supplierinfo')
-        company_id = self.pool.get('res.users').browse(cr, uid, uid).company_id.id
+        company_id = self.pool.get(
+            'res.users').browse(cr, uid, uid).company_id.id
         product_obj = self.pool.get('product.template')
-        if super(purchase_order, self).wkf_confirm_order(cr, uid, ids, context = context):
-            for po in self.browse(cr, uid, ids, context = context):
+        if super(purchase_order, self).wkf_confirm_order(cr, uid, ids, context=context):
+            for po in self.browse(cr, uid, ids, context=context):
                 partner_id = po.partner_id.id
                 for line in po.order_line:
                     product_id = line.product_id.product_tmpl_id.id
                     if not product_supp_obj.search(cr, uid, [('product_id', '=', product_id), ('name', '=', partner_id)]):
-                        product_obj.write(cr,uid,[product_id],{'seller_ids':[(0,0,{'name': partner_id, 'min_qty': 1.0, 'delay': 1,
-                            'sequence': 10, 'product_id': product_id, 'company_id': company_id, 'product_uom': line and line.product_id and line.product_id.uom_id and line.product_id.uom_id.id })]})
+                        product_obj.write(
+                            cr, uid, [
+                                product_id], {
+                                    'seller_ids': [(0, 0, {'name': partner_id, 'min_qty': 1.0, 'delay': 1,
+                                                           'sequence': 10, 'product_id': product_id, 'company_id': company_id, 'product_uom': line and line.product_id and line.product_id.uom_id and line.product_id.uom_id.id})]})
             return True
         else:
             return False
