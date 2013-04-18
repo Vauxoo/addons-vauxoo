9b778169c70d795d81454fb2c07cea6188325b4a
Julio Serna <julio@vauxoo.com>
2013-04-18 18:43:52 -0500

    [AUTOPEP8]

--- validate_type_line_invoice/account_invoice.py
+++ validate_type_line_invoice/account_invoice.py
@@ -29,21 +29,26 @@ from openerp.tools.translate import _
 
 class account_invoice(osv.Model):
     _inherit = 'account.invoice'
-    
+
     def action_move_create(self, cr, uid, ids, context=None):
-        invoice_line_obj=self.pool.get('account.invoice.line')
-        account_obj=self.pool.get('account.account')
+        invoice_line_obj = self.pool.get('account.invoice.line')
+        account_obj = self.pool.get('account.account')
         for id_ in ids:
-            lines = invoice_line_obj.search(cr,uid,[('invoice_id','=',id_)])
+            lines = invoice_line_obj.search(
+                cr, uid, [('invoice_id', '=', id_)])
             for line in lines:
-                id_account=invoice_line_obj.browse(cr,uid,line).account_id.id
-                type_line=account_obj.browse(cr,uid,id_account).type
+                id_account = invoice_line_obj.browse(
+                    cr, uid, line).account_id.id
+                type_line = account_obj.browse(cr, uid, id_account).type
                 if type_line == 'receivable' or type_line == 'payable':
-                    raise osv.except_osv(_('Error'), _("Type of account in line's must be differt to 'receivable' and 'payable'"))
-            type_acc_invo=self.browse(cr,uid,id_).account_id.type
-            type_invoice=self.browse(cr,uid,id_).type
-            if (type_invoice == 'out_invoice' or type_invoice=='out_refound') and type_acc_invo != 'receivable':
-                raise osv.except_osv(_('Error'), _("Type of account in invoice to Customer must be 'receivable'"))
-            if (type_invoice == 'in_invoice' or type_invoice=='in_refound') and type_acc_invo != 'payable':
-                raise osv.except_osv(_('Error'), _("Type of account in invoice to Partner must be 'payable'"))
+                    raise osv.except_osv(_('Error'), _(
+                        "Type of account in line's must be differt to 'receivable' and 'payable'"))
+            type_acc_invo = self.browse(cr, uid, id_).account_id.type
+            type_invoice = self.browse(cr, uid, id_).type
+            if (type_invoice == 'out_invoice' or type_invoice == 'out_refound') and type_acc_invo != 'receivable':
+                raise osv.except_osv(_('Error'), _(
+                    "Type of account in invoice to Customer must be 'receivable'"))
+            if (type_invoice == 'in_invoice' or type_invoice == 'in_refound') and type_acc_invo != 'payable':
+                raise osv.except_osv(_('Error'), _(
+                    "Type of account in invoice to Partner must be 'payable'"))
         return super(account_invoice, self).action_move_create(cr, uid, ids)
