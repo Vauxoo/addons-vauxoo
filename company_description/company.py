19248d86e42789c9c4eb117a5636ca4a308a626d
Julio Serna <julio@vauxoo.com>
2013-04-18 15:05:41 -0500

    [AUTOPEP8]

--- company_description/company.py
+++ company_description/company.py
@@ -25,11 +25,12 @@
 ##############################################################################
 from osv import osv, fields
 
+
 class res_company(osv.osv):
     _inherit = 'res.company'
     _columns = {
-        'description_invoice': fields.text('Description Invoice',translate=True),
-        'description_sale': fields.text('Description Sale',translate=True),
-        'description_purchase': fields.text('Description Purchase',translate=True),
+        'description_invoice': fields.text('Description Invoice', translate=True),
+        'description_sale': fields.text('Description Sale', translate=True),
+        'description_purchase': fields.text('Description Purchase', translate=True),
     }
 res_company()
