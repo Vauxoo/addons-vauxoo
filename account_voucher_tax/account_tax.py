bcb109f396b31ab317ae0fda8bbd67ea4bff0f8f
Julio Serna <julio@vauxoo.com>
2013-04-18 14:50:03 -0500

    [AUTOPEP8]

--- account_voucher_tax/account_tax.py
+++ account_voucher_tax/account_tax.py
@@ -27,14 +27,14 @@ from openerp.osv import osv, fields
 
 
 class account_tax(osv.Model):
-     _inherit = "account.tax"
-     
-     _columns = {
-          'tax_voucher_ok': fields.boolean('Tax Vocuher Ok',help='help'),
-          'account_collected_voucher_id': fields.many2one('account.account','Account Collected Voucher'),
-          'account_paid_voucher_id': fields.many2one('account.account','Account Paid Voucher'),
-          'account_expense_voucher_id': fields.many2one('account.account','Account Expense Voucher'),
-          'account_income_voucher_id': fields.many2one('account.account','Account Income Voucher'),
-     }
-     
+    _inherit = "account.tax"
+
+    _columns = {
+        'tax_voucher_ok': fields.boolean('Tax Vocuher Ok', help='help'),
+        'account_collected_voucher_id': fields.many2one('account.account', 'Account Collected Voucher'),
+        'account_paid_voucher_id': fields.many2one('account.account', 'Account Paid Voucher'),
+        'account_expense_voucher_id': fields.many2one('account.account', 'Account Expense Voucher'),
+        'account_income_voucher_id': fields.many2one('account.account', 'Account Income Voucher'),
+    }
+
 account_tax()
