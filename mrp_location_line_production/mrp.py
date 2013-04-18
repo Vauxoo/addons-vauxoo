66e322941b9f2021f204ed591ce9c19a5b531571
Julio Serna <julio@vauxoo.com>
2013-04-18 16:29:01 -0500

    [V7-MIGRATION]

--- mrp_location_line_production/mrp.py
+++ mrp_location_line_production/mrp.py
@@ -23,10 +23,10 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from osv import osv, fields
+from openerp.osv import osv, fields
 
 
-class mrp_production(osv.osv):
+class mrp_production(osv.Model):
     _inherit = 'mrp.production'
 
     def _make_production_produce_line(self, cr, uid, production, context=None):
