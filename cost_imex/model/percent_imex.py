c396334f770a732d9e782490ff50430df5d4c449
Julio Serna <julio@vauxoo.com>
2013-04-18 15:06:57 -0500

    [AUTOPEP8]

--- cost_imex/model/percent_imex.py
+++ cost_imex/model/percent_imex.py
@@ -35,60 +35,61 @@ import decimal_precision as dp
 
 class percent_imex(osv.osv):
     """ """
-    
-    _name='percent.imex'
 
-    _columns={
-    
-    'line_purchase_id':fields.many2one('purchase.order.line','Line',help='Purchase line to compute apply'),
-    'total_with_flete':fields.float('Total with Flete',digits_compute=dp.get_precision('Cost Imex'),help='Compute total with flete'),
-    'price_unit_bf_flete':fields.float('Price Unit before Flete',digits_compute=dp.get_precision('Cost Imex'),help='Price Unit compute before Flete'),
-    'tax_base':fields.float('Tax Base',digits_compute=dp.get_precision('Cost Imex'),help='Tax base is total + currency '),
-    'purchase_id':fields.many2one('purchase.order','Purchase'),
-    'percent_lines':fields.one2many('percent.imex.line','percent_id','Percents to Apply',help='Percent to compute'),
-    'total_national_expense':fields.float('Total national spending',digits_compute=dp.get_precision('Cost Imex'),help='Sum of all taxes calculated'),
-    'cost_unit':fields.float('Cost Unit',digits_compute=dp.get_precision('Cost Imex'),help='Cost unit comput betewen Total national spending,quantity and price '),
-    'cost_unit_total':fields.float('Cost Unit Total',digits_compute=dp.get_precision('Cost Imex'),help='Compute of cost with cost unit'),
-    'cost_qty':fields.float('Total',digits_compute=dp.get_precision('Cost Imex'),help='Compute betewen  Cost unit total ant quantity in the line'),
+    _name = 'percent.imex'
+
+    _columns = {
+
+        'line_purchase_id': fields.many2one('purchase.order.line', 'Line', help='Purchase line to compute apply'),
+        'total_with_flete': fields.float('Total with Flete', digits_compute=dp.get_precision('Cost Imex'), help='Compute total with flete'),
+        'price_unit_bf_flete': fields.float('Price Unit before Flete', digits_compute=dp.get_precision('Cost Imex'), help='Price Unit compute before Flete'),
+        'tax_base': fields.float('Tax Base', digits_compute=dp.get_precision('Cost Imex'), help='Tax base is total + currency '),
+        'purchase_id': fields.many2one('purchase.order', 'Purchase'),
+        'percent_lines': fields.one2many('percent.imex.line', 'percent_id', 'Percents to Apply', help='Percent to compute'),
+        'total_national_expense': fields.float('Total national spending', digits_compute=dp.get_precision('Cost Imex'), help='Sum of all taxes calculated'),
+        'cost_unit': fields.float('Cost Unit', digits_compute=dp.get_precision('Cost Imex'), help='Cost unit comput betewen Total national spending,quantity and price '),
+        'cost_unit_total': fields.float('Cost Unit Total', digits_compute=dp.get_precision('Cost Imex'), help='Compute of cost with cost unit'),
+        'cost_qty': fields.float('Total', digits_compute=dp.get_precision('Cost Imex'), help='Compute betewen  Cost unit total ant quantity in the line'),
     }
-    _rec_name='line_purchase_id'
-    
+    _rec_name = 'line_purchase_id'
+
 percent_imex()
 
 
 class percent_imex_line(osv.osv):
     """ """
-    
-    _name='percent.imex.line'
 
-    _columns={
-    'percent':fields.float('Percent',digits_compute=dp.get_precision('Cost Imex'),help='Percent to compute tax'),
-    #'purchase_id':fields.many2one('purchase.order','Purchase'),
-    'percent_id':fields.many2one('percent.imex','Percent'),
-    'date':fields.date('Date',help='Date apply by percent'),
-    'amount':fields.float('Amount',digits_compute=dp.get_precision('Cost Imex'),help='Quantity to compute by percent'),
+    _name = 'percent.imex.line'
+
+    _columns = {
+        'percent': fields.float('Percent', digits_compute=dp.get_precision('Cost Imex'), help='Percent to compute tax'),
+        #'purchase_id':fields.many2one('purchase.order','Purchase'),
+        'percent_id': fields.many2one('percent.imex', 'Percent'),
+        'date': fields.date('Date', help='Date apply by percent'),
+        'amount': fields.float('Amount', digits_compute=dp.get_precision('Cost Imex'), help='Quantity to compute by percent'),
     }
 
-    _rec_name='percent'
+    _rec_name = 'percent'
 
 
 percent_imex_line()
 
+
 class national_special_tax(osv.osv):
     """ """
-    
-    _name='national.special.tax'
 
-    _columns={
-    'name':fields.char('Name',50,help='Tax name to identified'),
-    'percent':fields.float('Percent',digits_compute=dp.get_precision('Cost Imex'),help='Percent tax to importation compute'),
-    'date':fields.date('Date',help='Date entered on file'),
-    
+    _name = 'national.special.tax'
+
+    _columns = {
+        'name': fields.char('Name', 50, help='Tax name to identified'),
+        'percent': fields.float('Percent', digits_compute=dp.get_precision('Cost Imex'), help='Percent tax to importation compute'),
+        'date': fields.date('Date', help='Date entered on file'),
+
     }
 
     _defaults = {
-    'date': lambda *a: time.strftime('%Y-%m-%d'),
-    
+        'date': lambda *a: time.strftime('%Y-%m-%d'),
+
     }
 
 national_special_tax()
--- cost_imex/model/purchase.py
+++ cost_imex/model/purchase.py
@@ -32,97 +32,99 @@ from tools.translate import _
 import time
 import decimal_precision as dp
 
+
 class inherit_purchase(osv.osv):
     """ """
-    
-    _inherit='purchase.order'
 
-    _columns={
-    'flete':fields.float('Flete',digits_compute=dp.get_precision('Cost Imex'),help='Price to be paid by renting a boat, plane or truck, or cargo carried'),
-    'percent_apply':fields.many2many('national.special.tax','special_national_tax_rel','purchase_id','special_tax_id','Percents to Apply',help='Percent to compute'),
-    'percent_special':fields.float('Other Percent',digits_compute=dp.get_precision('Cost Imex'),help='Percent to special compute'),
-    'import_purchase':fields.boolean('Importation Purchase',help='Indicate if purchase is a importation '),
-    'percent_imex_ids':fields.one2many('percent.imex','purchase_id','Percen',domain=[('percent_lines','!=',False)]),
-    'percent_imex_s_ids':fields.one2many('percent.imex','purchase_id','Percen',domain=[('percent_lines','=',False)]),
-    
-}
-    
-    
-    
-    def compute_percent(self,cr,uid,ids,imex_line,order_line,base,context=None):
+    _inherit = 'purchase.order'
+
+    _columns = {
+        'flete': fields.float('Flete', digits_compute=dp.get_precision('Cost Imex'), help='Price to be paid by renting a boat, plane or truck, or cargo carried'),
+        'percent_apply': fields.many2many('national.special.tax', 'special_national_tax_rel', 'purchase_id', 'special_tax_id', 'Percents to Apply', help='Percent to compute'),
+        'percent_special': fields.float('Other Percent', digits_compute=dp.get_precision('Cost Imex'), help='Percent to special compute'),
+        'import_purchase': fields.boolean('Importation Purchase', help='Indicate if purchase is a importation '),
+        'percent_imex_ids': fields.one2many('percent.imex', 'purchase_id', 'Percen', domain=[('percent_lines', '!=', False)]),
+        'percent_imex_s_ids': fields.one2many('percent.imex', 'purchase_id', 'Percen', domain=[('percent_lines', '=', False)]),
+
+    }
+
+    def compute_percent(self, cr, uid, ids, imex_line, order_line, base, context=None):
         if context is None:
-            context={} 
+            context = {}
         imex_line_obj = self.pool.get('percent.imex.line')
-        
-        amount= (base * (imex_line.percent/100))
+
+        amount = (base * (imex_line.percent/100))
         imex_lines = {
-        'percent':imex_line.percent,
-        'date':imex_line.date,
-        'amount':amount,
-        
+            'percent': imex_line.percent,
+            'date': imex_line.date,
+            'amount': amount,
+
         }
-        
-        
+
         return imex_lines
-    
-    def compute_import_taxes(self,cr,uid,ids,context=None):
-        
+
+    def compute_import_taxes(self, cr, uid, ids, context=None):
+
         if context is None:
-            context={} 
+            context = {}
         lines_s = []
         lines = []
         rate = False
-        for purchase in self.browse(cr,uid,ids,context=context):
+        for purchase in self.browse(cr, uid, ids, context=context):
             lines_s = []
             lines = []
-            print 'purchase.flete',purchase.flete
-            print 'purchase.percent_special',purchase.percent_special
+            print 'purchase.flete', purchase.flete
+            print 'purchase.percent_special', purchase.percent_special
             if purchase.flete and purchase.percent_special:
                 for line in purchase.order_line:
-                    total_with_flete = (purchase.flete * line.price_subtotal) + line.price_subtotal
-                    
+                    total_with_flete = (
+                        purchase.flete * line.price_subtotal) + line.price_subtotal
+
                     if purchase.pricelist_id and purchase.pricelist_id.currency_id and \
-                                                 purchase.pricelist_id.currency_id.id != purchase.company_id and \
-                                                                                 purchase.company_id.currency_id and \
-                                                                                purchase.company_id.currency_id.id: 
-                        rate = [ round((1 / rate.rate),4)   for rate in purchase.pricelist_id.currency_id.rate_ids if rate.name <= purchase.date_order]
-                    print 'rate',rate 
+                        purchase.pricelist_id.currency_id.id != purchase.company_id and \
+                        purchase.company_id.currency_id and \
+                            purchase.company_id.currency_id.id:
+                        rate = [round((
+                            1 / rate.rate), 4) for rate in purchase.pricelist_id.currency_id.rate_ids if rate.name <= purchase.date_order]
+                    print 'rate', rate
                     tax_base = total_with_flete * (rate and rate[0] or 1)
-                    price_unit_bf_flete = total_with_flete/line.product_qty 
-                    percent_lines = [(0,0,self.compute_percent(cr,uid,ids,i,line,tax_base,context=context)) for i in purchase.percent_apply]
-                    total_national_expense = sum([i[2].get('amount') for i in percent_lines])
-                    cost_unit = (total_national_expense + tax_base)/line.product_qty
-                    cost_unit_total =  (price_unit_bf_flete + (total_national_expense /(rate and rate[0] or 1))/ line.product_qty) * (purchase.percent_special)
-                    print '(price_unit_bf_flete + (total_national_expense /(rate and rate[0] or 1))/ line.product_qty)',(price_unit_bf_flete + (total_national_expense /(rate and rate[0] or 1))/ line.product_qty)
-                    #cost_unit_total =  ((total_with_flete * purchase.percent_special) + total_national_expense )/line.product_qty
-                    cost_qty = cost_unit_total * line.product_qty 
-                    lines.append((0,0,{
-                    'line_purchase_id':line.id,
-                    'total_with_flete':total_with_flete,
-                    'price_unit_bf_flete':price_unit_bf_flete,
-                    'tax_base':tax_base,
-                    'percent_lines': percent_lines,
-                    'total_national_expense':total_national_expense,
-                    'cost_unit':cost_unit ,
-                    
-                    }
-                    ))
-                    
-                    lines_s.append((0,0,{
-                    'line_purchase_id':line.id,
-                    'cost_unit_total':cost_unit_total,
-                    'cost_qty':cost_qty,
-                    
-                    }))
-                    
-                #print 'line',lines    
-                #print 'line_s',lines_s    
-                self.write(cr,uid,[purchase.id],{'percent_imex_ids':lines,
-                                                 'percent_imex_s_ids':lines_s},context=context)
+                    price_unit_bf_flete = total_with_flete/line.product_qty
+                    percent_lines = [(0, 0, self.compute_percent(
+                        cr, uid, ids, i, line, tax_base, context=context)) for i in purchase.percent_apply]
+                    total_national_expense = sum([i[2].get(
+                        'amount') for i in percent_lines])
+                    cost_unit = (
+                        total_national_expense + tax_base)/line.product_qty
+                    cost_unit_total = (price_unit_bf_flete + (total_national_expense / (
+                        rate and rate[0] or 1)) / line.product_qty) * (purchase.percent_special)
+                    print '(price_unit_bf_flete + (total_national_expense /(rate and rate[0] or 1))/ line.product_qty)', (price_unit_bf_flete + (total_national_expense / (rate and rate[0] or 1)) / line.product_qty)
+                    # cost_unit_total =  ((total_with_flete *
+                    # purchase.percent_special) + total_national_expense
+                    # )/line.product_qty
+                    cost_qty = cost_unit_total * line.product_qty
+                    lines.append((0, 0, {
+                                  'line_purchase_id': line.id,
+                                  'total_with_flete': total_with_flete,
+                                  'price_unit_bf_flete': price_unit_bf_flete,
+                                  'tax_base': tax_base,
+                                  'percent_lines': percent_lines,
+                                  'total_national_expense': total_national_expense,
+                                  'cost_unit': cost_unit,
+
+                                  }
+                                  ))
+
+                    lines_s.append((0, 0, {
+                                    'line_purchase_id': line.id,
+                                    'cost_unit_total': cost_unit_total,
+                                    'cost_qty': cost_qty,
+
+                                    }))
+
+                # print 'line',lines
+                # print 'line_s',lines_s
+                self.write(cr, uid, [purchase.id], {'percent_imex_ids': lines,
+                                                    'percent_imex_s_ids': lines_s}, context=context)
         return True
-        
+
 inherit_purchase()
-
-
-
-
