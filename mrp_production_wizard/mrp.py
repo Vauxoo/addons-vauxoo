6f6811a7c91144a5b18a6b4c98fda5a61c9c5574
Julio Serna <julio@vauxoo.com>
2013-04-18 16:36:23 -0500

    [AUTOPEP8]

--- mrp_production_wizard/mrp.py
+++ mrp_production_wizard/mrp.py
@@ -34,9 +34,10 @@ from openerp.osv import osv, fields
 import time
 from tools import ustr
 
+
 class mrp_production(osv.osv):
     _inherit = "mrp.production"
-    
+
     """
     """
     def create_production_wizard(self, cr, uid, product, list_produce, context):
@@ -47,27 +48,32 @@ class mrp_production(osv.osv):
 
         total_weight = 0
         for line in list_produce:
-                product_obj_data = self.pool.get('product.product').browse(cr, uid, line['product_id'], context=None)
+                product_obj_data = self.pool.get('product.product').browse(
+                    cr, uid, line['product_id'], context=None)
                 if product_obj_data.weight:
-                    total_weight += (line['product_qty'] * product_obj_data.weight * (product.uom_id.factor / product_obj_data.uom_id.factor))
+                    total_weight += (line['product_qty'] * product_obj_data.weight * (
+                        product.uom_id.factor / product_obj_data.uom_id.factor))
                 else:
-                    total_weight += line['product_qty'] * (product.uom_id.factor / product_obj_data.uom_id.factor)
-        total_weight = total_weight / ((product.weight or 1) * product.uom_id.factor)
-        
-        default_location_dict = self.product_id_change(cr, uid, [], product.id, context)
+                    total_weight += line['product_qty'] * (
+                        product.uom_id.factor / product_obj_data.uom_id.factor)
+        total_weight = total_weight / ((
+            product.weight or 1) * product.uom_id.factor)
+
+        default_location_dict = self.product_id_change(
+            cr, uid, [], product.id, context)
         if (default_location_dict['value']['location_src_id'] & default_location_dict['value']['location_dest_id']):
             production_order_dict = {
-                'name' : self.pool.get('ir.sequence').get(cr, uid, 'mrp.production'),
-                'date_planned' : time.strftime('%Y-%m-%d %H:%M:%S'),
-                'product_id' : product.id,
-                'product_qty' : total_weight,
-                'product_uom' : product.uom_id.id,
+                'name': self.pool.get('ir.sequence').get(cr, uid, 'mrp.production'),
+                'date_planned': time.strftime('%Y-%m-%d %H:%M:%S'),
+                'product_id': product.id,
+                'product_qty': total_weight,
+                'product_uom': product.uom_id.id,
                 'location_src_id': default_location_dict['value']['location_src_id'],
                 'location_dest_id': default_location_dict['value']['location_dest_id'],
-                'state' : 'draft'
+                'state': 'draft'
             }
             new_id = self.create(cr, uid, production_order_dict)
-            
+
             for line in list_produce:
                 production_scheduled_dict = {
                     'name': line['name'],
@@ -76,17 +82,20 @@ class mrp_production(osv.osv):
                     'product_uom': line['product_uom'],
                     'production_id': new_id,
                 }
-                self.pool.get('mrp.production.product.line').create(cr, uid, production_scheduled_dict)
-            
+                self.pool.get('mrp.production.product.line').create(
+                    cr, uid, production_scheduled_dict)
+
             mrp_pt_planifed_dict = {
-                'product_id' : product.id,
-                'quantity' : total_weight,
-                'production_id' : new_id,
-                'product_uom' : product.uom_id.id,
+                'product_id': product.id,
+                'quantity': total_weight,
+                'production_id': new_id,
+                'product_uom': product.uom_id.id,
             }
-            self.pool.get('mrp.pt.planified').create(cr, uid, mrp_pt_planifed_dict)
+            self.pool.get('mrp.pt.planified').create(
+                cr, uid, mrp_pt_planifed_dict)
         else:
-            raise osv.except_osv(_('Error'), _("The category of the product to produce has not predefined locations "))
+            raise osv.except_osv(_('Error'), _(
+                "The category of the product to produce has not predefined locations "))
         return new_id
-    
+
 mrp_production()
--- mrp_production_wizard/wizard/special_order_wizard.py
+++ mrp_production_wizard/wizard/special_order_wizard.py
@@ -28,18 +28,19 @@ from openerp.osv import osv, fields
 import decimal_precision as dp
 from tools.translate import _
 
+
 class mrp_production_wizard(osv.osv_memory):
-    _name='mrp.production.wizard'
-    _columns={
+    _name = 'mrp.production.wizard'
+    _columns = {
         'product_id': fields.many2one('product.product', 'Product', required=True, ),
         #'product_qty': fields.float('Product Qty', required=True),
         #'product_uom': fields.many2one('product.uom', 'Product UOM', required=True),
         'wiz_data': fields.one2many('wizard.data', 'mrp_production_wiz', 'Prod lines'),
     }
-    
-    def pass_products_to_parent(self,cr,uid,ids,context={}):
+
+    def pass_products_to_parent(self, cr, uid, ids, context={}):
         if not context:
-            context={}
+            context = {}
         wizard_data_data = self.browse(cr, uid, ids, context=context)
         list_product_lines = []
         dict_line = {}
@@ -47,15 +48,17 @@ class mrp_production_wizard(osv.osv_memory):
             product = line.product_id
             for move in line.wiz_data:
                 dict_line = {
-                    'name' : move.name,
-                    'product_id' : move.product_id_consume.id,
-                    'product_qty' : move.product_qty,
-                    'product_uom' : move.product_uom.id}
+                    'name': move.name,
+                    'product_id': move.product_id_consume.id,
+                    'product_qty': move.product_qty,
+                    'product_uom': move.product_uom.id}
                 list_product_lines.append(dict_line)
-        mrp_production_id = self.pool.get('mrp.production').create_production_wizard(cr, uid, product, list_product_lines, context=context)
-        
+        mrp_production_id = self.pool.get('mrp.production').create_production_wizard(
+            cr, uid, product, list_product_lines, context=context)
+
         mod_obj = self.pool.get('ir.model.data')
-        res = mod_obj.get_object_reference(cr, uid, 'mrp', 'mrp_production_form_view')
+        res = mod_obj.get_object_reference(
+            cr, uid, 'mrp', 'mrp_production_form_view')
         res_id = res and res[1] or False,
         return {
             'name': _('Manufacturing orders'),
@@ -69,39 +72,42 @@ class mrp_production_wizard(osv.osv_memory):
             'target': 'current',
             'res_id': mrp_production_id or False,
         }
-        
-    #def onchange_product_to_produce(self, cr, uid, ids, product_id):
+
+    # def onchange_product_to_produce(self, cr, uid, ids, product_id):
     #    res = self.pool.get('wizard.data').onchange_production_wizard_product_name(cr, uid, ids, product_id)
     #    print res['value'], " = res value"
     #    return res
-    
-    
+
+
 mrp_production_wizard()
 
+
 class wizard_data(osv.osv_memory):
-    _name='wizard.data'
-    
-    _columns={
+    _name = 'wizard.data'
+
+    _columns = {
         'mrp_production_wiz': fields.many2one('mrp.production.wizard', 'Padre'),
         'name': fields.char('Name', size=64, required=True),
         'product_id_consume': fields.many2one('product.product', 'Product', required=True),
         'product_qty': fields.float('Product Qty', required=True),
         'product_uom': fields.many2one('product.uom', 'Product UOM', required=True),
     }
-    
+
     def onchange_production_wizard_product_name(self, cr, uid, ids, product_id):
         if product_id:
             new_product_id = [product_id]
             product_product_obj = self.pool.get('product.product')
-            product_product_data = product_product_obj.browse(cr, uid, new_product_id, context=None)
+            product_product_data = product_product_obj.browse(
+                cr, uid, new_product_id, context=None)
             if product_product_data:
                 for line in product_product_data:
                     val = {
-                        'name' : line.name, 
-                        'product_uom' : line.uom_id.id,
-                        'product_qty' : line.qty_available,
+                        'name': line.name,
+                        'product_uom': line.uom_id.id,
+                        'product_qty': line.qty_available,
                     }
-                    domain_uom = {'product_uom':[('category_id', '=', line.uom_id.category_id.id)]}
+                    domain_uom = {'product_uom': [(
+                        'category_id', '=', line.uom_id.category_id.id)]}
                     return {'value': val, 'domain': domain_uom}
         return {}
 
