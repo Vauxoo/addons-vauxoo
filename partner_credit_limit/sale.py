9ee966329b14cceeb146343eda384525f97ef118
Julio Serna <julio@vauxoo.com>
2013-04-18 17:04:50 -0500

    [AUTOPEP8]

--- partner_credit_limit/sale.py
+++ partner_credit_limit/sale.py
@@ -28,6 +28,7 @@ from mx import DateTime
 from tools import config
 from tools.translate import _
 
+
 class sale_order(osv.osv):
     _inherit = "sale.order"
 
@@ -36,14 +37,15 @@ class sale_order(osv.osv):
         partner = so.partner_id
 
         moveline_obj = self.pool.get('account.move.line')
-        movelines = moveline_obj.search(cr, uid, [('partner_id', '=', partner.id),('account_id.type', 'in', ['receivable', 'payable']), ('state', '<>', 'draft'), ('reconcile_id', '=', False)])
+        movelines = moveline_obj.search(cr, uid, [('partner_id', '=', partner.id), ('account_id.type', 'in', [
+                                        'receivable', 'payable']), ('state', '<>', 'draft'), ('reconcile_id', '=', False)])
         movelines = moveline_obj.browse(cr, uid, movelines)
 
         debit, credit = 0.0, 0.0
         debit_maturity, credit_maturity = 0.0, 0.0
         #~ #~
         for line in movelines:
-            if line.date_maturity < time.strftime('%Y-%m-%d') and line.date_maturity <> False:
+            if line.date_maturity < time.strftime('%Y-%m-%d') and line.date_maturity != False:
                 credit_maturity += line.debit
                 debit_maturity += line.credit
             credit += line.debit
@@ -53,26 +55,31 @@ class sale_order(osv.osv):
         saldo_maturity = credit_maturity - debit_maturity
 
         if (saldo_maturity + so.amount_total) > partner.credit_maturity_limit or (saldo + so.amount_total) > partner.credit_limit:
-            if not partner.over_credit :
+            if not partner.over_credit:
                 if (saldo + so.amount_total) > partner.credit_limit and partner.credit_limit > 0.00:
-                    msg = _('Can not validate the Sale Order because it has exceeded the credit limit \nCredit Limit: %s \nCheck the credit limits on Partner')%(partner.credit_limit)
+                    msg = _('Can not validate the Sale Order because it has exceeded the credit limit \nCredit Limit: %s \nCheck the credit limits on Partner') % (
+                        partner.credit_limit)
                     #'Can not validate Invoice because Total Invoice is greater than credit_limit: %s\nCheck Partner Accounts or Credit Limits !'%(partner.credit_limit)
                     raise osv.except_osv(_('Credit Over Limits !'), (msg))
                     return False
             else:
-                self.pool.get('res.partner').write(cr, uid, [partner.id], {'credit_limit':credit - debit + so.amount_total})
+                self.pool.get('res.partner').write(cr, uid, [partner.id], {
+                                                   'credit_limit': credit - debit + so.amount_total})
 
             if not partner.maturity_over_credit:
                 if (saldo_maturity + so.amount_total) > partner.credit_maturity_limit and partner.credit_maturity_limit > 0.00:
                     #~ msg = 'Can not validate Invoice, Total mature due Amount %s as on %s !\nCheck Partner Accounts or Credit Limits !' % (credit - debit, time.strftime('%Y-%m-%d'))
-                    msg = _('Can not validate the Sale Order because it has exceeded the credit limit up to date: %s \nMaturity Amount: %s \nMaturity Credit Limit: %s \nCheck the credit limits on Partner')%(time.strftime('%Y-%m-%d'), saldo_maturity, partner.credit_maturity_limit)
+                    msg = _('Can not validate the Sale Order because it has exceeded the credit limit up to date: %s \nMaturity Amount: %s \nMaturity Credit Limit: %s \nCheck the credit limits on Partner') % (
+                        time.strftime('%Y-%m-%d'), saldo_maturity, partner.credit_maturity_limit)
 
-                    raise osv.except_osv(_('Maturity Credit Over Limits !'), (msg))
+                    raise osv.except_osv(_(
+                        'Maturity Credit Over Limits !'), (msg))
                     return False
                 else:
                     return True
             else:
-                self.pool.get('res.partner').write(cr, uid, [partner.id], {'credit_maturity_limit':credit_maturity - debit_maturity + so.amount_total})
+                self.pool.get('res.partner').write(cr, uid, [partner.id], {
+                                                   'credit_maturity_limit': credit_maturity - debit_maturity + so.amount_total})
                 return True
         else:
 
