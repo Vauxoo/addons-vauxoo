6b776d98e5275de0367634b909a80e98c4011784
Julio Serna <julio@vauxoo.com>
2013-04-18 14:58:42 -0500

    [AUTOPEP8]

--- base_user_signature_logo/res_users.py
+++ base_user_signature_logo/res_users.py
@@ -1,10 +1,11 @@
 from osv import fields, osv
 
+
 class res_users(osv.osv):
     _inherit = 'res.users'
     _columns = {
         'signature_logo': fields.binary('Signature Logo')
-     }
+    }
 res_users()
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
