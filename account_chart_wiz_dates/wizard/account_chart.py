074f0a7563294106e781ce152dabe32e3934551b
Julio Serna <julio@vauxoo.com>
2013-04-18 14:16:18 -0500

    [AUTOPEP8]

--- account_chart_wiz_dates/wizard/account_chart.py
+++ account_chart_wiz_dates/wizard/account_chart.py
@@ -26,44 +26,50 @@
 from osv import fields, osv
 import time
 
+
 class account_chart(osv.osv_memory):
-    _inherit= "account.chart"
-    
-    _columns={
-        'filter':fields.selection([ ('filter_no','Unfiltered'), ('periods','Periods'), ('dates','Dates')],'Filter',),
+    _inherit = "account.chart"
+
+    _columns = {
+        'filter': fields.selection([('filter_no', 'Unfiltered'), ('periods', 'Periods'), ('dates', 'Dates')], 'Filter',),
         'initial_date': fields.date('Initial date',),
         'end_date': fields.date('End date',),
-        }
-    
+    }
+
     _defaults = {
         'filter': 'filter_no',
-        }
-        
+    }
+
     def onchange_fiscalyear(self, cr, uid, ids, fiscalyear_id=False, val_filter=False, context=None):
-        res={}
-        if val_filter=='periods':
-            res=super(account_chart, self).onchange_fiscalyear(cr, uid, ids, fiscalyear_id=fiscalyear_id, context=context)
+        res = {}
+        if val_filter == 'periods':
+            res = super(account_chart, self).onchange_fiscalyear(
+                cr, uid, ids, fiscalyear_id=fiscalyear_id, context=context)
         return res
-        
+
     def onchange_val_filter(self, cr, uid, ids, val_filter=False, fiscalyear_id=False, context=None):
         res = {}
-        if val_filter=='dates':
-            res['value'] = {'period_from': False, 'period_to': False, 'initial_date': time.strftime('%Y-01-01'), 'end_date': time.strftime('%Y-%m-%d')}
-        elif val_filter=='periods':
-            res=super(account_chart, self).onchange_fiscalyear(cr, uid, ids, fiscalyear_id=fiscalyear_id, context=context)
-            res['value']['initial_date']= False
-            res['value']['end_date']= False
-        elif val_filter=='filter_no':
-            res['value'] = {'period_from': False, 'period_to': False, 'initial_date': False, 'end_date': False}
+        if val_filter == 'dates':
+            res['value'] = {'period_from': False, 'period_to': False, 'initial_date': time.strftime(
+                '%Y-01-01'), 'end_date': time.strftime('%Y-%m-%d')}
+        elif val_filter == 'periods':
+            res = super(account_chart, self).onchange_fiscalyear(
+                cr, uid, ids, fiscalyear_id=fiscalyear_id, context=context)
+            res['value']['initial_date'] = False
+            res['value']['end_date'] = False
+        elif val_filter == 'filter_no':
+            res['value'] = {'period_from': False, 'period_to':
+                            False, 'initial_date': False, 'end_date': False}
         return res
-    
+
     def account_chart_open_window(self, cr, uid, ids, context=None):
         data = self.read(cr, uid, ids, [], context=context)[0]
-        filter_val= data.get('filter', False) and data['filter'] or False
-        result=super(account_chart, self).account_chart_open_window(cr, uid, ids, context=context)
+        filter_val = data.get('filter', False) and data['filter'] or False
+        result = super(account_chart, self).account_chart_open_window(
+            cr, uid, ids, context=context)
         if filter_val == 'dates':
-            #date_from=str({'date_from':data.get('initial_date', False)})
-            #date_to=str({'date_to':data.get('end_date', False)})
+            # date_from=str({'date_from':data.get('initial_date', False)})
+            # date_to=str({'date_to':data.get('end_date', False)})
             #~ result_get=result.get('context')
             context_str = result['context']
             context_dict = eval(result['context'])
@@ -71,6 +77,7 @@ class account_chart(osv.osv_memory):
                 'date_from': data.get('initial_date', False),
                 'date_to': data.get('end_date', False),
             })
-            result['context']=str(context_dict)
-            #result['context']='{'+result_get[1:-1]+', '+date_from[1:-1]+', '+date_to[1:-1]+'}'
+            result['context'] = str(context_dict)
+            # result['context']='{'+result_get[1:-1]+', '+date_from[1:-1]+',
+            # '+date_to[1:-1]+'}'
         return result
