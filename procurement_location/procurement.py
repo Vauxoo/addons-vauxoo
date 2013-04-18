80434e83d5befe677b34042558380cb498db247d
Julio Serna <julio@vauxoo.com>
2013-04-18 17:13:15 -0500

    [AUTOPEP8]

--- procurement_location/procurement.py
+++ procurement_location/procurement.py
@@ -25,17 +25,22 @@
 ##############################################################################
 from osv import osv, fields
 
+
 class procurement_order(osv.osv):
     _inherit = 'procurement.order'
-    
+
     def make_mo(self, cr, uid, ids, context=None):
-        res = super(procurement_order,self).make_mo(cr, uid, ids, context=context)
-        mrp_prod_obj=self.pool.get('mrp.production')
-        cat_prod=self.browse(cr,uid,ids,context=context)[0].product_id.categ_id
-        loc_src=cat_prod.location_src_id and cat_prod.location_src_id.id or False
-        loc_dest=cat_prod.location_dest_id and cat_prod.location_dest_id.id or False
+        res = super(procurement_order, self).make_mo(
+            cr, uid, ids, context=context)
+        mrp_prod_obj = self.pool.get('mrp.production')
+        cat_prod = self.browse(
+            cr, uid, ids, context=context)[0].product_id.categ_id
+        loc_src = cat_prod.location_src_id and cat_prod.location_src_id.id or False
+        loc_dest = cat_prod.location_dest_id and cat_prod.location_dest_id.id or False
         if loc_src:
-            mrp_prod_obj.write(cr, uid, res.values()[0], {'location_src_id':loc_src})
+            mrp_prod_obj.write(cr, uid, res.values()[
+                               0], {'location_src_id': loc_src})
         if loc_dest:
-            mrp_prod_obj.write(cr, uid, res.values()[0], {'location_dest_id':loc_dest})
+            mrp_prod_obj.write(cr, uid, res.values()[
+                               0], {'location_dest_id': loc_dest})
         return res
