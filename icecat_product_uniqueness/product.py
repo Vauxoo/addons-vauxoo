f261ef1962f0bae1b83425e9fa3c15e59f121b49
Julio Serna <julio@vauxoo.com>
2013-04-18 15:28:32 -0500

    [V7-MIGRATION]

--- icecat_product_uniqueness/product.py
+++ icecat_product_uniqueness/product.py
@@ -25,12 +25,12 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 ###############################################################################
-from osv import osv
-from osv import fields
-from tools.translate import _
+from openerp.osv import fields, osv
+from openerp.tools.translate import _
 
 
-class product_inherited(osv.osv):
+
+class product_inherited(osv.Model):
     """
     Checks if a loaded product already exists on the database
     """
@@ -60,4 +60,3 @@ class product_inherited(osv.osv):
     _constraints = [(
         _check_uniqueness, 'ERROR, product already exists for this company', ['ean13'])]
 
-product_inherited()
