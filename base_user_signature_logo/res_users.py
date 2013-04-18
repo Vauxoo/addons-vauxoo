d808918c36176acdec46b87e0a93e09b2812be3e
Julio Serna <julio@vauxoo.com>
2013-04-18 14:59:05 -0500

    [V7-MIGRATION]

--- base_user_signature_logo/res_users.py
+++ base_user_signature_logo/res_users.py
@@ -1,11 +1,10 @@
-from osv import fields, osv
+from openerp.osv import osv, fields
 
 
-class res_users(osv.osv):
+class res_users(osv.Model):
     _inherit = 'res.users'
     _columns = {
         'signature_logo': fields.binary('Signature Logo')
     }
-res_users()
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
