3ebe2a81352988dd963499c46064ed8691db317c
Julio Serna <julio@vauxoo.com>
2013-04-18 12:43:26 -0500

    [ADD] added new module res_partner_btree to create parent_left and parent_right to partner

---
+++ res_partner_btree/__openerp__.py
@@ -0,0 +1,35 @@
+# -*- encoding: utf-8 -*-
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#
+#    Copyright (c) 2013 Vauxoo - http://www.vauxoo.com/
+#    All Rights Reserved.
+#    info Vauxoo (info@vauxoo.com)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name" : "Creacion de parent_left y parent_right en partner",
+    "version" : "1.0",
+    "author" : "Vauxoo",
+    "description":"""This module creates parent_left y paret_right en partners""",
+    "website" : "http://www.vauxoo.com/",
+    "depends" : ["base"],
+    "init_xml" : [],
+    "demo_xml" : [],
+    "update_xml" : [],
+    "installable" : True,
+    "active" : False,
+}
