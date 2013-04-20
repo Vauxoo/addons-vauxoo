3fd109a5e0a84569216b135104d7a5bfcf592b4e
Julio Serna <julio@vauxoo.com>
2013-04-20 15:44:55 -0500

    [AUTOPEP8]

--- account_periods_initial/account.py
+++ account_periods_initial/account.py
@@ -25,7 +25,6 @@ from openerp.osv import osv, fields, orm
 from openerp.tools.translate import _
 
 
-
 class account_period(osv.Model):
     _inherit = "account.period"
 
@@ -34,7 +33,9 @@ class account_period(osv.Model):
         period_date_start = period_to.date_start
         company_id = period_to.company_id.id
         fiscalyear_id = period_to.fiscalyear_id.id
-        return self.search(cr, uid, [('date_stop', '<=', period_date_start), ('company_id', '=', company_id), ('id', '<>', period_to_id), ('fiscalyear_id', '=', fiscalyear_id)])
+        return self.search(cr, uid, [('date_stop', '<=', period_date_start),
+                          ('company_id', '=', company_id), (
+                              'id', '<>', period_to_id),
+            ('fiscalyear_id', '=', fiscalyear_id)])
         # Falta validar cuando el period_to_id es special, ya que puede tomar
         # enero cuando no es necesario.
-
