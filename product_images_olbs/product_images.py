48595e340578e6930f7791d115118088dc9b1d20
Julio Serna <julio@vauxoo.com>
2013-04-24 14:56:57 -0500

    [AUTOPEP8]

--- product_images_olbs/product_images.py
+++ product_images_olbs/product_images.py
@@ -47,7 +47,9 @@ class product_images(osv.Model):
 
     _columns = {
         'name': fields.char('Image Title', size=100, required=True),
-        'link': fields.boolean('Link?', help="Images can be linked from files on your file system or remote (Preferred)"),
+        'link': fields.boolean('Link?',
+            help="Images can be linked from files on your file system or\
+                remote (Preferred)"),
         'image': fields.binary('Image', filters='*.png,*.jpg,*.gif'),
         'filename': fields.char('File Location', size=250),
         'preview': fields.function(_get_image, type="binary", method=True),
