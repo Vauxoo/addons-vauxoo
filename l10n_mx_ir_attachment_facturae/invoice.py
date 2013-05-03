9d70bd7b3f165a38e6e46f5c23e583abe4265185
Julio Serna <julio@vauxoo.com>
2013-05-03 11:46:04 -0500

    [IMP] added button create attachment to facturae

--- l10n_mx_ir_attachment_facturae/invoice.py
+++ l10n_mx_ir_attachment_facturae/invoice.py
@@ -42,7 +42,34 @@ class account_invoice(osv.osv):
                         wf_service.trg_validate(uid, 'ir.attachment.facturae.mx', attachment.id, 'action_cancel', cr)
         self.write(cr, uid, ids, {'date_invoice_cancel': time.strftime('%Y-%m-%d %H:%M:%S')})
         return super(account_invoice, self).action_cancel(cr, uid, ids, context)
-
+    
+    def create_ir_attachment_facturae(self, cr, uid, ids, context=None):
+        invoice = self.browse(cr, uid, ids, context=context)[0]
+        if invoice.invoice_sequence_id.approval_id:
+            if invoice.invoice_sequence_id.approval_id.type=='cfdi32':
+                pac=self.pool.get('params.pac').search(cr, uid, [('active','=', True)], context)
+                #if not pac:
+                    #raise osv.except_osv(_('Warning !'),_('Not Params PAC.'))
+            attach=self.pool.get('ir.attachment.facturae.mx').create(cr, uid, {
+            'name': invoice.fname_invoice,
+            'invoice_id': ids[0],
+            'type': invoice.invoice_sequence_id.approval_id.type }, context=context)
+            ir_model_data = self.pool.get('ir.model.data')
+            form_res = ir_model_data.get_object_reference(cr, uid, 'l10n_mx_ir_attachment_facturae', 'view_ir_attachment_facturae_mx_form')
+            form_id = form_res and form_res[1] or False
+            tree_res = ir_model_data.get_object_reference(cr, uid, 'l10n_mx_ir_attachment_facturae', 'view_ir_attachment_facturae_mx_tree')
+            tree_id = tree_res and tree_res[1] or False
+            return {
+                'name': _('Attachment Factura E MX'),
+                'view_type': 'form',
+                'view_mode': 'form,tree',
+                'res_model': 'ir.attachment.facturae.mx',
+                'res_id': attach,
+                'view_id': False,
+                'views': [(form_id, 'form'), (tree_id, 'tree')],
+                'type': 'ir.actions.act_window',
+            }
+            
     def action_create_ir_attachment_facturae(self, cr, uid, ids, context=None):
         invoice = self.browse(cr, uid, ids, context=context)[0]
         if invoice.invoice_sequence_id.approval_id:
