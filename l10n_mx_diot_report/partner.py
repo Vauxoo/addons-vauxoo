16bb25d52bbf7e2ccee0fb2f5bf0939d026ba32d
Federico Manuel Echeverri Choux <echeverrifm@gmail.com>
2013-05-09 11:09:51 -0500

    [ADD] wizard files

--- l10n_mx_diot_report/partner.py
+++ l10n_mx_diot_report/partner.py
@@ -24,7 +24,7 @@ from operator import itemgetter
 from osv import fields, osv
 
 class res_partner(osv.osv):
-    _name = 'res.partner'
+
     _inherit = 'res.partner'
     _description = 'Partner'
 
