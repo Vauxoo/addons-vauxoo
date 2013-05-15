020c98b696c163d9a54799de1ca741e947bdb6b6
Luis Tores <luis_t@vauxoo.com>
2013-05-15 11:56:26 -0500

    [AUTOPEP8]

--- l10n_mx_import_info/report/label_report.py
+++ l10n_mx_import_info/report/label_report.py
@@ -4,6 +4,7 @@ from openerp.report import report_sxw
 from openerp.tools.translate import _
 from openerp import pooler, tools
 
+
 class label_report(report_sxw.rml_parse):
     def __init__(self, cr, user, name, context):
         super(label_report, self).__init__(cr, user, name, context=context)
@@ -11,7 +12,7 @@ class label_report(report_sxw.rml_parse):
             'time': time,
             'getAdd': self._getAdd,
         })
-    
+
     def _getAdd(self, obj):
         """
     Custom method that process obj and return required data to report
@@ -19,13 +20,11 @@ class label_report(report_sxw.rml_parse):
         """
         print 'DEDEDEED'
         return 'ESTA ES LA DIRECCION Y EL TELEELFONO (52)222333444'
-    
-        
-        
+
+
 report_sxw.report_sxw(
     'report.label.report.import.info',
     'import.info',
     'addons/l10n_mx_import_info/report/label_report.rml',
     parser=label_report
 )
-
