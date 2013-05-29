45ba94a8ed7caf225b508ac40bfec40f2a599cc6
Jose Morales <jose@vauxoo.com>
2013-05-29 14:08:50 -0430

    [IMP] Added description in openerp file

--- document_page_comments/__openerp__.py
+++ document_page_comments/__openerp__.py
@@ -33,6 +33,14 @@
                  ],
     "author" : "Vauxoo",
     "description" : """
+
+Documents Page Comments
+=======================
+
+This module add messeage history and followers in document page model 
+to we can follow each document and receive a mail when those are modified if 
+you are follower
+
                     """,
     "website" : "http://vauxoo.com",
     "category" : "Generic Modules",
