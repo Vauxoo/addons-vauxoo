4a80de536ea509632785461c6ab4520a04e76078
Juan Carlos Funes <openerp1@hotmail.com>
2013-06-03 19:26:18 -0500

    [IMP][l10n_mx_payment_method]change inherit view to base.view_partner_form and delete dependence base_vat

--- l10n_mx_payment_method/__openerp__.py
+++ l10n_mx_payment_method/__openerp__.py
@@ -34,7 +34,7 @@
     """,
     "website" : "www.vauxoo.com",
     "license" : "AGPL-3",
-    "depends" : ["account", "base_vat",
+    "depends" : ["account"
         ],
     "init_xml" : [],
     "demo_xml" : [],
