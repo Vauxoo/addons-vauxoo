98545a2a452b38f0dd86835c163466b8f56ed376
Luis Escobar <vxkikevx@gmail.com>
2013-06-13 17:07:53 -0430

    [FIX] Added Recaptcha AJAX function to start section. [FIX] Added render_value section. [FIX] Definition element script of recaptcha to begin of file.

--- web_captcha/static/src/js/resource.js
+++ web_captcha/static/src/js/resource.js
@@ -1,18 +1,39 @@
+var script = document.createElement("script");
+console.log();
+script.type = "text/javascript";
+script.src = "http://www.google.com/recaptcha/api/js/recaptcha_ajax.js";
+$("head").append(script);
 openerp.web_captcha = function (openerp)
 {
     openerp.web.form.widgets.add('captcha', 'openerp.web.form.CaptchaWidget');
     openerp.web.form.CaptchaWidget = openerp.web.form.FieldChar.extend(
         {
-        template : "captcha",
+        template : "orp_captcha",
         init: function (view, code) {
             this._super(view, code);
             console.log(view); 
             console.log(code); 
             console.log('loading...');
         },
+        initialize_content: function() {
+            this._super();
+        },
         start: function () {
-            this.$el.text("Hello, world!");
-            console.log("loading dd2"); 
+               console.log("Hola");
+               console.log(this.$el);
+               Recaptcha.create("6Ldf-uESAAAAAMPNMWa8dD6tFZey9w_1FrcpOmIe", "or_recaptcha", {
+               theme: "red",
+               callback: Recaptcha.focus_response_field});
+               return this._super();
+          },
+        render_value: function() {
+                 console.log("tttt");
+                 this._super();
+                 var val = this.get('value').split(",");
+                 var torender = this.$el.find('div.oe_recaptcha');
+                 //torender.text(val[0]);
+                 $(val[0]).appendTo(torender);
+                 console.log(torender);
         }
     });
 
