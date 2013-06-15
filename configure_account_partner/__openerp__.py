cf827a8e1939838d7c042ba3832ed2bfb5915d8c
Nhomar Hernandez <nhomar@gmail.com>
2013-06-15 19:03:05 -0430

    [FIX] Documentation better done

--- configure_account_partner/__openerp__.py
+++ configure_account_partner/__openerp__.py
@@ -30,11 +30,25 @@
     "depends" : ["account"],
     "author" : "Vauxoo",
     "description" : """
-Configure Accounts:
-=========================================================
-    creates a wizard to choose a partner account and write
+Configure Accounts On imported Partners:
+========================================
 
-  
+Background:
+-----------
+
+When you import a lot of partners from several sources, It is so common that
+all of them are setted with a payable and receiveble account by default, with
+this wizard you can fix this quickly one time you upload all partners.
+
+1.- Creates a wizard where you choose an account and write all partners
+that are customers and selected on the wizard with this selected account.
+
+2.- Clean the company_id on the property to be able to use the same <head></head>
+on all reports.
+
+**TODO:** This feature is so wired, when we fix the correct behaviour this feature
+must be removed.
+
                     """,
     "website" : "http://vauxoo.com",
     "category" : "Customization",
