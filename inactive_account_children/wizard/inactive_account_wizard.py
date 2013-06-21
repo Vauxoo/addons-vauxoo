7a262ac2556d51d008399003eac42527bc5455e8
Luis Tores <luis_t@vauxoo.com>
2013-06-21 15:59:50 -0700

    [IMP][inactive_account_children]Added function for show that account can't delete and the propertys that not allow inactive account

--- inactive_account_children/wizard/inactive_account_wizard.py
+++ inactive_account_children/wizard/inactive_account_wizard.py
@@ -26,29 +26,60 @@
 
 from openerp.osv import fields, osv
 from openerp.tools.translate import _
+from openerp import SUPERUSER_ID
 
 
 class inactive_account_wizard(osv.osv_memory):
-	_name = 'inactive.account.wizard'
-	
-	def get_accounts(self, cr, uid, ids, context=None):
-		data = self.read(cr, uid, ids)[0]
-		account_ids = data['account_ids']
-		for account in account_ids:
-			cr.execute("""SELECT node.company_id,node.type,node.id as id,node.name
-				FROM account_account AS node,account_account AS parent
-				WHERE node.parent_left BETWEEN parent.parent_left
-				AND parent.parent_right
-				AND parent.id = %s
-				ORDER BY parent.parent_left""", (account,))
-			res = cr.dictfetchall()
-			for account_children in res:
-				self.pool.get('account.account').write(cr, uid,
-					account_children['id'], {'active' : False})
-			
-	_columns = {
-		'account_ids' : fields.many2many('account.account',
+    _name = 'inactive.account.wizard'
+    
+    def get_accounts(self, cr, uid, ids, context=None):
+        data = self.read(cr, uid, ids)[0]
+        account_ids = data['account_ids']
+        for account in account_ids:
+            cr.execute("""SELECT node.company_id,node.type,node.id as id,node.name
+                FROM account_account AS node,account_account AS parent
+                WHERE node.parent_left BETWEEN parent.parent_left
+                AND parent.parent_right
+                AND parent.id = %s
+                ORDER BY parent.parent_left""", (account,))
+            res = cr.dictfetchall()
+            for account_children in res:
+                self._check_moves(cr, uid, account_children.get('id'),\
+                    'write', account_children.get('name'), context=context)
+                self.pool.get('account.account').write(cr, uid,
+                    account_children['id'], {'active' : False})
+        return True
+                    
+    def _check_moves(self, cr, uid, ids, method, account=None, context=None):
+        account_obj = self.pool.get('account.account')
+        line_obj = self.pool.get('account.move.line')
+        account_ids = account_obj.search(cr, SUPERUSER_ID, [('id', 'child_of', ids)])
+        if line_obj.search(cr, uid, [('account_id', 'in', account_ids)]):
+            if method == 'write':
+                raise osv.except_osv(_('Error!'), _(
+                    'You cannot deactivate an account that contains journal items.'))
+            elif method == 'unlink':
+                raise osv.except_osv(_('Error!'), _(
+                    'You cannot remove an account that contains journal items.'))
+        #Checking whether the account is set as a property to any Partner or not
+        value = 'account.account,' + str(ids)
+        partner_prop_acc = self.pool.get('ir.property').search(cr, uid, [(
+            'value_reference','=',value)], context=context)
+        conc_acc = ''
+        if account:
+            conc_acc = '\nAccount: ' + account
+        if partner_prop_acc:
+            conc_acc = conc_acc + '\nIds Property:'
+            for proper in partner_prop_acc:
+                conc_acc = conc_acc + '\n- ' + str(proper)
+            raise osv.except_osv(_('Warning!'), _('You cannot \
+            remove/deactivate an account which is set on a customer \
+            or supplier.') + conc_acc)
+        return True
+            
+    _columns = {
+        'account_ids' : fields.many2many('account.account',
                                          'account_account_rel',
                                          'account_wizard_id',
                                          'account_id', 'accounts'),
-	}
+    }
