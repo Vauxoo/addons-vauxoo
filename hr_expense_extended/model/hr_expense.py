1c639c70767436ff8232dfaedadd0d8a2c2e0b4c
Katherine Zaoral (Vauxoo) <katherine.zaoral@vauxoo.com>
2013-07-01 18:01:07 -0430

    [ADD] skeleton for new module.

---
+++ hr_expense_extended/model/hr_expense.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+###############################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#    Copyright (C) OpenERP Venezuela (<http://openerp.com.ve>).
+#    All Rights Reserved
+############# Credits #########################################################
+#    Coded by: Katherine Zaoral          <kathy@vauxoo.com>
+#    Planified by: Humberto Arocha       <hbto@vauxoo.com>
+#    Audited by: Humberto Arocha         <hbto@vauxoo.com>
+###############################################################################
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+###############################################################################
+from openerp.osv import fields, osv
+
+class hr_expense_line(osv.osv):
+    _inherit = "hr.expense.line"
+
+    _columns = {
+        'invoice_id': fields.many2one('account.invoice', 'Invoice',
+                                      ondelete='cascade'),
+        #~ TODO: its really a one2one, evaluated another options
+    }
