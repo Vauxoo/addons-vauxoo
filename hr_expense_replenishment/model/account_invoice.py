570dbe2ec487d7c65cfcfc5b948668fdab4cd58c
Humberto Arocha <humbertoarocha@gmail.com>
2013-07-28 04:38:11 +0000

    [FIX] [hr_expense_replenishment] capturing missing key at default

--- hr_expense_replenishment/model/account_invoice.py
+++ hr_expense_replenishment/model/account_invoice.py
@@ -73,9 +73,9 @@ class account_invoice_line(osv.Model):
     def _get_analytic_exp(self, cr, uid, context=None):
         hr_expense_obj = self.pool.get('hr.expense.expense')
         context = context or {}
-        analytic_id = hr_expense_obj.browse(cr, uid,
-                                        context.get('analytic_exp'),
-                                        context=context).account_analytic_id.id
+        analytic_id = context.get('analytic_exp') and hr_expense_obj.browse(cr, uid,
+                            context.get('analytic_exp'),
+                            context=context).account_analytic_id.id or False
         return analytic_id
     
     _defaults = {
