050ec454d0ff2be3c9cd849fa86ef238274ff3a9
Fernando.rangel <fernando.rangel@vauxoo.com>
2013-07-30 11:23:45 -0700

    [IMP][partner_search_by_vat] improve file res_partner.py

--- partner_search_by_vat/model/res_partner.py
+++ partner_search_by_vat/model/res_partner.py
@@ -1,13 +1,13 @@
 # -*- encoding: utf-8 -*-
-###########################################################################
+#
 #    Module Writen to OpenERP, Open Source Management Solution
 #
 #    Copyright (c) 2013 Vauxoo - http://www.vauxoo.com/
 #    All Rights Reserved.
 #    info Vauxoo (info@vauxoo.com)
-############################################################################
+#
 #    Coded by: Fernando Rangel (fernando.rangel@vauxoo.com)
-############################################################################
+#
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -22,27 +22,25 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-##############################################################################
+#
 
 from openerp.osv import fields, osv
 
 
 class res_partner(osv.osv):
-    _name = 'res.partner'
+
     _inherit = 'res.partner'
 
-
-  
-    def name_search(self, cr, user, name, args=None, operator='ilike', context=None, limit=100):        
+    def name_search(self, cr, user, name, args=None, operator='ilike', context=None, limit=100):
         if not args:
             args = []
         if context is None:
             context = {}
         ids = []
         if name:
-            ids = self.search(cr, user, [('vat', 'ilike', name)]+ args, limit=limit, context=context)
+            ids = self.search(
+                cr, user, [('vat', operator, name)] + args, limit=limit, context=context)
         if not ids:
-            ids = self.search(cr, user, [('vat', 'ilike', name)]+ args, limit=limit, context=context)#fix it ilike should be replace with operator
-        return self.name_get(cr, user, ids, context=context)
-        
-        
+            ids = self.search(
+                cr, user, [('name', operator, name)] + args, limit=limit, context=context)
+        return super(res_partner, self).name_get(cr, user, ids, context=context)
