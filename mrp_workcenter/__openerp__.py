e67033311caf3589d05b3f1431c0703cbc5c5379
Yanina Aular <yanina.aular@vauxoo.com>
2013-08-06 16:38:10 -0430

    [ADD] module description

--- mrp_workcenter/__openerp__.py
+++ mrp_workcenter/__openerp__.py
@@ -33,8 +33,12 @@
     "description": """                                                          
                                                                                 
 MRP Management Workcenter.                                                   
-==========================                                                   
-                                                                                
+==========================
+
+This module adds a better management of Work Centers on Manufacturing module.
+List of new functionalities:
+
+- adds a new menuitem at **Manufacturing** > **Manufacturing** > **Work Centers**  that shows the work centers registered.
                                                                                  
  """,                                                                            
     "depends": ["mrp",],                                   
