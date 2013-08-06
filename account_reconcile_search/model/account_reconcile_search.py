2302b92b04d3b778480a4f9a28d8716415c8f2a0
Sabrina Romero <sabrina@vauxoo.com>
2013-08-06 16:10:11 -0700

    [ADD][account_reconcile_search] New module added to group by reconcile reference the journal items

---
+++ account_reconcile_search/model/account_reconcile_search.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import time
+from openerp.osv import osv, fields
+from openerp.tools.translate import _
+
+class account_move_line(osv.Model):
+    _description = 'Account Move Line Search'
+    _inherit = "account.move.line"
+
+    def _get_reconcile(self, cr, uid, ids,name, unknow_none, context=None):
+        res = super(account_move_line, self)._get_reconcile(cr, uid, ids, name, unknow_none, context)
+        print res
+        return res
+    
+    def fc(s, c, u, ids, cx):
+        print "****",ids
+        return ids
+    
+    _columns = {
+        'reconcile': fields.function(
+            _get_reconcile,
+            type ='char',
+            string ='Reconcile Ref',
+            store = {
+                'account.move.line':(fc, ['reconcile_id','partial_reconcile_id'],30),
+                },
+            ),
+    }
