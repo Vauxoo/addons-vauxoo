9f9ab7b62633b46b3c8802746cbaf853991eb936
Yanina Aular <yanina.aular@vauxoo.com>
2013-08-11 15:28:24 -0430

    [IMP] PEP8

--- mrp_workcenter/__openerp__.py
+++ mrp_workcenter/__openerp__.py
@@ -1,50 +1,50 @@
-#!/usr/bin/python                                                                                                                                  
-# -*- encoding: utf-8 -*-                                                       
-############################################################################### 
-#    Module Writen to OpenERP, Open Source Management Solution                  
-#    Copyright (C) OpenERP Venezuela (<http://openerp.com.ve>).                 
-#    All Rights Reserved                                                        
-# Credits######################################################                 
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+###############################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#    Copyright (C) OpenERP Venezuela (<http://openerp.com.ve>).
+#    All Rights Reserved
+# Credits######################################################
 #    Coded by: Yanina Aular <yanina.aular@vauxoo.com>
-#              Katherine Zaoral <katherine.zaoral@vauxoo.com>            
+#              Katherine Zaoral <katherine.zaoral@vauxoo.com>
 #    Planified by: Yanina Aular <yanina.aular@vauxoo.com>
 #    Audited by: Humberto Arocha <humbertoarocha@gmail.com>
-############################################################################### 
-#    This program is free software: you can redistribute it and/or modify       
-#    it under the terms of the GNU Affero General Public License as published   
-#    by the Free Software Foundation, either version 3 of the License, or       
-#    (at your option) any later version.                                        
-#                                                                               
-#    This program is distributed in the hope that it will be useful,            
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of             
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              
-#    GNU Affero General Public License for more details.                        
-#                                                                               
-#    You should have received a copy of the GNU Affero General Public License   
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.      
-############################################################################### 
-                                                                                
-{                                                                               
-    "name": "MRP Management Workcenter",                                     
-    "version": "1.0",                                                           
-    "author": "Vauxoo C.A",                                                     
-    "website": "http://vauxoo.com",                                             
-    "category": "MRP",                                                   
-    "description": """                                                          
-                                                                                
-MRP Management Workcenter.                                                   
+###############################################################################
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+###############################################################################
+
+{
+    "name": "MRP Management Workcenter",
+    "version": "1.0",
+    "author": "Vauxoo C.A",
+    "website": "http://vauxoo.com",
+    "category": "MRP",
+    "description": """
+
+MRP Management Workcenter.
 ==========================
 
 This module adds a better management of Work Centers on Manufacturing module.
 List of new functionalities:
 
 - adds a new menuitem at **Manufacturing** > **Manufacturing** > **Work Centers**  that shows the work centers registered.
-                                                                                 
+
  """,
-    "depends": ["base","mrp", "mrp_operations"],
+    "depends": ["base", "mrp", "mrp_operations"],
     "data": [
         "view/mrp_workcenter_view.xml",
-        ],                                                                      
+    ],
     "js": [],
     "qweb": [],
     "css": [],
@@ -53,4 +53,3 @@ List of new functionalities:
     "installable": True,
     "active": False,
 }
-                                                               
