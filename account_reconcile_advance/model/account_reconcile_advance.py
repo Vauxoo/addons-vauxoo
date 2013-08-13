a073092e143b3c3b7a58582ffaee186daf3ddc2b
Humberto Arocha <humbertoarocha@gmail.com>
2013-08-13 00:34:45 -0700

    [ADD][account_reconcile_advance] New functional field 'advance' for account.voucher was added

--- account_reconcile_advance/model/account_reconcile_advance.py
+++ account_reconcile_advance/model/account_reconcile_advance.py
@@ -25,3 +25,45 @@ class account_reconcile_advance(osv.Model):
     _defaults = {
         'state':'draft'        
     }
+
+class account_voucher(osv.Model):
+    _inherit = 'account.voucher'
+
+    def _get_advance(self, cr, uid, ids, name, args, context=None):
+        '''
+        Check if there is at least one payable or receivable line not
+        reconciled at all, if so it will be regarded as an advance
+        '''
+        context = context or {}
+        res = {}.fromkeys(ids,False)
+        for av_brw in self.browse(cr, uid, ids, context=context):
+            if av_brw.state != 'posted': continue
+            i = [l.reconcile_id and True or l.reconcile_partial_id and True or False 
+                    for l in av_brw.move_ids 
+                        if l.account_id.type in ('receivable','payable')]
+            res[av_brw.id] = not all(i) if i else False
+        return res
+
+    def _get_voucher_advance(self, cr, uid, ids, context=None):
+        context = context or {}
+        res = set()
+        aml_obj = self.pool.get('account.move.line')
+        for l in aml_obj.browse(cr, uid, ids, context=context):
+            if not l.move_id: continue
+            res.add(l.move_id.id)
+        res = list(res)
+
+        if not res: return []
+
+        av_obj = self.pool.get('account.voucher')
+        return av_obj.search(cr, uid, [('move_id','in',res)],context=context)
+
+    _columns = {
+        'advance':fields.function(_get_advance, method=True, string='Is an Advance?',
+        store={
+            'account.voucher':(lambda s,c,u,ids,cx:ids,['move_ids','move_id','state'],15),
+            'account.move.line':(_get_voucher_advance,['reconcile_id','reconcile_partial_id'],30)
+            },
+        help='If the payable or receivable are not fully reconcile then it is advance', 
+        type='boolean')
+    }
