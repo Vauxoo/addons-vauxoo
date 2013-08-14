700a8708d2ab269c09d45c954232a6dda3ce5845
Katherine Zaoral (Vauxoo) <katherine.zaoral@vauxoo.com>
2013-08-14 16:47:07 -0430

    [INIT] new module to take in count the work center capacity for generating the work orders.

---
+++ mrp_byworkcenter_capacity/__openerp__.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+###############################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#    Copyright (C) OpenERP Venezuela (<http://openerp.com.ve>).
+#    All Rights Reserved
+############# Credits #########################################################
+#    Coded by: Katherine Zaoral          <kathy@vauxoo.com>
+#    Planified by: Katherine Zaoral      <kathy@vauxoo.com>
+#    Audited by: Humberto Arocha         <hbto@vauxoo.com>
+###############################################################################
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+###############################################################################
+
+{
+    "name": "MRP Work Center Capacity",
+    "version": "1.0",
+    "author": "Vauxoo C.A.",
+    "website": "http://www.openerp.com.ve",
+    "category": "MRP",
+    "description": """
+MRP Work Center Capacity
+========================
+
+This module add the feuture of take in count the workcenters minimum and
+maximum when automatic generating the work orders for a production order.
+Instead of generate one work order by every activity loaded in the production
+order routing (the basic process) it create so many work orders needed to the
+production capacity in every work center.
+ 
+For example. If I want to produce 100 pounds of meat but my related work center
+process only 20 pounds at time, this feature will create 5 work orders each to
+process 20 pounds of the production order 100 pounds.
+
+This is helpfull because in real life does not happend that all the process
+of a big capacity production order is process at once.
+
+""",
+    "depends": ["mrp"],
+    "data": [
+    ],
+    "demo": [],
+    "test": [],
+    "active": False,
+    "installable": True,
+}
