30fdfeb3b12550eb46249085eb8ab1c6b927d8ca
Luis Escobar <vxkikevx@gmail.com>
2013-08-20 16:08:54 -0430

    [PEP8]

--- contract_enterprise_openerp/model/account_analytic_analysis.py
+++ contract_enterprise_openerp/model/account_analytic_analysis.py
@@ -18,14 +18,19 @@ class warranty_oerp(osv.Model):
         obj_warranty = self.browse(cr, uid, ids[0], context=context) 
         if obj_warranty.enddate < obj_warranty.startdate:                        
             return False                                                        
-        pids = self.search(cr, uid, [('enddate','>=',obj_warranty.startdate),('startdate','<=',obj_warranty.enddate),('id','<>',obj_warranty.id)])
+        pids = self.search(cr, uid,
+                [('enddate','>=',obj_warranty.startdate),
+                    ('startdate','<=',obj_warranty.enddate),
+                    ('id','<>',obj_warranty.id)])
         if len(pids) > 0:
             return False
         return True
 
     def _check_duplicity(self,cr,uid,ids,context=None):
         obj_warranty = self.browse(cr, uid, ids[0], context=context)
-        pids = self.search(cr, uid, [('enterprise_key','=',obj_warranty.enterprise_key),('id','<>',obj_warranty.id)])
+        pids = self.search(cr, uid,
+                [('enterprise_key','=',obj_warranty.enterprise_key),
+                    ('id','<>',obj_warranty.id)])
         if len(pids) > 0:
             return False
         return True
@@ -33,14 +38,15 @@ class warranty_oerp(osv.Model):
     _columns = {
         'startdate':fields.date('Start Date', help='Start Date'), 
         'enddate':fields.date('End date', help='End Date'), 
-        'enterprise_key':fields.char('Enterprise Contract Key', 64, help="""Enterprise
-            Key"""), 
+        'enterprise_key':fields.char('Enterprise Contract Key', 64,
+            help="""Enterprise Key"""), 
         'contract_id':fields.many2one('account.analytic.account', 'Contract',
             help='fields help'), 
     }
     _constraints = [                                                            
-         (_check_overlapping, 'Error!\n Date is/are invalid.', ['enddate']),
-         (_check_duplicity, 'Error!\n Enterprise Key exists in other contract.', ['enterprise_key']),
+         (_check_overlapping, """Error!\n Date is/are invalid.""", ['enddate']),
+         (_check_duplicity, """Error!\n Enterprise Key exists in other
+             contract.""", ['enterprise_key']),
     ]
                                                                                 
 class account_analytic_account(osv.Model):
@@ -48,5 +54,7 @@ class account_analytic_account(osv.Model):
     _columns = {
         'license_oerp':fields.boolean('This contract has Enterprise License',
             help='This contract has Enterprise License'), 
-        'warranty_oerp_ids':fields.one2many('account.warranty_oerp', 'contract_id', 'Warranties Enterprise', help='Warranties Enterprise'), 
+        'warranty_oerp_ids':fields.one2many('account.warranty_oerp',
+            'contract_id', 'Warranties Enterprise', help="""Warranties
+            Enterprise"""), 
     }
