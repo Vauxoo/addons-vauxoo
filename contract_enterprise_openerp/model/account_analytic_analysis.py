9be31dd6ac9fc935b1131ffdcc1b77fd86d15b6e
Luis Escobar <vxkikevx@gmail.com>
2013-08-20 15:51:15 -0430

    [FIX] Added view. [IMP] Added constraint to validate duplicity and overlapping dates.

--- contract_enterprise_openerp/model/account_analytic_analysis.py
+++ contract_enterprise_openerp/model/account_analytic_analysis.py
@@ -13,6 +13,23 @@ _logger = logging.getLogger(__name__)
 
 class warranty_oerp(osv.Model):
     _name = 'account.warranty_oerp'
+
+    def _check_overlapping(self,cr,uid,ids,context=None):
+        obj_warranty = self.browse(cr, uid, ids[0], context=context) 
+        if obj_warranty.enddate < obj_warranty.startdate:                        
+            return False                                                        
+        pids = self.search(cr, uid, [('enddate','>=',obj_warranty.startdate),('startdate','<=',obj_warranty.enddate),('id','<>',obj_warranty.id)])
+        if len(pids) > 0:
+            return False
+        return True
+
+    def _check_duplicity(self,cr,uid,ids,context=None):
+        obj_warranty = self.browse(cr, uid, ids[0], context=context)
+        pids = self.search(cr, uid, [('enterprise_key','=',obj_warranty.enterprise_key),('id','<>',obj_warranty.id)])
+        if len(pids) > 0:
+            return False
+        return True
+
     _columns = {
         'startdate':fields.date('Start Date', help='Start Date'), 
         'enddate':fields.date('End date', help='End Date'), 
@@ -21,6 +38,10 @@ class warranty_oerp(osv.Model):
         'contract_id':fields.many2one('account.analytic.account', 'Contract',
             help='fields help'), 
     }
+    _constraints = [                                                            
+         (_check_overlapping, 'Error!\n Date is/are invalid.', ['enddate']),
+         (_check_duplicity, 'Error!\n Enterprise Key exists in other contract.', ['enterprise_key']),
+    ]
                                                                                 
 class account_analytic_account(osv.Model):
     _inherit = 'account.analytic.account'
