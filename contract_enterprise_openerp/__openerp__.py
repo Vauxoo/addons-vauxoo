d41ff3f668ac219bd0d6f9a4be9c7e4676c0e0bb
Nhomar Hernandez <nhomar@gmail.com>
2013-08-22 12:04:49 -0430

    [AUTOPEP8]

--- contract_enterprise_openerp/__openerp__.py
+++ contract_enterprise_openerp/__openerp__.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python
-# -*- encoding: utf-8 -*-                                                       
-############################################################################### 
-#    Module Writen to OpenERP, Open Source Management Solution                  
+# -*- encoding: utf-8 -*-
+###############################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
 #    Copyright (C) Vauxoo (<http://vauxoo.com>).
-#    All Rights Reserved                                                        
+#    All Rights Reserved
 ################# Credits######################################################
-#    Coded by: Luis Escobar <luis@vauxoo.com>                                  
-#    Audited by: Nhomar Hernandez <nhomar@vauxoo.com> 
-############################################################################### 
-#    This program is free software: you can redistribute it and/or modify      
-#    it under the terms of the GNU Affero General Public License as published   
-#    by the Free Software Foundation, either version 3 of the License, or      
-#    (at your option) any later version.                                        
-#                                                                               
-#    This program is distributed in the hope that it will be useful,            
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of             
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              
-#    GNU Affero General Public License for more details.                        
-#                                                                               
-#    You should have received a copy of the GNU Affero General Public License   
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
-############################################################################### 
+#    Coded by: Luis Escobar <luis@vauxoo.com>
+#    Audited by: Nhomar Hernandez <nhomar@vauxoo.com>
+###############################################################################
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+###############################################################################
 
 {
-    "name": "Warranties Enterprise",                                                  
-    "version": "0.1",                                                           
+    "name": "Warranties Enterprise",
+    "version": "0.1",
     "depends": ["base", "account_analytic_analysis"],
     "author": "Vauxoo",
     "description": """
