f81be5b604b80cddbfcd20fed3dae6a8cbc84ee2
Julio Serna <julio@vauxoo.com>
2013-08-21 17:25:07 -0700

    [IMP] canceled expense when state in paid or done

--- hr_expense_replenishment_cancel/model/hr_expense_replenishment_cancel.py
+++ hr_expense_replenishment_cancel/model/hr_expense_replenishment_cancel.py
@@ -1,1 +1,46 @@
- 
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+###############################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#    Copyright (C) OpenERP Venezuela (<http://openerp.com.ve>).
+#    All Rights Reserved
+############# Credits #########################################################
+#    Coded by: Katherine Zaoral          <kathy@vauxoo.com>
+#    Planified by: Humberto Arocha       <hbto@vauxoo.com>
+#    Audited by: Humberto Arocha         <hbto@vauxoo.com>
+###############################################################################
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+###############################################################################
+import time
+from openerp.osv import fields, osv
+from openerp import netsvc
+import openerp.addons.decimal_precision as dp
+from openerp.tools.translate import _
+
+class hr_expense_expense(osv.Model):
+    _inherit = "hr.expense.expense"
+    
+    def expense_canceled(self, cr, uid, ids, context=None):
+        obj_move_line = self.pool.get('account.move.line')
+        obj_move = self.pool.get('account.move')
+        res = super(hr_expense_expense,
+                        self).expense_canceled(cr, uid, ids, context=context)
+        for expense in self.browse(cr, uid, ids, context=context):
+            if expense.account_move_id:
+                obj_move_line._remove_move_reconcile(cr, uid,
+                    [move_line.id
+                        for move_line in expense.account_move_id.line_id],
+                    context=context)
+                obj_move.unlink(cr, uid, [expense.account_move_id.id])
+        return res
