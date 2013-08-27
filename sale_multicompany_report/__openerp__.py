2718f0e87fa6b24651987c8620077be34c4cd2e9
Luis Escobar <luis@vauxoo.com>
2013-08-27 17:48:58 -0430

    [IMP] Changed to version 7.0 structure file.

--- sale_multicompany_report/__openerp__.py
+++ sale_multicompany_report/__openerp__.py
@@ -1,6 +1,28 @@
-# -*- encoding: utf-8 -*-
+#!/usr/bin/python                                                               
+# -*- encoding: utf-8 -*-                                                       
+############################################################################### 
+#    Module Writen to OpenERP, Open Source Management Solution
+#    Copyright (C) Vauxoo (<http://vauxoo.com>).
+#    All Rights Reserved                                                        
+################# Credits###################################################### 
+#    Coded by: Luis Escobar <luis@vauxoo.com>
+#    Audited by: Nhomar Hernandez <nhomar@vauxoo.com>
+############################################################################### 
+#    This program is free software: you can redistribute it and/or modify       
+#    it under the terms of the GNU Affero General Public License as published   
+#    by the Free Software Foundation, either version 3 of the License, or       
+#    (at your option) any later version.                                        
+#                                                                               
+#    This program is distributed in the hope that it will be useful,            
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of             
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              
+#    GNU Affero General Public License for more details.                        
+#                                                                               
+#    You should have received a copy of the GNU Affero General Public License   
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.      
+###############################################################################    
 {
-    "name" : "Sale report for AMD Consorcium",
+    "name" : "Sale Report for MultiReports Environment",
     "version" : "0.1",
     "depends" : ["sale", "multireport_base"],
     "author" : "Vauxoo",
@@ -10,16 +32,11 @@
                     """,
     "website" : "http://vauxoo.com",
     "category" : "Generic Modules/Sales",
-    "init_xml" : [
-    ],
-    "demo_xml" : [
-    ],
-    "update_xml" : [
+    "data" : [
         "wizard/sale_order_multicompany.xml",
         "sale_multicompany_report_view.xml",
         "sale_order_view.xml",
-
     ],
     "active": False,
-    "installable": False,
+    "installable": True,
 }
