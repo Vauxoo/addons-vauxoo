0a12d4ce82f838952f946cff532a37b99ecd873c
Luis Escobar <luis@vauxoo.com>
2013-08-28 16:57:23 -0430

    [IMP] Overwritten method to printing from default button.

--- sale_multicompany_report/order.py
+++ sale_multicompany_report/order.py
@@ -27,3 +27,25 @@ class sale_order(osv.Model):
                 if ol.att_bro:
                     print "Im Here i will go to print %s " % ol.name
         return True
+
+    def __get_company_object(self, cr, uid):
+        user = self.pool.get('res.users').browse(cr, uid, uid)
+        print user
+        if not user.company_id:
+            raise except_osv(_('ERROR !'), _(
+                'There is no company configured for this user'))
+        return user.company_id
+
+    def _get_report_name(self, cr, uid, context):
+        report = self.__get_company_object(cr, uid).sale_report_id
+        if not report:
+            rep_id = self.pool.get("ir.actions.report.xml").search(
+                cr, uid, [('model', '=', 'sale.order'), ], order="id")[0]
+            report = self.pool.get(
+                "ir.actions.report.xml").browse(cr, uid, rep_id)
+        return report.report_name
+
+    def print_quotation(self, cr, uid, ids, context=None):
+        pq = super(sale_order, self).print_quotation(cr,uid,ids, context)
+        return  {'type': 'ir.actions.report.xml', 'report_name': self._get_report_name(cr, uid,
+            context), 'datas': pq['datas'], 'nodestroy': True} 
