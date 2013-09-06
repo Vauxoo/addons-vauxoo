ee4506fa9450752354ff9ddbb187795031c41e24
Yanina Aular <yanina.aular@vauxoo.com>
2013-09-06 09:46:22 -0430

    [ADD] New module control of quantities of products in the WorkOrder mrp_workorder_variation

---
+++ mrp_workorder_variation/__openerp__.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-                                                                           
+###############################################################################                     
+#    Module Writen to OpenERP, Open Source Management Solution                                      
+#    Copyright (C) Vauxoo (<http://www.vauxoo.com>).                                                
+#    All Rights Reserved                                                                            
+# Credits######################################################                                     
+#    Coded by: Yanina Aular <yani@vauxoo.com> 
+#    Planified by: Humberto Arocha <humbertoarocha@gmail.com>
+#    Audited by: Humberto Arocha <humbertoarocha@gmail.com>
+###############################################################################                     
+#    This program is free software: you can redistribute it and/or modify                           
+#    it under the terms of the GNU Affero General Public License as published                       
+#    by the Free Software Foundation, either version 3 of the License, or                           
+#    (at your option) any later version.                                                            
+#                                                                                                   
+#    This program is distributed in the hope that it will be useful,                                
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of                                 
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                                  
+#    GNU Affero General Public License for more details.                                            
+#                                                                                                   
+#    You should have received a copy of the GNU Affero General Public License                       
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.                          
+###############################################################################
+
+{                                                                                                                                                  
+    'name': 'MRP WorkOrder Variation',                                                       
+    'version': '1.0',                                                                               
+    'summary': 'Allows you to enter actual quantities of products in a workorder',
+    'description': """                                                                             
+
+MRP WorkOrder Variation
+=======================
+
+Lets have a detailed control of the number of products in and out in a work order. That is, using
+mrp_byworkcenter_capacity module that adds a table of product needed in each routing operation,
+this module adds a table in the work orders with the real products that are received and outgoing.
+In this way the user can make a comparison of the variation of quantities and avoid loss of
+material.
+
+Compare the amount of operation scheduled products to the real products in a work order.
+""",
+    'category': 'MRP',                                                                       
+    'author': 'Vauxoo C.A',                                                                         
+    'website': 'http://vauxoo.com',                                                                 
+    'license' : 'AGPL-3',
+    'depends': ['mrp'],
+    'data': [],
+    'demo': [],
+    'installable': True,                                                                            
+    'auto_install' : False,
+    'js': [],
+    'qweb': [],
+    'css': [],
+    'images': [],
+    'test' : [],
+}
