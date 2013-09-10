239906da94cc954ab88279dc57d560d81360a779
Jorge Angel Naranjo Rogel <jorge_nr@vauxoo.com>
2013-09-10 09:50:33 -0500

    [ADD][l10n_mx_facturae] Added invoice in data demo for cfd in state draft [ADD][l10n_mx_facturae_cbb] Added invoice in data demo for cbb in state draft [ADD][l10n_mx_facturae_pac_sf] Added invoice in data demo for cfdi in state draft

--- l10n_mx_facturae_cbb/__openerp__.py
+++ l10n_mx_facturae_cbb/__openerp__.py
@@ -50,7 +50,8 @@ http://www.sat.gob.mx/sitio_internet/asistencia_contribuyente/principiantes/comp
             "l10n_mx_facturae_report",
             "l10n_mx_company_multi_address",
         ],
-    "demo" : ["demo/l10n_mx_facturae_seq_demo.xml",],
+    "demo" : ["demo/l10n_mx_facturae_seq_demo.xml",
+              "demo/account_invoice_cbb.yml"],
     "data" : [
         #'security/l10n_mx_facturae_cbb_security.xml',
         'ir_sequence_view_v6.xml',
