91e5eda5f345557accef7070d66fff3df28c3acd
Yanina Aular <yanina.aular@vauxoo.com>
2013-09-11 13:00:08 -0430

    [ADD] new module for Packaging stock movements for a delivery order

---
+++ mrp_shipping_packaging/model/mrp_shipping_packaging.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+###############################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#    Copyright (C) OpenERP Venezuela (<http://openerp.com.ve>).
+#    All Rights Reserved
+############# Credits #########################################################
+#    Coded by: Yanina Aular <yanina.aular@vauxoo.com
+#    Planified by: Humberto Arocha <hbto@vauxoo.com>
+#    Audited by: Humberto Arocha <hbto@vauxoo.com>
+###############################################################################
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+###############################################################################
+
+from openerp.osv import fields, osv, orm
+from openerp.tools.translate import _
+from openerp import tools
+
+
+class mrp_shipping_packaging(osv.Model):
+
+    _name = 'mrp.shipping.packaging'
+    _description = _('Need to set the model name')
+    _inherit = ['mail.thread']
+
+    '''
+    Need to set the model description
+    '''
+
+    _columns = {
+        'name': fields.char(
+            _('Name'),
+            required=True,
+            size=64,
+            help=_('Name')),
+    }
+
+    _defaults = {
+    }
+
