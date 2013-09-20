3525383965ff04ea9061114345d26e4f499a2738
Luis Escobar <luis@vauxoo.com>
2013-09-20 12:12:19 -0430

    [IMP] Moved start function to lp:vauxoo-private/cms to generic functionality.

--- portal_crm_vauxoo/static/src/js/portal_crm.js
+++ portal_crm_vauxoo/static/src/js/portal_crm.js
@@ -3,18 +3,6 @@ var _t = instance.web._t,
    _lt = instance.web._lt;                                                                          
 var QWeb = instance.web.qweb; 
 
-instance.web.FormView.include({
-    start: function(){
-        self = this        
-        this._super.apply(this, arguments).done(function () {
-            if (typeof self.$(".bs3-footer")[0] != "undefined") {
-                var Footer = new instance.portal_news.FooterWeb();
-                Footer.appendTo(self.$el);
-            } 
-        }); 
-    }
-});
-
 instance.web.form.FieldCharBS3 = instance.web.form.FieldChar.extend({                                    
     template: 'FieldCharBS3',
 });  
