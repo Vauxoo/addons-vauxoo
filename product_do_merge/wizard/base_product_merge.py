9ce9d5a88b5f42446875e64d095729a2eedb4bb4
Sabrina Romero <sabrina@vauxoo.com>
2013-09-23 12:41:20 -0700

    [FIX] Some changes in the view and translation file added and exception handled.

--- product_do_merge/wizard/base_product_merge.py
+++ product_do_merge/wizard/base_product_merge.py
@@ -224,8 +224,7 @@ class MergeProductAutomatic(osv.TransientModel):
                         #Handle exception for the flag validation
                         if False in flag:
                             raise osv.except_osv(_('Error!'), _(
-                                """
-                                You must verify the units of measurement in which
+                                """You must verify the units of measurement in which
                                 the products do you wish to merge already have operations.
                                 """))
                         else:
@@ -534,12 +533,17 @@ class MergeProductAutomatic(osv.TransientModel):
         context = dict(context or {}, active_test=False)
         this = self.browse(cr, uid, ids[0], context=context)
         p_ids = this.product_ids and this.product_ids
-        p_ids and p_ids.append(this.product_to)
+        p_ids and p_ids.append(this.product_to)        
         product_ids = set(map(int, this.product_from and
-                              [this.product_to, this.product_from] or
-                              p_ids))
-        self._merge(cr, uid, product_ids, this.product_to,
-                    context=context)
+                                [this.product_to, this.product_from] or
+                                p_ids))
+        if not list(product_ids):
+            raise osv.except_osv(_('Error!'), _(
+                """The product from must be selected for 
+                    this option."""))
+        else:
+            self._merge(cr, uid, product_ids, this.product_to,
+                            context=context)
         return True
 
     def _compute_selected_groupby(self, this):
@@ -561,7 +565,7 @@ class MergeProductAutomatic(osv.TransientModel):
         if not groups:
             raise osv.except_osv(_('Error!'),
                                  _("""You have to specify a filter for your
-                                      selection"""))
+                                      selection."""))
         return groups
 
     def _generate_query(self, fields, maximum_group=100):
