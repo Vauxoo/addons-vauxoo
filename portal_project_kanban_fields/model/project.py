6ba80d26c61523f019e741e1d76f1a6ce9c8777d
Yanina Aular <yanina.aular@vauxoo.com>
2013-09-23 19:42:57 -0430

    [ADD] image to the form view

--- portal_project_kanban_fields/model/project.py
+++ portal_project_kanban_fields/model/project.py
@@ -30,42 +30,41 @@ from openerp import netsvc
 
 
 class portal_project(osv.Model):
-    """ 
+    """
     """
     _inherit = 'project.project'
     _description = 'Portal Project'
 
+    def _get_image(self, cr, uid, ids, name, args, context=None):
+        result = dict.fromkeys(ids, False)
+        for obj in self.browse(cr, uid, ids, context=context):
+            result[obj.id] = tools.image_get_resized_images(obj.image)
+        return result
+
+    def _set_image(self, cr, uid, id, name, value, args, context=None):
+        return self.write(cr, uid, [id], {'image': tools.image_resize_image_big(value)}, context=context)
+
     _columns = {
         'project_url': fields.char('Project URL', 256, help='Launchpad Link to Project'),
         'documentation_url': fields.char('Documentation URL', 256, help='Documentation Link'),
+        'image': fields.binary("Image",
+                               help="This field holds the image used as avatar for this project, limited to 1024x1024px"),
+
+        'image_medium': fields.function(_get_image, fnct_inv=_set_image,
+                                        string="Medium-sized image", type="binary", multi="_get_image",
+                                        store={
+                                        'project.project': (lambda self, cr, uid, ids, c={}: ids, ['image'], 10),
+                                        },
+                                        help="Medium-sized image of this contact. It is automatically "
+                                        "resized as a 128x128px image, with aspect ratio preserved. "
+                                        "Use this field in form views or some kanban views."),
+
+        'image_small': fields.function(_get_image, fnct_inv=_set_image,
+                                       string="Small-sized image", type="binary", multi="_get_image",
+                                       store={
+                                       'project.project': (lambda self, cr, uid, ids, c={}: ids, ['image'], 10),
+                                       },
+                                       help="Small-sized image of this contact. It is automatically "
+                                       "resized as a 64x64px image, with aspect ratio preserved. "
+                                       "Use this field anywhere a small image is required."),
     }
-"""
-        'image': fields.binary("Image",                                                             
-            help="This field holds the image used as avatar for this project, limited to 1024x1024px"),
-        'image_medium': fields.function(_get_image, fnct_inv=_set_image,                            
-            string="Medium-sized image", type="binary", multi="_get_image",                         
-            store={                                                                                 
-                'res.partner': (lambda self, cr, uid, ids, c={}: ids, ['image'], 10),               
-            },                                                                                      
-            help="Medium-sized image of this contact. It is automatically "\                        
-                 "resized as a 128x128px image, with aspect ratio preserved. "\                     
-                 "Use this field in form views or some kanban views."),                             
-        'image_small': fields.function(_get_image, fnct_inv=_set_image,                             
-            string="Small-sized image", type="binary", multi="_get_image",                          
-            store={                                                                                 
-                'res.partner': (lambda self, cr, uid, ids, c={}: ids, ['image'], 10),               
-            },                                                                                      
-            help="Small-sized image of this contact. It is automatically "\                         
-                 "resized as a 64x64px image, with aspect ratio preserved. "\                       
-                 "Use this field anywhere a small image is required."),         
-"""            
-"""
-    def _get_image(self, cr, uid, ids, name, args, context=None):                                                                  
-        result = dict.fromkeys(ids, False)                                                                                         
-        for obj in self.browse(cr, uid, ids, context=context):                                      
-            result[obj.id] = tools.image_get_resized_images(obj.image)                                                                             
-        return result                                                                               
-                                                                                                    
-    def _set_image(self, cr, uid, id, name, value, args, context=None):                             
-        return self.write(cr, uid, [id], {'image': tools.image_resize_image_big(value)}, context=context)
-"""
