d72206a14769617bd73fb2154a00b1b9ffa2f03d
Yanina Aular <yanina.aular@vauxoo.com>
2013-10-03 15:37:58 -0430

    [IMP] Filtering process is said in widget.js class because it is not scalable

---
+++ delivery_restaurant/static/src/js/widgets.js
@@ -0,0 +1,30 @@
+function delivery_restaurant_widgets(instance, module){
+    module.ProductCategoriesWidget.include({
+        search_and_categories: function(category){
+            self = this;
+            this._super();
+/*           
+            var products = this.pos.db.get_product_by_category(this.category.id);
+            
+            console.log( "hola " + this.pos.get('pos_config').deli_rest );
+            //console.log('hola extend');
+            //var products = this.pos.get('products');
+            //console.log( products);
+
+             products = _.filter(products, function(prod){            
+                 var res_or_del = self.pos.get('pos_config').deli_rest;                              
+            //        console.log( prod.name );
+                 if( (res_or_del === 'restaurant' && prod.restaurant == true) ||                     
+                     (res_or_del === 'delivery' && prod.delivery == true) ){                         
+                     return true;                                                                    
+                 }                                                                                   
+                 return false;                                                                       
+             });
+  
+          self.pos.get('products').reset(products);
+         */
+          },
+
+    })
+}
+
