186c0dc12edf0b6ac49ce918732cdd3affc5074b
Yanina Aular <yanina.aular@vauxoo.com>
2013-10-03 15:46:26 -0430

    [IMP] you try to change the function add_products, to return the filtered products. Because override the search_and_categories, no easy task for now.

---
+++ delivery_restaurant/static/src/js/db.js
@@ -0,0 +1,22 @@
+function delivery_restaurant_db(instance, module){
+    module.PosLS.include({
+    
+        init: function(options){
+            this._super(options);           
+            this.product_by_id = {};
+        },
+
+        add_products: function(products){
+            this._super(products);
+            var self = this;
+
+            for(var i = 0, len = products.length; i < len; i++){
+                var product = products[i];
+             console.log('hola ' + product.name + product.restaurant); 
+        
+                
+            }     
+       },  
+    
+    })
+}
