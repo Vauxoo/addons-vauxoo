c490c48681f758206c7629570f09cdc0258bfa84
Yanina Aular <yanina.aular@vauxoo.com>
2013-10-07 17:06:17 -0430

    [ADD] to filter the products in the Pos, depending on if they are for or delivery restaurant

---
+++ pos_product_filter/static/src/js/db.js
@@ -0,0 +1,46 @@
+function delivery_restaurant_db(instance, module){
+    module.PosLS.include({
+    
+        init: function(options){
+            this._super(options);           
+        },
+
+        add_products: function(products){
+            this._super(products);
+            var self = this;
+
+          for(var i = 0, len = products.length; i < len; i++){
+            var prod = products[i];
+            //Aqui se tienen los productos filtrados desde db.js
+
+            //console.log('product_by:');
+            //console.log(this.product_by_id[prod.id]); 
+                
+          }     
+       },  
+    
+    })
+}
+            
+
+/*this.product_by_id[prod.id].restaurant =
+            prod.restaurant;
+            this.product_by_id[prod.id].delivery = prod.delivery;
+          */
+                                                             
+            
+             
+             /*
+                        _.filter(products, function(prod){            
+                var res_or_del = self.pos.get('pos_config').deli_rest;                              
+                     console.log(prod); 
+                 if( (res_or_del === 'restaurant' && prod.restaurant == true) ){
+                     console.log(this.product_by_id[prod.id]); 
+                     return true;                                                                    
+                 }                     
+                 if( (res_or_del === 'delivery' && prod.delivery == true) ){                         
+                     console.log(this.product_by_id[prod.id]); 
+                     return true;                                                                    
+                 }                                                                                   
+                 return false;
+                 });*/
