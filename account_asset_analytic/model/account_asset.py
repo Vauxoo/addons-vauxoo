6a4fee7bcab53cd81a4e8a204677eec642565afb
Humberto Arocha <humbertoarocha@gmail.com>
2013-10-10 00:06:22 -0430

    [IMP][account_asset_analytic] move_create method was superwrite to allow the use of the analytic account in the asset if available

--- account_asset_analytic/model/account_asset.py
+++ account_asset_analytic/model/account_asset.py
@@ -24,3 +24,20 @@ class account_asset_asset(osv.osv):
         'account_analytic_id': fields.many2one('account.analytic.account', 'Analytic account'),
     }
 
+class account_asset_depreciation_line(osv.osv):
+    _inherit = 'account.asset.depreciation.line'
+
+    def create_move(self, cr, uid, ids, context=None):
+        context = context or {}
+        created_move_ids = super(account_asset_depreciation_line, self).create_move(cr, uid, ids, context=None)
+        if not created_move_ids: return created_move_ids
+        move_line_obj = self.pool.get('account.move.line')
+        for line in self.browse(cr, uid, ids, context=context):
+            aml_ids = move_line_obj.search(cr,uid, [('asset_id','=',line.asset_id.id),
+                ('move_id','in',created_move_ids)],context=context)
+            if aml_ids and line.asset_id.account_analytic_id:
+                move_line_obj.write(cr, uid, aml_ids, {
+                    'analytic_account_id': line.asset_id.account_analytic_id.id,
+                    },context=context)
+        return created_move_ids
+
