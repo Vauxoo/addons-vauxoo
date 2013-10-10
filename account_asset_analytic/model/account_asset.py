e66001ad8d69495a7f989b9bd18a69ddbbf45f03
Humberto Arocha <humbertoarocha@gmail.com>
2013-10-09 22:13:29 -0430

    [ADD][account_asset_analytic] Added new analytic field into assets

---
+++ account_asset_analytic/model/account_asset.py
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+import time
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+from openerp.osv import fields, osv
+import openerp.addons.decimal_precision as dp
+from tools.translate import _
+
+class account_asset_asset(osv.osv):
+    _inherit = 'account.asset.asset'
+
+    def onchange_account_analytic_id(self, cr, uid, ids, category_id=False, context=None):
+        context = context or {}
+        val = {}
+        if category_id:
+            category = self.pool.get('account.asset.category').browse(cr, uid, category_id, context=context)
+            if category.account_analytic_id:
+                val['account_analytic_id'] = category.account_analytic_id.id
+        return {'value': val}
+
+    _columns = {
+        'account_analytic_id': fields.many2one('account.analytic.account', 'Analytic account'),
+    }
+
