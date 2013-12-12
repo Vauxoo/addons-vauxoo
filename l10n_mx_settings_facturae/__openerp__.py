f471d40cfd6f5c0583b9e0adcb84bace689c5385
Juan Carlos Funes <juan@vauxoo.com>
2013-12-11 18:22:19 -0600

    [IMP][l10n_mx_settings_facturae]delete dependence "ir_actions_report_xml_multicompany"

--- l10n_mx_settings_facturae/__openerp__.py
+++ l10n_mx_settings_facturae/__openerp__.py
@@ -28,7 +28,6 @@
         'account',
         'email_template',
         'email_template_multicompany',
-        'ir_actions_report_xml_multicompany'
     ],
     "author": "Vauxoo",
     "description" : """
