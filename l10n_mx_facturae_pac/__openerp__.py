1bec45e9fc65bf5dc16e89b6f5212dfecc8141bc
Jorge Angel Naranjo Rogel <jorge_nr@vauxoo.com>
2013-12-17 12:37:49 -0600

    [FIX][l10n_mx_facturae_pac, l10n_mx_facturae_pac_sf] Fixed depends in this modules

--- l10n_mx_facturae_pac/__openerp__.py
+++ l10n_mx_facturae_pac/__openerp__.py
@@ -34,7 +34,7 @@
 """,
     "website" : "http://www.vauxoo.com/",
     "license" : "AGPL-3",
-    "depends" : ["l10n_mx_facturae_base"],
+    "depends" : ["l10n_mx_facturae"],
     "demo" : [],
     "data" : [],
     "installable" : True,
