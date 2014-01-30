0316d81530020714cabf148692be98f548d3d02c
Julio Serna <julio@vauxoo.com>
2014-01-30 10:23:58 -0600

    [IMP] validate line of refund

--- account_voucher_no_check_default/account_voucher.py
+++ account_voucher_no_check_default/account_voucher.py
@@ -28,17 +28,98 @@ from openerp.osv import fields, osv
 class account_voucher(osv.Model):
     _inherit = 'account.voucher'
     
+    def get_cr_dr(self, cr, uid, ids, line, context=None):
+        acc_move_line_obj = self.pool.get('account.move.line')
+        acc_invoice_obj = self.pool.get('account.invoice')
+        
+        acc_line = line.get('move_line_id', False)
+        move_exclude = False
+        move_id = acc_line and acc_move_line_obj.browse(cr, uid, acc_line,\
+            context=context).move_id or False
+        invoice_line = move_id and acc_invoice_obj.search(cr, uid, [\
+            ('move_id', '=', move_id.id)], context=context) or False
+        if invoice_line:
+            line.update({'reconcile' : False, 'line_to_invoice' : True, 'amount': 0})
+            move_exclude = acc_line
+        return move_exclude
+    
+    def move_include(self, cr, uid, values, val_exclude, context=None):
+        print values,'valuesvaluesvalues'
+        value_cr = [val_cr.get('move_line_id', False) for val_cr in values.get('line_cr_ids', {})]
+        value_dr = [val_dr.get('move_line_id', False) for val_dr in values.get('line_dr_ids', {})]
+        return list(set(value_cr + value_dr) - set(val_exclude))
+    
+    def recompute_moves(self, cr, uid, ids, partner_id, journal_id, amount, currency_id, ttype, date, values, move_exclude, context=None):
+        print values,'values'
+        move_include = self.move_include(cr, uid, values, move_exclude, context=context)
+        context['move_line_ids'] = move_include
+        result = self.recompute_voucher_lines(cr, uid, ids, partner_id, journal_id, amount, currency_id, ttype, date, context=context)
+        values_new = result.get('value', {})
+        
+        for val_cr in values.get('line_cr_ids', {}):
+            val_cr.update({'amount': line['amount'] for line in values_new.get('line_cr_ids') if line['move_line_id'] == val_cr['move_line_id']})
+        for val_dr in values.get('line_dr_ids', {}):
+            val_dr.update({'amount': line['amount'] for line in values_new.get('line_dr_ids') if line['move_line_id'] == val_dr['move_line_id']})
+        return True
+    
     def onchange_partner_id(self, cr, uid, ids, partner_id, journal_id,\
         amount, currency_id, ttype, date, context=None):
-        res = super(account_voucher, self).onchange_partner_id(cr, uid, ids,\
-            partner_id, journal_id, amount, currency_id, ttype, date,\
-            context=context)
-        values = res.get('value', False)
-        if values and values.get('line_cr_ids') and ttype == 'payment':
+        if context is None:
+            context = {}
+        res = super(account_voucher, self).onchange_partner_id(cr, uid, ids, partner_id,\
+            journal_id, amount, currency_id, ttype, date, context=context)
+        values = res.get('value', {})
+        if values.get('line_cr_ids', False) and ttype == 'payment' and amount>=0:
+                move_include1 = [
+                    self.get_cr_dr(cr, uid, ids, line, context=context)
+                    for line in values.get('line_cr_ids') ]
+        if values.get('line_dr_ids', False) and ttype == 'receipt' and amount>=0:
+                move_include2 = [
+                    self.get_cr_dr(cr, uid, ids, line, context=context)
+                    for line in values.get('line_dr_ids') ]
+        return res
+        
+    def onchange_amount(self, cr, uid, ids, amount, rate, partner_id, journal_id, currency_id,\
+        ttype, date, payment_rate_currency_id, company_id, context=None):
+        if context is None:
+            context = {}
+        res = super(account_voucher, self).onchange_partner_id(cr, uid, ids, partner_id,\
+            journal_id, amount, currency_id, ttype, date, context=context)
+        values = res.get('value', {})
+        if values.get('line_cr_ids', False) and ttype == 'payment':
             for line in values.get('line_cr_ids'):
-                line.update({'reconcile' : False})
-        if values and values.get('line_dr_ids') and ttype == 'receipt':
+                self.get_cr_dr(cr, uid, ids, line, context=context)
+        if values.get('line_dr_ids', False) and ttype == 'receipt':
             for line in values.get('line_dr_ids'):
-                line.update({'reconcile' : False})
+                self.get_cr_dr(cr, uid, ids, line, context=context)
+        return res
+        
+    def onchange_journal(self, cr, uid, ids, journal_id, line_ids, tax_id,\
+        partner_id, date, amount, ttype, company_id, context=None):
+        if context is None:
+            context = {}
+        res = super(account_voucher, self).onchange_journal(cr, uid, ids, journal_id, line_ids,\
+            tax_id, partner_id, date, amount, ttype, company_id, context=context)
+        print res
+        values = res and res.get('value', {}) or {}
+        move_exclude = []
+        if values.get('line_cr_ids', False) and ttype == 'payment' and amount>=0:
+            move_exclude = [
+                self.get_cr_dr(cr, uid, ids, line, context=context)
+                for line in values.get('line_cr_ids') ]
+        if values.get('line_dr_ids', False) and ttype == 'receipt' and amount>=0:
+            move_exclude = [
+                self.get_cr_dr(cr, uid, ids, line, context=context)
+                for line in values.get('line_dr_ids') ]
+                
+        self.recompute_moves(cr, uid, ids, partner_id, journal_id, amount, values.get('currency_id', False), ttype, date, values, move_exclude, context=context)
+            
         return res
     
+class account_voucher_line(osv.Model):
+    _inherit = 'account.voucher.line'
+    
+    _columns = {
+        'line_to_invoice': fields.boolean('Line to Invoice', help='If this field is true indicates'\
+            ' that this line coming from an invoice refound')
+    }
