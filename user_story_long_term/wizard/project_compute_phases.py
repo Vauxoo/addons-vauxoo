31b5babd417781cd602924e3feac72fb86c66b32
Yanina Aular <yanina.aular@vauxoo.com>
2014-02-06 11:50:37 -0430

    [FIX] License

--- user_story_long_term/wizard/project_compute_phases.py
+++ user_story_long_term/wizard/project_compute_phases.py
@@ -1,13 +1,18 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
-#
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+###############################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#    Copyright (C) OpenERP Venezuela (<http://www.vauxoo.com>).
+#    All Rights Reserved
+############# Credits #########################################################
+#    Coded by: Yanina Aular <yani@vauxoo.com>
+#    Planified by: Maria Gabriela Quilarque <gabriela@vauxoo.com>
+#    Audited by: Maria Gabriela Quilarque <gabriela@vauxoo.com>
+###############################################################################
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,8 +21,8 @@
 #
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+###############################################################################
+
 from openerp.tools.translate import _
 from openerp.osv import fields, osv
 
