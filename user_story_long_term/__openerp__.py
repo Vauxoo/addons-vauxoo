3475d81a500e2d4ba396bd1f3fffed23d99eccf7
Yanina Aular <yanina.aular@vauxoo.com>
2014-02-14 09:11:08 -0430

    [IMP] workflow file reestructured

--- user_story_long_term/__openerp__.py
+++ user_story_long_term/__openerp__.py
@@ -54,7 +54,7 @@ Features:
     'data': [
         #'security/ir.model.access.csv',
         'user_story_long_term_view.xml',
-        'user_story_long_term_workflow.xml',
+        'workflow/user_story_long_term_workflow.xml',
         'data/user_story_template.xml',
         #'wizard/user_story_compute_phases_view.xml',
         #'wizard/user_story_compute_tasks_view.xml',
