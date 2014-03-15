219866fc435c214f6e45e496060f969030b0ea9c
Jorge Angel Naranjo Rogel <jorge_nr@vauxoo.com>
2014-03-14 19:18:22 -0600

    [IMP]Improvement descriptions in __openerp__.py in modules of payroll

--- l10n_mx_payroll_base/__openerp__.py
+++ l10n_mx_payroll_base/__openerp__.py
@@ -40,6 +40,19 @@
     "description": """
 Mexican Payroll
 ===============
+
+This module installs the necessary work to CFDI MX Payroll. 
+For example, data banks, catalogs required for SAT to generate payroll, 
+fields needed in view of employees and contracts.
+
+
+ftp://ftp2.sat.gob.mx/asistencia_servicio_ftp/publicaciones/cfdi/catalogoscomplementonomina.pdf
+
+
+ftp://ftp2.sat.gob.mx/asistencia_servicio_ftp/publicaciones/cfd/nomina11.pdf
+
+
+ftp://ftp2.sat.gob.mx/asistencia_servicio_ftp/publicaciones/cfdi/guianomina.pdf
     """,
     "website": "http://vauxoo.com",
     "category": "Addons Vauxoo",
