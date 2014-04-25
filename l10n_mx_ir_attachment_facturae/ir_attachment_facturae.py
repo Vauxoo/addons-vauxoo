cbe96757dea44e302be9a88885d510f6d1b32bf4
Luis Torres <luis_t@vauxoo.com>
2014-04-25 13:54:53 -0500

    [IMP][l10n_mx_ir_attachment_facturae]Delete in if that check that state was diferent to cancel

--- l10n_mx_ir_attachment_facturae/ir_attachment_facturae.py
+++ l10n_mx_ir_attachment_facturae/ir_attachment_facturae.py
@@ -585,7 +585,7 @@ class ir_attachment_facturae_mx(osv.Model):
             #~ else:
                 #wf_service.trg_validate(uid, 'account.invoice', invoice.id, 'invoice_cancel', cr)
             #~ if 'cfdi' in ir_attach_facturae_mx_id.type:
-            if not ir_attach_facturae_mx_id.state in ['cancel', 'draft', 'confirmed']:
+            if not ir_attach_facturae_mx_id.state in ['draft', 'confirmed']:
                 type__fc = self.get_driver_fc_cancel()
                 if ir_attach_facturae_mx_id.res_pac.name_driver in type__fc.keys():
                     cfdi_cancel = res = type__fc[ir_attach_facturae_mx_id.res_pac.name_driver](
