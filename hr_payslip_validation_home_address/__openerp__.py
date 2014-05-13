fc305d79fd0963b2543241e4f9f27bdc77312002
Juan Carlos Funes <juan@vauxoo.com>
2014-05-13 18:55:11 -0500

    [IMP][hr_payslip_validation_home_address]add description of module

--- hr_payslip_validation_home_address/__openerp__.py
+++ hr_payslip_validation_home_address/__openerp__.py
@@ -30,7 +30,11 @@
     "author" : "Vauxoo",
     "category" : "Localization/Mexico",
     "description" : """
-    This module adds validation to the home address of the employee in payroll
+    This module adds validation for the field home_address of the employee in payroll
+    Just have to install the module, not to do any configuration.
+    This field home_address is found in form view HR / Payroll Employee / Employee_id in "Personal Information" tab
+    If the field home_address is empty you can not confirm that employee's payroll and will see 
+    the message that the employee needs to have a home address, otherwise if may confirm the payroll.
     """,
     "website" : "http://www.vauxoo.com/",
     "license" : "AGPL-3",
