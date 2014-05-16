59e42546156483d1c96250dbf439e946da5782a1
Luis Ernesto Garcia Medina <ernesto_gm@vauxoo.com>
2014-05-16 18:28:15 -0500

    [REF][project_followers_rule] changes in variable names

--- project_followers_rule/model/project.py
+++ project_followers_rule/model/project.py
@@ -31,13 +31,13 @@ class project(osv.Model):
 
     def _get_followers(self, cr, uid, ids, field_name, arg, context=None):
         result = {}
-        for record in self.browse(cr, uid, ids, context=context):
+        for project in self.browse(cr, uid, ids, context=context):
             task_ids = self.pool.get('project.task').search(
-                cr, uid, [('project_id', '=', record.id)])
+                cr, uid, [('project_id', '=', project.id)])
             for task in self.pool.get('project.task').browse(cr, uid, task_ids):
                 if task.message_follower_ids:
-                    result[record.id] = [
-                        x.id for x in task.message_follower_ids]
+                    result[project.id] = [
+                        follower.id for follower in task.message_follower_ids]
         return result
 
     def _search_project(self, cr, uid, obj, name, args, context):
