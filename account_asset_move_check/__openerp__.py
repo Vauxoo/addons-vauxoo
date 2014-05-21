2f1bbb25bac1228d6552a4731c0d7d4c15098aa5
Jose Morales <jose@vauxoo.com>
2014-05-21 00:12:34 -0500

    [ADD] This module add a field that makes posible check like posted some deprecations lines with an special condition using the "check_posted" field in the validation of "_get_move_check" function.

---
+++ account_asset_move_check/__openerp__.py
@@ -0,0 +1,62 @@
+# -*- encoding: utf-8 -*-
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#
+#    Copyright (c) 2013 Vauxoo - http://www.vauxoo.com/
+#    All Rights Reserved.
+#    info Vauxoo (info@vauxoo.com)
+############################################################################
+#    Coded by: Sabrina Romero (sabrina@vauxoo.com)
+############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name' : 'Account Asset Move Check',
+    'version' : '0.1',                                                             
+    'author': 'Vauxoo',
+    'website': 'http://www.vauxoo.com',
+    'category' : 'Accounting',
+    'description' : """
+Account Asset Move Check
+========================
+
+This module add a field that makes posible check like posted some
+deprecations lines with an special condition using the "check_posted"
+field in the validation of "_get_move_check" function.
+
+    """,
+    'images' : [],                                                                 
+    'depends' : [
+        'account',
+        'account_asset',
+    ],                                                                
+    'data': [                                                                      
+    ],                                                                                 
+    'js': [                                                                        
+    ],                                                                                 
+    'qweb' : [                                                                     
+    ],                                                                                 
+    'css':[                                                                        
+    ],                                                                                 
+    'demo': [                                                                      
+    ],                                                                                 
+    'test': [                                                                      
+    ], 
+    'installable': True,                                                           
+    'auto_install': False,                                                         
+}                                                                                  
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
