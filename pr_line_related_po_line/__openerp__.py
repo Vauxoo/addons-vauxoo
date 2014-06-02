2a564f4bb9c495fde9af7acd8bc98caeae552aad
Yanina Aular <yanina.aular@vauxoo.com>
2014-06-02 17:46:01 -0430

    [ADD] description added to pr_line_related_po_line, purchase_requisition_line_analytic, and purchase_requisition_line_description modules

--- pr_line_related_po_line/__openerp__.py
+++ pr_line_related_po_line/__openerp__.py
@@ -30,6 +30,12 @@
     'website': 'http://www.vauxoo.com/',
     'category': '',
     'description': '''
+Add purchase_requisition_line_id field, it is id of purchase requisition line from where purchase
+order line is created, overwrite  the make_purchase_order method for add value of
+purchase_requisition_line_id to record purchase order line, it is help to make best inherit and
+modification of make_purchase_order method, as can be seen in
+purchase_requisition_line_description, purchase_requisition_line_analytic and
+purchase_requisition_requisitor modules.  
 ''',
     'depends': [
         'purchase',
