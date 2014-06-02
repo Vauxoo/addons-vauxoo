ab646d8bf717978baa20ce4ee1e5e64bd28ead8f
Yanina Aular <yanina.aular@vauxoo.com>
2014-06-02 14:06:34 -0430

    [ADD] new module, related field purchase_order_line is added in purchase_requisition_line

---
+++ pr_line_related_po_line/__openerp__.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+###############################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#    Copyright (C) OpenERP Venezuela (<http://www.vauxoo.com>).
+#    All Rights Reserved
+############# Credits #########################################################
+#    Coded by: Yanina Aular <yani@vauxoo.com>
+#    Planified by: Humberto Arocha <hbto@vauxoo.com>
+#    Audited by: Humberto Arocha <hbto@vauxoo.com>
+###############################################################################
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+###############################################################################
+
+{
+    'name': 'pr_line_related_po_line',
+    'version': '1.0',
+    'author': 'Vauxoo',
+    'website': 'http://www.vauxoo.com/',
+    'category': '',
+    'description': '''
+''',
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'test': [],
+    'qweb': [],
+    'js': [],
+    'css': [],
+    'active': False,
+    'installable': True,
+}
