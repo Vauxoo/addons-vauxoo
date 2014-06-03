7938e922fafe57cbf149a0dc3ed80d7581120dab
Katherine Zaoral <katherine.zaoral@vauxoo.com>
2014-06-03 17:33:35 -0430

    [ADD] add new model python file to inherit from the purchase requisition model and to add the 'priority' selection field.

--- purchase_requisition_priority/model/purchase_requisition.py
+++ purchase_requisition_priority/model/purchase_requisition.py
@@ -23,4 +23,21 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ###############################################################################
 
-import purchase_requisition
+from openerp.osv import fields, osv, orm
+from openerp.tools.translate import _
+from openerp import tools
+
+priority_options = [
+    (05, 'emergency'),
+    (10, 'urgent'),
+    (15, 'normal')
+] 
+
+class purchase_requisition(osv.Model):
+    _inherit = 'purchase.requisition'
+    _columns = {
+        'priority': fields.selection(
+            priority_options,
+            'Priority',
+            help='The priority of the document.'),
+    }
