0db34ed77abaf40f092aa6edec66b809b2188319
Katherine Zaoral <katherine.zaoral@vauxoo.com>
2014-06-12 11:41:35 -0430

    [ADD] extend the module description with more details.

--- purchase_requisition_currency/__openerp__.py
+++ purchase_requisition_currency/__openerp__.py
@@ -33,11 +33,15 @@
 Purchase Requisition Currency
 =============================
 
-This module add the currency field in the purchase requisition model.
-You need to active some user technical settings to take advantage of this
-functionality:
-  - Multi Currencies.
-  - Purchase Pricelist.
+Adds the currency field in the purchase requisition model. When you use the
+Request a Quotation button the purchase requisition currency new field will be
+take into account to generate the purchase orders with a pricelist that have
+the same currency. If there is not pricelist with the same purchase requisition
+currency then it will raise an exception. You need to active some User
+Technical Settings to take advantage of this functionality:
+
+- Multi Currencies.
+- Purchase Pricelist.
 ''',
     'depends': [
         'base',
