153c36cbd99f18fe026a06c37cf7e9fc1924dd2e
Juan Carlos Funes <juan@vauxoo.com>
2014-06-24 18:23:57 -0500

    [IMP][l10n_mx_cfdi_test]add autopep8

--- l10n_mx_cfdi_test/model/validate_structure.py
+++ l10n_mx_cfdi_test/model/validate_structure.py
@@ -1,13 +1,13 @@
 # -*- encoding: utf-8 -*-
-###########################################################################
+#
 #    Module Writen to OpenERP, Open Source Management Solution
 #
-#    Copyright (c) 2011 Vauxoo - http://www.vauxoo.com
+#    Copyright (c) 2014 Vauxoo - http://www.vauxoo.com
 #    All Rights Reserved.
 #    info@vauxoo.com
-############################################################################
+#
 #    Coded by: vauxoo consultores (info@vauxoo.com)
-############################################################################
+#
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -22,12 +22,13 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-##############################################################################
+#
 
 from openerp.tools.translate import _
 from openerp.osv import fields, osv
 import base64
 import xml
+
 
 class account_invoice(osv.Model):
     _inherit = 'account.invoice'
@@ -38,11 +39,11 @@ class account_invoice(osv.Model):
         ir_model_data = self.pool.get('ir.model.data')
         aml_obj = self.pool.get('account.invoice.line')
         tax_obj = self.pool.get('account.invoice.tax')
-        data_xml = base64.decodestring( xml_binary )
+        data_xml = base64.decodestring(xml_binary)
         doc_xml = xml.dom.minidom.parseString(data_xml)
-        tax_lines = self.read(cr, uid, [ids],['tax_line'])
-        invoice_lines = self.read(cr, uid, [ids],['invoice_line'])
-        data_invoice = self.read(cr, uid, ids,[])
+        tax_lines = self.read(cr, uid, [ids], ['tax_line'])
+        invoice_lines = self.read(cr, uid, [ids], ['invoice_line'])
+        data_invoice = self.read(cr, uid, ids, [])
         list_price_subtotal = []
         list_price_unit = []
         list_importe_xml = []
@@ -61,8 +62,10 @@ class account_invoice(osv.Model):
             for n in doc_xml.getElementsByTagName("cfdi:Comprobante"):
                 xml_total = n.getAttribute("total")
                 xml_subTotal = n.getAttribute("subTotal")
-                assert data_invoice['amount_total'] == eval(xml_total), 'No matches Total'
-                assert data_invoice['amount_untaxed'] == eval(xml_subTotal), 'No matches Subtotal'
+                assert data_invoice['amount_total'] == eval(
+                    xml_total), 'No matches Total'
+                assert data_invoice['amount_untaxed'] == eval(
+                    xml_subTotal), 'No matches Subtotal'
             for line in invoice_lines[0]['invoice_line']:
                 for r in aml_obj.read(cr, uid, [line]):
                     list_price_subtotal.append(r['price_subtotal'])
@@ -71,7 +74,7 @@ class account_invoice(osv.Model):
                 for conceptos in n.getElementsByTagName("cfdi:Conceptos"):
                     for concepto in conceptos.getElementsByTagName("cfdi:Concepto"):
                         importe = concepto.getAttribute("importe")
-                        valorUnitario =  concepto.getAttribute("valorUnitario")
+                        valorUnitario = concepto.getAttribute("valorUnitario")
                         list_importe_xml.append(eval(importe))
                         list_valorUnitario_xml.append(eval(valorUnitario))
             assert list_price_subtotal == list_importe_xml, 'No matches Importe of product'
@@ -87,16 +90,20 @@ class account_invoice(osv.Model):
                         list_retenciones.append(retencion)
                         totalImpuestosRetenidos += retencion or 0.0
             for taxes in n.getElementsByTagName("cfdi:Impuestos"):
-                totalImpuestosTrasladados_xml = eval(taxes.getAttribute("totalImpuestosTrasladados"))
-                totalImpuestosRetenidos_xml = eval(taxes.getAttribute("totalImpuestosRetenidos"))
+                totalImpuestosTrasladados_xml = eval(
+                    taxes.getAttribute("totalImpuestosTrasladados"))
+                totalImpuestosRetenidos_xml = eval(
+                    taxes.getAttribute("totalImpuestosRetenidos"))
                 if taxes.getElementsByTagName("cfdi:Retenciones"):
                     for retenciones in taxes.getElementsByTagName("cfdi:Retenciones"):
                         for ret in retenciones.getElementsByTagName("cfdi:Retencion"):
-                            list_importe_ret_xml.append(eval(ret.getAttribute("importe")))
+                            list_importe_ret_xml.append(
+                                eval(ret.getAttribute("importe")))
                 if taxes.getElementsByTagName("cfdi:Traslados"):
                     for traslados in taxes.getElementsByTagName("cfdi:Traslados"):
                         for traslado in traslados.getElementsByTagName("cfdi:Traslado"):
-                            list_importe_traslados_xml.append(eval(traslado.getAttribute("importe")))
+                            list_importe_traslados_xml.append(
+                                eval(traslado.getAttribute("importe")))
             assert totalImpuestosRetenidos == totalImpuestosRetenidos_xml, 'No matches Total Impuestos Retenidos of product'
             assert totalImpuestosTrasladados == totalImpuestosTrasladados_xml, 'No matches Total Impuestos Trasladados of product'
             assert list_retenciones == list_importe_ret_xml, 'No matches Importe Retenido of product'
