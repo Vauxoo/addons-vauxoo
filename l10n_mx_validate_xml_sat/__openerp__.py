321246f6561790b0d667a88dfa2917833a325186
Jorge Angel Naranjo Rogel <jorge_nr@vauxoo.com>
2014-06-26 10:17:08 -0500

    [ADD][l10n_mx_validate_xml_sat] Add test yaml for wizard of validation XML with SAT

--- l10n_mx_validate_xml_sat/__openerp__.py
+++ l10n_mx_validate_xml_sat/__openerp__.py
@@ -1,13 +1,13 @@
 # -*- encoding: utf-8 -*-
-###########################################################################
+#
 #    Module Writen to OpenERP, Open Source Management Solution
 #
 #    Copyright (c) 2014 Vauxoo - http://www.vauxoo.com/
 #    All Rights Reserved.
 #    info Vauxoo (info@vauxoo.com)
-############################################################################
+#
 #    Coded by: Luis Torres (luis_t@vauxoo.com)
-############################################################################
+#
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -22,37 +22,37 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-##############################################################################
+#
 
 {
-    "name" : "Validate XML SAT",
-    "version" : "1.0",
-    "author" : "Vauxoo",
-    "category" : "Localization/Mexico",
+    "name": "Validate XML SAT",
+    "version": "1.0",
+    "author": "Vauxoo",
+    "category": "Localization/Mexico",
     "description" : """
     This module add an wizard to validate if an XLM already was validateD in the SAT
-    
-    Added 
-    sudo apt-get install python-soappy 
+
+    Added
+    sudo apt-get install python-soappy
     sudo pip install pillow qrcode
     wget http://security.ubuntu.com/ubuntu/pool/main/s/suds/python-suds_0.4.1-2ubuntu1_all.deb
     sudo dpkg -i python-suds_0.4.1-2ubuntu1_all.deb
-    
+
     """,
-    "website" : "http://www.vauxoo.com/",
-    "license" : "AGPL-3",
-    "depends" : [
+    "website": "http://www.vauxoo.com/",
+    "license": "AGPL-3",
+    "depends": [
         "l10n_mx_facturae_base",
-        ],
+    ],
     "demo": [
         "demo/attachment_validate_sat_demo.xml"
-        ],
-    "test" : [
+    ],
+    "test": [
         "test/xml_validate_sat.yml",
-        ],
-    "data" : [
+    ],
+    "data": [
         "view/wizard_validate_uuid_xml_view.xml",
-        ],
-    "installable" : True,
-    "active" : False,
+    ],
+    "installable": True,
+    "active": False,
 }
