fa8203680ffd1076b5a56707e1a5feb4e79bfc32
Juan Carlos Funes <juan@vauxoo.com>
2014-07-03 18:45:03 -0500

    [ADD][l10n_facturae_groups_multipac_vauxoo]add module l10n_facturae_groups_multipac_vauxoo

---
+++ l10n_facturae_groups_multipac_vauxoo/__openerp__.py
@@ -0,0 +1,46 @@
+# -*- encoding: utf-8 -*-
+#
+#    Module Writen to OpenERP, Open Source Management Solution
+#
+#    Copyright (c) 2014 Vauxoo - http://www.vauxoo.com/
+#    All Rights Reserved.
+#    info Vauxoo (info@vauxoo.com)
+#
+#    Coded by: vauxoo consultores (info@vauxoo.com)
+#
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+
+{
+    "name" : "Multipac Vauxoo Groups",
+    "version" : "1.0",
+    "author" : "Vauxoo",
+    "category" : "Localization/Mexico",
+    "description" : """Create the groups: User y Manager for Multipac Vauxoo
+    """,
+    "website" : "http://vauxoo.com",
+    "license" : "AGPL-3",
+    "depends" : ["base"
+        ],
+    "demo" : [],
+    "test": [],
+    "data" : ["security/l10n_mx_facturae_multipac_security_groups.xml",
+            ],
+    "images": [],
+    'application': False,
+    "active" : False,
+    "installable" : True,
+}
