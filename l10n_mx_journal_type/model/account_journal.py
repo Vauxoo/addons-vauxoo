fe9ff5858d6fd3ca9bfa47de2790fe9cef5704d0
Sabrina Romero <sabrina@vauxoo.com>
2014-07-16 12:14:56 -0500

    [ADD] l10n_mx_journal_type module added, for add field that corresponds to the attribute requiered by SAT to express the nature of the account entry.

---
+++ l10n_mx_journal_type/model/account_journal.py
@@ -0,0 +1,42 @@
+# -*- encoding: utf-8 -*-
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#
+#    Copyright (c) 2012 Vauxoo - http://www.vauxoo.com/
+#    All Rights Reserved.
+#    info Vauxoo (info@vauxoo.com)
+############################################################################
+#    Coded by: Isaac Lopez (isaac@vauxoo.com)
+#              Moises Lopez (moylop260@vauxoo.com)
+############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp.tools.translate import _
+from openerp.osv import fields, osv
+
+
+class account_journal(osv.Model):
+    _inherit = 'account.journal'
+
+    _columns = {
+        'l10n_mx_type': fields.selection([
+            ('receipts','Receipts'),
+            ('Expenses','expenses'),
+            ('journal','Journal'),
+            ],'Mx Type', required=False,
+            help="This field corresponds to the attribute required by \
+            the SAT to express the nature of the account entry."),
+    }
