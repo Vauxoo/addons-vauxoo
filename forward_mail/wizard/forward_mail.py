845450349c30580c2090bc1c1ccac103069162f1
Julio Serna <julio@vauxoo.com>
2014-07-16 13:57:02 -0500

    [IMP] forward mail when mail not is notification and state is exception

--- forward_mail/wizard/forward_mail.py
+++ forward_mail/wizard/forward_mail.py
@@ -23,12 +23,29 @@
 
 from openerp.osv import fields, osv
 from openerp.tools.translate import _
-from openerp import netsvc
+from openerp import SUPERUSER_ID
 
 class forward_mail(osv.osv_memory):
     
     _name = 'forward.mail'
     
     def action_forward_mail(self, cr, uid, ids, context=None):
-        
+        mail_pool = self.pool.get('mail.mail')
+        if context is None:
+            context = {}
+        for mail in mail_pool.browse(cr, uid, context.get('active_ids', []), context=context):
+            if mail.state == 'exception' and mail.type in ("email", "comment"):
+                mail_pool.mark_outgoing(cr, uid, mail.id, context=context)
+                partners_to_notify = set([])
+                partner_follower = self.pool.get('mail.followers')
+                
+                fol_ids = partner_follower.search(cr, SUPERUSER_ID, [
+                    ('res_model', '=', mail.model),
+                    ('res_id', '=', mail.res_id),
+                    ], context=context)
+                    
+                partners_to_notify |= set(fo.partner_id.id\
+                        for fo in partner_follower.browse(cr, SUPERUSER_ID, fol_ids, context=context))
+                mail_pool.send(cr, uid, [mail.id], recipient_ids=partners_to_notify, context=context)
+                
         return True
