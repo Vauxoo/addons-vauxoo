6be9dd0f2837a91bf0c4b116d5a145499175bc73
Sabrina Romero <sabrina@vauxoo.com>
2014-07-28 18:26:40 -0500

    [ADD] user_story_messages module structure added.

---
+++ user_story_messages/__openerp__.py
@@ -0,0 +1,41 @@
+# -*- encoding: utf-8 -*- #
+############################################################################
+#    Module Writen to OpenERP, Open Source Management Solution             #
+#    Copyright (C) Vauxoo (<http://vauxoo.com>).                           #
+#    All Rights Reserved                                                   #
+###############Credits######################################################
+#    Coded by: Sabrina Romero (sabrina@vauxoo.com)                         #
+#    Planified by: Nhomar Hernandez (nhomar@vauxoo.com)                    #
+#    Finance by: Vauxoo <info@vauxoo.com>                                  #
+#    Audited by: Moises Lopez <moylop260@vauxoo.com>                       #
+############################################################################
+#    This program is free software: you can redistribute it and/or modify  #
+#    it under the terms of the GNU General Public License as published by  #
+#    the Free Software Foundation, either version 3 of the License, or     #
+#    (at your option) any later version.                                   #
+#                                                                          #
+#    This program is distributed in the hope that it will be useful,       #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#    GNU General Public License for more details.                          #
+#                                                                          #
+#    You should have received a copy of the GNU General Public License     #
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>. #
+############################################################################
+
+{
+    "name" : "User Stroy Messages",
+    "version" : "1.0",
+    "author" : "Vauxoo",
+    "category" : "Generic Modules",
+    "description" : 
+    """
+    """,
+    "website" : "http://www.vauxoo.com/",
+    "license" : "AGPL-3",
+    "depends" : [],
+    "demo" : [],
+    "data" : [],
+    "installable" : True,
+    "active" : False,
+}
