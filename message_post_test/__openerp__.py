0a7a041768392843b71d897b05a85fe0be52c30d
Jose Morales <jose@vauxoo.com>
2014-07-28 02:45:59 -0500

    [ADD] Added new model to test the functionality of message_post_model module, this module creates a test model with line to change it a regiter all changes over this model in an unit test

---
+++ message_post_test/__openerp__.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#    Copyright (C) Vauxoo (<http://vauxoo.com>).
+#    All Rights Reserved
+###############Credits######################################################
+#    Coded by: Vauxoo C.A.
+#    Planified by: Nhomar Hernandez
+#    Audited by: Vauxoo C.A.
+#############################################################################
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+{
+    "name" : "Message Post Test",
+    "version" : "0.1",
+    "depends" : ['message_post_model'],
+    "author" : "Vauxoo",
+    "description" : """
+Test for message_post_model module
+==================================
+
+- This module only is used to test the functionality of message_post_model module
+
+    """,
+    "website" : "http://vauxoo.com",
+    "category" : "Generic Modules",
+    "data" : [
+       'security/ir.model.access.csv',
+        ],
+    "active": False,
+    "installable": True,
+
+}
---
+++ message_post_test/tests/__init__.py
@@ -0,0 +1,5 @@
+from . import test_message_post
+
+fast_suite = [
+	test_message_post
+]
