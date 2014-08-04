a685109410889f721c5fd4b9a22f58b41fd2d156
Katherine Zaoral <katherine.zaoral@vauxoo.com>
2014-08-04 15:57:30 -0430

    [ADD] extend the description of the module to indicate the new menu.

--- account_move_line_extended/__openerp__.py
+++ account_move_line_extended/__openerp__.py
@@ -33,7 +33,9 @@
 Account Move Line Extended
 ==========================
 
-Add a new view for the account move lines.
+Add a new view for the account move lines at the menu ``Accounting or Invocing
+> Journal Entries > Journal Items Extended``. The currency and the amount
+currency fields are visible.
 ''',
     'depends': [
         'account',
