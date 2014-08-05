1eb1c87ae8e178033aefcaf61c8cb9199e7cb91b
Jorge Angel Naranjo Rogel <jorge_nr@vauxoo.com>
2014-08-05 13:38:42 -0500

    [IMP][aging_due_report] Added supplier formal due report webkit

--- aging_due_report/__openerp__.py
+++ aging_due_report/__openerp__.py
@@ -45,6 +45,7 @@ A Supplier's Aging Due Report.
         "data/aging_due_webkit_header.xml",
         "data/formal_due_webkit_header.xml",
         "data/supplier_aging_due_webkit_header.xml",
+        "data/supplier_formal_due_webkit_header.xml",
         "report/aging_due_report_webkit.xml",
 
     ],
