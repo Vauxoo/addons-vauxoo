570a455dfc252a24111e493bd3ab0571237bea96
Julio Serna <julio@vauxoo.com>
2014-08-06 18:52:14 -0500

    [ADD] added module to allow get attachmnet by default in mail_composse_wizard

---
+++ email_template_att_dinamic/model/email_template.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#    Copyright (C) 2013 Vauxoo (<http://vauxoo.com>).
+#    All Rights Reserved
+###############Credits######################################################
+#    Coded by: vauxoo consultores (info@vauxoo.com)
+#############################################################################
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+
+from openerp.osv import osv, fields
+
+class email_template(osv.Model):
+    
+    _inherit = "email.template"
+    
+    _columns = {
+        'att_default': fields.boolean('Add attachment', help='Add attachment of record by default'),
+        'att_other_field': fields.text('Add attachment from other filed',
+                help='specify from which fields are to get attachments' \
+                '(only fields with relation to ir.attachment)')
+    }
