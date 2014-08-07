dcf07aa690e39ba7235f2b235f656fe4b3f0645b
Julio Serna <julio@vauxoo.com>
2014-08-06 20:01:48 -0500

    [IMP] modified onchange of mail_compose_message to get attachment from field with relation to ir.attachment

--- email_template_att_dinamic/model/email_template.py
+++ email_template_att_dinamic/model/email_template.py
@@ -42,16 +42,26 @@ class mail_compose_message(osv.TransientModel):
                         composition_mode, model, res_id, context=None):
         if not context:
             context = {}
+        
+        template_obj = self.pool.get('email.template')
+        
         res = super(mail_compose_message,
                         self).onchange_template_id(cr, uid, ids, template_id,
                             composition_mode, model, res_id, context=context)
                             
-        template = self.pool.get('email.template').browse(cr, uid, template_id, context)
+        template = template_obj.browse(cr, uid, template_id, context)
         
+        attach = []
         if template.att_default:
             attach = self.pool.get('ir.attachment').search(cr, uid,
                 [('res_id', '=', res_id), ('res_model', '=', model)])
-            attach += res.get('value',{}).pop('attachment_ids', [])
-            res.get('value', {}).update({'attachment_ids': [(6, 0, attach)]})
+        
+        if template.att_other_field:
+            att_field_render = template_obj.render_template(cr, uid,
+                template.att_other_field, template.model, res_id, context=context)
+            attach += [ id_att for id_att in eval("["+att_field_render+"]") if att_field_render ]
             
+        attach += res.get('value',{}).pop('attachment_ids', [])
+        res.get('value', {}).update({'attachment_ids': [(6, 0, attach)]})
+        
         return res
