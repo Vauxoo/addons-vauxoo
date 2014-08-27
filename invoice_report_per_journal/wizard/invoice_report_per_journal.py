ae63125251ca3de7917a853ced909224333a5869
Luis Torres <luis_t@vauxoo.com>
2014-08-27 16:09:08 -0500

    [IMP][invoice_report_per_journal]Was added option to print report of invoices in view tree of invoice

--- invoice_report_per_journal/wizard/invoice_report_per_journal.py
+++ invoice_report_per_journal/wizard/invoice_report_per_journal.py
@@ -36,16 +36,21 @@ class invoice_report_per_journal(osv.TransientModel):
     """
     _name = "invoice.report.per.journal"
  
-    def get_journal_object(self, cr, uid, context=None):
+    def get_journal_object(self, cr, uid, ids, context=None):
+        if context is None:
+            context = {}
         record_brw = self.pool.get(context['active_model']).browse(
-                                cr, uid, context['active_ids'][0])
+                                cr, uid, ids, context=context)
         if not record_brw.journal_id:
             raise except_osv(_('ERROR !'), _(
                 'There is no journal configured for this invoice.'))
         return record_brw.journal_id
 
     def _get_journal(self, cr, uid, context=None):
-        return self.get_journal_object(cr, uid, context=context).name
+        if context is None:
+            context = {}
+        ids = context.get('active_ids', [])
+        return self.get_journal_object(cr, uid, ids[0], context=context).name
         
     def _prepare_service(self, cr, uid, report, context=None):
         service = netsvc.LocalService('report.' + report.report_name)
@@ -54,31 +59,42 @@ class invoice_report_per_journal(osv.TransientModel):
         return (result, format)
 
     def _get_report(self, cr, uid, context=None):
-        report = self.get_journal_object(
-                            cr, uid, context=context).invoice_report_id
-        try:
-            (result, format) = self._prepare_service(cr, uid, report, context=context)
-        except:
-            if report:
-                _logger.warning("Error occurred in the report, the report set to the journal will be ignored.")
-            rep_id = self.pool.get("ir.actions.report.xml").search(
-                cr, uid, [('model', '=', 'account.invoice'),], order="id",
-                        context=context)[0]
-            report_ = self.pool.get(
-                    "ir.actions.report.xml").browse(cr, uid, rep_id, context=context)
-            (result, format) = self._prepare_service(cr, uid, report_, context=context)
-        try:
-            act_id = self.pool.get('ir.actions.act_window').search(cr, uid, [('name','=', report.name + ' txt')], context=context)[0]
-            if act_id:
-                act_brw = self.pool.get('ir.actions.act_window').browse(cr, uid, act_id, context=context)
-                wiz_obj = self.pool.get(act_brw.res_model)
-                wiz_id = wiz_obj.create(cr, uid, {}, context=context)
-                wiz_brw = wiz_obj.browse(cr, uid, wiz_id, context=context)
-                result = base64.decodestring(wiz_brw.fname_txt)
-        except:
-            if report:
-                _logger.info("txt report not defined for the report assigned to journal.")
-        return base64.encodestring(result)
+        if context is None:
+            context = {}
+        res = ''
+        for inv_id in context.get('active_ids', []):
+            report = self.get_journal_object(
+                        cr, uid, inv_id, context=context).invoice_report_id
+            try:
+                (result, format) = self._prepare_service(cr, uid, report,
+                    context=context)
+            except:
+                if report:
+                    _logger.warning("Error occurred in the report, the "\
+                    "report set to the journal will be ignored.")
+                rep_id = self.pool.get("ir.actions.report.xml").search(
+                    cr, uid, [('model', '=', 'account.invoice'),], order="id",
+                            context=context)[0]
+                report_ = self.pool.get(
+                        "ir.actions.report.xml").browse(cr, uid, rep_id,
+                        context=context)
+                (result, format) = self._prepare_service(cr, uid, report_,
+                    context=context)
+            try:
+                act_id = self.pool.get('ir.actions.act_window').search(cr, uid,
+                    [('name','=', report.name + ' txt')], context=context)[0]
+                if act_id:
+                    act_brw = self.pool.get('ir.actions.act_window').browse(
+                        cr, uid, act_id, context=context)
+                    wiz_obj = self.pool.get(act_brw.res_model)
+                    wiz_id = wiz_obj.create(cr, uid, {}, context=context)
+                    wiz_brw = wiz_obj.browse(cr, uid, wiz_id, context=context)
+                    result = base64.decodestring(wiz_brw.fname_txt)
+            except:
+                if report:
+                    _logger.info("txt report not defined for the report assigned to journal.")
+            res += result
+        return base64.encodestring(res)
 
     def _get_report_name(self, cr, uid, context=None):
         report = self.get_journal_object(cr, uid,
