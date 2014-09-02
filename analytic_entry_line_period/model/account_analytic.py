ef853ada3e129d02855f667d52b8e59e25cecfd8
Humberto Arocha <hbto@vauxoo.com>
2014-09-01 20:53:37 -0430

    [IMP] Redefining field period_id in account analytic line model

--- analytic_entry_line_period/model/account_analytic.py
+++ analytic_entry_line_period/model/account_analytic.py
@@ -31,25 +31,7 @@ class account_analytic_line(osv.Model):
     _inherit = 'account.analytic.line'
 
     _columns = {
-        'period_id': fields.many2one('account.period', 'Period'),
+        'period_id':fields.related('move_id', 'period_id', string='Period',
+            type='many2one', relation='account.period', store=True),
     }
 
-class account_move_line(osv.osv):
-
-    _inherit = "account.move.line"
-
-    def create_analytic_lines(self, cr, uid, ids, context=None):
-        if context is None:
-            context = {}
-        ids = isinstance(ids, (int, long)) and [ids] or ids
-
-        res = super(account_move_line, self).create_analytic_lines(cr, uid, ids,context=context)
-
-        analytic_line_obj = self.pool.get('account.analytic.line')
-        for move_line in self.browse(cr, uid, ids, context=context):
-           if move_line.analytics_id:
-               for line in move_line.analytic_lines:
-                   analytic_line_obj.write(cr, uid, line.id, {
-                       'period_id': move_line.period_id.id,
-                       }, context=context)
-        return res
