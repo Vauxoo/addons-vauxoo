d25202f0089f2d7a4fc59f7260b0cb09c14e6833
Katherine Zaoral <katherine.zaoral@vauxoo.com>
2014-09-16 09:39:44 -0430

    [ADD] new method that will be used for the new button to 'pay' the account.voucher.

--- account_voucher_draft/model/account_voucher.py
+++ account_voucher_draft/model/account_voucher.py
@@ -23,4 +23,21 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ###############################################################################
 
-import account_voucher
+from openerp.osv import fields, osv, orm
+from openerp.tools.translate import _
+from openerp import tools
+
+class account_voucher(osv.Model):
+
+    _inherit = 'account.voucher'
+
+    def button_draft_voucher(self, cr, uid, ids, context=None):
+        """
+        method used in the new button used when paying the account voucher.
+        @return close the pop up window.
+        """
+        context = context or {}
+        ids = isinstance(ids, (long, int) and [ids] or ids
+        for av_id in ids:
+            self.write(cr, uid, av_id, {'state': 'draft'}, context=context)
+        return {'type': 'ir.actions.act_window_close'}
