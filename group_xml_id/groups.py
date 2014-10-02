2a28112c87f736c1399b6ce71eb5abe48d257a22
Sabrina Romero <sabrina@vauxoo.com>
2014-10-02 17:24:38 -0500

    [FIX] Trailing whitespace fixed.

--- group_xml_id/groups.py
+++ group_xml_id/groups.py
@@ -27,7 +27,7 @@ from openerp.osv import fields, osv
 
 class res_groups(osv.Model):
     _inherit = 'res.groups'
-    
+
     _columns={
         'xml_id': fields.function(osv.osv.get_xml_id, type='char', size=128, string="External ID",
                                   help="ID of the view defined in xml file"),
