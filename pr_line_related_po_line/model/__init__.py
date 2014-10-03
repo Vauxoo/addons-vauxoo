f9749d6bef9fe3450cba627b200e3e02a237b865
Moises Lopez <moylop260@vauxoo.com>
2014-10-02 19:28:23 -0500

    [FIX] Fix pylint error W0403 - relative-import

--- pr_line_related_po_line/model/__init__.py
+++ pr_line_related_po_line/model/__init__.py
@@ -22,4 +22,4 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ###############################################################################
-import purchase_order_line
+from . import purchase_order_line
