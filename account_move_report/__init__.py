abe72d93afc6f67f5454cb635d1b71bcc2b990fb
Moises Lopez <moylop260@vauxoo.com>
2014-10-02 19:28:23 -0500

    [FIX] Fix pylint error W0403 - relative-import

--- account_move_report/__init__.py
+++ account_move_report/__init__.py
@@ -20,4 +20,4 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
-import report
+from . import report
