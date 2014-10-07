2c8873d9a6427e497e61e1e41eb6edd2f822168a
Luis Ernesto Garcia Medina <ernesto_gm@vauxoo.com>
2014-10-07 16:42:33 -0500

    [REF][invoice_datetime] change test to validate datetime with two time zones

--- invoice_datetime/tests/test_invoice_datetime.py
+++ invoice_datetime/tests/test_invoice_datetime.py
@@ -34,6 +34,8 @@ from openerp.tools import mute_logger
 import pytz
 import time
 import datetime
+import logging
+_logger = logging.getLogger(__name__)
 
 class TestInvoiceDatetime(TransactionCase):
     def setUp(self):
@@ -51,19 +53,33 @@ class TestInvoiceDatetime(TransactionCase):
         datetime_object = datetime.datetime.strptime(dt_server,
                                                      '%Y-%m-%d %H:%M:%S')
         # create list with tz to apply in datetime
-        tz_to_user = ['America/Caracas', 'America/Mexico_City']
-        for tz in tz_to_user:
-            self.user.write(cr, uid, uid, {'tz': tz}) #Assign tz to user
-            #call the function _get_datetime_with_user_tz to get the date with tz applied
-            datetime_tz = self.invoice._get_datetime_with_user_tz(cr, uid,
-                                                             datetime_object)
-            print datetime_tz, 'Datetime with tz: %s applied'%tz
+        self.user.write(cr, uid, uid, {'tz': 'Europe/Rome'}) #Assign tz to user
+        #call the function _get_datetime_with_user_tz to get the date with tz applied
+        datetime_tz_rome = self.invoice._get_datetime_with_user_tz(cr, uid,
+                                                         datetime_object)
         # Create an invoice with last tz of cycle America/Mexico_City
-        res_id = self.invoice.create(cr, uid, {
+        invoice_tz_rome_id = self.invoice.create(cr, uid, {
                                                 'partner_id':1,
                                                 'account_id':1,
                                                 'invoice_datetime': dt_server})
-        dt_res = self.invoice.read(cr, uid, res_id, []).get('date_invoice_tz')
-        # compare date calculated with function vs date of invoice record .
-        self.assertEquals(datetime_tz,
-                 dt_res, 'Date calculated and date of invoice are not equals')
+        self.user.write(cr, uid, uid, {'tz': 'America/Mexico_City'}) #Assign tz to user
+        datetime_tz_mx = self.invoice._get_datetime_with_user_tz(cr, uid,
+                                                         datetime_object)
+        invoice_tz_mx_id = self.invoice.create(cr, uid, {
+                                                'partner_id':1,
+                                                'account_id':1,
+                                                'invoice_datetime': dt_server})
+        dt_inv_tz_rome = self.invoice.read(cr, uid, invoice_tz_rome_id, []).get('date_invoice_tz')
+        dt_inv_tz_mx = self.invoice.read(cr, uid, invoice_tz_mx_id, []).get('date_invoice_tz')
+        _logger.info("Validate datetime of function, for Rome-Mexico_City TZ")
+        self.assertNotEqual(datetime_tz_rome,
+                 datetime_tz_mx, 'Dates are equal, should be different')
+        _logger.info("Validate datetime of function versus invoice datetime")
+        self.assertEquals(datetime_tz_rome, dt_inv_tz_rome,
+                'Date calculated and date of invoice are not equals')
+        self.assertEquals(datetime_tz_mx, dt_inv_tz_mx,
+                'Date calculated and date of invoice are not equals')
+        _logger.info("Validate invoice datetime, for Rome-Mexico_City TZ")
+        self.assertNotEqual(dt_inv_tz_rome,
+                dt_inv_tz_mx, 'Dates are equal, must be different on invoices')
+        
