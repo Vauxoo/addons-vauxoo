167bc3329dd3d5ce94de8cb69c1ef5aef4fa7f38
Luis Torres <luis_t@vauxoo.com>
2014-10-16 16:01:09 -0500

    [IMP][hr_children]Added pylint & docstring

--- hr_childrens/model/hr_children.py
+++ hr_childrens/model/hr_children.py
@@ -23,19 +23,30 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+'''
+File to add fields date_start and children_ids in hr.employee, and the
+attributes to childrens of employee.
+'''
 from openerp.osv import osv, fields
 
 
 class hr_employee(osv.Model):
+    '''
+    Inherit hr_employee to add date_start and to add yours childrens
+    '''
     _inherit = "hr.employee"
 
     _columns = {
         'date_start': fields.date('Date Start'),
-        'children_ids': fields.one2many('hr.children', 'employee_id', 'Childrens')
+        'children_ids': fields.one2many(
+            'hr.children', 'employee_id', 'Childrens')
     }
 
 
 class hr_children(osv.Model):
+    '''
+    Class to add object of childrens to employee
+    '''
     _name = "hr.children"
 
     _order = 'name'
@@ -43,8 +54,10 @@ class hr_children(osv.Model):
     _columns = {
         'name': fields.char('Name', size=64),
         'date_of_birth': fields.date('Date of birth'),
-        'schooling': fields.selection([('elementary', 'Elementary'),
-            ('high_school', 'High School'), ('preparatory', 'Preparatory'),
-            ('university', 'University')], 'Schooling'),
+        'schooling': fields.selection(
+            [('elementary', 'Elementary'),
+             ('high_school', 'High School'),
+             ('preparatory', 'Preparatory'),
+             ('university', 'University')], 'Schooling'),
         'employee_id': fields.many2one('hr.employee', 'Employee')
     }
