e0930885af1e3dc2d2c3215a16fd1021916170da
Luis Torres <luis_t@vauxoo.com>
2014-10-16 17:47:00 -0500

    [IMP][hr_lastname]Added fields to set the full name to employee

---
+++ hr_lastname/__openerp__.py
@@ -0,0 +1,47 @@
+# -*- encoding: utf-8 -*-
+#
+#    Module Writen to OpenERP, Open Source Management Solution
+#
+#    Copyright (c) 2010 Vauxoo - http://www.vauxoo.com/
+#    All Rights Reserved.
+#    info Vauxoo (info@vauxoo.com)
+#
+#    Coded by: Luis Torres (luis_t@vauxoo.com)
+#
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+
+{
+    "name": "HR Employee Last Name",
+    "version": "1.0",
+    "author": "Vauxoo",
+    "category": "hr",
+    "description": """
+        This module add fields to set last name, second name, second last name
+        & coupe last name, and and a field function to concatenate this values.
+    """,
+    "website": "http://www.vauxoo.com/",
+    "license": "AGPL-3",
+    "depends": [
+        "hr",
+        ],
+    "data": [
+        "view/hr_employee_view.xml",
+    ],
+    'installable': True,
+    'application': False,
+    'active': False
+}
---
+++ hr_lastname/model/hr_employee.py
@@ -0,0 +1,59 @@
+# -*- encoding: utf-8 -*-
+#
+#    Module Writen to OpenERP, Open Source Management Solution
+#
+#    Copyright (c) 2014 Vauxoo - http://www.vauxoo.com/
+#    All Rights Reserved.
+#    info Vauxoo (info@vauxoo.com)
+#
+#    Coded by: Luis Torres (luis_t@vauxoo.com)
+#
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+'''
+File to inherit hr.employee and added fields to complete name
+'''
+from openerp.osv import osv, fields
+
+
+class hr_employee(osv.Model):
+    '''
+    Inherit hr.employee to added fields to complete name
+    '''
+    _inherit = 'hr.employee'
+
+    def _get_full_name(self, cr, uid, ids, fields_name, args, context=None):
+        '''
+        Methot to concatenate last_name, second_last_name, name & second_name
+        in a new field function
+        '''
+        if context is None:
+            context = {}
+        res = {}
+        for emp in self.browse(cr, uid, ids, context=context):
+            res[emp.id] = (emp.last_name or '') + ' ' + (
+                emp.second_last_name or '') + ' ' + (emp.name or '') + ' ' + (
+                emp.second_name or '')
+        return res
+
+    _columns = {
+        'second_name': fields.char('Second Name',),
+        'last_name': fields.char('Last Name',),
+        'second_last_name': fields.char('Second Last Name',),
+        'couple_last_name': fields.char('Couple Last Name',),
+        'full_name': fields.function(
+            _get_full_name, string='Full Name', type='char', store=True,),
+        }
