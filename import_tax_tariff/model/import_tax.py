7201d165bfde5e7b0e18fdf019de5d48b8a94692
Yanina Aular <yanina.aular@vauxoo.com>
2014-11-07 08:44:17 -0430

    [ADD] Minimal model import.tax.tariff

---
+++ import_tax_tariff/model/import_tax.py
@@ -0,0 +1,31 @@
+# -*- encoding: utf-8 -*-
+# ############################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#    Copyright (C) OpenERP Venezuela (<http://www.vauxoo.com>).
+#    All Rights Reserved
+# ############ Credits #######################################################
+#    Coded by: Yanina Aular <yani@vauxoo.com>
+#    Planified by: Moises Lopez <moises@vauxoo.com>
+#    Audited by: Humberto Arocha <hbto@vauxoo.com>
+# ############################################################################
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from openerp import models, fields, api, _
+from openerp.exceptions import except_orm, Warning, RedirectWarning
+
+class import_tax_tariff(models.Model):
+    _name = "import.tax.tariff"
+    _description = "Import Tax Tariff"
+
+    name = field.Char(required=True)
