015f8d994d5d162970a645c80eecca0bbf1f86b5
Humberto Arocha <hbto@vauxoo.com>
2014-11-22 02:45:07 -0430

    [ADD] Added a Delegation Wizard for Purchaser Analyst

--- purchase_requisition_contract_analyst/__openerp__.py
+++ purchase_requisition_contract_analyst/__openerp__.py
@@ -23,31 +23,32 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ###############################################################################
 {
-    "name": "Purchase Requisition Contract Analyst", 
-    "version": "1.0", 
-    "author": "Vauxoo", 
-    "category": "purchase", 
+    "name": "Purchase Requisition Contract Analyst",
+    "version": "1.0",
+    "author": "Vauxoo",
+    "category": "purchase",
     "description": """
 Purchase Requisition Contract Analyst
 =====================================
 
 Extend the purchase requisition document to add an contract analyst that will
 be responsible for evaluate the purchase requisition.
-""", 
-    "website": "http://www.vauxoo.com/", 
-    "license": "", 
+""",
+    "website": "http://www.vauxoo.com/",
+    "license": "",
     "depends": [
         "purchase_requisition"
-    ], 
-    "demo": [], 
+    ],
+    "demo": [],
     "data": [
-        "view/purchase_requisition_view.xml"
-    ], 
-    "test": [], 
-    "js": [], 
-    "css": [], 
-    "qweb": [], 
-    "installable": True, 
-    "auto_install": False, 
+        "view/purchase_requisition_view.xml",
+        "wizard/analyst_delegation_view.xml"
+    ],
+    "test": [],
+    "js": [],
+    "css": [],
+    "qweb": [],
+    "installable": True,
+    "auto_install": False,
     "active": False
 }
