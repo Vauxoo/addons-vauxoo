fce1a7b921d6e240d9e94b585f1dea68b362fcaf
Federico Manuel Echeverri Choux <echeverrifm@gmail.com>
2014-12-12 21:11:53 -0600

    [ADD] Add procurement_cancel module

---
+++ procurement_cancel/wizard/procurement_cancel.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import logging
+import threading
+from openerp import SUPERUSER_ID
+from openerp import tools
+
+from openerp.osv import osv
+from openerp.api import Environment
+
+_logger = logging.getLogger(__name__)
+
+class cancel_procurement_order(osv.osv_memory):
+    _name = 'cancel.procurement.order'
+    _description = 'Cancel procurement'
+
+    def cancel_procurement(self, cr, uid, ids, context=None):
+        """
+        @param self: The object pointer.
+        @param cr: A database cursor
+        @param uid: ID of the user currently logged in
+        @param ids: List of IDs selected
+        @param context: A standard dictionary
+        """
+
+        procurement_obj = self.pool['procurement.order']
+        procurement_ids = context.get('active_ids', False)
+        for procurement_id in context.get('active_ids', False):
+            state = procurement_obj.browse(cr, uid, procurement_id,
+                context=context).state
+            if state in ('exception', 'confirmed', 'running'):
+                procurement_obj.write(cr, uid, procurement_id,
+                    {'state': 'cancel'}, context=context)
+                print state, procurement_id
+        return {'type': 'ir.actions.act_window_close'}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
