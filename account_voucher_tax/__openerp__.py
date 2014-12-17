557ae3b1f4df4d4b30cb577332862544c769cf12
julioserna <julio@vauxoo.com>
2014-12-17 13:12:23 -0600

    [IMP][account_vocuher_tax] added validation of the update_entry of journal of bank statement when not allowed cancel entry to move of generated of taxes with bank statement

--- account_voucher_tax/__openerp__.py
+++ account_voucher_tax/__openerp__.py
@@ -30,67 +30,44 @@
     "author": "Vauxoo",
     "category": "Localization/Mexico",
     "description": """
-
 The tax actually paid/cashed in the move of payment,
 ====================================================
-
 Creditable VAT.
-
 #    This program is distributed in the hope that it will be useful,
 It is the charge to all your payments i.e:
-
 Bought a desktop that charge VAT on this desk is the creditable taxes
-
 #    This program is distributed in the hope that it will be useful,
 Retained VAT is retained by suppliers
-
 - The rule is that only a natural person can hold a moral person.
-
 - The exception to this rule is for freight and rental.
-
 Caused VAT is that actually charged to customers.
-
 - When you make a cash sale that VAT is caused
-
 - When you make a sale on credit is transferred iva but when you pay that sale becomes caused VAT
-
 Integration of payment taxes with account voucher
 =================================================
-
 Tested cases
 ------------
-
 * Payment one or more invoices of the same partner
 * Payment in advance of one or more invoices of the same partner
-
 known failing Cases(to fix)
 ---------------------------
-
 * Allow payment invoices with other movements like invoices, debit/credit refunds(resolved account_voucher_no_check_default module)
 * When there are multiple payments may be that the amount of taxes paid is not equally payable by decimal rouding
 * Error with Period field(not is set default)
-
 Integration of payment taxes with Bank Statement
 ================================================
-
 Tested cases
 ------------
-
 * An statement bank with one imported invoice
 * An statement bank with two or more imported invoices of the same partner
 * An statement bank with two or more imported invoices of different partners
 * Payment in advance of an invoice with partner(when the amount of the advance is less than or equal to the amount of the invoice)
-
-
 known failing Cases(to fix)
 ---------------------------
-
 * Payment in advance that will pay more than one invoice
 * When there are multiple payments may be that the amount of taxes paid is not equally payable by decimal rouding
-
 Note: The difference for loss or gain exchange rate or writeoff not generate journal items of payment taxes
 the fiscal experct adviced us it is right on that way.
-
     """,
     "website": "http://www.vauxoo.com/",
     "license": "AGPL-3",
@@ -99,7 +76,8 @@ the fiscal experct adviced us it is right on that way.
         "account_voucher",
         "account_invoice_tax",
         "analytic",
-        "account_move_line_base_tax"
+        "account_move_line_base_tax",
+        "account_cancel",
     ],
     "demo": [
         "demo/account_voucher_tax_demo.xml"
