d2cf99967ba1f152ca1b302d2b4b6c903c5782eb
julioserna <julio@vauxoo.com>
2014-12-18 12:07:44 -0600

    [IMP][sale_order_customized] changed .format to %s by error of unicode

--- sale_order_customized/model/sale_order.py
+++ sale_order_customized/model/sale_order.py
@@ -56,7 +56,7 @@ class sale_order_line(osv.osv):
                 cr, uid, product, context=context)
             prod_category = product_id.categ_id and product_id.categ_id.name
             name_description = res.get('value', {}).pop('name', '')
-            name_description = '[{}]'.format(prod_category) + name_description
+            name_description =  '[%s]' % (prod_category) + name_description
             res.get('value', {}).update({'name': name_description})
         return res
 
