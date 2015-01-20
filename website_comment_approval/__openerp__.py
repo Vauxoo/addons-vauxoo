b4662993834179c0c844ce8509f44a29900e8d82
oalca <oszckar@gmail.com>
2015-01-20 12:40:00 -0600

    [INIT]

---
+++ website_comment_approval/__openerp__.py
@@ -0,0 +1,29 @@
+# -*- encoding: utf-8 -*-
+{
+    "name": "Website Comment Approval",
+    "version": "1.0",
+    "author": "Vauxoo",
+    "category": "Website",
+    "description": """
+Website Comment Approval
+========================
+    This App lets the user approve comments on the products published on the
+    website, it sets the value published to `False`.
+
+    """,
+    "website": "http://www.vauxoo.com/",
+    "license": "AGPL-3",
+    "depends": [
+        "website_sale"
+    ],
+    "demo": [],
+    "data": [
+    ],
+    "test": [],
+    "js": [],
+    "css": [],
+    "qweb": [],
+    "installable": True,
+    "auto_install": False,
+    "active": False
+}
