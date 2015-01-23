b134998356aee9abfcc60ab6dcc7618a4ab31a8a
Server Lodi <server@lodi.com>
2015-01-23 23:50:18 +0000

    [IMP] Apply new changes to work with child bom and update only the prices with difference

--- product_extended_variants/template.py
+++ product_extended_variants/template.py
@@ -26,6 +26,64 @@ from openerp.osv import osv
 class product_template(models.Model):
     _inherit = 'product.template'
 
+    def compute_price(self, cr, uid, product_ids, template_ids=False,
+                      recursive=False, test=False, real_time_accounting=False,
+                      context=None):
+        '''
+        Will return test dict when the test = False
+        Multiple ids at once?
+        testdict is used to inform the user about the changes to be made
+        '''
+        testdict = {}
+        real_time = real_time_accounting
+        if product_ids:
+            ids = product_ids
+            model = 'product.product'
+        else:
+            ids = template_ids
+            model = 'product.template'
+        for prod_id in ids:
+            bom_obj = self.pool.get('mrp.bom')
+            if model == 'product.product':
+                bom_id = bom_obj._bom_find(cr, uid,
+                                           product_id=prod_id, context=context)
+            else:
+                bom_id = bom_obj._bom_find(cr, uid, product_tmpl_id=prod_id,
+                                           context=context)
+            if bom_id:
+                # In recursive mode, it will first compute the prices of child
+                # boms
+                if recursive:
+                    # Search the products that are components of this bom of
+                    # prod_id
+                    bom = bom_obj.browse(cr, uid, bom_id, context=context)
+
+                    # Call compute_price on these subproducts
+                    prod_set = set([x.product_id.id for x in bom.bom_line_ids])
+                    res = self.\
+                        compute_price(cr, uid, product_ids=list(prod_set),
+                                      template_ids=[], recursive=recursive,
+                                      test=test,
+                                      real_time_accounting=real_time,
+                                      context=context)
+                    if test:
+                        testdict.update(res)
+                # Use calc price to calculate and put the price on the product
+                # of the BoM if necessary
+                price = self._calc_price(cr, uid,
+                                         bom_obj.browse(cr, uid,
+                                                        bom_id,
+                                                        context=context),
+                                         test=test,
+                                         real_time_accounting=real_time,
+                                         context=context)
+                if test:
+                    testdict.update({prod_id: price})
+        if test:
+            return testdict
+        else:
+            return True
+
     def do_change_standard_price(self, cr, uid, ids, new_price, context=None):
         """ Changes the Standard Price of Product and creates an account move
         accordingly."""
@@ -51,9 +109,7 @@ class product_template(models.Model):
 
                 diff = product.standard_price - new_price
                 if not diff:
-                    raise osv.except_osv(_('Error!'),
-                                         _("No difference between standard "
-                                           "price and new price!"))
+                    continue
                 for prod_variant in product.product_variant_ids:
                     qty = prod_variant.qty_available
                     if qty:
