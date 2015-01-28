4eda0c69d91db90f35a5dad238ede139df2a7667
Humberto Arocha <hbto@vauxoo.com>
2015-01-27 20:19:16 -0430

    [IMP] Report can be gotten in XLS

--- product_pricelist_report_qweb/controllers/__init__.py
+++ product_pricelist_report_qweb/controllers/__init__.py
@@ -2,11 +2,14 @@
 # -*- encoding: utf-8 -*-
 ###########################################################################
 #    Module Writen to OpenERP, Open Source Management Solution
-#    Copyright (C) Vauxoo (<http://vauxoo.com>).
+#    Copyright (C) Vauxoo (<http://www.vauxoo.com>).
 #    All Rights Reserved
-# #############Credits#########################################################
+# #############Credits######################################################
 #    Coded by: Humberto Arocha <hbto@vauxoo.com>
-###############################################################################
+#    Coded by: Humberto Arocha <hbto@vauxoo.com>
+#    Planified by: Humberto Arocha <hbto@vauxoo.com>
+#    Audited by: Humberto Arocha <hbto@vauxoo.com>
+#############################################################################
 #    This program is free software: you can redistribute it and/or modify it
 #    under the terms of the GNU Affero General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or (at your
@@ -20,6 +23,6 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ###############################################################################
-from . import wizard
-from . import report
-from . import controllers
+
+
+from . import main
