11281fc98e86fdf91b0033d01c2b301182ed5639
oalca <oszckar@gmail.com>
2015-01-30 12:40:00 -0600

    [INIT]

---
+++ website_rate_product/__openerp__.py
@@ -0,0 +1,31 @@
+# -*- encoding: utf-8 -*-
+{
+    "name": "Website Product Rate",
+    "version": "1.0",
+    "author": "Vauxoo",
+    "category": "Website",
+    "description": """
+Website Product Rate
+====================
+    This app shows you a widget with five stars to give product ratings on the
+    e-commerce platform.
+    """,
+    "website": "http://www.vauxoo.com/",
+    "license": "AGPL-3",
+    "depends": [
+        "website_sale"
+    ],
+    "demo": [],
+    "data": [
+        "website_rate_product/views/layout.xml",
+    ],
+    "test": [],
+    "js": [],
+    "css": [],
+    "qweb": [
+        "static/src/xml/rate.xml",
+             ],
+    "installable": True,
+    "auto_install": False,
+    "active": False
+}
