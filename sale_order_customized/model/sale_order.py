e66feb6c2616cbb1d98d168ac08641d03d838f5c
Luis Ernesto García Medina <ernesto_gm@vauxoo.com>
2015-01-30 16:58:32 -0600

    [FIX][sale_order_customized] problem is solved with empty description lines, to change the quantity.

--- sale_order_customized/model/sale_order.py
+++ sale_order_customized/model/sale_order.py
@@ -56,6 +56,8 @@ class sale_order_line(osv.osv):
                 cr, uid, product, context=context)
             prod_category = product_id.categ_id and product_id.categ_id.name
             name_description = res.get('value', {}).pop('name', '')
+            if not name_description:
+                return res
             name_description = '[%s]' % (prod_category) + name_description
             res.get('value', {}).update({'name': name_description})
         return res
