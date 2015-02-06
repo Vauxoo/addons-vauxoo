94e14b483d2ca1561e8a6fd64a49367ba1adcb11
oalca <oszckar@gmail.com>
2015-02-06 12:40:00 -0600

    [ADD] new module that creates a new field on the model product_product

---
+++ product_model_no/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- encoding: utf-8 -*-
+{
+    "name": "Product Model Number",
+    "version": "1.0",
+    "author": "Vauxoo",
+    "category": "Sales",
+    "description": """
+Product Model
+=============
+    This App adds the model_number field to 'product.template' model.
+
+    """,
+    "website": "http://www.vauxoo.com/",
+    "license": "AGPL-3",
+    "depends": [
+        "product",
+    ],
+    "demo": [],
+    "data": [
+        "views/product_view.xml"
+    ],
+    "test": [],
+    "qweb": [],
+    "installable": True,
+    "auto_install": False,
+    "active": False
+}
