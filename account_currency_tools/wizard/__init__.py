15f4330e6ccc72a63f9577c2f4fd66a1c0994c1b
Humberto Arocha <hbto@vauxoo.com>
2015-02-12 13:09:46 -0430

    [RENAMED]

--- account_currency_tools/wizard/__init__.py
+++ account_currency_tools/wizard/__init__.py
@@ -20,4 +20,4 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ###############################################################################
-from . import foreign_exchange_realization
+from . import exchange_realization
