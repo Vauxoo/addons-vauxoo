c2095afea75102685fd4ebd113d18026c7ff7ec5
Olar Alca [Vauxoo] <oszckar@gmail.com>
2015-02-20 11:57:26 -0600

    Update mail_rate.js
    [REM] Console.log

--- website_rate_product/static/src/js/mail_rate.js
+++ website_rate_product/static/src/js/mail_rate.js
@@ -2,7 +2,6 @@ openerp.website_rate_product = function(instance) {
     instance.mail.MessageCommon.include({
         init: function (parent, datasets, options){
             this.rating = datasets.rating || 0;
-            console.log(this);
             this._super.apply(this, arguments);
         }
     });
