6a9f3506f6da35d1da83b3d5b49b94bf5ba6ac7c
oalca <oszckar@gmail.com>
2015-02-20 13:29:33 -0600

    [IMP] wired view to show wether the customer bought the item or not, no calculation yet

---
+++ website_product_comment_purchased/__openerp__.py
@@ -0,0 +1,31 @@
+# -*- encoding: utf-8 -*-
+{
+    "name": "Website Comments With Purchase",
+    "version": "1.0",
+    "author": "Vauxoo",
+    "category": "Website",
+    "description": """
+Website Comments With Purchase
+==============================
+    This app shows you on the comments on website/e-commerce whether the
+    customer purchased the item reviewed or not.
+    """,
+    "website": "http://www.vauxoo.com/",
+    "license": "LGPL",
+    "depends": [
+        "website_sale"
+    ],
+    "demo": [],
+    "data": [
+        "views/layout.xml",
+        "views/website_comment_purchased.xml",
+    ],
+    "test": [],
+    "js": [],
+    "css": [],
+    "qweb": [
+    ],
+    "installable": True,
+    "auto_install": False,
+    "active": False
+}
---
+++ website_product_comment_purchased/models/mail.py
@@ -0,0 +1,44 @@
+# -*- coding:utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C)2010-  OpenERP SA (<http://openerp.com>). All Rights Reserved
+#    App Author: Vauxoo
+#
+#    Developed by Oscar Alcala <oszckar@gmail.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as
+#    published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp.osv import osv, fields
+
+
+class mail_message(osv.Model):
+    _inherit = 'mail.message'
+    _defaults = {
+        'comment_bought': False,
+    }
+    _columns = {
+        'comment_bought': fields.boolean('Comment Bought'),
+    }
+
+    def _message_read_dict(self, cr, uid, message, parent_id=False,
+                           context=None):
+        r = super(mail_message, self)._message_read_dict(cr, uid,
+                                                         message,
+                                                         parent_id=parent_id,
+                                                         context=context)
+        r['comment_bought'] = True
+        return r
