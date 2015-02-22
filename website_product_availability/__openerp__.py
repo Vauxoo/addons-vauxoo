7a21eff91e0a2e3a4fa9edc0214e165a32ebee24
oalca <oszckar@gmail.com>
2015-02-21 23:43:19 -0600

    [INIT]

---
+++ website_product_availability/__openerp__.py
@@ -0,0 +1,31 @@
+# -*- encoding: utf-8 -*-
+{
+    "name": "Website Product Availability",
+    "version": "1.0",
+    "author": "Vauxoo",
+    "category": "Website",
+    "description": """
+Website Product Availability
+==============================
+    This app shows you on the product template on the
+    website if a product is available, low Availability or
+    if the product is make to order.
+    """,
+    "website": "http://www.vauxoo.com/",
+    "depends": [
+        "website_sale"
+    ],
+    "demo": [],
+    "data": [
+        "views/layout.xml",
+        "views/templates.xml",
+    ],
+    "test": [],
+    "js": [],
+    "css": [],
+    "qweb": [
+    ],
+    "installable": True,
+    "auto_install": False,
+    "active": False
+}
