76fee1273167dea9c119a04df449bb82fa71ab42
Luis Ernesto García Medina <ernesto_gm@vauxoo.com>
2015-03-09 17:07:31 -0600

    [PYLINT][account_move_report] apply pylint

--- account_move_report/report/account_move_html.py
+++ account_move_report/report/account_move_html.py
@@ -3,12 +3,12 @@
 #    Module Writen to OpenERP, Open Source Management Solution
 #    Copyright (C) Vauxoo (<http://vauxoo.com>).
 #    All Rights Reserved
-###############Credits######################################################
+# ##############Credits######################################################
 #    Coded by: Luis Ernesto García (ernesto_gm@vauxoo.com)
 #############################################################################
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
@@ -18,7 +18,7 @@
 #
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-################################################################################
+###############################################################################
 from openerp.report import report_sxw
 import time
 from .webkit_parser_header_fix import HeaderFooterTextWebKitParser
@@ -40,11 +40,13 @@ class account_move_report_html(report_sxw.rml_parse):
         for line in line_ids:
             sum_tot_debit += (line.debit)
             sum_tot_credit += (line.credit)
-        return {'sum_tot_debit': sum_tot_debit, 'sum_tot_credit': sum_tot_credit}
+        return {
+            'sum_tot_debit': sum_tot_debit, 'sum_tot_credit': sum_tot_credit}
 
-HeaderFooterTextWebKitParser('report.account.move.report.webkit',
-                      'account.move',
-                      'addons/report_account_move/report/account_move_report_html.mako',
-                      parser=account_move_report_html)
+HeaderFooterTextWebKitParser(
+    'report.account.move.report.webkit',
+    'account.move',
+    'addons/report_account_move/report/account_move_report_html.mako',
+    parser=account_move_report_html)
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
