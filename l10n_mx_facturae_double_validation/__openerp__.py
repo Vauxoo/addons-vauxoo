1a795ffd7b3a1230021e105ca98fc537fd83ae59
Hugo Adan <hugo@vauxoo.com>
2015-03-17 12:32:27 -0600

    [FIX][facturae_double_validation] Add double invoice validation

---
+++ l10n_mx_facturae_double_validation/__openerp__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    'name': 'Add double account_invoice validation',
+    'version': '1.0',
+    'author': 'Vauxoo',
+    'category': '',
+    'depends': [
+        'base',
+        'account',
+    ],
+    'demo': [],
+    'website': 'https://www.vauxoo.com',
+    'description': """
+This module add  a field into account_invoice to show the
+Number of attachments by record in the tree view
+    """,
+    'data': [
+        #'views/account_invoice.xml',
+    ],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+    'images': [],
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
