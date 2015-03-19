62f3b7a3490c17d37f9a067946e9e552f7c41446
oalca <oszckar@gmail.com>
2015-03-18 23:13:47 -0600

    [FIX] removed files that were not part of the product owner HU

--- website_comment_approval/static/src/js/mail.js
+++
@@ -1,9 +0,0 @@
-openerp.website_comment_approval = function(instance) {
-    var mail = instance.mail;
-    instance.mail.MessageCommon.include({
-        init: function (parent, datasets, options){
-            this.website_published = datasets.website_published || false;
-            this._super.apply(this, arguments);
-        }
-    });
-};
