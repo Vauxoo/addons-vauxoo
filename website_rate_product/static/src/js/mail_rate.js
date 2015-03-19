036a89168ec7bdf1d671612b4411618c47e83f41
oalca <oszckar@gmail.com>
2015-03-18 23:15:29 -0600

    [CLEAN]

--- website_rate_product/static/src/js/mail_rate.js
+++
@@ -1,9 +0,0 @@
-openerp.website_rate_product = function(instance) {
-    instance.mail.MessageCommon.include({
-        init: function (parent, datasets, options){
-            this.rating = datasets.rating || 0;
-            console.log(this);
-            this._super.apply(this, arguments);
-        }
-    });
-};
