405f1e58492988c244ce39924d52334ae9151154
oalca <oszckar@gmail.com>
2015-03-23 11:12:26 -0600

    [INIT]

---
+++ website_product_manufacturer_url/__openerp__.py
@@ -0,0 +1,30 @@
+# -*- encoding: utf-8 -*-
+{
+    "name": "Website Product Manufacturer URL",
+    "version": "1.0",
+    "author": "Vauxoo",
+    "category": "eCommerce",
+    "description": """
+Website Product Manufacturer URL
+================================
+    This app shows you on the product url of the Manufacturer
+    of the product shown on the website.
+    """,
+    "website": "http://www.vauxoo.com/",
+    "depends": [
+        "purchase",
+        "website_sale",
+        "product_manufacturer",
+    ],
+    "demo": [],
+    "data": [
+    ],
+    "test": [],
+    "js": [],
+    "css": [],
+    "qweb": [
+    ],
+    "installable": True,
+    "auto_install": False,
+    "active": False
+}
