2726eb52bb1e9f9736d45903b948a558e2e271a1
oalca <oszckar@gmail.com>
2015-03-24 20:00:39 -0600

    [INIT] basic empty structure created

---
+++ website_authorized_service_centers/__openerp__.py
@@ -0,0 +1,31 @@
+{
+    'name': 'Autorized Service Centers',
+    'category': 'Website',
+    'summary': 'Show your trusted service centers',
+    'website': 'https://www.vauxoo.com/',
+    'version': '1.0',
+    'description': """
+Autorized Service Centers
+=========================
+    This App lets you create some Brands on the backend
+    and associate the given brands to partners marked as
+    service providers to be displayed on the 'Authorized
+    Service Centers' page.
+
+        """,
+    'author': 'Vauxoo',
+    'depends': [
+        'website',
+    ],
+    'data': [
+
+    ],
+    'demo': [
+
+    ],
+    'qweb': [
+
+    ],
+    'installable': True,
+    'application': True,
+}
