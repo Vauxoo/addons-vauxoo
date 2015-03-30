2517a65672bf1c66948bf82fdc150eefa946c831
luistorresm <luis_t@vauxoo.com>
2015-03-30 11:08:57 -0600

    [REF][account_tax_importation]Better sintax in common file

--- account_tax_importation/tests/common.py
+++ account_tax_importation/tests/common.py
@@ -14,38 +14,25 @@ class TestTaxCommon(common.TransactionCase):
         self.acc_bank_stmt_model = self.registry('account.bank.statement')
         self.acc_bank_stmt_line_model = self.registry(
             'account.bank.statement.line')
-        _, self.account_vou = imd_model.get_object_reference(
-            cr, uid, "account", "cash")
-        _, self.account_inv = imd_model.get_object_reference(
-            cr, uid, "account", "a_pay")
-        _, self.partner_id = imd_model.get_object_reference(
-            cr, uid, "account_tax_importation",
-            "res_partner_supplier_broker")
-        _, self.company_id = imd_model.get_object_reference(
-            cr, uid, "base", "main_company")
-        _, self.journal_inv_id = imd_model.get_object_reference(
-            cr, uid, "account", "expenses_journal")
-        _, self.journal_vou_id = imd_model.get_object_reference(
-            cr, uid, "account", "bank_journal")
-        _, self.acc_line_id = imd_model.get_object_reference(
-            cr, uid, "account", "a_expense")
-        _, self.prod_line1 = imd_model.get_object_reference(
-            cr, uid, "account_tax_importation",
-            "product_product_broker_payment")
-        _, self.prod_line2 = imd_model.get_object_reference(
-            cr, uid, "product", "product_product_39")
-        _, self.tax_brok16_id = imd_model.get_object_reference(
-            cr, uid, "account_tax_importation",
-            "account_tax_purchase_iva16_broker")
-        _, self.tax_16_id = imd_model.get_object_reference(
-            cr, uid, "account_voucher_tax",
-            "account_voucher_tax_purchase_iva16")
-        _, self.tax_inv_2_id = imd_model.get_object_reference(
-            cr, uid, "account", "test_invoice_1")
-        _, self.account_receivable_id = imd_model.get_object_reference(
-            cr, uid, "account", "a_recv")
-        _, self.account_voucher_tax_16 = imd_model.get_object_reference(
-            cr, uid, "account_voucher_tax", "account_iva_voucher_16")
+        self.account_vou = self.ref("account.cash")
+        self.account_inv = self.ref("account.a_pay")
+        self.partner_id = self.ref(
+            "account_tax_importation.res_partner_supplier_broker")
+        self.company_id = self.ref("base.main_company")
+        self.journal_inv_id = self.ref("account.expenses_journal")
+        self.journal_vou_id = self.ref("account.bank_journal")
+        self.acc_line_id = self.ref("account.a_expense")
+        self.prod_line1 = self.ref(
+            "account_tax_importation.product_product_broker_payment")
+        self.prod_line2 = self.ref("product.product_product_39")
+        self.tax_brok16_id = self.ref(
+            "account_tax_importation.account_tax_purchase_iva16_broker")
+        self.tax_16_id = self.ref(
+            "account_voucher_tax.account_voucher_tax_purchase_iva16")
+        self.tax_inv_2_id = self.ref("account.test_invoice_1")
+        self.account_receivable_id = self.ref("account.a_recv")
+        self.account_voucher_tax_16 = self.ref(
+            "account_voucher_tax.account_iva_voucher_16")
 
     def create_statement(self, cr, uid, line_invoice, partner, amount, journal,
                          date_bank=None):
