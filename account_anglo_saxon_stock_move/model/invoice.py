eb4fa770e7298245f439659a25854bbe3c192c6e
Humberto Arocha <hbto@vauxoo.com>
2015-03-31 11:35:48 -0430

    [IMP] Added method to include sm_id into account_move_line from invoice

--- account_anglo_saxon_stock_move/model/invoice.py
+++ account_anglo_saxon_stock_move/model/invoice.py
@@ -20,3 +20,47 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+
+from openerp.osv import osv
+
+
+class account_invoice_line(osv.osv):
+    _inherit = "account.invoice.line"
+
+    def _anglo_saxon_purchase_stock_move_lines(self, cr, uid, res,
+                                               context=None):
+        ail_obj = self.pool.get('account.invoice.line')
+        fp_obj = self.pool.get('account.invoice.line')
+        rex = []
+        for line in res:
+            if line.get('invl_id', False):
+                ail_brw = ail_obj.browse(cr, uid, line.get('invl_id'), context=context)
+                pp_brw = ail_brw.product_id
+                if pp_brw and pp_brw.valuation == 'real_time' and \
+                        pp_brw.type != 'service':
+                    a = None
+                    oa = pp_brw.property_stock_account_input and \
+                        pp_brw.property_stock_account_input.id or \
+                        pp_brw.categ_id.property_stock_account_input_categ and\
+                        pp_brw.categ_id.property_stock_account_input_categ.id
+                    if oa:
+                        fpos = ail_brw.invoice_id.fiscal_position or False
+                        a = fp_obj.map_account(cr, uid, fpos, oa)
+                    if a == line['account_id'] and ail_brw.move_id:
+                        line['sm_id'] = ail_brw.move_id.id
+            rex.append(line)
+        return rex
+
+    def move_line_get(self, cr, uid, invoice_id, context=None):
+        res = super(account_invoice_line,
+                    self).move_line_get(cr, uid, invoice_id, context=context)
+        inv = self.pool.get('account.invoice').browse(
+            cr, uid, invoice_id, context=context)
+        if inv.type in ('out_invoice', 'out_refund'):
+            res = self._anglo_saxon_sale_stock_move_lines(
+                cr, uid, res, context=context)
+        elif inv.type in ('in_invoice', 'in_refund'):
+            res = self._anglo_saxon_purchase_stock_move_lines(
+                cr, uid, res, context=context)
+        return res
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
