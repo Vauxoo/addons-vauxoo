e45a6ae96d25b09be484124e2d10e8f046317959
oalca <oszckar@gmail.com>
2015-04-09 12:40:00 +0000

    [ADD] Basic Empty Structure of the module

---
+++ website_blog_rss/__openerp__.py
@@ -0,0 +1,26 @@
+{
+    'name': 'Website Blog RSS',
+    'category': 'website',
+    'summary': 'This module enables RSS on Blog',
+    'website': 'http://vauxoo.com',
+    'version': '1.0',
+    'description': """
+        This App lets you create a RSS feed on populated by the
+        blog posts stored on the database.
+        """,
+    'author': 'Vauxoo',
+    'depends': [
+        'website_blog',
+    ],
+    'data': [
+
+    ],
+    'demo': [
+
+    ],
+    'qweb': [
+
+    ],
+    'installable': True,
+    'application': True,
+}
