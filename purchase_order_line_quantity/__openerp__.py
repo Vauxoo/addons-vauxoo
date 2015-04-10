6a073edd2dc4b2a53f03e5add47c213c55c55b9d
Humberto Arocha <hbto@vauxoo.com>
2015-04-10 00:19:18 -0430

    [IMP] Added new module to compute how much of a line has already been delivered & invoiced

---
+++ purchase_order_line_quantity/__openerp__.py
@@ -0,0 +1,52 @@
+# -*- encoding: utf-8 -*-
+###############################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#    Copyright (C) OpenERP Venezuela (<http://www.vauxoo.com>).
+#    All Rights Reserved
+############# Credits #########################################################
+#    Coded by: Yanina Aular <yanina.aular@vauxoo.com>
+#    Planified by: Humberto Arocha <hbto@vauxoo.com>
+#    Audited by: Humberto Arocha <hbto@vauxoo.com>
+###############################################################################
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+###############################################################################
+{
+    "name": "Purchase Order Line Quantities",
+    "version": "1.0",
+    "author": "Vauxoo",
+    "category": "purchase",
+    "description": """
+Purchase Order Line Quantities
+==============================
+
+    Adds two new fields in Purchase Order Line that compute how much of a line
+    has already been delivery or how much has already been invoiced
+""",
+    "website": "http://www.vauxoo.com/",
+    "license": "",
+    "depends": [
+        "purchase",
+    ],
+    "demo": [],
+    "data": [
+        "view/purchase_order_view.xml"
+    ],
+    "test": [],
+    "js": [],
+    "css": [],
+    "qweb": [],
+    "installable": True,
+    "auto_install": False,
+    "active": False
+}
