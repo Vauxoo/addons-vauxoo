bc923b5c1202fa5f7e16fc13b96cb7e08e2b02cf
Humberto Arocha <hbto@vauxoo.com>
2015-04-20 15:51:42 -0430

    [IMP] Added new model to house line that will print report

--- stock_accrual_report/wizard/wizard.py
+++ stock_accrual_report/wizard/wizard.py
@@ -22,10 +22,64 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ###############################################################################
 from openerp.osv import osv, fields
+import openerp.addons.decimal_precision as dp
+
 SELECTION_TYPE = [
     ('sale', 'Sale Order'),
     ('purchase', 'Purchase Order'),
 ]
+
+SELECT_ORDER = [
+    ('sale.order', 'Sale Order'),
+    ('purchase.order', 'Purchase Order'),
+]
+
+
+SELECT_LINE = [
+    ('sale.order.line', 'Sale Order Line'),
+    ('purchase.order.line', 'Purchase Order Line'),
+]
+
+
+class stock_accrual_wizard_line(osv.osv_memory):
+    _name = 'stock.accrual.wizard.line'
+    _columns = {
+        'wzd_id': fields.many2one(
+            'stock.accrual.wizard',
+            'Wizard'),
+        'order_id': fields.reference(
+            'Order',
+            selection=SELECT_ORDER,
+            size=128),
+        'line_id': fields.reference(
+            'Order Line',
+            selection=SELECT_LINE,
+            size=128),
+        'product_qty': fields.float(
+            'Quantity',
+            digits_compute=dp.get_precision('Product UoS'),
+        ),
+        'product_uom': fields.many2one(
+            'product.uom',
+            'Unit of Measure ',
+        ),
+        'qty_delivered': fields.float(
+            'Delivered Quantity',
+            digits_compute=dp.get_precision('Product UoS'),
+        ),
+        'qty_invoiced': fields.float(
+            'Invoiced Quantity',
+            digits_compute=dp.get_precision('Product UoS'),
+        ),
+        'debit': fields.float(
+            'Debit',
+            digits_compute=dp.get_precision('Account')
+        ),
+        'credit': fields.float(
+            'Credit',
+            digits_compute=dp.get_precision('Account')
+        ),
+    }
 
 
 class stock_accrual_wizard(osv.osv_memory):
@@ -35,7 +89,18 @@ class stock_accrual_wizard(osv.osv_memory):
         'type': fields.selection(
             SELECTION_TYPE,
             string='Type',
-            required=False,
+            required=True
+        ),
+        'report_format': fields.selection([
+            ('pdf', 'PDF'),
+            ('xls', 'Spreadsheet')],
+            'Report Format',
+            required=True
+        ),
+        'line_ids': fields.one2many(
+            'stock.accrual.wizard.line',
+            'wzd_id',
+            'Lines',
         )
     }
 
