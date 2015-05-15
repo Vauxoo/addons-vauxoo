398c76e9af353184e3b645ca6bb58d95d69670c0
JulioSerna <julio@vauxoo.com>
2015-05-15 13:19:12 -0500

    [IMP][account_voucher_tax] added test to customer when currency invoice is in secondary currency and payment in company currency

--- account_voucher_tax/tests/test_bank_statement_customer.py
+++ account_voucher_tax/tests/test_bank_statement_customer.py
@@ -263,3 +263,84 @@ class TestPaymentTaxCustomer(TestTaxCommon):
                 continue
 
         self.assertEquals(checked_line, 4)
+
+    def test_iva_16_customer_currency_different_USD(self):
+        """
+            Tests Customer with currency USD and tax IVA 16% and
+            payment with currency of company EUR
+            and specific rate in statement line
+        """
+        cr, uid = self.cr, self.uid
+        invoice_id = self.account_invoice_model.create(cr, uid, {
+            'partner_id': self.partner_agrolait_id,
+            'journal_id': self.invoice_journal_id,
+            'reference_type': 'none',
+            'name': 'invoice to customer',
+            'account_id': self.account_receivable_id,
+            'type': 'out_invoice',
+            'date_invoice': time.strftime('%Y')+'-06-01',
+            'currency_id': self.currency_usd_id
+            })
+        self.account_invoice_line_model.create(cr, uid, {
+            'product_id': self.product_id,
+            'quantity': 1,
+            'price_unit': 100,
+            'invoice_line_tax_id': [(6, 0, [self.tax_16_customer])],
+            'invoice_id': invoice_id,
+            'name': 'product that cost 100'})
+
+        # validate invoice
+        self.registry('account.invoice').signal_workflow(
+            cr, uid, [invoice_id], 'invoice_open')
+        invoice_record = self.account_invoice_model.browse(
+            cr, uid, [invoice_id])
+
+        # we search aml with account payable
+        for line_invoice in invoice_record.move_id.line_id:
+            if line_invoice.account_id.id == self.account_receivable_id:
+                line_id = line_invoice
+                break
+
+        # create payment complete
+        move_line_ids = self.create_statement(
+            cr, uid, line_id, self.partner_agrolait_id, 100,
+            self.bank_journal_id, time.strftime('%Y')+'-06-01',
+            currency=self.currency_usd_id, amount_currency=116)
+
+        checked_line = 0
+        for move_line_complete in move_line_ids:
+            if move_line_complete.account_id.id ==\
+                    self.acc_tax16_customer and\
+                    move_line_complete.amount_currency:
+                self.assertEquals(round(move_line_complete.debit, 2), 13.79)
+                self.assertEquals(move_line_complete.credit, 0.0)
+                self.assertEquals(move_line_complete.amount_residual, 0)
+                self.assertEquals(move_line_complete.amount_currency, 16)
+                self.assertTrue(move_line_complete.reconcile_id,
+                                "Reconcile should be created.")
+                checked_line += 1
+                continue
+            if move_line_complete.account_id.id ==\
+                    self.acc_tax_16_payment_customer:
+                self.assertEquals(move_line_complete.debit, 0.0)
+                self.assertEquals(round(move_line_complete.credit, 2), 13.79)
+                self.assertEquals(move_line_complete.amount_currency, -16.0)
+                checked_line += 1
+                continue
+            if move_line_complete.account_id.id == self.acc_tax16_customer:
+                self.assertEquals(move_line_complete.debit, 0.0)
+                self.assertEquals(move_line_complete.credit, 1.32)
+                self.assertEquals(move_line_complete.amount_residual, 0)
+                self.assertEquals(move_line_complete.amount_currency, 0)
+                self.assertTrue(move_line_complete.reconcile_id,
+                                "Partial Reconcile should be created.")
+                checked_line += 1
+                continue
+            if move_line_complete.account_id.id == self.acc_loss_tax:
+                self.assertEquals(move_line_complete.debit, 1.32)
+                self.assertEquals(move_line_complete.credit, 0.0)
+                self.assertEquals(move_line_complete.amount_currency, 0)
+                checked_line += 1
+                continue
+
+        self.assertEquals(checked_line, 4)
