b26f6d7a091862e5cf8080a2cba159c53bfdc3da
JulioSerna <julio@vauxoo.com>
2015-05-21 19:57:40 -0500

    [IMP][acount_voucher_tax] added test to customer to validate journal entry when advance payment is reconciled with invoice with voucher with difference currency

--- account_voucher_tax/tests/test_bank_statement_cash_flow.py
+++ account_voucher_tax/tests/test_bank_statement_cash_flow.py
@@ -297,7 +297,9 @@ class TestCashFlowTaxCustomer(TestTaxCommon):
     def test_cf_iva_16_customer_eur(self):
         """
             Test customer advance with payment in currency company EUR
-            and specific rate in currency secondary USD
+                and specific rate in currency secondary USD
+            Invoice with secondary currency USD
+            Reconcile with special journal with secondary currency USD
         """
 
         cr, uid = self.cr, self.uid
@@ -321,7 +323,7 @@ class TestCashFlowTaxCustomer(TestTaxCommon):
                 self.assertEquals(move_line.amount_currency, 20.69)
                 self.assertEquals(
                     move_line.currency_id.id, self.currency_usd_id)
-                # move_line_tax_rec_id = move_line
+                move_line_tax_rec_id = move_line
                 checked_line += 1
                 continue
             if move_line.account_id.id == self.acc_tax_16_payment_customer:
@@ -333,11 +335,129 @@ class TestCashFlowTaxCustomer(TestTaxCommon):
                 checked_line += 1
                 continue
             if move_line.account_id.id == self.account_receivable_id:
-                # move_line_rec_id = move_line
+                move_line_rec_id = move_line
                 checked_line += 1
                 continue
 
         self.assertEquals(checked_line, 3)
+
+        # create invoice
+        invoice_id = self.account_invoice_model.create(cr, uid, {
+            'partner_id': self.partner_agrolait_id,
+            'journal_id': self.invoice_journal_id,
+            'reference_type': 'none',
+            'name': 'invoice currency USD to customer',
+            'account_id': self.account_receivable_id,
+            'type': 'out_invoice',
+            'date_invoice': time.strftime('%Y')+'-06-01',
+            'currency_id': self.currency_usd_id
+            })
+        self.account_invoice_line_model.create(cr, uid, {
+            'product_id': self.product_id,
+            'quantity': 1,
+            'price_unit': 131,
+            'invoice_line_tax_id': [(6, 0, [self.tax_16_customer])],
+            'invoice_id': invoice_id,
+            'name': 'product that cost 100'})
+
+        self.registry('account.invoice').signal_workflow(
+            cr, uid, [invoice_id], 'invoice_open')
+        invoice_record = self.account_invoice_model.browse(
+            cr, uid, [invoice_id])
+
+        for line_invoice in invoice_record.move_id.line_id:
+            if line_invoice.account_id.id == self.account_receivable_id:
+                move_line_inv_id = line_invoice.id
+                continue
+            if line_invoice.account_id.id == self.acc_tax16_customer:
+                move_line_inv_tax_rec_id = line_invoice
+
+        # create reconciliation with account voucher
+        voucher_id = self.account_voucher_model.create(cr, uid, {
+            'partner_id': self.partner_agrolait_id,
+            'journal_id': self.journal_bank_special_usd,
+            'account_id': self.account_bnk_id,
+            'date': time.strftime('%Y')+'-06-01',
+            'type': 'sale',
+            'amount': 0,
+            'payment_rate_currency_id': self.currency_usd_id,
+            })
+        self.account_voucher_line_model.create(cr, uid, {
+            'voucher_id': voucher_id,
+            'type': 'dr',
+            'move_line_id': move_line_rec_id.id,
+            'partner_id': self.partner_agrolait_id,
+            'account_id': self.account_receivable_id,
+            'amount': 150
+            })
+        self.account_voucher_line_model.create(cr, uid, {
+            'voucher_id': voucher_id,
+            'type': 'cr',
+            'move_line_id': move_line_inv_id,
+            'partner_id': self.partner_agrolait_id,
+            'account_id': self.account_receivable_id,
+            'amount': 151.96
+            })
+
+        self.account_voucher_model.proforma_voucher(cr, uid, voucher_id, {})
+
+        self.assertEquals(
+            len(self.account_voucher_model.browse(
+                cr, uid, voucher_id).move_id.line_id), 11)
+        checked_line = 0
+        for voucher_line in self.account_voucher_model.browse(
+                cr, uid, voucher_id).move_id.line_id:
+            if voucher_line.account_id.id ==\
+                    self.acc_tax16_customer and voucher_line.credit:
+                self.assertEquals(voucher_line.debit, 0.0)
+                self.assertEquals(voucher_line.credit, 0.21)
+                self.assertEquals(voucher_line.amount_currency, -0.27)
+                self.assertEquals(
+                    voucher_line.currency_id.id, self.currency_usd_id)
+                checked_line += 1
+                continue
+            if voucher_line.account_id.id ==\
+                    self.acc_provision_tax_16_customer and\
+                    voucher_line.amount_currency:
+                self.assertEquals(voucher_line.debit, 0.0)
+                self.assertEquals(voucher_line.credit, 16.12)
+                self.assertEquals(voucher_line.amount_residual, 0.0)
+                self.assertEquals(voucher_line.amount_currency, -20.69)
+                self.assertEquals(
+                    voucher_line.currency_id.id, self.currency_usd_id)
+                self.assertEquals(
+                    voucher_line.reconcile_id.name,
+                    move_line_tax_rec_id.reconcile_id.name)
+                checked_line += 1
+                continue
+            if voucher_line.account_id.id ==\
+                    self.acc_provision_tax_16_customer and\
+                    not voucher_line.amount_currency:
+                self.assertEquals(voucher_line.debit, 0.12)
+                self.assertEquals(voucher_line.credit, 0.0)
+                self.assertEquals(voucher_line.amount_residual, 0.0)
+                self.assertEquals(
+                    voucher_line.currency_id.id, self.currency_usd_id)
+                self.assertEquals(
+                    voucher_line.reconcile_id.name,
+                    move_line_tax_rec_id.reconcile_id.name)
+                checked_line += 1
+                continue
+            if voucher_line.account_id.id == self.acc_tax16_customer and\
+                    voucher_line.amount_currency and voucher_line.debit:
+                self.assertEquals(round(voucher_line.debit, 2), 16.33)
+                self.assertEquals(voucher_line.credit, 0.0)
+                self.assertEquals(voucher_line.amount_residual, 0.0)
+                self.assertEquals(voucher_line.amount_currency, 20.96)
+                self.assertEquals(
+                    voucher_line.currency_id.id, self.currency_usd_id)
+                self.assertEquals(
+                    voucher_line.reconcile_id.name,
+                    move_line_inv_tax_rec_id.reconcile_id.name)
+                checked_line += 1
+                continue
+
+        self.assertEquals(checked_line, 4)
 
     def test_cf_iva_16_customer_usd(self):
         """
