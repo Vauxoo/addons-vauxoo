60a9eb7c6060fce2281514cec6790844bcaa13c4
Hugo Adan <hugo@vauxoo.com>
2015-05-29 19:37:12 -0500

    [REF][supply_chain_settings]

--- supply_chain_settings/__init__.py
+++ supply_chain_settings/__init__.py
@@ -22,4 +22,4 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from . import models
+from . import model
