dc80949932c64e7f227897c0f4ca94a9a9b2b4c5
Katherine Zaoral <kathy@vauxoo.com>
2015-06-02 10:52:02 -0500

    [ADD] add category to the new module.

--- purchase_incoming_qty/__openerp__.py
+++ purchase_incoming_qty/__openerp__.py
@@ -29,7 +29,7 @@
     "version": "1.0",
     "author": "Vauxoo",
     "website": "http://www.vauxoo.com/",
-    "category": "",
+    "category": "Inventory Control",
     "depends": [
         "stock",
     ],
