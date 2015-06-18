39008f8132fdfdde8ed133eba6368215aba3167e
Katherine Zaoral <kathy@vauxoo.com>
2015-06-17 23:04:38 -0500

    [FIX] resolve problem with the unit test. The test was not doing a rollback to the end of the test so when trying to run the test after the first time the database was corrupted and the test return errors. Change the schema of the test from a SingleTransactionCase to a TransactionCase. Now every test method is separate. Some changes to modularizate the test were made to fullfile the invidivuality per test method. Also the not functional commented test were uncommented and are working propertly.

--- product_lifecycle/tests/test_product.py
+++ product_lifecycle/tests/test_product.py
@@ -28,7 +28,7 @@ from openerp import exceptions
 import time
 
 
-class TestProductLifecycle(common.SingleTransactionCase):
+class TestProductLifecycle(common.TransactionCase):
 
     """
     - CRUD Tests over product.product
@@ -87,31 +87,39 @@ class TestProductLifecycle(common.SingleTransactionCase):
     """
 
     def setUp(self):
-        """
-        just in case need to use it
-        """
+        """ Add global varaible for the model and xml id record used  """
         super(TestProductLifecycle, self).setUp()
-        self.sellable_product = self.browse_ref('product.product_product_4c')
-        self.obsolete_product = self.browse_ref('product.product_product_4b')
-        self.draft_product = self.browse_ref('product.product_product_4')
-        self.sellable_replacement = self.browse_ref(
+        self.sellable_product = self.ref('product.product_product_4c')
+        self.obsolete_product = self.ref('product.product_product_4b')
+        self.draft_product = self.ref('product.product_product_4')
+        self.sellable_replacement = self.ref(
             'product_lifecycle.product_product_4g')
-        self.obsolete_replacement = self.browse_ref(
+        self.obsolete_replacement = self.ref(
             'product_lifecycle.product_product_4f')
+        self.product_obj = self.env['product.product']
+        self.order_obj = self.env['purchase.order']
+        self.imd_obj = self.env['ir.model.data']
+        self.wiz_obj = self.env['replacement.product']
 
-    def create_xml(self, name, model, res_id):
-        """
-        create a xml for the record.
-        """
-        ir_model = self.env['ir.model.data']
-        ir_model.create({
-            'name': name, 'model': model, 'res_id': res_id,
-            'module': 'product_lifecycle'})
+    def create_product(self):
+        """ Create a new product """
+        product = self.product_obj.create({
+            "default_code": 'A2330',
+            "product_tmpl_id":
+            self.ref("product.product_product_4_product_template"),
+            "attribute_value_ids": [(6, 0, [
+                self.ref('product.product_attribute_value_1'),
+                self.ref('product_lifecycle.product_attribute_value_6'),
+                self.ref('product.product_attribute_value_5')])],
+            "replacement_product_ids": [(
+                6, 0, [self.ref('product_lifecycle.product_product_4e')]
+            )]})
+        return product
 
     def create_pol(self, order, product):
         """
         Create a new purchase order line for the given purchase order taking
-        as input only the product.
+        as input only the product
         """
         order.write({
             'order_line': [(0, 0, {
@@ -124,26 +132,23 @@ class TestProductLifecycle(common.SingleTransactionCase):
                 'date_planned': time.strftime('%Y-%m-%d')
             })]})
 
+    def create_po(self):
+        """ Create a purchase order """
+        pricelist_id = 1
+        partner_id = self.ref('base.res_partner_1')
+        order = self.order_obj.create({
+            'partner_id': partner_id,
+            'location_id': self.ref('stock.stock_location_stock'),
+            'pricelist_id': pricelist_id})
+        return order
+
     def test_01_product_create(self):
         """
-        Create a new product and check it was correctly created with the
-        default product lifecycle state and the replacements products were
-        link.
+        Create a new product and check it was correctly with the default
+        product lifecycle state and the replacements products were link.
         """
         # Create new product with a replacement product
-        product = self.env['product.product']
-        product = product.create({
-            "default_code": 'A2330',
-            "product_tmpl_id":
-            self.ref("product.product_product_4_product_template"),
-            "attribute_value_ids": [(6, 0, [
-                self.ref('product.product_attribute_value_1'),
-                self.ref('product_lifecycle.product_attribute_value_6'),
-                self.ref('product.product_attribute_value_5')])],
-            "replacement_product_ids": [(
-                6, 0, [self.ref('product_lifecycle.product_product_4e')]
-            )]})
-        self.create_xml('product_product_4h', product._name, product.id)
+        product = self.create_product()
 
         # Check recently was created product with default 'In Development'
         # value state and that the replacement was assigned. This case also
@@ -160,23 +165,18 @@ class TestProductLifecycle(common.SingleTransactionCase):
         Update the product status. Get an already exist product and update its
         product lifecifle status from draft to sellable.
         """
-        # TODO Comment testing related to demo data because changes made have
-        # been commited to the database. This generate a unit test error when
-        # running test a second time. This was report to odoo. after is fixes
-        # uncomment the section of code.
-
         # Update new product state2 from default draft to sellable
-        new_product = self.browse_ref('product_lifecycle.product_product_4h')
+        new_product = self.create_product()
         self.assertEqual(new_product.state2, 'draft')
         new_product.state2 = 'sellable'
         self.assertEqual(new_product.state2, 'sellable')
 
-        # # Same but to an existing demo product.
-        # demo_product = self.browse_ref(
-        #       'product_lifecycle.product_product_4g')
-        # self.assertEqual(demo_product.state2, 'sellable')
-        # demo_product.state2 = 'draft'
-        # self.assertEqual(demo_product.state2, 'draft')
+        # Same but to an existing demo product.
+        demo_product = self.product_obj.browse(
+            self.ref('product_lifecycle.product_product_4g'))
+        self.assertEqual(demo_product.state2, 'sellable')
+        demo_product.state2 = 'draft'
+        self.assertEqual(demo_product.state2, 'draft')
 
         # Update new product invividual field (field defined in product.product
         # model).
@@ -184,28 +184,27 @@ class TestProductLifecycle(common.SingleTransactionCase):
         new_product.default_code = 'A2330-1'
         self.assertEqual(new_product.default_code, 'A2330-1')
 
-        # # Same but to an existing demo product.
-        # self.assertEqual(demo_product.default_code, 'A2329')
-        # demo_product.default_code = 'A2329-1'
-        # self.assertEqual(demo_product.default_code, 'A2329-1')
+        # Same but to an existing demo product.
+        self.assertEqual(demo_product.default_code, 'A2329')
+        demo_product.default_code = 'A2329-1'
+        self.assertEqual(demo_product.default_code, 'A2329-1')
 
-        # # Update new product commom characteristic (field defined in
-        # # product.template) and check that affects the another product
-        # # variants
-        # self.assertFalse(new_product.description)
-        # new_product.description = 'This is a New Product'
-        # self.assertEqual(new_product.description, 'This is a New Product')
-        # self.assertEqual(demo_product.description, 'This is a New Product')
-        # demo_product.description = False
-        # self.assertFalse(demo_product.description)
+        # Update new product commom characteristic (field defined in
+        # product.template) and check that affects the another product
+        # variants
+        self.assertFalse(new_product.description)
+        new_product.description = 'This is a New Product'
+        self.assertEqual(new_product.description, 'This is a New Product')
+        self.assertEqual(demo_product.description, 'This is a New Product')
+        demo_product.description = False
+        self.assertFalse(demo_product.description)
 
     def test_03_product_delete(self):
         """
-        Now we are going to delete the recently created xml id
-        'product_lifecycle.product_product_4h' record.
+        Now we are going to delete a recently created product record.
         """
-        product = self.browse_ref('product_lifecycle.product_product_4h')
-        products = self.env['product.product'].search([])
+        product = self.create_product()
+        products = self.product_obj.search([])
         self.assertIn(product, products)
         product.unlink()
         self.assertNotIn(product.exists(), products)
@@ -214,7 +213,7 @@ class TestProductLifecycle(common.SingleTransactionCase):
         """
         Duplicate product record.
         """
-        new_product = self.obsolete_product.copy()
+        new_product = self.product_obj.browse(self.obsolete_product).copy()
         self.assertTrue(new_product)
         self.assertEqual(new_product.state2, 'draft')
 
@@ -224,36 +223,30 @@ class TestProductLifecycle(common.SingleTransactionCase):
         """
         # Create purchase Order and check purchase order was created correctly
         # (without lines)
-        order = self.env['purchase.order']
-        pricelist_id = 1
-        partner_id = self.ref('base.res_partner_1')
-        order = order.create({
-            'partner_id': partner_id,
-            'location_id': self.ref('stock.stock_location_stock'),
-            'pricelist_id': pricelist_id})
+        order = self.create_po()
         self.assertTrue(order)
         self.assertFalse(order.order_line)
-        self.create_xml('test_purchase_order', order._name, order.id)
 
         # Add one sellable line (first line)
-        self.create_pol(order, self.sellable_product)
+        sellable_product = self.product_obj.browse(self.sellable_product)
+        self.create_pol(order, sellable_product)
         self.assertTrue(order.order_line)
         self.assertEquals(len(order.order_line), 1)
-        self.assertIn(self.sellable_product,
-                      order.order_line.mapped('product_id'))
+        self.assertIn(sellable_product, order.order_line.mapped('product_id'))
         self.assertEquals(order.order_line.product_id.state2, 'sellable')
 
         # Add one draft line (second line)
-        self.create_pol(order, self.draft_product)
+        draft_product = self.product_obj.browse(self.draft_product)
+        self.create_pol(order, draft_product)
         self.assertEquals(len(order.order_line), 2)
-        self.assertIn(self.draft_product,
-                      order.order_line.mapped('product_id'))
+        self.assertIn(draft_product, order.order_line.mapped('product_id'))
         self.assertEquals(set(order.order_line.mapped('product_id.state2')),
                           set(['sellable', 'draft']))
 
         # Add one obsolete line. This will raise an exception.
+        obsolete_product = self.product_obj.browse(self.obsolete_product)
         with self.assertRaises(exceptions.Warning):
-            self.create_pol(order, self.obsolete_product)
+            self.create_pol(order, obsolete_product)
 
     def test_06_replacement_product_wizard(self):
         """
@@ -268,32 +261,34 @@ class TestProductLifecycle(common.SingleTransactionCase):
          (obsolete,           [sellable, obsolete])
          (product_product_4c, [product_product_4g product_product_4f])
         """
-        # Check if the purchase order have lines with discontiued products.
-        order = self.browse_ref('product_lifecycle.test_purchase_order')
+        # Create a purchase order with two lines.
+        order = self.create_po()
+        sellable_product = self.product_obj.browse(self.sellable_product)
+        draft_product = self.product_obj.browse(self.draft_product)
+        self.create_pol(order, sellable_product)
+        self.create_pol(order, draft_product)
         self.assertNotIn('obsolete',
                          order.order_line.mapped('product_id.state2'))
 
         # Update sellable product to obsolete
         # NOTE: This check check the write() method of the product.product
         # record.
-        self.assertIn(self.sellable_product,
-                      order.order_line.mapped('product_id'))
-        self.assertEqual(self.sellable_product.state2, 'sellable')
-        self.sellable_product.state2 = 'obsolete'
-        self.assertEqual(self.sellable_product.state2, 'obsolete')
+        self.assertIn(sellable_product, order.order_line.mapped('product_id'))
+        self.assertEqual(sellable_product.state2, 'sellable')
+        sellable_product.state2 = 'obsolete'
+        self.assertEqual(sellable_product.state2, 'obsolete')
 
         # Check that the purchase order line now have a obsolete line.
         obsolete_order_line = order.order_line.filtered(
             lambda line: line.product_id.state2 == 'obsolete')
         self.assertTrue(obsolete_order_line)
-        self.assertEqual(obsolete_order_line.product_id, self.sellable_product)
+        self.assertEqual(obsolete_order_line.product_id, sellable_product)
 
         # Simulate click on the "Check Discontinued Products" button to run the
         # replacemenet product wizard.
-        wiz = self.env['replacement.product']
-        wiz = wiz.with_context({
-            'active_id': self.ref('product_lifecycle.test_purchase_order'),
-            'active_ids': [self.ref('product_lifecycle.test_purchase_order')],
+        wiz = self.wiz_obj.with_context({
+            'active_id': order.id,
+            'active_ids': [order.id],
             'active_model': 'purchase.order',
         }).create({})
 
@@ -316,7 +311,7 @@ class TestProductLifecycle(common.SingleTransactionCase):
 
         # Add a sellable replacement product in the replacement line.
         wiz_line.replacement_product_id = self.sellable_replacement
-        self.assertEqual(wiz_line.replacement_product_id,
+        self.assertEqual(wiz_line.replacement_product_id.id,
                          self.sellable_replacement)
 
         # Make the replacement in the purchase order by clicking the button
