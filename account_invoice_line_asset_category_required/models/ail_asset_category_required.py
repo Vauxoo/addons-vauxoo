51014b2a8a3c2e9d16204c68362b60b4da33e7fb
Jorge Angel Naranjo Rogel <jorge_nr@vauxoo.com>
2015-07-09 12:16:36 -0500

    ADD: Added Account Invoice Line Asset Category Required  module this module to validate when select an account asset and the asset category is required in account invoice line

---
+++ account_invoice_line_asset_category_required/models/ail_asset_category_required.py
@@ -0,0 +1,82 @@
+# -*- encoding: utf-8 -*-
+############################################################################
+#    Module Writen For Odoo, Open Source Management Solution
+#
+#    Copyright (c) 2015 Vauxoo - http://www.vauxoo.com
+#    All Rights Reserved.
+#    info Vauxoo (info@vauxoo.com)
+#    coded by: jorge_nr@vauxoo.com
+#    planned by: Humberto Arocha <hbto@vauxoo.com>
+############################################################################
+
+from openerp.osv import orm, fields
+from openerp.tools.translate import _
+
+
+class account_account_type(orm.Model):
+    _inherit = "account.account.type"
+
+    def _get_policies(self, cr, uid, context=None):
+        """This is the method to be inherited for adding policies"""
+        return [('optional', _('Optional')),
+                ('always', _('Always')),
+                ('never', _('Never'))]
+
+    _columns = {
+        'asset_policy': fields.selection(
+            lambda self, *args, **kwargs: self._get_policies(*args, **kwargs),
+            'Policy for asset category',
+            required=True,
+            help="Set the policy for the asset category field : if you select "
+                 "'Optional', the accountant is free to put a asset category "
+                 "on an account invoice line with this type of account ; "
+                 "if you select 'Always', the accountant will get an error "
+                 "message if there is no asset category ; "
+                 "if you select 'Never', the accountant will get an "
+                 "error message if a asset category is present."),
+    }
+
+    _defaults = {
+        'asset_policy': 'optional',
+    }
+
+
+class account_invoice_line(orm.Model):
+    _inherit = "account.invoice.line"
+
+    def _get_asset_category_policy(self, cr, uid, account, context=None):
+        """ Extension point to obtain analytic policy for an account """
+        return account.user_type.asset_policy
+
+    def _check_asset_category_required_msg(self, cr, uid, ids, context=None):
+        for account_move_line in self.browse(cr, uid, ids, context):
+
+            policy = self._get_asset_category_policy(
+                cr, uid, account_move_line.account_id, context=context)
+            if policy == 'always' and not account_move_line.asset_category_id:
+                return _("Asset policy is set to 'Always' "
+                         "with account %s '%s' but the "
+                         "asset category is missing in the account "
+                         "invoice line with description is '%s'." %
+                         (account_move_line.account_id.code,
+                          account_move_line.account_id.name,
+                          account_move_line.name))
+            elif policy == 'never' and account_move_line.asset_category_id:
+                return _("Asset policy is set to 'Never' "
+                         "with account %s '%s' but the "
+                         "account invoice line with description is '%s' " %
+                         (account_move_line.account_id.code,
+                          account_move_line.account_id.name,
+                          account_move_line.name))
+
+    def _check_asset_category_required(self, cr, uid, ids, context=None):
+        return not self._check_asset_category_required_msg(
+            cr, uid, ids, context=context)
+
+    _constraints = [
+        (_check_asset_category_required,
+         _check_asset_category_required_msg,
+         ['account_id']),
+    ]
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
