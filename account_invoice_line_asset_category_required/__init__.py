a77202a5bf829f04b9f870c64bf39acef27f0326
Jorge Angel Naranjo Rogel <jorge_nr@vauxoo.com>
2015-07-09 12:16:36 -0500

    ADD: Added Account Invoice Line Asset Category Required  module this module to validate when select an account asset and the asset category is required in account invoice line

---
+++ account_invoice_line_asset_category_required/__init__.py
@@ -0,0 +1,14 @@
+# -*- encoding: utf-8 -*-
+############################################################################
+#    Module Writen For Odoo, Open Source Management Solution
+#
+#    Copyright (c) 2015 Vauxoo - http://www.vauxoo.com
+#    All Rights Reserved.
+#    info Vauxoo (info@vauxoo.com)
+#    coded by: jorge_nr@vauxoo.com
+#    planned by: Humberto Arocha <hbto@vauxoo.com>
+############################################################################
+
+from . import models
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
