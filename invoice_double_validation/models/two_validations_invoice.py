4d30adc7a99ec5fa71f07ca1556b6a55e188df4d
Hugo Adan <hugo@vauxoo.com>
2015-07-10 14:13:04 -0500

    [FIX] change development to v7.0

--- invoice_double_validation/models/two_validations_invoice.py
+++ invoice_double_validation/models/two_validations_invoice.py
@@ -9,23 +9,37 @@
 #    planned by: Nhomar Hernandez <nhomar@vauxoo.com>
 ############################################################################
 
-from openerp import fields, models
+from openerp.osv import fields, osv
 
 
-class AccountInvoice(models.Model):
+class AccountInvoice(osv.Model):
     _inherit = 'account.invoice'
 
-    state = fields.Selection(
-        selection_add=[('validate', 'By validating')],
-        help=' * The \'Draft\' status is used when a user is \
-            encoding a new and unconfirmed Invoice. \
-            \n* The \'By validating\' status is used when an invoice is \
-            ready to be validate. \
-            \n* The \'Pro-forma\' when invoice is in Pro-forma \
-            status,invoice does not have an invoice number. \
-            \n* The \'Open\' status is used when user create invoice,\
-            a invoice number is generated.Its in open status till user \
-            does not pay invoice. \
-            \n* The \'Paid\' status is set automatically when the invoice \
-            is paid. Its related journal entries may or may not be reconciled.\
-            \n* The \'Cancelled\' status is used when user cancel invoice.')
+    def get_states(self, cr, uid, context):
+        return [
+            ('draft', 'Draft'),
+            ('proforma', 'Pro-forma'),
+            ('proforma2', 'Pro-forma'),
+            ('validate', 'By validating'),
+            ('open', 'Open'),
+            ('paid', 'Paid'),
+            ('cancel', 'Cancelled'), ]
+
+    _columns = {
+        'state': fields.selection(
+            get_states,
+            help=' * The \'Draft\' status is used when a user is \
+                encoding a new and unconfirmed Invoice. \
+                \n* The \'By validating\' status is used when an invoice is \
+                ready to be validate. \
+                \n* The \'Pro-forma\' when invoice is in Pro-forma \
+                status,invoice does not have an invoice number. \
+                \n* The \'Open\' status is used when user create invoice,\
+                a invoice number is generated.Its in open status till user \
+                does not pay invoice. \
+                \n* The \'Paid\' status is set automatically when the invoice \
+                is paid. Its related journal entries may or may not be \
+                reconciled. \
+                \n* The \'Cancelled\' status is used when user cancel invoice.'
+        )
+    }
