522b438d294e6d05269a4463bec4cfd3d09a86fd
Katherine Zaoral <kathy@vauxoo.com>
2015-07-16 08:53:06 -0500

    [ADD] Move website_product_rss module description from the openerp descriptor to a README.md file.

--- website_product_rss/__openerp__.py
+++ website_product_rss/__openerp__.py
@@ -4,10 +4,6 @@
     'summary': 'This module enables RSS on Products',
     'website': 'http://vauxoo.com',
     'version': '1.0',
-    'description': """
-        This App lets you create a RSS feed on populated by the
-        products stored on the database.
-        """,
     'author': 'Vauxoo',
     'depends': [
         'website_sale',
