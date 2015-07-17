dc9f6c247cbb2e6482d39ddfbe45f647414d52c8
Katherine Zaoral <kathy@vauxoo.com>
2015-07-17 13:04:58 -0500

    [ADD] Move account_invoice_number module description from the openerp descriptor to a README.md file.

---
+++ account_invoice_number/README.md
@@ -0,0 +1,7 @@
+Account Invoice Number v7
+=========================
+
+This module add field internal_number to invoice view.
+When you cancel a customer invoice will not delete the invoice number.
+
+It also adds field Supplier Invoice Number for suppliers in tree view.
--- account_invoice_number/__openerp__.py
+++ account_invoice_number/__openerp__.py
@@ -28,12 +28,6 @@
     "version": "1.0", 
     "author": "Vauxoo", 
     "category": "Localization/Mexico", 
-    "description": """
-This module add field internal_number to invoice view.
-When you cancel a customer invoice will not delete the invoice number.
-
-It also adds field Supplier Invoice Number for suppliers in tree view.
-    """, 
     "website": "http://www.vauxoo.com/", 
     "license": "", 
     "depends": [
