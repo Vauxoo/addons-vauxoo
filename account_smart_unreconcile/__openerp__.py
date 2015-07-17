6bf4d2224f04025ed03123f0df4db9f77b54118e
Katherine Zaoral <kathy@vauxoo.com>
2015-07-17 13:04:55 -0500

    [ADD] Move account_smart_unreconcile module description from the openerp descriptor to a README.md file.

--- account_smart_unreconcile/__openerp__.py
+++ account_smart_unreconcile/__openerp__.py
@@ -27,16 +27,6 @@
     "version": "1.0",
     "author": "Vauxoo",
     "category": "Generic Modules/Accounting",
-    "description": """
-        Allows send and account_move_reconcile and
-        a list of aml_ids to be excluded from reconciliation,
-        so that the result is a new account_move_reconcile
-        without aml_ids.
-
-        In Future this module is intended to grow adding a wizard
-        that would allow an end user to do smart unreconciliations
-
-    """,
     "website": "http://vauxoo.com",
     "license": "",
     "depends": [
