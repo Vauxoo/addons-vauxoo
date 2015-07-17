98d1f09f0b0d052cf3362c711b7afc87f4a3f44f
Katherine Zaoral <kathy@vauxoo.com>
2015-07-17 13:04:39 -0500

    [ADD] Move user_wzd module description from the openerp descriptor to a README.md file.

--- user_wzd/__openerp__.py
+++ user_wzd/__openerp__.py
@@ -30,17 +30,6 @@
     "version": "1.0.1",
     "author": "Vauxoo",
     "category": "Generic Modules/Human Resources",
-    "description": """
-Add wizard for create employee from users
-=========================================
-
-In order to load timesheet correctly is necesary an employee per user.
-
-This wizard enable a technical wizard to create an employee one time the user exists.
-
-The Wizard is added in uses vies model and generate an employee from one or many users,
-only if there is no other record of employee assigned to the user in question.
-""", 
     "website": "http://www.vauxoo.com/",
     "license": "AGPL-3",
     "depends": [
