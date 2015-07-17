8508af366ad9e00c73b43fbbc021cd1bcb6d76f3
Julio Serna <julio@vauxoo.com>
2015-07-17 23:05:21 +0000

    [IMP][send_author_email]return value of partner to notify outside cycle fof

--- send_author_mail/model/mail_notification.py
+++ send_author_mail/model/mail_notification.py
@@ -41,4 +41,4 @@ class mail_notification(osv.Model):
 		(message.author_id.receive_my_emails and
 		    message.author_id.notify_email != "none"):
 		res.append(message.author_id.id)
-	    return res
+	return res
