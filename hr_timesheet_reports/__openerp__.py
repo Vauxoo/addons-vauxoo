b98bfbd75ba6dd1f7834a68e2a9bc17ccd32753b
Katherine Zaoral <kathy@vauxoo.com>
2015-07-17 13:04:58 -0500

    [ADD] Move hr_timesheet_reports module description from the openerp descriptor to a README.md file.

--- hr_timesheet_reports/__openerp__.py
+++ hr_timesheet_reports/__openerp__.py
@@ -5,24 +5,6 @@
     "author": "Vauxoo",
     "category": "Tools",
     "summary": "A great way to report timesheet's consumption of time",
-    "description": """
-Timesheet reports:
-==================
-
-1.- Wizard to report timesheets to deliver to customers well presented and
-correctly ordered and audited.
-
-a. Filter between dates.
-b. Group by month, by user or by analytic account.
-c. Deliver how much was consumed by user story.
-
-2.- Improve your communication to your customers delivering directly this
-report.
-
-3.- Save your reports to use them as auditory process.
-
-** This module is not migrated yet to v8 **
-    """,
     "website": "http://www.vauxoo.com/",
     "license": "AGPL-3",
     "depends": [
