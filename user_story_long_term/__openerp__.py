1d3f68ce94b0a3c2d5abaa9c266974c35b3c7b63
Katherine Zaoral <kathy@vauxoo.com>
2015-07-17 13:05:16 -0500

    [ADD] Move user_story_long_term module description from the openerp descriptor to a README.md file.

--- user_story_long_term/__openerp__.py
+++ user_story_long_term/__openerp__.py
@@ -31,17 +31,6 @@
     "version": "1.0", 
     "author": "Vauxoo", 
     "category": "User Story Management", 
-    "description": """
-Long Term User Story Management module
-======================================
-
-Features:
----------
-    * Define various Phases of User Story
-    * Send mail to the followers of the user story as an acceptance criteria is accepted.
-    * Send mail to the owner, responsible supervisor and execution responsible when the user story
-      pass from in progress to pending state.
-    """, 
     "website": "http://www.vauxoo.com", 
     "license": "", 
     "depends": [
