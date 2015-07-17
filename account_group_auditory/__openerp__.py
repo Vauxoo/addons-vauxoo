ba4c3970deed259e0459f5547ec78d65c436e53c
Katherine Zaoral <kathy@vauxoo.com>
2015-07-17 13:04:51 -0500

    [ADD] Move account_group_auditory module description from the openerp descriptor to a README.md file. [IMP] Modificate the description, remove redundant text.

--- account_group_auditory/__openerp__.py
+++ account_group_auditory/__openerp__.py
@@ -29,18 +29,6 @@
     "version": "0.1",
     "author": "Vauxoo",
     "category": "Generic Modules",
-    "description": """
-This module adds the following features:
-========================================
-
-* Odoo does not come with a read-only accountant group out of the box, which
-is inconvenient when you need to have an external accountant go through your
-accounting records in Odoo.  This module adds a group 'Auditor Read-Only' so
-it's easy to grant an external accountant access to your Odoo system.
-* This group inherits the group 'Employee', thus minimum level of
-create/write/delete rights will be granted for some non-accounting related
-models.
-""",
     "website": "http://www.vauxoo.com",
     "license": "",
     "depends": [
