c5ca7999de0f3fafef05fb4a15abe51325cde98a
Katherine Zaoral <kathy@vauxoo.com>
2015-07-17 13:04:55 -0500

    [ADD] Move message_post_model module description from the openerp descriptor to a README.md file. [TODO] This module need a description

--- message_post_model/__openerp__.py
+++ message_post_model/__openerp__.py
@@ -27,9 +27,6 @@
     "version": "0.1",
     "author": "Vauxoo",
     "category": "Generic Modules",
-    "description": """
-
-""",
     "website": "http://vauxoo.com",
     "license": "",
     "depends": [
