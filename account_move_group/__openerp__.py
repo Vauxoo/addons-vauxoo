b0dedeb75d65ad81c4be9b20897e863b54bca8a5
Katherine Zaoral <kathy@vauxoo.com>
2015-07-17 13:05:14 -0500

    [ADD] Move account_move_group module description from the openerp descriptor to a README.md file.

--- account_move_group/__openerp__.py
+++ account_move_group/__openerp__.py
@@ -29,8 +29,6 @@
     "version": "1.1", 
     "author": "Vauxoo", 
     "category": "Generic Modules/Account", 
-    "description": """ Group Entries
-    """, 
     "website": "http://www.vauxoo.com/", 
     "license": "", 
     "depends": [
