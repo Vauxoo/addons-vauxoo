b873b944796b985d38440de221b2a5cac5937924
Katherine Zaoral <kathy@vauxoo.com>
2015-07-17 13:04:51 -0500

    [ADD] Move payment_terms module description from the openerp descriptor to a README.md file.

--- payment_terms/__openerp__.py
+++ payment_terms/__openerp__.py
@@ -27,16 +27,6 @@
     "version": "0.1", 
     "author": "Vauxoo", 
     "category": "Generic Modules", 
-    "description": """
-    Add Payments terms by partner, Each payments termn is set in each partner
-    and each partner have a different payment termn by company,
-
-    These fields are located in the Accounting tab, above Others terms added by
-    accounting module.
-
-    The Payment term is in sale, stock,purchase and invoice modules and is send
-    through are models
-                    """, 
     "website": "http://www.vauxoo.com", 
     "license": "", 
     "depends": [
