acadc295ee307710be88daa961437dfa4a7bafee
Katherine Zaoral <kathy@vauxoo.com>
2015-07-17 13:05:19 -0500

    [ADD] Move contract_enterprise_openerp module description from the openerp descriptor to a README.md file.

--- contract_enterprise_openerp/__openerp__.py
+++ contract_enterprise_openerp/__openerp__.py
@@ -26,24 +26,6 @@
     "version": "0.1", 
     "author": "Vauxoo", 
     "category": "Contracts", 
-    "description": """
-Enterprise Contract Management.
-===============================
-
-When you are partner sometimes need to be sure manage internally the tickets
-and the renewal process of OpenERP Enterprise licences regarding to your
-customer contracts this module allow you manage this informatio related to
-this contracts.
-
-* Start Date.
-* End Date.
-* Licence KEy.
-
-#TODO:
-
-* Expiration Warnings.
-* Manage Comunication with OpenERP.
-    """, 
     "website": "http://vauxoo.com", 
     "license": "", 
     "depends": [
