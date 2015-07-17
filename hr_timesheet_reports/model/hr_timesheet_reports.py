44b937a8a7ff5e52dd14565ec0eceac920253215
Nhomar Hernandez <nhomar@vauxoo.com>
2015-07-17 03:32:16 +0000

    [MIG] Nueva api just for fields

--- hr_timesheet_reports/model/hr_timesheet_reports.py
+++ hr_timesheet_reports/model/hr_timesheet_reports.py
@@ -1,5 +1,5 @@
 # -*- encoding: utf-8 -*-
-from openerp.osv import osv, fields
+from openerp import models, fields, api, _
 from openerp.tools.safe_eval import safe_eval
 from openerp import SUPERUSER_ID
 
@@ -19,7 +19,7 @@ def clean_name(name):
     return text
 
 
-class hr_timesheet_reports_base(osv.Model):
+class hr_timesheet_reports_base(models.Model):
 
     _name = "hr.timesheet.reports.base"
     _inherit = ['mail.thread']
@@ -119,16 +119,16 @@ class hr_timesheet_reports_base(osv.Model):
                 context=context)
             group_prod = inv_line_obj.read_group(
                 cr, uid, [('invoice_id', 'in', inv_line_ids)],
-                         ['product_id', 'price_subtotal', ],
-                         ['product_id', 'price_subtotal', ], context=context)
+                ['product_id', 'price_subtotal', ],
+                ['product_id', 'price_subtotal', ], context=context)
             total_ent = sum([gr['price_subtotal'] for gr in group_prod
-                            if gr['product_id'][0] in ent_ids])
+                             if gr['product_id'][0] in ent_ids])
             total_cons = sum([gr['price_subtotal'] for gr in group_prod
-                             if gr['product_id'][0] in cons_ids])
+                              if gr['product_id'][0] in cons_ids])
             total_train = sum([gr['price_subtotal'] for gr in group_prod
-                              if gr['product_id'][0] in train_ids])
+                               if gr['product_id'][0] in train_ids])
             total_others = sum([gr['price_subtotal'] for gr in group_prod
-                               if gr['product_id'][0] not in all_ids])
+                                if gr['product_id'][0] not in all_ids])
             total = total_ent + total_ent + total_train + total_others
             curr_from = wzr_brw.currency_id.id
             curr_to = currency[0]
@@ -284,93 +284,81 @@ class hr_timesheet_reports_base(osv.Model):
             info['data'][proj] = [r for r in res if r['analytic'] == proj]
         return info
 
-    _columns = {
-        'name': fields.char('Report Title'),
-        'comment_invoices': fields.text('Comment about Invoices',
-                                        help="It will appear just above "
-                                        "list of invoices."),
-        'comment_timesheet': fields.text('Comment about Timesheets',
-                                         help='It will appear just above '
-                                         'the resumed timesheets.'),
-        'comment_issues': fields.text('Comment about Timesheets',
-                                      help='It will appear just above '
-                                      'the resumed issues.'),
-        'comment_hu': fields.text('Comment about Timesheets',
-                                  help='It will appear just above '
-                                  'the resumed hu status.'),
-        'user_id': fields.many2one(
-            'res.users', 'Responsible',
-            help='Owner of the report, generally the person that create it.'),
-        'partner_id': fields.many2one(
-            'res.partner', 'Contact',
-            help='Contact which you will send this report.'),
-        'filter_hu_id': fields.many2one(
-            'ir.filters', 'User Stories',
-            domain=[('model_id', 'ilike', 'user.story')],
-            help='Set the filter for issues TIP: go to issues and look for '
-            'the filter that adjust to your needs of issues to be shown.'),
-        'filter_issue_id': fields.many2one(
-            'ir.filters', 'Issues',
-            domain=[('model_id', 'ilike', 'project.issue')],
-            help='Set the filter for issues TIP: go to issues and look for '
-            'the filter that adjust to your needs of issues to be shown.'),
-        'filter_invoice_id': fields.many2one(
-            'ir.filters', 'Invoices',
-            domain=[('model_id', 'ilike', 'account.invoice')],
-            help='Filter of Invoices to be shown TIP: '
-            'Go to Accounting/Customer '
-            'Invoices in order to create the filter you want to show on this'
-            'report.',),
-        'filter_id': fields.many2one(
-            'ir.filters', 'Filter',
-            domain=[('model_id', 'ilike', 'hr.analytic.timesheet')],
-            help="Filter should be by date, group_by is ignored, the model "
-            "which the filter should belong to is timesheet."),
-        'show_details': fields.boolean('Show Detailed Timesheets'),
-        'records': fields.function(_get_print_data,
-                                   string='Records', type="text"),
-        'state': fields.selection([('draft', 'Draft'),
-                                   ('sent', 'Sent')],
-                                  'Status',
-                                  help='Message sent already to customer '
-                                  '(it will block edition)'),
-        'company_id': fields.many2one(
-            'res.company', 'Company',
-            help='Company which this report belongs to'),
-        'product_id': fields.many2one(
-            'product.product', 'Product to Compute Totals',
-            help='This product will be used to compute totals'),
-        'currency_id': fields.many2one(
-            'res.currency', 'Currency',
-            help='This product will be used to compute totals'),
-        'prod_ent_ids': fields.many2many('product.product',
-                                         'prod_report_timesheet_rel1',
-                                         'report_id', 'prod_ent_id',
-                                         'Products for Enterprises',
-                                         help="All lines on invoices the "
-                                         "have this product will "
-                                         "be ignored as Effectivally "
-                                         "Invoiced time already invoiced"),
-        'prod_train_ids': fields.many2many('product.product',
-                                           'prod_report_timesheet_rel2',
-                                           'report_id', 'prod_train_id',
-                                           'Products for Training',
-                                           help="All lines that have this "
-                                           "products will "
-                                           "Be ignored due to this is just "
-                                           "for products"),
-        'prod_cons_ids': fields.many2many('product.product',
-                                          'prod_report_timesheet_rel3',
-                                          'report_id', 'prod_cons_id',
-                                          'Products for Consultancy',
-                                          help="All products here will be "
-                                          "considered as consultancy"
-                                          "then it will be compared by "
-                                          "currency and by "
-                                          "considering the product "
-                                          "in this reports to use "
-                                          "the unit_price and the currency"),
-    }
+    name = fields.Char('Report Title')
+    comment_invoices = fields.Text('Comment about Invoices',
+                                   help="It will appear just above "
+                                   "list of invoices.")
+    comment_timesheet = fields.Text('Comment about Timesheets',
+                                    help='It will appear just above '
+                                    'the resumed timesheets.')
+    comment_issues = fields.Text('Comment about Timesheets',
+                                 help='It will appear just above '
+                                 'the resumed issues.')
+    comment_hu = fields.Text('Comment about Timesheets',
+                             help='It will appear just above '
+                             'the resumed hu status.')
+    user_id = fields.Many2one(
+        'res.users', 'Responsible',
+        help='Owner of the report, generally the person that create it.')
+    partner_id = fields.Many2one(
+        'res.partner', 'Contact',
+        help='Contact which you will send this report.')
+    filter_hu_id = fields.Many2one(
+        'ir.filters', 'User Stories',
+        domain=[('model_id', 'ilike', 'user.story')],
+        help='Set the filter for issues TIP = go to issues and look for '
+        'the filter that adjust to your needs of issues to be shown.')
+    filter_issue_id = fields.Many2one(
+        'ir.filters', 'Issues',
+        domain=[('model_id', 'ilike', 'project.issue')],
+        help='Set the filter for issues TIP = go to issues and look for '
+        ' the filter that adjust to your needs of issues to be shown.')
+    filter_invoice_id = fields.Many2one(
+        'ir.filters', 'Invoices',
+        domain=[('model_id', 'ilike', 'account.invoice')],
+        help='Filter of Invoices to be shown TIP = '
+        'Go to Accounting/Customer '
+        'Invoices in order to create the filter you want to show on this'
+        'report.',)
+    filter_id = fields.Many2one('ir.filters', 'Filter',
+                                domain=[
+                                    ('model_id', 'ilike', 'hr.analytic.timesheet')],
+                                help="Filter should be by date, group_by is ignored, the model "
+                                "which the filter should belong to is timesheet.")
+    show_details = fields.Boolean('Show Detailed Timesheets')
+    records = fields.Text('Records', compute='_get_print_data',)
+    state = fields.Selection([('draft', 'Draft'), ('sent', 'Sent')], 'Status',
+                             help='Message sent already to customer (it will block edition)')
+    company_id = fields.Many2one(
+        'res.company', 'Company', help='Company which this report belongs to')
+    product_id = fields.Many2one(
+        'product.product', 'Product to Compute Totals', help='This product will be used to compute totals')
+    currency_id = fields.Many2one(
+        'res.currency', 'Currency', help='This product will be used to compute totals')
+    prod_ent_ids = fields.Many2many('product.product', 'prod_report_timesheet_rel1', 'report_id', 'prod_ent_id', 'Products for Enterprises',
+                                    help="All lines on invoices the "
+                                    "have this product will "
+                                    "be ignored as Effectivally "
+                                    "Invoiced time already invoiced")
+    prod_train_ids = fields.Many2many('product.product',
+                                      'prod_report_timesheet_rel2',
+                                      'report_id', 'prod_train_id',
+                                      'Products for Training',
+                                      help="All lines that have this "
+                                      "products will "
+                                      "Be ignored due to this is just "
+                                      "for products")
+    prod_cons_ids = fields.Many2many('product.product',
+                                     'prod_report_timesheet_rel3',
+                                     'report_id', 'prod_cons_id',
+                                     'Products for Consultancy',
+                                     help="All products here will be "
+                                     "considered as consultancy"
+                                     "then it will be compared by "
+                                     "currency and by "
+                                     "considering the product "
+                                     "in this reports to use "
+                                     "the unit_price and the currency")
 
     _defaults = {
         'state': lambda * a: 'draft',
