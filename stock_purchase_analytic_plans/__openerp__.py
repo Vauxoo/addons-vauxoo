2a09e6e4ce990ede4e9b114a5499251929cc6804
Katherine Zaoral <kathy@vauxoo.com>
2015-07-17 13:04:44 -0500

    [ADD] Move stock_purchase_analytic_plans module description from the openerp descriptor to a README.md file.

--- stock_purchase_analytic_plans/__openerp__.py
+++ stock_purchase_analytic_plans/__openerp__.py
@@ -26,15 +26,6 @@
     "version": "1.0", 
     "author": "Vauxoo", 
     "category": "", 
-    "description": """
-Stock Purchase Analytic Plans
------------------------------
-
-This module sets Analytic Distribution on the Journal Entry Lines when the
-Journal Entries are created because of Real Time Inventory Valuation from an
-Stock Move originated from Purchase Order
-
-""", 
     "website": "http://www.vauxoo.com/", 
     "license": "", 
     "depends": [
