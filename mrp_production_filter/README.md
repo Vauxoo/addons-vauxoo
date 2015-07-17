0d7ecb83296246dccb1c8798864de01d3bd0c50f
Katherine Zaoral <kathy@vauxoo.com>
2015-07-17 13:04:54 -0500

    [ADD] Move mrp_production_filter module description from the openerp descriptor to a README.md file.

---
+++ mrp_production_filter/README.md
@@ -0,0 +1,4 @@
+Filters Account
+===============
+
+This module add filter for order's in state done, and state different to cancel
--- mrp_production_filter/__openerp__.py
+++ mrp_production_filter/__openerp__.py
@@ -28,9 +28,6 @@
     "version": "1.0", 
     "author": "Vauxoo", 
     "category": "Generic Modules", 
-    "description": """
-        This module add filter for order's in state done, and state different to cancel
-    """, 
     "website": "http://www.vauxoo.com/", 
     "license": "AGPL-3", 
     "depends": [
