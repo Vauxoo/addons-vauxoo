2b83965f10c549def962ea76010ed223fd35016e
Katherine Zaoral <kathy@vauxoo.com>
2015-07-17 13:05:23 -0500

    [ADD] Move payroll_amount_residual module description from the openerp descriptor to a README.md file.

--- payroll_amount_residual/__openerp__.py
+++ payroll_amount_residual/__openerp__.py
@@ -28,11 +28,6 @@
     "version": "1.0", 
     "author": "Vauxoo", 
     "category": "Generic", 
-    "description": """
-
-    This module adda field to save the amount residual in an payslip
-
-    """, 
     "website": "http://www.vauxoo.com/", 
     "license": "AGPL-3", 
     "depends": [
