87debc1b7b87c17d9d9d4164eed4da59080b2e37
Nhomar Hernandez <nhomar@vauxoo.com>
2015-07-17 19:07:18 +0000

    [IMP] Doc and testing.

--- hr_timesheet_reports/model/rst2html/html.py
+++ hr_timesheet_reports/model/rst2html/html.py
@@ -4,36 +4,49 @@ Very simple tool to convert rst fields to html
 TODO: make generic
 '''
 
-from docutils.core import publish_string
-
-from openerp import modules
+from docutils.core import publish_parts
+from tempfile import NamedTemporaryFile
 
 # see http://docutils.sourceforge.net/docs/user/config.html
 default_rst_opts = {
     'no_generator': True,
     'no_source_link': True,
     'tab_width': 4,
-    'file_insertion_enabled': False,
-    'raw_enabled': False,
+    'file_insertion_enabled': True,
+    'raw_enabled': True,
     'stylesheet_path': None,
-    'traceback': True,
-    'halt_level': 5,
+    'traceback': False,
+    'halt_level': 2,
 }
+
+TEMPLATE = '''
+%(body)s
+'''
 
 
 def rst2html(rst, opts=None):
+    '''Simple tool to convert rst to html to be embeded in any document
+
+    Example:
+
+        >>> chain = "Text in rst"
+        >>> rst2html(chain)
+        u'<div class="document">\\n<p>Text in rst</p>\\n</div>\\n'
+
+    @param: rst Text multiline in rst format
+    '''
+    template = NamedTemporaryFile('w', suffix='.txt', delete=False)
     rst_opts = default_rst_opts.copy()
     if opts:
         rst_opts.update(opts)
-    # TODO: this should be a parameter
-    rst_opts['template'] = modules.get_module_resource('hr_timesheet_reports',
-                                                       'model/rst2html',
-                                                       'template.txt')
-    out = publish_string(rst, writer_name='html', settings_overrides=rst_opts)
+    template.write(TEMPLATE)
+    template.close()
+    rst_opts['template'] = template.name
+    out = publish_parts(rst,
+                        writer_name='html',
+                        settings_overrides=rst_opts)['html_body']
     return out
 
 if __name__ == '__main__':
-    print rst2html("""
-                   - Hola
-                   - Mundo
-                   """)
+    import doctest
+    doctest.testmod()
