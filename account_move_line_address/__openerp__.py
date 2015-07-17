3b86cbd53aa86dd61536537fbc7e71f46f0533e9
Katherine Zaoral <kathy@vauxoo.com>
2015-07-17 13:04:52 -0500

    [ADD] Move account_move_line_address module description from the openerp descriptor to a README.md file.

--- account_move_line_address/__openerp__.py
+++ account_move_line_address/__openerp__.py
@@ -29,8 +29,6 @@
     "version": "1.1", 
     "author": "Vauxoo", 
     "category": "Generic Modules/Account", 
-    "description": """ Add field address in account_move_line
-    """, 
     "website": "http://www.vauxoo.com/", 
     "license": "", 
     "depends": [
