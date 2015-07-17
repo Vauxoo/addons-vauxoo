edd2b2949ccffd3e3d39a8d27cbf75e32d165a70
Katherine Zaoral <kathy@vauxoo.com>
2015-07-17 13:05:15 -0500

    [ADD] Move check_unit_measure module description from the openerp descriptor to a README.md file.

--- check_unit_measure/__openerp__.py
+++ check_unit_measure/__openerp__.py
@@ -24,19 +24,6 @@
     "version": "1", 
     "author": "Vauxoo", 
     "category": "Sale, Purchase and Warehousee", 
-    "description": """
-
-Unit Measure Check
-====================================
-This module was created to check the units measured in all operations where we use a product,
-for always set the unit measure that this product has and confirm quantity for operation line
-
-Improvements
-
-Inherit Sale, Purchase, Stock and Account to add constraint to avoid sale order validate with diferent
-unit measure indicated on the product or diferent quantity compute on the unit measure.
-
-    """, 
     "website": "http://www.vauxoo.com", 
     "license": "", 
     "depends": [
