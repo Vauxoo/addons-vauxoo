5477ef9a3648cd700d0e36b4db9378f2cfe244ca
Katherine Zaoral <kathy@vauxoo.com>
2015-07-17 13:05:27 -0500

    [ADD] Move mrp_button_box module description from the openerp descriptor to a README.md file.

--- mrp_button_box/__openerp__.py
+++ mrp_button_box/__openerp__.py
@@ -26,13 +26,6 @@
     "version": "1.0", 
     "author": "Vauxoo C.A.", 
     "category": "", 
-    "description": """
-MRP Button Box
-==============
-
-Dummy module to add a button box at the manufacturing order right uper corner.
-This module can be find at lp:addons-vauxoo/7.0/mrp_button_box
-""", 
     "website": "http://www.openerp.com.ve", 
     "license": "", 
     "depends": [
