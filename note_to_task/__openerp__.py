98fa1563e690091b7cfd9162e426f84aecf23594
Katherine Zaoral <kathy@vauxoo.com>
2015-07-17 13:04:50 -0500

    [ADD] Move note_to_task module description from the openerp descriptor to a README.md file. [IMP] Modificate the description to be render propertly in the instance.

--- note_to_task/__openerp__.py
+++ note_to_task/__openerp__.py
@@ -25,15 +25,6 @@
     "version": "1.0", 
     "author": "Vauxoo", 
     "category": "Tools", 
-    "description": """
-Convert Note to Task
-====================
-    Add a button in Notes showing a wizard to convert the note into a task
-    asking:
-            1. - Estimated time for this task.
-            2.- Associate Project.
-            3.- Date to end.
-            """, 
     "website": "http://vauxoo.com", 
     "license": "", 
     "depends": [
