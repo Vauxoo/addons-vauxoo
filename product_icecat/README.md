56cd9b624b12c73fb5c4475c8fbf9dca7c20fe71
Katherine Zaoral <kathy@vauxoo.com>
2015-07-17 13:04:52 -0500

    [ADD] Move product_icecat module description from the openerp descriptor to a README.md file. [IMP] Modifcate the description to render correctly into the instance.

---
+++ product_icecat/README.md
@@ -0,0 +1,13 @@
+Product Information Import from icecat
+======================================
+
+Import information XML from icecat to OpenERP products.
+
+This wizard download XML in openerp-server (addons/product_icecat/xml) and
+after process data mapping line to product import.
+
+- Language import: User preference or force into wizard (option)
+- HTML Category option: create list details
+- FTP image
+
+http://www.icecat.biz/
--- product_icecat/__openerp__.py
+++ product_icecat/__openerp__.py
@@ -23,15 +23,6 @@
     "version": "1.1",
     "author": "Vauxoo, Zikzakmedia SL",
     "category": "Added functionality",
-    "description": """
-    Import information XML from icecat to OpenERP products.
-    This wizard download XML in openerp-server (addons/product_icecat/xml) and
-    after process data mapping line to product import.
-    - Language import: User preference or force into wizard (option)
-    - HTML Category option: create list details
-    - FTP image
-    http://www.icecat.biz/
-    """,
     "website": "http://www.zikzakmedia.com",
     "license": "",
     "depends": [
