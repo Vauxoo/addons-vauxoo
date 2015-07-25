5159aa823e334f1bb8404ab3952d253f9b2577ef
Nhomar Hernandez <nhomar@gmail.com>
2015-07-25 16:16:55 -0500

    [IMP] changing versions forcing *.5 on versions to have them as reference. [IMP] .version.py now works with ReGex

--- account_currency_tools/__openerp__.py
+++ account_currency_tools/__openerp__.py
@@ -22,7 +22,7 @@
 ###############################################################################
 {
     "name": "Account Currency Tools",
-    "version": "1.0",
+    "version": "1.5",
     "author": "Vauxoo",
     "category": "Tools",
     "website": "http://www.vauxoo.com/",
