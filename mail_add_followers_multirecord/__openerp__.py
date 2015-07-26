f2f2f1bc67918ece58830c388ae3dc5e8bfc0de2
Nhomar Hernandez <nhomar@gmail.com>
2015-07-26 18:39:55 -0500

    [REN] add_followers -> mail_add_followers_multirecord: Following best standards with module name

--- mail_add_followers_multirecord/__openerp__.py
+++ mail_add_followers_multirecord/__openerp__.py
@@ -25,7 +25,7 @@
 ##############################################################################
 {
     "name": "Add many followers in many task or project",
-    "version": "1.5",
+    "version": "1.6",
     "author": "Vauxoo",
     "category": "Task and Project",
     "website": "http://www.vauxoo.com/",
