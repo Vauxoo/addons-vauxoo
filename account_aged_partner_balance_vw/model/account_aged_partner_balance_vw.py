423f15fbcf629b59405cb1e3c7389c6e2f58d754
Nhomar Hernández [Vauxoo] <nhomar@vauxoo.com>
2015-07-29 12:45:32 -0500

    mx lib is optional.

--- account_aged_partner_balance_vw/model/account_aged_partner_balance_vw.py
+++ account_aged_partner_balance_vw/model/account_aged_partner_balance_vw.py
@@ -26,8 +26,10 @@
 
 from openerp.osv import fields, osv
 import time
-import mx.DateTime
-
+try:
+    import mx.DateTime
+except ImportError:
+    _logger.info('account_aged_partner_balance_vw: need mx library')
 
 class account_aged_partner_balance_vw(osv.TransientModel):
     _name = 'account.aged.partner.balance.vw'
