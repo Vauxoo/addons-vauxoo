cabacd573d11343e99ae635ea0ee8ae988b17c30
Nhomar Hernandez <nhomar@gmail.com>
2015-07-31 05:32:52 -0500

    [FIX] pandas importing to not brake virgin enviroments with our repositories.

--- account_currency_tools/__openerp__.py
+++ account_currency_tools/__openerp__.py
@@ -44,5 +44,10 @@
     "qweb": [],
     "installable": True,
     "auto_install": False,
-    "active": False
+    "active": False,
+    "external_dependencies": {
+        "python": [
+            'pandas'
+        ]
+    }
 }
