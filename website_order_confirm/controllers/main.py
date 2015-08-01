a13fdb6b0333533898d42093b7e079189558aa55
oalca <oszckar@gmail.com>
2015-08-01 09:57:55 -0500

    [AUTOPEP8]

--- website_order_confirm/controllers/main.py
+++ website_order_confirm/controllers/main.py
@@ -7,8 +7,16 @@ from openerp.addons.website_sale.controllers.main import website_sale
 
 class website_sale_inh(website_sale):
 
-    @http.route('/shop/payment/validate', type='http', auth="public", website=True)
-    def payment_validate(self, transaction_id=None, sale_order_id=None, **post):
+    @http.route(
+        '/shop/payment/validate',
+        type='http',
+        auth="public",
+        website=True)
+    def payment_validate(
+            self,
+            transaction_id=None,
+            sale_order_id=None,
+            **post):
         """ Method that should be called by the server when receiving an update
         for a transaction. State at this point :
 
@@ -21,28 +29,42 @@ class website_sale_inh(website_sale):
         if transaction_id is None:
             tx = request.website.sale_get_transaction()
         else:
-            tx = request.registry['payment.transaction'].browse(cr, uid, transaction_id, context=context)
+            tx = request.registry['payment.transaction'].browse(
+                cr,
+                uid,
+                transaction_id,
+                context=context)
 
         if sale_order_id is None:
             order = request.website.sale_get_order(context=context)
         else:
-            order = request.registry['sale.order'].browse(cr, SUPERUSER_ID, sale_order_id, context=context)
+            order = request.registry['sale.order'].browse(
+                cr,
+                SUPERUSER_ID,
+                sale_order_id,
+                context=context)
             assert order.id == request.session.get('sale_last_order_id')
 
         if not order or (order.amount_total and not tx):
             return request.redirect('/shop')
 
-        if (not order.amount_total and not tx) or tx.state in ['pending', 'done']:
+        if (not order.amount_total and not tx) or tx.state in [
+                'pending', 'done']:
             if (not order.amount_total and not tx):
-                # Orders are confirmed by payment transactions, but there is none for free orders,
+                # Orders are confirmed by payment transactions, but there is
+                # none for free orders,
                 # (e.g. free events), so confirm immediately
                 order.action_button_confirm()
             # send by email
-            email_act = sale_order_obj.action_quotation_send(cr, SUPERUSER_ID, [order.id], context=request.context)
+            email_act = sale_order_obj.action_quotation_send(
+                cr, SUPERUSER_ID, [
+                    order.id], context=request.context)
             order.action_button_confirm()
         elif tx and tx.state == 'cancel':
             # cancel the quotation
-            sale_order_obj.action_cancel(cr, SUPERUSER_ID, [order.id], context=request.context)
+            sale_order_obj.action_cancel(
+                cr, SUPERUSER_ID, [
+                    order.id], context=request.context)
 
         # send the email
         if email_act and email_act.get('context'):
@@ -55,14 +77,32 @@ class website_sale_inh(website_sale):
                 email_ctx.get('default_model'),
                 email_ctx.get('default_res_id'),
             ]
-            composer_values.update(composer_obj.onchange_template_id(cr, SUPERUSER_ID, None, *template_values, context=context).get('value', {}))
-            if not composer_values.get('email_from') and uid == request.website.user_id.id:
-                composer_values['email_from'] = request.website.user_id.company_id.email
+            composer_values.update(
+                composer_obj.onchange_template_id(
+                    cr,
+                    SUPERUSER_ID,
+                    None,
+                    *template_values,
+                    context=context).get(
+                    'value',
+                    {}))
+            if not composer_values.get(
+                    'email_from') and uid == request.website.user_id.id:
+                composer_values[
+                    'email_from'] = request.website.user_id.company_id.email
             for key in ['attachment_ids', 'partner_ids']:
                 if composer_values.get(key):
                     composer_values[key] = [(6, 0, composer_values[key])]
-            composer_id = composer_obj.create(cr, SUPERUSER_ID, composer_values, context=email_ctx)
-            composer_obj.send_mail(cr, SUPERUSER_ID, [composer_id], context=email_ctx)
+            composer_id = composer_obj.create(
+                cr,
+                SUPERUSER_ID,
+                composer_values,
+                context=email_ctx)
+            composer_obj.send_mail(
+                cr,
+                SUPERUSER_ID,
+                [composer_id],
+                context=email_ctx)
 
         # clean context and session, then redirect to the confirmation page
         request.website.sale_reset(context=context)
