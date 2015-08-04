85eac0743c20f7c1f0e4a6c40caac3ce887f86a6
Katherine Zaoral <kathy@vauxoo.com>
2015-08-04 09:10:58 -0500

    [IMP] Update invoice_datetime module version +0.1 points to ensure that odoo apps refresh the new index.html description

--- invoice_datetime/__openerp__.py
+++ invoice_datetime/__openerp__.py
@@ -28,7 +28,7 @@
 ##############################################################################
 {
     "name": "Invoice Datetime",
-    "version": "1.5",
+    "version": "1.6",
     "author": "Vauxoo",
     "category": "Localization/Mexico",
     "website": "http://www.vauxoo.com/",
