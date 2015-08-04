12573cbb1bc6283b7d9829bafc2a3b43d813dc33
Katherine Zaoral <kathy@vauxoo.com>
2015-08-04 09:10:59 -0500

    [IMP] Update mrp_production_wizard module version +0.1 points to ensure that odoo apps refresh the new index.html description

--- mrp_production_wizard/__openerp__.py
+++ mrp_production_wizard/__openerp__.py
@@ -25,7 +25,7 @@
 ##############################################################################
 {
     "name": "MPR Production Wizard", 
-    "version": "1.5", 
+    "version": "1.6", 
     "author": "Vauxoo", 
     "category": "Generic Modules/Production", 
     "website": "http://www.vauxoo.com/", 
