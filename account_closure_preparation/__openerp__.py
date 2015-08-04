7824ce8e9831ef0bf4cf8e9aaa8addf3bdd3fc61
Katherine Zaoral <kathy@vauxoo.com>
2015-08-04 09:10:54 -0500

    [IMP] Update account_closure_preparation module version +0.1 points to ensure that odoo apps refresh the new index.html description

--- account_closure_preparation/__openerp__.py
+++ account_closure_preparation/__openerp__.py
@@ -23,7 +23,7 @@
 ###############################################################################
 {
     "name": "Accounting Closure Preparation", 
-    "version": "1.5", 
+    "version": "1.6", 
     "author": "Vauxoo", 
     "category": "Accounting", 
     "website": "http://vauxoo.com", 
