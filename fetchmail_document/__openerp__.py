c875504b6d251257b73d2f38e67a248352effae7
Katherine Zaoral <kathy@vauxoo.com>
2015-08-04 09:10:44 -0500

    [IMP] Update fetchmail_document module version +0.1 points to ensure that odoo apps refresh the new index.html description

--- fetchmail_document/__openerp__.py
+++ fetchmail_document/__openerp__.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 {
     "name": "Fetchmail Corrections", 
-    "version": "0.5", 
+    "version": "0.6", 
     "author": "Vauxoo", 
     "category": "Tools", 
     "website": "http://vauxoo.com", 
