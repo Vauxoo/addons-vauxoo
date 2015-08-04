bf7cd02e279632e1c26a6c980e0cf27680d7b4a2
Katherine Zaoral <kathy@vauxoo.com>
2015-08-04 09:10:30 -0500

    [IMP] Update picking_verification_report module version +0.1 points to ensure that odoo apps refresh the new index.html description

--- picking_verification_report/__openerp__.py
+++ picking_verification_report/__openerp__.py
@@ -23,7 +23,7 @@
 ############################################################################
 {
     "name": "Picking Verification Report", 
-    "version": "0.5", 
+    "version": "0.6", 
     "author": "Vauxoo", 
     "category": "Generic Modules", 
     "website": "http://www.vauxoo.com", 
