8def34d5209bf0fe4be97e9c2a40b0be448f37a1
Katherine Zaoral <kathy@vauxoo.com>
2015-08-04 09:10:38 -0500

    [IMP] Update sale_uncommitted_product module version +0.1 points to ensure that odoo apps refresh the new index.html description

--- sale_uncommitted_product/__openerp__.py
+++ sale_uncommitted_product/__openerp__.py
@@ -23,7 +23,7 @@
 ################################################################################
 {
     "name": "Sale of Uncommitted Products", 
-    "version": "0.5", 
+    "version": "0.6", 
     "author": "Vauxoo", 
     "category": "Generic Modules", 
     "website": "http://www.vauxoo.com", 
