47047fff3be80d938de912684b75167d605dfef5
Katherine Zaoral <kathy@vauxoo.com>
2015-08-04 09:11:04 -0500

    [IMP] Update project_issue_management module version +0.1 points to ensure that odoo apps refresh the new index.html description

--- project_issue_management/__openerp__.py
+++ project_issue_management/__openerp__.py
@@ -24,7 +24,7 @@
 ###########################################################################
 {
     "name": "Issue Management enhancement.",
-    "version": "0.5",
+    "version": "0.6",
     "author": "Vauxoo",
     "category": "Generic Modules",
     "website": "http://vauxoo.com",
