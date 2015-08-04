88b87d0b1f44f17cac0a23228df524e95adf2722
Katherine Zaoral <kathy@vauxoo.com>
2015-08-04 09:10:57 -0500

    [IMP] Update stock_sale_invoice_line module version +0.1 points to ensure that odoo apps refresh the new index.html description

--- stock_sale_invoice_line/__openerp__.py
+++ stock_sale_invoice_line/__openerp__.py
@@ -23,7 +23,7 @@
 ###############################################################################
 {
     "name": "Stock Sale Invoice Line",
-    "version": "1.5",
+    "version": "1.6",
     "author": "Vauxoo",
     "category": "sale",
     "website": "http://www.vauxoo.com/",
