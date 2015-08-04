05f59040d4ac6e73553e58e54de61dbcaf4dc95d
Katherine Zaoral <kathy@vauxoo.com>
2015-08-04 09:10:31 -0500

    [IMP] Update analytic_entry_line_partner module version +0.1 points to ensure that odoo apps refresh the new index.html description

--- analytic_entry_line_partner/__openerp__.py
+++ analytic_entry_line_partner/__openerp__.py
@@ -23,7 +23,7 @@
 ###############################################################################
 {
     "name": "Analytic Entry Line Partner", 
-    "version": "1.5", 
+    "version": "1.6", 
     "author": "Vauxoo", 
     "category": "", 
     "website": "http://www.vauxoo.com/", 
