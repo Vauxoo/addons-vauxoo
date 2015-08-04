32c8d7b55d37365793d16a47d02ec87931a40bc8
oalca <oszckar@gmail.com>
2015-08-04 11:21:55 -0500

    [INIT] basic empty module

---
+++ website_form_validations/README.rst
@@ -0,0 +1,7 @@
+Website Form Validations
+========================
+
+This app allows you to render error messages on sale
+form after checking out on an online purchase, the main
+purpose is to show the messages instead of just making 
+red the input fields.
---
+++ website_form_validations/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- encoding: utf-8 -*-
+{
+    "name": "Website Form Validations",
+    "version": "1.5",
+    "author": "Vauxoo",
+    "category": "Website",
+    "website": "http://www.vauxoo.com/",
+    "license": "AGPL-3",
+    "depends": [
+        "website_sale",
+    ],
+    "demo": [],
+    "data": [
+    ],
+    "test": [],
+    "qweb": [
+        'views/templates.xml'
+             ],
+    "installable": True,
+    "auto_install": False,
+    "active": False
+}
