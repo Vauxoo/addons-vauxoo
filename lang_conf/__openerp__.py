a295d318e4beee8d0ece67e4ddb88ca5d6982591
Katherine Zaoral <kathy@vauxoo.com>
2015-08-04 09:10:47 -0500

    [IMP] Update lang_conf module version +0.1 points to ensure that odoo apps refresh the new index.html description

--- lang_conf/__openerp__.py
+++ lang_conf/__openerp__.py
@@ -25,7 +25,7 @@
 ##############################################################################
 {
     "name": "Complete data from lang english",
-    "version": "1.5",
+    "version": "1.6",
     "author": "Vauxoo",
     "category": "Generics Modules",
     "website": "http://www.vauxoo.com/",
