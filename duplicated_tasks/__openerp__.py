7eb544c308befbe3c699365a1db40fd89d43e701
Katherine Zaoral <kathy@vauxoo.com>
2015-08-04 09:10:30 -0500

    [IMP] Update duplicated_tasks module version +0.1 points to ensure that odoo apps refresh the new index.html description

--- duplicated_tasks/__openerp__.py
+++ duplicated_tasks/__openerp__.py
@@ -21,7 +21,7 @@
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 {
     "name": "Duplicated Tasks", 
-    "version": "0.5", 
+    "version": "0.6", 
     "author": "Vauxoo", 
     "category": "Projects", 
     "website": "http://www.vauxoo.com", 
