f6b679022a709a417b681cd6c4ebac7fd4ed4c87
Katherine Zaoral <kathy@vauxoo.com>
2015-08-04 09:11:04 -0500

    [IMP] Update mrp_consume_produce module version +0.1 points to ensure that odoo apps refresh the new index.html description

--- mrp_consume_produce/__openerp__.py
+++ mrp_consume_produce/__openerp__.py
@@ -27,7 +27,7 @@
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 {
     "name": "MRP Consume Produce", 
-    "version": "1.5", 
+    "version": "1.6", 
     "author": "Vauxoo", 
     "category": "Generic Modules/Production", 
     "website": "http://www.vauxoo.com/", 
