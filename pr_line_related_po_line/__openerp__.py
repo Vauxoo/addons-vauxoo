bc2cca8a3741afef75b397f42b7b95a764391c64
Katherine Zaoral <kathy@vauxoo.com>
2015-08-04 09:10:38 -0500

    [IMP] Update pr_line_related_po_line module version +0.1 points to ensure that odoo apps refresh the new index.html description

--- pr_line_related_po_line/__openerp__.py
+++ pr_line_related_po_line/__openerp__.py
@@ -23,7 +23,7 @@
 ###############################################################################
 {
     "name": "PR Line related PO Line", 
-    "version": "1.5", 
+    "version": "1.6", 
     "author": "Vauxoo", 
     "category": "", 
     "website": "http://www.vauxoo.com/", 
