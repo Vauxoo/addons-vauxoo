995a521d84cfb85a2d5c23c35403b694975f8d0f
Katherine Zaoral <kathy@vauxoo.com>
2015-08-04 09:11:03 -0500

    [IMP] Update stock_picking_validate_past module version +0.1 points to ensure that odoo apps refresh the new index.html description

--- stock_picking_validate_past/__openerp__.py
+++ stock_picking_validate_past/__openerp__.py
@@ -25,7 +25,7 @@
 ##############################################################################
 {
     "name": "Stock Validate Past", 
-    "version": "1.5", 
+    "version": "1.6", 
     "author": "Vauxoo", 
     "category": "Warehouse Management", 
     "website": "http://www.vauxoo.com/", 
