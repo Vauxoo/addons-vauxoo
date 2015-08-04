9f661f1dc8372fce62f366c882ec1612162abcb4
Katherine Zaoral <kathy@vauxoo.com>
2015-08-04 09:11:01 -0500

    [IMP] Update account_bank_statement_unfucked module version +0.1 points to ensure that odoo apps refresh the new index.html description

--- account_bank_statement_unfucked/__openerp__.py
+++ account_bank_statement_unfucked/__openerp__.py
@@ -22,7 +22,7 @@
 ##############################################################################
 {
     "name": "Account Bank Statement Unfucked",
-    "version": "1.5",
+    "version": "1.6",
     "author": "Vauxoo",
     "category": "Localization/Mexico",
     "website": "http://www.vauxoo.com/",
