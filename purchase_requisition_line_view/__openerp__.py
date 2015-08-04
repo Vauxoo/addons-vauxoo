e2d0919d2d8bd0036ff41efeb261cad7db195cc2
Katherine Zaoral <kathy@vauxoo.com>
2015-08-04 09:10:59 -0500

    [IMP] Update purchase_requisition_line_view module version +0.1 points to ensure that odoo apps refresh the new index.html description

--- purchase_requisition_line_view/__openerp__.py
+++ purchase_requisition_line_view/__openerp__.py
@@ -23,7 +23,7 @@
 ###############################################################################
 {
     "name": "Purchase Requisition Line View", 
-    "version": "1.5", 
+    "version": "1.6", 
     "author": "Vauxoo", 
     "category": "", 
     "website": "http://www.vauxoo.com/", 
