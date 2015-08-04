74ff5637fc962a50bddeac7953756313b975e4b0
Katherine Zaoral <kathy@vauxoo.com>
2015-08-04 09:10:50 -0500

    [IMP] Update partner_notification_advance module version +0.1 points to ensure that odoo apps refresh the new index.html description

--- partner_notification_advance/__openerp__.py
+++ partner_notification_advance/__openerp__.py
@@ -25,7 +25,7 @@
 #
 {
     "name": "Partner Notification Advance",
-    "version": "1.5",
+    "version": "1.6",
     "author": "Vauxoo",
     "category": "Localization/Mexico",
     "website": "http://www.vauxoo.com/",
