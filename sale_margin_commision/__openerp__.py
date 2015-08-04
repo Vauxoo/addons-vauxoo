f9511f839e54a7ae948686dd8e90b19b9ff0b39c
Katherine Zaoral <kathy@vauxoo.com>
2015-08-04 09:10:56 -0500

    [IMP] Update sale_margin_commision module version +0.1 points to ensure that odoo apps refresh the new index.html description

--- sale_margin_commision/__openerp__.py
+++ sale_margin_commision/__openerp__.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 {
     "name": "Commision against Margin", 
-    "version": "0.5", 
+    "version": "0.6", 
     "author": "Vauxoo", 
     "category": "Sales & Purchases", 
     "website": "http://vauxoo.com", 
