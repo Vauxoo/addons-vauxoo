74eaf524ef252bbdf1d038a956aa299a45186bcc
Katherine Zaoral <kathy@vauxoo.com>
2015-08-04 09:11:00 -0500

    [IMP] Update hr_payslip_validation_home_address module version +0.1 points to ensure that odoo apps refresh the new index.html description

--- hr_payslip_validation_home_address/__openerp__.py
+++ hr_payslip_validation_home_address/__openerp__.py
@@ -25,7 +25,7 @@
 #
 {
     "name": "Hr Payroll Home Address Validation", 
-    "version": "1.5", 
+    "version": "1.6", 
     "author": "Vauxoo", 
     "category": "Localization/Mexico", 
     "website": "http://www.vauxoo.com/", 
