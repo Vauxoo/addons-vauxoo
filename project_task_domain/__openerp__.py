cbf91f56f9700ab8a5f8ef408518b7209e70fa18
Katherine Zaoral <kathy@vauxoo.com>
2015-08-04 09:10:31 -0500

    [IMP] Update project_task_domain module version +0.1 points to ensure that odoo apps refresh the new index.html description

--- project_task_domain/__openerp__.py
+++ project_task_domain/__openerp__.py
@@ -25,7 +25,7 @@
 #
 {
     "name": "Project Required", 
-    "version": "1.5", 
+    "version": "1.6", 
     "author": "Vauxoo", 
     "category": "Generic Modules/Project", 
     "website": "http://www.vauxoo.com/", 
