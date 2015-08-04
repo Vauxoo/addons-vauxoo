bb80b80f8ff88b02424e7ce3f3c810712b1180be
Katherine Zaoral <kathy@vauxoo.com>
2015-08-04 09:10:43 -0500

    [IMP] Update pos_delivery_restaurant module version +0.1 points to ensure that odoo apps refresh the new index.html description

--- pos_delivery_restaurant/__openerp__.py
+++ pos_delivery_restaurant/__openerp__.py
@@ -23,7 +23,7 @@
 ###############################################################################
 {
     "name": "POS Delivery Restaurant", 
-    "version": "1.5", 
+    "version": "1.6", 
     "author": "Vauxoo", 
     "category": "Point Of Sale", 
     "website": "http://www.vauxoo.com/", 
