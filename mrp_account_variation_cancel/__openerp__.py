6e9675a599eaa8a3b2c6fd1640c0b9a17ee9987a
Katherine Zaoral <kathy@vauxoo.com>
2015-08-04 09:10:35 -0500

    [IMP] Update mrp_account_variation_cancel module version +0.1 points to ensure that odoo apps refresh the new index.html description

--- mrp_account_variation_cancel/__openerp__.py
+++ mrp_account_variation_cancel/__openerp__.py
@@ -26,7 +26,7 @@
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 {
     "name": "Account Variation Cancel", 
-    "version": "1.5", 
+    "version": "1.6", 
     "author": "Vauxoo", 
     "category": "Generic Modules/Account", 
     "website": "http://www.vauxoo.com/", 
