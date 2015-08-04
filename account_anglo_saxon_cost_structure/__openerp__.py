11d5ba879b1caeb079db2d818a6f9d3c94a279a6
Katherine Zaoral <kathy@vauxoo.com>
2015-08-04 09:10:36 -0500

    [IMP] Update account_anglo_saxon_cost_structure module version +0.1 points to ensure that odoo apps refresh the new index.html description

--- account_anglo_saxon_cost_structure/__openerp__.py
+++ account_anglo_saxon_cost_structure/__openerp__.py
@@ -23,7 +23,7 @@
 ################################################################################
 {
     "name": "Cost assigned in the account move", 
-    "version": "0.5", 
+    "version": "0.6", 
     "author": "Vauxoo", 
     "category": "Generic Modules", 
     "website": "http://vauxoo.com", 
