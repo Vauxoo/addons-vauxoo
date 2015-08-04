1aa6eb297c6da4a90fa5e44d77e5a9bc8666daee
Katherine Zaoral <kathy@vauxoo.com>
2015-08-04 09:10:33 -0500

    [IMP] Update mrp_workorder_variation module version +0.1 points to ensure that odoo apps refresh the new index.html description

--- mrp_workorder_variation/__openerp__.py
+++ mrp_workorder_variation/__openerp__.py
@@ -23,7 +23,7 @@
 ###############################################################################
 {
     "name": "MRP WorkOrder Variation", 
-    "version": "1.5", 
+    "version": "1.6", 
     "author": "Vauxoo", 
     "category": "MRP", 
     "website": "http://vauxoo.com", 
