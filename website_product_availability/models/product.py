9a0357244725cfd1d2969aae34a36e4b71d81341
Hugo Adan <hugo@vauxoo.com>
2015-08-06 17:33:19 +0000

    [REF] change development to new api

--- website_product_availability/models/product.py
+++ website_product_availability/models/product.py
@@ -22,26 +22,26 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from openerp.osv import osv, fields
+from openerp import models, fields, api
 
 
-class stock_location_route(osv.Model):
+class stock_location_route(models.Model):
     _inherit = 'stock.location.route'
-    _columns = {
-        'consider_on_request': fields.boolean('Route On Request',
-                                              help="Checking this you will\
-                                              consider this route as an \
-                                              'On Request' route, this means\
-                                              that every product that has this\
-                                              route assigned will show on the\
-                                              website the legend 'On Request'\
-                                              on the availability field,\
-                                              please be careful to set up this\
-                                              route."),
-    }
+
+    consider_on_request = fields.Boolean(
+        'Route On Request',
+        help='Checking this you will '
+             'consider this route as an '
+             '\'On Request\' route, this means '
+             'that every product that has this '
+             'route assigned will show on the '
+             'website the legend \'On Request\' '
+             'on the availability field, '
+             'please be careful to set up this '
+             'route.')
 
 
-class product_product(osv.Model):
+class product_product(models.Model):
     _inherit = 'product.product'
     SELECTION_LIST = [
         ('1', 'Available'),
@@ -50,116 +50,90 @@ class product_product(osv.Model):
         ('4', 'On Request'),
     ]
 
-    def _get_availability(self, cr, uid, ids, name, args, context=None):
-        res = {}
-        for product in self.browse(cr, uid, ids):
-            for route in product.route_ids:
-                if route.consider_on_request:
-                    res[product.id] = '4'
-                    return res
-            if product.qty_available > product.low_stock:
-                res[product.id] = '1'
-                return res
-            elif 0 < product.qty_available <= product.low_stock:
-                res[product.id] = '3'
-                return res
-            elif product.qty_available <= 0:
-                res[product.id] = '2'
-                return res
+    @api.one
+    @api.depends('qty_available', 'low_stock')
+    def _get_availability(self):
+        product = self
+        for route in product.route_ids:
+            if route.consider_on_request:
+                product.stock_state = '4'
+                return
+        if product.qty_available > product.low_stock:
+            product.stock_state = '1'
+        elif 0 < product.qty_available <= product.low_stock:
+            product.stock_state = '3'
+        elif product.qty_available <= 0:
+            product.stock_state = '2'
 
-    _columns = {
-        'low_stock': fields.integer('Low Stock', help="This field is used to show\
-    on the website when a product has low availability, any number of stock\
-    equals the value set or lower will show 'low avilability' on product "),
-        'stock_state': fields.function(
-            _get_availability, type='selection',
-            method=True, selection=SELECTION_LIST,
-            string="Website Stock State",
-            store={'product.product': (lambda self, cr, uid, ids,
-                                       context={}: ids, ['qty_available',
-                                                         'low_stock'], 15)}),
-    }
-    _default = {
-        'low_stock': 0,
-    }
+    low_stock = fields.Integer(
+        'Low Stock',
+        help='This field is used to show '
+        'on the website when a product has '
+        'low availability, any number of stock'
+        'equals the value set or lower will show '
+        '\'low avilability\' on product')
+    stock_state = fields.Selection(
+        compute=_get_availability,
+        selection=SELECTION_LIST,
+        string="Website Stock State",
+        default=0)
 
 
-class LocationQuants(osv.Model):
+class LocationQuants(models.Model):
     _name = 'location.quants'
 
-    _columns = {
-        'product_id': fields.many2one('product.template', 'Product'),
-        'location_id': fields.many2one('stock.location', 'Stock Location'),
-        'qty': fields.float('Quantity')
-    }
+    product_id = fields.Many2one('product.template', 'Product')
+    location_id = fields.Many2one('stock.location', 'Stock Location')
+    qty = fields.Float('Quantity')
 
 
-class ProductTemplate(osv.Model):
+class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    def _get_locations_quants(
-            self, cr, uid, ids, field_names, arg=None, context=None):
-        stock_locations_obj = self.pool.get('location.quants')
-        stock_quants_obj = self.pool.get('stock.quant')
-        res = {}
-        for product_id in ids:
-            products = self._get_products(cr, uid, product_id, context=context)
-            quants = stock_quants_obj.search(
-                cr, uid,
-                [('product_id', 'in', products),
-                 ('location_id.usage', '=', 'internal')])
-            stock_locations_ids = []
-            for quant in stock_quants_obj.browse(
-                    cr, uid, quants, context=context):
-                if quant.location_id.id in stock_locations_ids:
-                    continue
-                stock_locations_ids.append(quant.location_id.id)
+    @api.multi
+    def _get_locations_quants(self):
+        stock_locations_obj = self.env['location.quants']
+        stock_quants_obj = self.env['stock.quant']
+        product_id = self.id
+        products = self._get_products()
+        quants = stock_quants_obj.search(
+            [('product_id', 'in', products),
+             ('location_id.usage', '=', 'internal')])
+        stock_locations_ids = []
+        for quant in quants:
+            if quant.location_id.id in stock_locations_ids:
+                continue
+            stock_locations_ids.append(quant.location_id.id)
+        location_quant_ids = stock_locations_obj.search(
+            [('product_id', 'in', products),
+             ('location_id', 'in', stock_locations_ids)])
+        if location_quant_ids:
+            stock_locations_obj.unlink(location_quant_ids)
+        ctx = dict(self._context)
+        new_quants = []
+        for location in stock_locations_ids:
+            ctx['location'] = location
+            qtys = self._product_available(None, False, context=ctx)
+            new_id = stock_locations_obj.create(
+                {'product_id': product_id,
+                 'location_id': location,
+                 'qty': qtys.get(product_id).get('qty_available')})
+            new_quants.append(new_id.id)
+        self.product_stock_quants_ids = new_quants
 
-            location_quant_ids = stock_locations_obj.search(
-                cr, uid, [('product_id', 'in', products),
-                          ('location_id', 'in', stock_locations_ids)],
-                context=context)
+    @api.multi
+    def _get_planned_dates(self):
+        pol_obj = self.env['purchase.order.line']
+        products = self._get_products()
+        pol_ids = pol_obj.search([('product_id', 'in', products),
+                                  ('state', 'in', ('draft', 'confirmed'))])
+        self.product_planned_dates_ids = pol_ids
 
-            if location_quant_ids:
-                stock_locations_obj.unlink(
-                    cr, uid, location_quant_ids, context=context)
-            ctx = dict(context)
-            new_quants = []
-            for location in stock_locations_ids:
-                ctx['location'] = location
-                qtys = self._product_available(
-                    cr, uid, ids, field_names, arg, context=ctx)
-                new_id = stock_locations_obj.create(
-                    cr, uid,
-                    {'product_id': product_id,
-                     'location_id': location,
-                     'qty': qtys.get(ids[0]).get('qty_available')})
-                new_quants.append(new_id)
-
-            res[product_id] = new_quants
-        return res
-
-    def _get_planned_dates(
-            self, cr, uid, ids, field_names, arg=None, context=None):
-        pol_obj = self.pool.get('purchase.order.line')
-        res = {}
-        for product_id in ids:
-            products = self._get_products(cr, uid, ids, context=context)
-            pol_ids = pol_obj.search(
-                cr, uid, [('product_id', 'in', products),
-                          ('state', 'in', ('draft', 'confirmed'))],
-                context=context)
-            res[product_id] = pol_ids
-        return res
-
-    _columns = {
-        'product_stock_quants_ids': fields.function(
-            _get_locations_quants,
-            relation='location.quants',
-            type='one2many',
-            string='Locations Quants'),
-        'product_planned_dates_ids': fields.function(
-            _get_planned_dates,
-            relation='purchase.order.line',
-            type='one2many',
-            string='Locations Quants'), }
+    product_stock_quants_ids = fields.One2many(
+        compute=_get_locations_quants,
+        comodel_name='location.quants',
+        string='Locations Quants')
+    product_planned_dates_ids = fields.One2many(
+        compute=_get_planned_dates,
+        comodel_name='purchase.order.line',
+        string='Locations Quants')
