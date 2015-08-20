9d635f0675834295bb75b5781a324fe8077f0a85
Matjaž Mozetič <info@matmoz.si>
2015-08-20 08:12:09 +0200

    pep-8 cleaning

--- note_to_task/model/__init__.py
+++ note_to_task/model/__init__.py
@@ -3,10 +3,11 @@
 #    Module Writen to OpenERP, Open Source Management Solution
 #    Copyright (C) Vauxoo (<http://vauxoo.com>).
 #    All Rights Reserved
-################# Credits######################################################
+#
+#    CREDITS
 #    Coded by: Luis Escobar <luis@vauxoo.com>
 #    Audited by: Nhomar Hernandez <nhomar@vauxoo.com>
-###############################################################################
+#
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published
 #    by the Free Software Foundation, either version 3 of the License, or
