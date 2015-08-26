de0aba5c06507abe014412fbcd9fabd9db35d1ea
julioserna <julio@vauxoo.com>
2015-08-26 23:06:04 +0000

    [IMP][payment_term_type] Added module to compute payment type credit/cash depending of the lines to compute in payment terms

---
+++ payment_term_type/__openerp__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    'name': 'Payment Terms Type',
+    'version': '1.0',
+    'author': 'Vauxoo',
+    'category': '',
+    'depends': [
+        'account',
+    ],
+    'demo': [],
+    'website': 'https://www.vauxoo.com',
+    'data': [
+        'views/account_payment_type_view.xml',
+    ],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:soft:tabstop=4:shiftwidth=4:
