7ed1e3e9aea62b3e8a0bfa5153a5ae6a9f8df2d2
Hugo Adan <hugo@vauxoo.com>
2015-09-04 00:04:33 +0000

    [ADD] added partner_credit_limit module from addons-vauxoo/6.1

---
+++ partner_credit_limit/__init__.py
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+############################################################################
+#    Module Writen For Odoo, Open Source Management Solution
+#
+#    Copyright (c) 2011 Vauxoo - http://www.vauxoo.com
+#    All Rights Reserved.
+#    info Vauxoo (info@vauxoo.com)
+#    coded by: hugo@vauxoo.com
+#    planned by: Nhomar Hernandez <nhomar@vauxoo.com>
+############################################################################
+from . import model
