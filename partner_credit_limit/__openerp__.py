965d274cffd9420a838b1b72ff60b9782a7472e1
Hugo Adan <hugo@vauxoo.com>
2015-09-05 16:17:49 +0000

    [FIX]flake8

--- partner_credit_limit/__openerp__.py
+++ partner_credit_limit/__openerp__.py
@@ -8,23 +8,23 @@
 #    coded by: hugo@vauxoo.com
 #    planned by: Nhomar Hernandez <nhomar@vauxoo.com>
 ############################################################################
-{"name": "Partner Credit Limit",
- "version": "1.0",
- "author": "Vauxoo",
- "category": "",
- "website": "http://www.vauxoo.com/",
- "license": "AGPL-3",
- "depends": [
-    "account",
-    "sale",
-    "payment_term_type",
- ],
- "demo": [],
- "data": [
-    "view/invoice_workflow.xml",
-    "view/partner_view.xml",
-    # "view/sale_order_view.xml",
- ],
- "installable": True,
- "active": False,
- }
+{
+    "name": "Partner Credit Limit",
+    "version": "1.0",
+    "author": "Vauxoo",
+    "category": "",
+    "website": "http://www.vauxoo.com/",
+    "license": "AGPL-3",
+    "depends": [
+        "account",
+        "sale",
+        "payment_term_type", ],
+    "demo": [],
+    "data": [
+        "view/invoice_workflow.xml",
+        "view/partner_view.xml",
+        # "view/sale_order_view.xml",
+    ],
+    "installable": True,
+    "active": False,
+}
