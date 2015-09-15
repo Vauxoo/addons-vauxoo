ee719dd7ed1456c02ec13b7a42ea5c00e5dac29c
Hugo Adan <hugo@vauxoo.com>
2015-09-15 23:05:52 +0000

    [REF] added some comments to the test

---
+++ partner_credit_limit/tests/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+############################################################################
+#    Module Writen For Odoo, Open Source Management Solution
+#
+#    Copyright (c) 2011 Vauxoo - http://www.vauxoo.com
+#    All Rights Reserved.
+#    info Vauxoo (info@vauxoo.com)
+#    coded by: hugo@vauxoo.com
+#    planned by: Nhomar Hernandez <nhomar@vauxoo.com>
+############################################################################
+from . import test_invoice_credit_limit
