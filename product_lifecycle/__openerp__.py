8b09a1370cb4fa9cc4bae1c47a8e90f7c87d7b27
Moises Lopez <moylop260@vauxoo.com>
2015-09-16 00:07:03 -0500

    all py: Delete vim comment, add coding comment with pep8 style
    '[REF] addons-vauxoo: Standardize project with odoo guidelines.'

--- product_lifecycle/__openerp__.py
+++ product_lifecycle/__openerp__.py
@@ -1,4 +1,4 @@
-# -*- encoding: utf-8 -*-
+# coding: utf-8
 ###############################################################################
 #    Module Writen to OpenERP, Open Source Management Solution
 #    Copyright (C) OpenERP Venezuela (<http://www.vauxoo.com>).
@@ -29,6 +29,7 @@
     "version": "1.6",
     "author": "Vauxoo",
     "website": "http://www.vauxoo.com/",
+    "license": "AGPL-3",
     "category": "product",
     "depends": [
         "product",
--- product_lifecycle/model/purchase.py
+++ product_lifecycle/model/purchase.py
@@ -1,4 +1,4 @@
-# -*- encoding: utf-8 -*-
+# coding: utf-8
 ###############################################################################
 #    Module Writen to OpenERP, Open Source Management Solution
 #    Copyright (C) OpenERP Venezuela (<http://www.vauxoo.com>).
@@ -100,7 +100,7 @@ class PurchaseOrderLine(models.Model):
                 if replacements:
                     msg += _('Select one of the replacement products.')
                 else:
-                    msg += ('\n'*2 + _(
+                    msg += ('\n' * 2 + _(
                         'The are not replacement products defined for the'
                         ' product you selected. Please select another product'
                         ' or define a replacement product in the product form'
