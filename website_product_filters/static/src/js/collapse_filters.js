43df25287f5c3f672d69f5055fb8ef63597d5d14
oalca <oszckar@gmail.com>
2015-09-21 14:01:47 -0500

    [IMP] documentation of method

--- website_product_filters/static/src/js/collapse_filters.js
+++ website_product_filters/static/src/js/collapse_filters.js
@@ -7,24 +7,19 @@
     start: function(){
       this.redrow();
     },
-    // stop: function(){
-    //   this.clean();
-    // },
 
     redrow: function(debug){
-      // this.clean(debug);
       this.build(debug);
     },
 
-    // clean:function(debug){
-    //   this.$target.empty();
-    // },
-
+      /* this method hides and shows all lists of attributes that are longer
+         than 4 lines. */
     build: function(debug){
-      $(".show-more").click(function(){
-        var parent = $(".show-more").parent();
-        var button = $(".show-more");
-        var hidden_elements = parent.find(".hidden");
+      var $show_more = this.$(".show-more");
+      this.$(".show-more").click(function(){
+        var parent = $show_more.parent(),
+            button = $show_more,
+            hidden_elements = parent.find(".hidden");
         if (button.hasClass("clicked")){
           var shown_elements = parent.find(".un-hidden");
           shown_elements.addClass("hidden");
