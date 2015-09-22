46aecf8fe33d9d2c9787bfb8ee91c423972d2e07
luistorresm <luis_t@vauxoo.com>
2015-09-22 15:49:54 -0500

    [IMP][purchase_third_validation]Fixed pylint

--- purchase_third_validation/__openerp__.py
+++ purchase_third_validation/__openerp__.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 ############################################################################
 #    Module Writen For Odoo, Open Source Management Solution
 #
@@ -31,4 +32,3 @@
     "installable": True,
     "auto_install": False
 }
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
