2ac71e77781044c870c7483cd6e56180428035a8
luistorresm <luis_t@vauxoo.com>
2015-09-22 15:01:55 -0500

    [ADD][purchase_third_validation]Added third level of approval in purchase order

---
+++ purchase_third_validation/__init__.py
@@ -0,0 +1,10 @@
+############################################################################
+#    Module Writen For Odoo, Open Source Management Solution
+#
+#    Copyright (c) 2015 Vauxoo - http://www.vauxoo.com
+#    All Rights Reserved.
+#    info Vauxoo (info@vauxoo.com)
+#    coded by: aguzman@vauxoo.com
+#    planned by: Sabrina Romero <sabrina@vauxoo.com>
+############################################################################
+from . import models
