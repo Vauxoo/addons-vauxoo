0afd8795bcf9d32518ac262d2611773069074061
Hugo Adan <hugo@vauxoo.com>
2015-09-23 23:24:18 +0000

    [IMP]Payments terms based on qty of payments and based on date of payments

--- payment_term_type/models/__init__.py
+++ payment_term_type/models/__init__.py
@@ -23,4 +23,4 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from . import payment_term
+from . import account_settings, payment_term
