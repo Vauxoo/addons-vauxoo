d69772ae0e7c771133d9465a2ea616ba7df05330
Hugo Adan <hugo@vauxoo.com>
2015-09-26 01:37:19 +0000

    [REF]change all api.one by api.multi

--- partner_credit_limit/model/accout_invoice.py
+++ partner_credit_limit/model/accout_invoice.py
@@ -16,19 +16,21 @@ class AccontInvoice(models.Model):
 
     _inherit = 'account.invoice'
 
-    @api.one
+    @api.multi
     def check_limit_credit(self):
-        if self.payment_term.payment_type != 'credit':
-            return True
-        allowed_sale = self.env['res.partner'].with_context(
-            {'new_amount': self.amount_total,
-             'new_currency': self.currency_id.id}).browse(
-                 self.partner_id.id).allowed_sale
-        if allowed_sale:
-            return True
-        else:
-            msg = _('Can not validate the Invoice because Partner '
-                    'has late payments or has exceeded the credit limit.'
-                    '\nPlease cover the late payment or check credit limit'
-                    '\nCreadit Limit : %s') % (self.partner_id.credit_limit)
-            raise exceptions.Warning(('Warning!'), msg)
+        for invoice in self:
+            if invoice.payment_term.payment_type != 'credit':
+                return True
+            allowed_sale = self.env['res.partner'].with_context(
+                {'new_amount': invoice.amount_total,
+                 'new_currency': invoice.currency_id.id}).browse(
+                     invoice.partner_id.id).allowed_sale
+            if allowed_sale:
+                return True
+            else:
+                msg = _('Can not validate the Invoice because Partner '
+                        'has late payments or has exceeded the credit limit.'
+                        '\nPlease cover the late payment or check credit limit'
+                        '\nCreadit'
+                        ' Limit : %s') % (invoice.partner_id.credit_limit)
+                raise exceptions.Warning(('Warning!'), msg)
--- partner_credit_limit/model/sale.py
+++ partner_credit_limit/model/sale.py
@@ -15,19 +15,20 @@ from openerp import exceptions
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    @api.one
+    @api.multi
     def check_limit(self):
-        if self.payment_term.payment_type != 'credit':
-            return True
-        allowed_sale = self.env['res.partner'].with_context(
-            {'new_amount': self.amount_total,
-             'new_currency': self.company_id.currency_id.id}).browse(
-                 self.partner_id.id).allowed_sale
-        if allowed_sale:
-            return True
-        else:
-            msg = _('Can not confirm the Sale Order because Partner '
-                    'has late payments or has exceeded the credit limit.'
-                    '\nPlease cover the late payment or check credit limit'
-                    '\nCreadit Limit : %s') % (self.partner_id.credit_limit)
-            raise exceptions.Warning(('Warning!'), msg)
+        for so in self:
+            if so.payment_term.payment_type != 'credit':
+                return True
+            allowed_sale = self.env['res.partner'].with_context(
+                {'new_amount': so.amount_total,
+                 'new_currency': so.company_id.currency_id.id}).browse(
+                     so.partner_id.id).allowed_sale
+            if allowed_sale:
+                return True
+            else:
+                msg = _('Can not confirm the Sale Order because Partner '
+                        'has late payments or has exceeded the credit limit.'
+                        '\nPlease cover the late payment or check credit limit'
+                        '\nCreadit Limit : %s') % (so.partner_id.credit_limit)
+                raise exceptions.Warning(('Warning!'), msg)
