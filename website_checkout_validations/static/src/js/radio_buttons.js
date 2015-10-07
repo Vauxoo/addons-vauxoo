4d845288beb970773c35e636dc5570011945055a
Hugo Adan <hugo@vauxoo.com>
2015-10-07 02:32:36 +0000

    [REF]Hide input ruc when company is foreign

--- website_checkout_validations/static/src/js/radio_buttons.js
+++ website_checkout_validations/static/src/js/radio_buttons.js
@@ -25,34 +25,48 @@
          if ($("select[name=country_id]").find(":selected").attr("id") == 'PA')
          {
           $("div[id=zip]").hide();
+          $("div[id=ruc_values]").css('visibility','visible');
+         }
+         else{
+          $("div[id=ruc_values]").css('visibility','hidden');
          }
        $("select[name=country_id]").click(function(){
          if ($("select[name=country_id]").find(":selected").attr("id") == 'PA')
          {
           $("div[id=zip]").hide();
+          $("div[id=ruc_values]").css('visibility','visible');
          }
          else
-         {
+         {  
           $("div[id=zip]").show();
+          $("div[id=ruc_values]").css('visibility','hidden');
          }
        });
        // THIS CODE HIDES THE COMPANY INPUT IF PARTNER IS PARTICULAR
        if ($("#partner_type_p").is(':checked'))
         {
-          $("div[id=ruc_values]").hide();
+          $("div[id=ruc_values]").css('visibility','hidden');
           $("div[id=company]").hide();
         }
        $(".partner_type").click(function(){
          var selectedBox = this.id;
          if (this.value == 'particular')
          {
-          $("div[id=ruc_values]").hide();
+          $("div[id=ruc_values]").css('visibility','hidden');
           $("div[id=company]").hide();
          }
          else
          {
           $("div[id=company]").show();
-          $("div[id=ruc_values]").show();
+          // If is company and is foreign
+          if ($("select[name=country_id]").find(":selected").attr("id") == 'PA')
+         {
+          $("div[id=ruc_values]").css('visibility','visible');
+         }
+         else
+         {
+          $("div[id=ruc_values]").css('visibility','hidden');
+         }
          }
 
        });
