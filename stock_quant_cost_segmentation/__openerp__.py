86bc83b482b2eeb963c4adc661a6a73c44ff2158
Humberto Arocha <hbto@vauxoo.com>
2015-10-09 02:41:02 -0430

    [IMP] Scaffolding module for Segmentation for Costs in Quants.

---
+++ stock_quant_cost_segmentation/__openerp__.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+
+{
+    "name": "Stock Quant Cost Segmentation",
+    "version": "1.0",
+    "author": "Vauxoo",
+    "category": "Tools",
+    "website": "http://www.vauxoo.com/",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+        "stock",
+    ],
+    "demo": [
+    ],
+    "data": [
+        'view/view.xml',
+    ],
+    "test": [],
+    "js": [],
+    "css": [],
+    "qweb": [],
+    "installable": True,
+    "auto_install": False,
+    "external_dependencies": {
+    }
+}
