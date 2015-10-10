f25eb073a2fc2cb942b16b54bb5db6e5d55537e1
Humberto Arocha <hbto@vauxoo.com>
2015-10-10 00:42:49 -0430

    [IMP] Writing Material Cost to newly created field. At time of quant creation Material Cost is the same than Unit Cost

--- stock_quant_cost_segmentation/model/stock.py
+++ stock_quant_cost_segmentation/model/stock.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-from openerp import models, fields
+from openerp import models, fields, api
 
 
 class StockQuant(models.Model):
@@ -10,3 +10,16 @@ class StockQuant(models.Model):
     production_cost = fields.Float(string='Production Cost')
     subcontracting_cost = fields.Float(string='Subcontracting Cost')
     landed_cost = fields.Float(string='Landed Cost')
+
+    @api.v7
+    def _quant_create(
+            self, cr, uid, qty, move, lot_id=False, owner_id=False,
+            src_package_id=False, dest_package_id=False,
+            force_location_from=False, force_location_to=False, context=None):
+        quant = super(StockQuant, self)._quant_create(
+            cr, uid, qty, move, lot_id=lot_id, owner_id=owner_id,
+            src_package_id=src_package_id, dest_package_id=dest_package_id,
+            force_location_from=force_location_from,
+            force_location_to=force_location_to, context=context)
+        quant.write({'material_cost': quant.cost})
+        return quant
