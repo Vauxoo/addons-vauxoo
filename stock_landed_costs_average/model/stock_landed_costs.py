2e0105a3ff83e38dc05c851d882b8d90bd0d6cbc
Humberto Arocha <hbto@vauxoo.com>
2015-10-12 03:40:16 -0430

    [IMP] Redefined compute_landed_cost method to allow taking into account stock_move coming from production

--- stock_landed_costs_average/model/stock_landed_costs.py
+++ stock_landed_costs_average/model/stock_landed_costs.py
@@ -345,3 +345,74 @@ class StockLandedCost(models.Model):
             cost.write(
                 {'state': 'done', 'account_move_id': move_id})
         return True
+
+    @api.v7
+    def compute_landed_cost(self, cr, uid, ids, context=None):
+        line_obj = self.pool.get('stock.valuation.adjustment.lines')
+        unlink_ids = line_obj.search(
+            cr, uid, [('cost_id', 'in', ids)], context=context)
+        line_obj.unlink(cr, uid, unlink_ids, context=context)
+        digits = dp.get_precision('Product Price')(cr)
+        towrite_dict = {}
+        for cost in self.browse(cr, uid, ids, context=None):
+            if not cost.picking_ids and not cost.move_ids:
+                continue
+            picking_ids = [p.id for p in cost.picking_ids]
+            total_qty = 0.0
+            total_cost = 0.0
+            total_weight = 0.0
+            total_volume = 0.0
+            total_line = 0.0
+            vals = self.get_valuation_lines(
+                cr, uid, [cost.id], picking_ids=picking_ids, context=context)
+            for v in vals:
+                for line in cost.cost_lines:
+                    v.update({'cost_id': cost.id, 'cost_line_id': line.id})
+                    line_obj.create(cr, uid, v, context=context)
+                total_qty += v.get('quantity', 0.0)
+                total_cost += v.get('former_cost', 0.0)
+                total_weight += v.get('weight', 0.0)
+                total_volume += v.get('volume', 0.0)
+                total_line += 1
+
+            for line in cost.cost_lines:
+                value_split = 0.0
+                for valuation in cost.valuation_adjustment_lines:
+                    value = 0.0
+                    if valuation.cost_line_id and \
+                            valuation.cost_line_id.id == line.id:
+                        if line.split_method == 'by_quantity' and total_qty:
+                            per_unit = (line.price_unit / total_qty)
+                            value = valuation.quantity * per_unit
+                        elif line.split_method == 'by_weight' and total_weight:
+                            per_unit = (line.price_unit / total_weight)
+                            value = valuation.weight * per_unit
+                        elif line.split_method == 'by_volume' and total_volume:
+                            per_unit = (line.price_unit / total_volume)
+                            value = valuation.volume * per_unit
+                        elif line.split_method == 'equal':
+                            value = (line.price_unit / total_line)
+                        elif line.split_method == 'by_current_cost_price' and \
+                                total_cost:
+                            per_unit = (line.price_unit / total_cost)
+                            value = valuation.former_cost * per_unit
+                        else:
+                            value = (line.price_unit / total_line)
+
+                        if digits:
+                            value = float_round(
+                                value, precision_digits=digits[1],
+                                rounding_method='UP')
+                            value = min(value, line.price_unit - value_split)
+                            value_split += value
+
+                        if valuation.id not in towrite_dict:
+                            towrite_dict[valuation.id] = value
+                        else:
+                            towrite_dict[valuation.id] += value
+        if towrite_dict:
+            for key, value in towrite_dict.items():
+                line_obj.write(
+                    cr, uid, key, {'additional_landed_cost': value},
+                    context=context)
+        return True
