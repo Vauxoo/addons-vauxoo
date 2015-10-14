65f9e624d2ed9e4f464caf74602175c4ffbc9ae7
Luis Torres <luis_t@vauxoo.com>
2015-10-14 21:38:19 +0000

    [IMP]Fixed pylint

--- aging_due_report/tests/common.py
+++ aging_due_report/tests/common.py
@@ -1,4 +1,4 @@
-# -*- encoding: utf-8 -*-
+# coding: utf-8
 # ##########################################################################
 #    Module Writen to ODOO, Open Source Management Solution
 #
