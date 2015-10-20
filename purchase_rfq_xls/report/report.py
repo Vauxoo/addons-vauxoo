e07ad9a4d46881681e4b77faf93df0e64c98a9bd
Jose Suniaga <josemiguel@vauxoo.com>
2015-10-20 19:14:56 +0000

    [REF] purchase_rfq_xls: Updated spanish i18n

--- purchase_rfq_xls/report/report.py
+++ purchase_rfq_xls/report/report.py
@@ -23,13 +23,13 @@ from openerp.osv import osv
 from openerp.report import report_sxw
 from datetime import datetime
 
+
 class Parser(report_sxw.rml_parse):
     def __init__(self, cr, uid, name, context):
         super(Parser, self).__init__(cr, uid, name, context=context)
         self.localcontext.update({
-                        'datetime': datetime,
+            'datetime': datetime,
         })
-
 
 
 class PurchaseQuotationReportXLS(osv.AbstractModel):
