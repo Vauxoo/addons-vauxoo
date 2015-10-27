34b4cbf85a8e735bd21f9341523f1e3591f709f9
Humberto Arocha <hbto@vauxoo.com>
2015-10-27 14:50:30 -0430

    [IMP] Module renaming

---
+++ account_move_line_production/__openerp__.py
@@ -0,0 +1,45 @@
+# coding: utf-8
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#
+#    Copyright (c) 2012 Vauxoo - http://www.vauxoo.com
+#    All Rights Reserved.
+#    info@vauxoo.com
+############################################################################
+#    Coded by: fernandoL (fernando_ld@vauxoo.com)
+############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+#'account_view.xml',
+{
+    "name": "Account relation move",
+    "version": "1.6",
+    "author": "Vauxoo",
+    "category": "Generic Modules/Account",
+    "website": "http://www.vauxoo.com/",
+    "license": "",
+    "depends": [
+        "mrp"
+    ],
+    "demo": [],
+    "data": [],
+    "test": [],
+    "js": [],
+    "css": [],
+    "qweb": [],
+    "installable": True,
+    "auto_install": False,
+}
---
+++ account_move_line_production/model/account.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+
+from openerp import models, fields
+
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    production_id = fields.Many2one(
+        'mrp.production',
+        string='Production ID',
+        readonly=True,
+        )
