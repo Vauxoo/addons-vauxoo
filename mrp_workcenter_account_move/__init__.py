e7393d8bd9e73590e54bcde699d2af7f6f524398
Humberto Arocha <hbto@vauxoo.com>
2015-10-27 13:25:13 -0430

    [IMP] Added scaffolding module for mrp_workcenter_account_move

---
+++ mrp_workcenter_account_move/__init__.py
@@ -0,0 +1,5 @@
+# coding: utf-8
+
+from . import model
+from . import wizard
+from . import report
---
+++ mrp_workcenter_account_move/model/mrp.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+from openerp import models, api
+
+
+class MrpProduction(models.TransientModel):
+    """
+    Production Orders / Manufacturing Orders
+    """
+    _inherit = 'mrp.production'
+    _description = 'Manufacturing Order'
+
+    @api.v7
+    def _costs_generate(self, cr, uid, production):
+        """ Calculates total costs at the end of the production.
+        @param production: Id of production order.
+        @return: Calculated amount.
+        """
+        return super(MrpProduction, self)._costs_generate(cr, uid, production)
