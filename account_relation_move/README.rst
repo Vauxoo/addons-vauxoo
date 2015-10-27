758da8140a2c9f9cf2a6ee581c0500063c1e12dc
Humberto Arocha <hbto@vauxoo.com>
2015-10-27 14:58:20 -0430

    [IMP] Backward compatibility module

---
+++ account_relation_move/README.rst
@@ -0,0 +1,5 @@
+Account Relation Move
+=====================
+
+This is a deprecated module. It is here to allow backward compatibility.
+Instead use `Account Move Line Production`
---
+++ account_relation_move/__openerp__.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+{
+    "name": "Account relation move",
+    "version": "1.6",
+    "author": "Vauxoo",
+    "category": "Generic Modules/Account",
+    "website": "http://www.vauxoo.com/",
+    "license": "",
+    "depends": [
+        'account_move_line_production',
+    ],
+    "demo": [],
+    "data": [],
+    "test": [],
+    "js": [],
+    "css": [],
+    "qweb": [],
+    "installable": True,
+    "auto_install": False,
+}
