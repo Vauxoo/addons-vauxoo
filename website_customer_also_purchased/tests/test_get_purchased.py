07f84c4ec12e9191061effc4a6f981d4af03e14d
oalca <oszckar@gmail.com>
2015-10-28 21:13:29 -0600

    [FIX] author

--- website_customer_also_purchased/tests/test_get_purchased.py
+++ website_customer_also_purchased/tests/test_get_purchased.py
@@ -5,8 +5,8 @@
 #    Copyright (c) 2011 Vauxoo - http://www.vauxoo.com
 #    All Rights Reserved.
 #    info Vauxoo (info@vauxoo.com)
-#    coded by: hugo@vauxoo.com
-#    planned by: Nhomar Hernandez <nhomar@vauxoo.com>
+#    coded by: oscar@vauxoo.com
+#    planned by: Oscar Alcala <oscar@vauxoo.com>
 ############################################################################
 
 from openerp.tests.common import TransactionCase
