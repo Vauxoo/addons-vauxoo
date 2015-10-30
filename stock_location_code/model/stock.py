e39173e5953eebe9d9d6fa0c812ce7a2126e59b1
KarenKawaii <karenamaroc@gmail.com>
2015-10-30 01:02:34 +0000

    [IMP] stock_location_code: Add warehouse to location in m2o fields.

--- stock_location_code/model/stock.py
+++ stock_location_code/model/stock.py
@@ -3,6 +3,9 @@
 """
 Inherit the stock location model to add a code attribute and make the code
 searchable.
+
+New feature: Add the warehouse to location.
+e.g. [A012345] Stock(El Dorado)
 """
 
 ###########################################################################
@@ -53,7 +56,9 @@ class StockLocation(osv.Model):
             if not ids:
                 ids = set()
                 ids.update(self.search(cr, user, args + [(
-                    'loc_barcode', operator, name)], limit=limit, context=context))
+                    'loc_barcode', operator, name)],
+                    limit=limit,
+                    context=context))
                 if not limit or len(ids) < limit:
                     # we may underrun the limit because of dupes in the
                     # results, that's fine
@@ -75,32 +80,43 @@ class StockLocation(osv.Model):
         result = self.name_get(cr, user, ids, context=context)
         return result
 
-    def _name_get(self, data_dict, context=None):
+    def _name_get(self, cr, uid, location, context=None):
         """
-        @return dictionary
+        Implements the stock location code in a new feauture, where if the
+        location has a reference to a warehouse, the name in a m2o search
+        concatenates the code and warehouse to a location if they exist.
+        Visually, it's better to know which owns the warehouse location.
         """
+        wh_obj = self.pool.get('stock.warehouse')
         context = context or {}
-        name = data_dict.get('name', '')
-        loc_barcode = data_dict.get('loc_barcode', False)
-        if loc_barcode:
-            name = '[%s] %s' % (loc_barcode, name)
-        return (data_dict['id'], name)
-
-    def name_get(self, cr, user, ids, context=None):
-        """
-        overwrite openerp method like the one for product.product model in the
-        product module.
-        """
-        context = context or {}
-        ids = isinstance(ids, (int, long)) and [ids] or ids
-        result = []
-        if not len(ids):
-            return result
-        for location in self.browse(cr, user, ids, context=context):
-            mydict = {
-                'id': location.id,
-                'name': location.name,
-                'loc_barcode': location.loc_barcode,
-            }
-            result.append(self._name_get(mydict))
-        return result
+        name = super(StockLocation, self)._name_get(cr,
+                                                    uid,
+                                                    location,
+                                                    context=context)
+        barcode = ''
+        bracket_left = '['
+        u_bracket_l = unicode(bracket_left, "utf-8")
+        bracket_right = ']'
+        u_bracket_r = unicode(bracket_right, "utf-8")
+        parenth_left = '('
+        u_parenth_l = unicode(parenth_left, "utf-8")
+        parenth_right = ')'
+        u_parenth_r = unicode(parenth_right, "utf-8")
+        if location.loc_barcode:
+            barcode = "{bracket_l}{barcode}{bracket_r}".format(
+                bracket_l=u_bracket_l,
+                barcode=location.loc_barcode.encode("utf-8"),
+                bracket_r=u_bracket_r)
+        wh_id = self.get_warehouse(cr, uid, location, context=context)
+        if wh_id:
+            name_wh = wh_obj.browse(cr, uid, wh_id).name
+            name = "{loc_name} {parenth_l}{warehouse}{parenth_r}".format(
+                loc_name=location.name.encode("utf-8"),
+                parenth_l=u_parenth_l,
+                warehouse=name_wh.encode("utf-8"),
+                parenth_r=u_parenth_r)
+            name = unicode(name, "utf-8")
+        name = "{barcode_loc} {name}".format(
+            barcode_loc=barcode,
+            name=name.encode("utf-8"))
+        return name
