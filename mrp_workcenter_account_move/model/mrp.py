f0acc379eafe6d234c20ecd1020bb4e6a2ac9897
Humberto Arocha <hbto@vauxoo.com>
2015-11-02 03:49:14 -0430

    [IMP] Adjustment Entry Lines for Production Products.

--- mrp_workcenter_account_move/model/mrp.py
+++ mrp_workcenter_account_move/model/mrp.py
@@ -101,6 +101,43 @@ class MrpProduction(models.Model):
         return am_obj.with_context(ctx).create(vals)
 
     @api.multi
+    def _create_adjustment_account_move_line(
+            self, move_id, production_account_id, valuation_account_id, diff):
+        """
+        Generate the account.move.line values to track the production cost.
+        """
+        self.ensure_one()
+        aml_obj = self.env['account.move.line']
+
+        base_line = dict(
+            move_id=move_id.id,
+            name=self.product_id.name + _(' (Valuation Adjustment)'),
+            product_id=self.product_id.id,)
+
+        if diff > 0:
+            debit_line = dict(
+                base_line,
+                debit=diff,
+                account_id=valuation_account_id,)
+            credit_line = dict(
+                base_line,
+                credit=diff,
+                account_id=production_account_id,)
+        else:
+            debit_line = dict(
+                base_line,
+                debit=-diff,
+                account_id=production_account_id,)
+            credit_line = dict(
+                base_line,
+                credit=-diff,
+                account_id=valuation_account_id,)
+
+        aml_obj.create(debit_line)
+        aml_obj.create(credit_line)
+        return True
+
+    @api.multi
     def _create_account_move_line(self, move_id, production_account_id):
         """
         Generate the account.move.line values to track the production cost.
@@ -166,6 +203,22 @@ class MrpProduction(models.Model):
             valuation_in_account_id.id
         return self._create_account_move_line(move_id, production_account_id)
 
+    @api.multi
+    def _create_adjustment_accounting_entries(self, move_id, diff):
+        self.ensure_one()
+        ctx = dict(self._context)
+        template_obj = self.pool.get('product.template')
+        production_account_id = self.product_id.property_stock_production.\
+            valuation_in_account_id.id
+        accounts = template_obj.get_product_accounts(
+            self._cr, self._uid, self.product_id.product_tmpl_id.id,
+            context=ctx)
+        # TODO: make different between (REAL, AVG) and STD products
+        # STD diff shall be booked onto a deviation account
+        valuation_account_id = accounts['property_stock_valuation_account_id']
+        return self._create_adjustment_account_move_line(
+            move_id, production_account_id, valuation_account_id, diff)
+
     @api.v7
     def _costs_generate(self, cr, uid, production):
         """ Calculates total costs at the end of the production.
@@ -173,12 +226,41 @@ class MrpProduction(models.Model):
         @return: Calculated amount.
         """
         amount = self.check_create_accounting_entry(cr, uid, production.id)
+
+        # TODO: This piece of code shall be taken to a new method
+        amount_consumed = 0.0
+        for raw_mat in production.move_lines2:
+            for quant in raw_mat.quant_ids:
+                amount_consumed += quant.cost * quant.qty
+
+        amount_produced = 0.0
+        for raw_mat in production.move_created_ids2:
+            for quant in raw_mat.quant_ids:
+                amount_produced += quant.cost * quant.qty
+
+        diff = amount + amount_consumed - amount_produced
+
+        if not diff and not amount:
+            return amount
+
         if not amount:
+            return amount
+
+        if not any([amount, diff]):
             return amount
 
         move_id = self._create_account_move(cr, uid, production.id)
         production.write({'account_move_id': move_id.id})
-        self._create_accounting_entries(cr, uid, production.id, move_id)
+
+        if amount:
+            self._create_accounting_entries(cr, uid, production.id, move_id)
+        if diff:
+            # NOTE: Subproduct are not taken into accounting in this approach
+            self._create_adjustment_accounting_entries(
+                cr, uid, production.id, move_id, diff)
+            # TODO: if product produced is AVG recompute avg value
+            # TODO: increase/decrease quant value
+            # TODO: increase/decrease segmentation cost on quants
 
         return amount
 
