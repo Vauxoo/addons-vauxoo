ef7373d9eb1a8e40eb6339f4fb0630bab70dc135
oalca <oszckar@gmail.com>
2015-11-05 11:02:15 -0600

    [MONKEYPATCH]

--- website_product_filters/static/src/js/test_attributes.js
+++ website_product_filters/static/src/js/test_attributes.js
@@ -1,17 +1,6 @@
 (function () {
     'use strict';
     var _t = openerp._t;
-    var steps = openerp.Tour.tours.shop_buy_product.steps;
-    for (var k=0; k<steps.length; k++) {
-        if (steps[k].title === "click on add to cart") {
-            steps.splice(k+1, 0, {
-                title:     _t("click in modal on 'Proceed to checkout' button"),
-                element:   '.modal a:contains("Proceed to checkout")',
-            });
-            break;
-        }
-    }
-
     openerp.Tour.register({
         id:   'shop_customize',
         name: "Customize the page and search a product",
