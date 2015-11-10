0d0aa3ffc5f68878666ef70318256054f0b44036
Humberto Arocha <hbto@vauxoo.com>
2015-11-10 15:07:59 -0430

    [IMP] Added Openerp Descriptor

---
+++ product_extended_segmentation/__openerp__.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+{
+    "name": "Product Extension to track Segmentation Cost",
+    "version": "1.6",
+    "author": "Vauxoo",
+    'website': 'https://www.vauxoo.com',
+    "license": "AGPL-3",
+    "depends": [
+        'product_extended_variants',
+        ],
+    "category": "Generic Modules/Inventory Control",
+    "data": [
+        'view/view.xml',
+        'data/data.xml',
+        ],
+    "installable": True
+}
