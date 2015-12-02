5f32a67aeb5149130a1b84c5c116d0268f9cd920
Humberto Arocha <hbto@vauxoo.com>
2015-12-01 23:32:43 -0430

    [IMP] By overriding compute_price method in template, it is possible now to provide a better way to compute Standard price of product by providing a bom_id in a product variant when there is not specific bom for any variant.
    Check `_bom_find` method inside compute_price

--- product_extended_segmentation/model/template.py
+++ product_extended_segmentation/model/template.py
@@ -96,3 +96,64 @@ class ProductTemplate(models.Model):
                 wizard_obj.change_price(cr, uid, [wiz_id], context=ctx)
             tmpl_obj.write(cr, uid, [product.id], sgmnt_dict, context=context)
         return price
+
+    def compute_price(self, cr, uid, product_ids, template_ids=False,
+                      recursive=False, test=False, real_time_accounting=False,
+                      context=None):
+        '''
+        Will return test dict when the test = False
+        Multiple ids at once?
+        testdict is used to inform the user about the changes to be made
+        '''
+        bom_obj = self.pool.get('mrp.bom')
+        prod_obj = self.pool.get('product.product')
+        testdict = {}
+        real_time = real_time_accounting
+        ids = template_ids
+        model = 'product.template'
+        if product_ids:
+            ids = product_ids
+            model = 'product.product'
+
+        def _bom_find(prod_id):
+            if model == 'product.product':
+                # if not look for template
+                bom_id = bom_obj._bom_find(
+                    cr, uid, product_id=prod_id, context=context)
+                if bom_id:
+                    return bom_id
+                prod_id = prod_obj.browse(
+                    cr, uid, prod_id, context=context).product_tmpl_id.id
+            return bom_obj._bom_find(
+                cr, uid, product_tmpl_id=prod_id, context=context)
+
+        for prod_id in ids:
+            bom_id = _bom_find(prod_id)
+            if not bom_id:
+                continue
+            # In recursive mode, it will first compute the prices of child
+            # boms
+            if recursive:
+                # Search the products that are components of this bom of
+                # prod_id
+                bom = bom_obj.browse(cr, uid, bom_id, context=context)
+
+                # Call compute_price on these subproducts
+                prod_set = set([x.product_id.id for x in bom.bom_line_ids])
+                res = self. compute_price(
+                    cr, uid, product_ids=list(prod_set), template_ids=[],
+                    recursive=recursive, test=test,
+                    real_time_accounting=real_time, context=context)
+                if test:
+                    testdict.update(res)
+
+            # Use calc price to calculate and put the price on the product
+            # of the BoM if necessary
+            price = self._calc_price(
+                cr, uid, bom_obj.browse(cr, uid, bom_id, context=context),
+                test=test, real_time_accounting=real_time, context=context)
+            if test:
+                testdict.update({prod_id: price})
+        if test:
+            return testdict
+        return True
