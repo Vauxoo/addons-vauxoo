cce10af2eb66b2d542dbaeafd1d05dc7fe003eeb
Humberto Arocha <hbto@vauxoo.com>
2015-12-06 23:51:05 -0430

    [IMP] Spinning off module to add journal_id field on mrp.routing model

---
+++ mrp_routing_account_journal/model/mrp.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+from openerp import models, fields
+
+
+class MrpRouting(models.Model):
+    """
+    For specifying the routings of Work Centers.
+    """
+    _inherit = 'mrp.routing'
+    journal_id = fields.Many2one(
+        'account.journal',
+        string='Journal',
+        readonly=False,
+        )
