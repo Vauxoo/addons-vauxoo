0c98c2d9727fbee8d6c1748cb81d5a74284075a6
KarenKawaii <karenamaroc@gmail.com>
2015-12-10 19:47:16 +0000

    [IMP] mrp_workcenter_account_move: Test for mrp production workflow until the Add financial account on location warning.

--- mrp_workcenter_account_move/tests/test_mrp_production.py
+++ mrp_workcenter_account_move/tests/test_mrp_production.py
@@ -18,16 +18,39 @@ class TestMrpProduction(TransactionCase):
             5.- Check move_id does exist in this test.
             6.- Check move_id.line_id.account_id=rev_production_cost_account,
                 credit ==15
-            7.- Check move_id.line_id.account_id=rev_inventory_deviation_account,
-                credit ==5
+            7.- Check move_id.line_id.account_id=
+                    rev_inventory_deviation_account,credit ==5
     '''
+
+
+    # Pseudo-constructor method of the setUp test.
     def setUp(self):
-        # Pseudo-constructor method of the setUp test.
         super(TestMrpProduction, self).setUp()
         # Define required global variables.
         self.mrp_production = self.env['mrp.production'].browse(self.ref(
              'mrp_workcenter_account_move.rev_mrp_production'))
 
-    def test_10_approve_mrp_production(self):
+    # Test methods.
+    def test_10_approve_begin_consumpt_finish_mrp_production(self):
         # This method approve a mrp production.
         mrp_product = self.mrp_production
+        # Confirm the mrp production.
+        mrp_product.signal_workflow('button_confirm')
+        self.assertEqual(mrp_product.state,
+                         'confirmed',
+                         "The mrp production doesn't confirmed.")
+        # Create the moves needed by mrp production.
+        mrp_product.signal_workflow('moves_ready')
+        self.assertEqual(mrp_product.state,
+                         'ready',
+                         "The moves aren't ready.")
+        # Begin mrp production.
+        mrp_product.signal_workflow('button_produce')
+        self.assertEqual(mrp_product.state,
+                         'in_production',
+                         "The mrp is not in production.")
+        # Consumption and finish production.
+        mrp_product.signal_workflow('button_produce_done')
+        self.assertEqual(mrp_product.state,
+                         'done',
+                         "The mrp production doesn't done.")
