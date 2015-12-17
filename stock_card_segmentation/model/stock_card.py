672098f72cd6a8a40424b94d33b3c551f754f5a1
Humberto Arocha <hbto@vauxoo.com>
2015-12-16 23:35:33 -0430

    [IMP] Including new fields to fetch with _get_quant_values method

--- stock_card_segmentation/model/stock_card.py
+++ stock_card_segmentation/model/stock_card.py
@@ -6,7 +6,13 @@ from openerp import models
 
 
 class StockCardProduct(models.TransientModel):
-    _inherit = 'stock.card.product'
+    _inherit = ['stock.card.product']
+
+    def _get_quant_values(self, move_id, col='', inner='', where=''):
+        col = (', material_cost, landed_cost'
+               ', production_cost, subcontracting_cost')
+        return super(StockCardProduct, self)._get_quant_values(
+            move_id=move_id, col=col, inner=inner, where=where)
 
 
 class StockCardMove(models.TransientModel):
