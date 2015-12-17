02b36476d72a159cf382cf115d30311019e64abb
Humberto Arocha <hbto@vauxoo.com>
2015-12-17 02:36:25 -0430

    [IMP] _get_default_params got updated with segmentation

--- stock_card_segmentation/model/stock_card.py
+++ stock_card_segmentation/model/stock_card.py
@@ -14,6 +14,16 @@ class StockCardProduct(models.TransientModel):
         return super(StockCardProduct, self)._get_quant_values(
             move_id=move_id, col=col, inner=inner, where=where)
 
+    def _get_default_params(self):
+        res = super(StockCardProduct, self)._get_default_params()
+        return dict(
+            res,
+            material=0.0,
+            landed=0.0,
+            production=0.0,
+            subcontracting=0.0,
+        )
+
 
 class StockCardMove(models.TransientModel):
     _inherit = 'stock.card.move'
