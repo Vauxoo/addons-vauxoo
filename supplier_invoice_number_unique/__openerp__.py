889d915fb93293a97ca3c598fa93448810d4d78d
dsabrinarg <sabrina@vauxoo.com>
2015-12-23 10:55:16 -0600

    [IMP][supplier_invoice_number_unique] Invoice number validation with regex and commercial partner added.

--- supplier_invoice_number_unique/__openerp__.py
+++ supplier_invoice_number_unique/__openerp__.py
@@ -1,14 +1,14 @@
 # coding: utf-8
-###########################################################################
+# ##########################################################################
 #    Module Writen to OpenERP, Open Source Management Solution
 #    Copyright (C) Vauxoo (<http://vauxoo.com>).
 #    All Rights Reserved
-###############Credits######################################################
+# ##############Credits######################################################
 #    Coded by: Luis Ernesto García Medina(ernesto_gm@vauxoo.com)
-#############################################################################
+# ############################################################################
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
@@ -18,7 +18,7 @@
 #
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-##########################################################################
+# #########################################################################
 {
     "name": "supplier_invoice_number_unique",
     "version": "1.6",
