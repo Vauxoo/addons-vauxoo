46ae6f7ee198a1976e3bd0f5432a2e34f414cfa2
Osval Reyes <osval@vauxoo.com>
2015-12-23 17:11:35 -0430

    [REF] stock_landed_costs_segmentation: Enhanced tests asserting quant costs, product costs, splitted costs and inventary values, add standard price value in demo data

--- stock_landed_costs_segmentation/tests/test_landed_costs_segmentation.py
+++ stock_landed_costs_segmentation/tests/test_landed_costs_segmentation.py
@@ -30,24 +30,35 @@ class TestLandedCostsSegmentation(TransactionCase):
         self.wizard = self.env['stock.transfer_details']
         self.wizard_item = self.env['stock.transfer_details_items']
 
-        self.picking_id = self.env.ref(
+        self.picking_01_id = self.env.ref(
             'stock_landed_costs_segmentation.picking_01')
+        self.picking_02_id = self.env.ref(
+            'stock_landed_costs_segmentation.picking_02')
         self.product_freight_id = self.env.ref(
             'stock_landed_costs_average'
             '.service_standard_periodic_landed_cost_1')
         self.product_insurance_id = self.env.ref(
             'stock_landed_costs_average'
             '.service_standard_periodic_landed_cost_2')
+
+        self.product_01 = self.env.ref(
+            'stock_landed_costs_segmentation.product_std_01')
+
+        self.product_02 = self.env.ref(
+            'stock_landed_costs_segmentation.product_avg_01')
+
+        self.product_03 = self.env.ref(
+            'stock_landed_costs_segmentation.product_real_01')
         pass
 
-    def do_picking(self):
+    def do_picking(self, picking_id=False):
         transfer_details = self.env['stock.transfer_details']
-        self.picking_id.action_confirm()
+        picking_id.action_confirm()
         wizard_id = self.wizard.create({
-            'picking_id': self.picking_id.id,
+            'picking_id': picking_id.id,
         })
 
-        for move_id in self.picking_id.move_lines:
+        for move_id in picking_id.move_lines:
             self.wizard_item.create({
                 'transfer_id': wizard_id.id,
                 'product_id': move_id.product_id.id,
@@ -58,25 +69,27 @@ class TestLandedCostsSegmentation(TransactionCase):
             })
 
         wizard_id.do_detailed_transfer()
-        self.assertEqual(self.picking_id.state, 'done')
+        self.assertEqual(picking_id.state, 'done')
 
-    def test_01_segmentations(self):
-        self.do_picking()
+    def create_and_validate_landed_costs(self, picking_id=False,
+                                         insurance_cost=15000, freight_cost=18000):
         slc_id = self.slc.create({
             'account_journal_id': self.ref(
                 'stock_landed_costs_average.stock_landed_cost_1'),
-            'picking_ids': [(4, self.picking_id.id), ],
+            'picking_ids': [(4, picking_id.id), ],
             'cost_lines': [
                 (0, 0, {
+                    'name': 'insurance',
                     'product_id': self.product_insurance_id.id,
                     'split_method': 'by_quantity',
-                    'price_unit': 150,
+                    'price_unit': insurance_cost,
                     'segmentation_cost': 'subcontracting_cost',
                 }),
                 (0, 0, {
+                    'name': 'freight',
                     'product_id': self.product_freight_id.id,
                     'split_method': 'by_quantity',
-                    'price_unit': 180,
+                    'price_unit': freight_cost,
                     'segmentation_cost': 'landed_cost',
                 }),
             ]
@@ -92,8 +105,98 @@ class TestLandedCostsSegmentation(TransactionCase):
         # validate landed costs
         slc_id.button_validate()
 
-        for quant in self.picking_id.move_lines.quant_ids:
-            self.assertEqual(quant.landed_cost, 50,
-                             'Landed Cost should be 50')
-            self.assertEqual(quant.subcontracting_cost, 60,
+        return slc_id
+
+    def get_quant(self, picking_id=False, product_id=False):
+        for quant_id in picking_id.mapped('move_lines.quant_ids'):
+            if quant_id.product_id == product_id:
+                return quant_id
+
+    def test_01_segmentations(self):
+
+        # check initial product costs
+        self.assertEqual(self.product_01.standard_price, 100)
+        self.assertEqual(self.product_02.standard_price, 100)
+        self.assertEqual(self.product_03.standard_price, 100)
+
+        # make incoming stock movements
+        self.do_picking(self.picking_01_id)
+        self.create_and_validate_landed_costs(self.picking_01_id,
+                                              insurance_cost=15000,
+                                              freight_cost=18000)
+
+        # check inventory valuations
+        self.assertEqual(sorted(self.picking_01_id.mapped(
+            'move_lines.quant_ids.inventory_value')),
+            [10000, 21000, 21000],
+            "Inventory valuations from picking #1 doesn't match")
+
+        product_std_quant_id = self.get_quant(self.picking_01_id,
+                                              self.product_01)
+        product_avg_quant_id = self.get_quant(self.picking_01_id,
+                                              self.product_02)
+        product_real_quant_id = self.get_quant(self.picking_01_id,
+                                               self.product_03)
+
+        # check product costs
+        self.assertEqual(self.product_01.standard_price, 100)
+        self.assertEqual(self.product_02.standard_price, 210)
+        self.assertEqual(self.product_03.standard_price, 100)
+
+        # check quant costs
+        self.assertEqual(product_std_quant_id.cost, 100)
+        self.assertEqual(product_avg_quant_id.cost, 210)
+        self.assertEqual(product_real_quant_id.cost, 210)
+
+        # check segmentation costs (M+L+P+S)
+        self.assertEqual(product_std_quant_id.segmentation_cost, 110)
+        self.assertEqual(product_avg_quant_id.segmentation_cost, 110)
+        self.assertEqual(product_real_quant_id.segmentation_cost, 110)
+
+        # check splitted costs
+        for quant in self.picking_01_id.mapped('move_lines.quant_ids'):
+            self.assertEqual(quant.landed_cost, 60,
                              'Landed Cost should be 60')
+            self.assertEqual(quant.subcontracting_cost, 50,
+                             'Subcontrating Cost should be 50')
+
+        # Receive products
+        self.do_picking(self.picking_02_id)
+        self.create_and_validate_landed_costs(self.picking_02_id,
+                                              insurance_cost=31500,
+                                              freight_cost=9000)
+
+        # check inventory valuations
+        self.assertEqual(sorted(self.picking_02_id.mapped(
+            'move_lines.quant_ids.inventory_value')),
+            [15000, 28500, 39600],
+            "Inventory valuations from picking #2 doesn't match")
+
+        product_std_quant_id = self.get_quant(self.picking_02_id,
+                                              self.product_01)
+        product_avg_quant_id = self.get_quant(self.picking_02_id,
+                                              self.product_02)
+        product_real_quant_id = self.get_quant(self.picking_02_id,
+                                               self.product_03)
+
+        # check quant costs
+        self.assertEqual(product_std_quant_id.cost, 100)
+        self.assertEqual(product_avg_quant_id.cost, 300)
+        self.assertEqual(product_real_quant_id.cost, 190)
+
+        # check segmentation costs
+        self.assertEqual(product_std_quant_id.segmentation_cost, 90)
+        self.assertEqual(product_avg_quant_id.segmentation_cost, 90)
+        self.assertEqual(product_real_quant_id.segmentation_cost, 90)
+
+        # check splitted costs
+        for quant in self.picking_02_id.mapped('move_lines.quant_ids'):
+            self.assertEqual(quant.landed_cost, 20,
+                             'Landed Cost should be 20')
+            self.assertEqual(quant.subcontracting_cost, 70,
+                             'Subcontrating Cost should be 70')
+
+        # check product costs
+        self.assertEqual(self.product_01.standard_price, 100)
+        self.assertEqual(self.product_02.standard_price, 264)
+        self.assertEqual(self.product_03.standard_price, 100)
