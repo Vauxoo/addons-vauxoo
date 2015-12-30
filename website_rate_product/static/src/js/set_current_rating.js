d7e43824d11d9e6306651b113a15134f6d6d4395
oalca <oszckar@gmail.com>
2015-12-30 10:26:11 -0600

    [FIX] wrong use of selector, using id it was only useful when there was only one product on the dom, when calling the rating template somwhere else it was only rendering rating for the first element

--- website_rate_product/static/src/js/set_current_rating.js
+++ website_rate_product/static/src/js/set_current_rating.js
@@ -2,12 +2,15 @@
     'use strict';
 
     openerp.website.if_dom_contains('#stars_no_edit', function(){
-      var $stars = $("span.fa-star-o.no-edit");
-      var rating = $("#stars_no_edit").data("rating");
-      $stars.each(function(index){
-        if (index <= rating-1){
-          $(this).removeClass("fa-star-o").addClass("fa-star");
-        }
+      var $star_groups = $(".stars-no-edit");
+      $star_groups.each(function(index){
+        var $stars = $(this).find("span"),
+            rating = $(this).data("rating");
+        $stars.each(function(index){
+          if (index <= rating-1){
+            $(this).removeClass("fa-star-o").addClass("fa-star");
+          }
+        });
       });
 
     });
