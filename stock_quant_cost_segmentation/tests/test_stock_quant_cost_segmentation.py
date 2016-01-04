731572b2417c32c96fe87a84efe6c844f2baa75f
Humberto Arocha <hbto@vauxoo.com>
2016-01-04 02:13:52 -0430

    [IMP] Testing real_time accounting valuation with stock_quant_cost_segmentation

--- stock_quant_cost_segmentation/tests/test_stock_quant_cost_segmentation.py
+++ stock_quant_cost_segmentation/tests/test_stock_quant_cost_segmentation.py
@@ -15,8 +15,12 @@ class TestsStockQuantCostSegmentation(TestStockCommon):
         """
         super(TestsStockQuantCostSegmentation, self).setUp()
         self.quant = self.env['stock.quant']
+        self.aml_obj = self.env['account.move.line']
+        self.stock_inv_obj = self.env['stock.inventory']
         self.product_id = self.ref(
-            'stock_quant_cost_segmentation.product_real_periodic')
+            'stock_quant_cost_segmentation.product_real_realtime')
+        self.inventory_id = self.stock_inv_obj.browse(self.ref(
+            'stock_quant_cost_segmentation.stock_inventory_01'))
 
     def asserting_cost_segmentation(self):
         quant = self.quant.search(
@@ -30,5 +34,26 @@ class TestsStockQuantCostSegmentation(TestStockCommon):
 
     def test_basic_landed(self):
         self.asserting_cost_segmentation()
+
+        return True
+
+    def asserting_real_time_accounting(self):
+        aml_ids = []
+        for sm_brw in self.inventory_id.move_ids:
+            aml_ids += self.aml_obj.search(
+                [('sm_id', '=', sm_brw.id)])
+
+        self.assertEquals(
+            bool(aml_ids),
+            True,
+            'Something went wrong. There should be Journal Entries!!!')
+        self.assertEquals(
+            len(aml_ids),
+            2,
+            'Something went wrong. There should be Two Journal Entries!!!')
+        return True
+
+    def test_basic_real_time_accouting(self):
+        self.asserting_real_time_accounting()
 
         return True
