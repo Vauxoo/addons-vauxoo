20f308c111c40e0af5311c578f9256aa1f08800d
oalca <oszckar@gmail.com>
2016-01-19 20:54:06 -0600

    [REF] number with qty of products per filter now are shown on parentheses instead of badges

--- website_product_filters/static/src/js/product_quantities.js
+++ website_product_filters/static/src/js/product_quantities.js
@@ -11,10 +11,10 @@
           }
         }).done(function( data ) {
               $.each(JSON.parse(data), function(i, obj) {
-                $('span.att-value#'+obj.id).next().replaceWith('<span class="badge">'+obj.qty+'</span>');
-                $('span.att-value').next('.fa-spinner').replaceWith(' <span class="badge">0</span>');
+                $('span.att-value#'+obj.id).next().replaceWith('<span class="black-text">('+obj.qty+')</span>');
+                $('span.att-value').next('.fa-spinner').replaceWith(' <span class="black-text">(0)</span>');
               });
-          $(".js_attributes li.attribute").has("span.badge:contains('0')").remove();
+          $(".js_attributes li.attribute").has("span.black-text:contains('0')").remove();
           $('div.attr-list').each(function(){
             var $div_list = $(this);
             if ($div_list.find('li.attribute').length <= 5) {
