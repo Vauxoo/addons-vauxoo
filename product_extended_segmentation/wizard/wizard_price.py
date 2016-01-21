14e2858e52ef8bcfe28fbbe19cea85076fbb680f
Humberto Arocha <hbto@vauxoo.com>
2016-01-20 20:18:23 -0430

    [FIX] Wrong inherited class

--- product_extended_segmentation/wizard/wizard_price.py
+++ product_extended_segmentation/wizard/wizard_price.py
@@ -2,7 +2,7 @@
 from openerp import fields, models
 
 
-class WizardPrice(models.TransientModel):
+class WizardPrice(models.Model):
     _inherit = "wizard.price"
 
     update_avg_costs = fields.Boolean("Update Average Product Costs")
