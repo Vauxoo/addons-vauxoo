7541ab0db7cc17eecda2ba1e26e686763c832906
Humberto Arocha <hbto@vauxoo.com>
2016-01-20 22:52:24 -0430

    [FIX] Bringing missing method from trunk to branch to resolve conflicts

--- product_extended_segmentation/wizard/wizard_price.py
+++ product_extended_segmentation/wizard/wizard_price.py
@@ -5,4 +5,15 @@ from openerp import fields, models
 class WizardPrice(models.Model):
     _inherit = "wizard.price"
 
+    def onchange_recursive(self, cr, uid, ids, recursive, context=None):
+        template_id = context.get('active_id')
+        res = self.pool.get('product.template').\
+            compute_price(cr,  uid, product_ids=[],
+                          template_ids=[template_id], test=True,
+                          recursive=recursive, real_time_accounting=False,
+                          context=context)
+
+        res = str({template_id: sum([res[x] for x in res.keys()])})
+        return {'value': {'info_field': res}}
+
     update_avg_costs = fields.Boolean("Update Average Product Costs")
