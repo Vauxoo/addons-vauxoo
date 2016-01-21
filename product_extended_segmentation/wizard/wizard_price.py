4e8b831735be09ba67697770a8f8a71bf8d37b60
Osval Reyes <osval@vauxoo.com>
2016-01-21 11:31:38 -0430

    [REF] product_extended_segmentation: update check option in context before calling bom cost processing

--- product_extended_segmentation/wizard/wizard_price.py
+++ product_extended_segmentation/wizard/wizard_price.py
@@ -5,6 +5,17 @@ from openerp import fields, models
 class WizardPrice(models.Model):
     _inherit = "wizard.price"
 
+    update_avg_costs = fields.Boolean("Update Average Product Costs")
+
+    def compute_from_bom(self, cr, uid, ids, context=None):
+        if context is None:
+            context = {}
+        context.update({
+            'update_avg_costs': self.browse(cr, uid, ids).update_avg_costs
+        })
+
+        res = super(WizardPrice, self).compute_from_bom(cr, uid, ids, context)
+
     # /!\ NOTE: https://goo.gl/7wfpt1 Needs to use this approach
     def onchange_recursive(self, cr, uid, ids, recursive, context=None):
         template_id = context.get('active_id')
@@ -16,5 +27,3 @@ class WizardPrice(models.Model):
 
         res = str({template_id: sum([res[x] for x in res.keys()])})
         return {'value': {'info_field': res}}
-
-    update_avg_costs = fields.Boolean("Update Average Product Costs")
