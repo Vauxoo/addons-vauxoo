fce998f26ed8cee6a3ba20b91f7c36959996eff8
Osval Reyes <osval@vauxoo.com>
2016-01-22 13:34:59 -0430

    [REF] product_extended_segmentation: change how test wizard is created, use safe_eval() instead eval()

--- product_extended_segmentation/tests/test_wizard.py
+++ product_extended_segmentation/tests/test_wizard.py
@@ -20,6 +20,7 @@
 #
 ##############################################################################
 from openerp.tests.common import TransactionCase
+from openerp.tools.safe_eval import safe_eval
 
 
 class TestWizard(TransactionCase):
@@ -33,15 +34,21 @@ class TestWizard(TransactionCase):
         self.producto_e_id = self.env.ref(
             'product_extended_segmentation.producto_e').product_tmpl_id
 
-    def create_wizard(self, product_tmpl_id):
+    def create_wizard(self, product_tmpl_id, recursive=False,
+                      real_time_accounting=False, update_avg_costs=False):
         return self.wizard.with_context({
             'active_model': product_tmpl_id._name,
             'active_id': product_tmpl_id.id,
             'active_ids': product_tmpl_id.ids,
-        }).create({})
+            'update_avg_costs': recursive and update_avg_costs,
+        }).create({
+            'recursive': recursive,
+            'real_time_accounting': real_time_accounting,
+            'update_avg_costs': recursive and update_avg_costs,
+        })
 
     def get_product_cost(self, info_field, product_tmpl_id):
-        return eval(info_field)[product_tmpl_id]
+        return safe_eval(info_field)[product_tmpl_id]
 
     def check_wizard_values(self, product_id, vals):
         # check before wizard
@@ -52,16 +59,15 @@ class TestWizard(TransactionCase):
         self.assertEqual(wizard_id.recursive, False,
                          'WizardPrice recursive check should be unchecked'
                          'by default')
-        cost = self.get_product_cost(
-            wizard_id.info_field, product_id.id)
+        cost = self.get_product_cost(wizard_id.info_field, product_id.id)
         # check what a wizard returns as default value
         self.assertEqual(cost, vals['default'],
                          'Non-recursive cost valuation for {0} should be {1}'.
                          format(product_id.name, vals['default']))
-
-        res = wizard_id.onchange_recursive(True)['value']
-        cost = self.get_product_cost(res['info_field'], product_id.id)
-
+        wizard_id = self.create_wizard(
+            product_id, recursive=True, update_avg_costs=True)
+        wizard_id.compute_from_bom()
+        cost = product_id.standard_price
         # check what recursive returns
         self.assertEqual(cost, vals['after'],
                          'Recursive cost for {0} should be keep at {1}'.
@@ -80,3 +86,9 @@ class TestWizard(TransactionCase):
             'after': 75
         }
         self.check_wizard_values(self.producto_e_id, vals)
+
+        # check production_cost
+        self.assertEqual(self.producto_d_id.production_cost, 5,
+                         'Production Cost for D should be 5')
+        self.assertEqual(self.producto_e_id.production_cost, 15,
+                         'Production Cost for E should be 15')
