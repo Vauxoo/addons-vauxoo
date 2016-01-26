45fd912378ef2dc1ec8990876c32b3a0872d978b
oalca <oszckar@gmail.com>
2016-01-26 15:42:46 -0600

    [FIX] lint errors

--- website_product_filters/static/src/js/test_attributes.js
+++ website_product_filters/static/src/js/test_attributes.js
@@ -39,8 +39,8 @@
             });
         }
     }
-    var steps = openerp.Tour.tours.shop_buy_product.steps;
-    for (var k=0; k<steps.length; k++) {
+    steps = openerp.Tour.tours.shop_buy_product.steps;
+    for (k=0; k<steps.length; k++) {
         // Inserting extra steps for the pertinent DOM changes made by this module
         if (steps[k].title === "finish") {
             steps.splice(k, 1);
