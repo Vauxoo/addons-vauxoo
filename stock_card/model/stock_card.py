608238514c43a212b9819d858b99d8ce08a4add2
Osval Reyes <osval@vauxoo.com>
2016-01-28 17:05:49 -0430

    [FIX] stock_card: F401 and F821

--- stock_card/model/stock_card.py
+++ stock_card/model/stock_card.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-from openerp import models, fields, api
+from openerp import models, fields, api, _
 import openerp.addons.decimal_precision as dp
 from openerp.exceptions import Warning as UserError
 
