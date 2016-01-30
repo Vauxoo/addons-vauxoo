89da5d96537d1d2d76242534334d07c1ce2ff62a
Humberto Arocha <hbto@vauxoo.com>
2016-01-29 19:48:06 -0430

    [IMP] Threshold is to be set on company to allow later its use

---
+++ product_extended_segmentation/model/company.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+from openerp import models, fields
+
+
+class ResCompany(models.Model):
+    _inherit = 'res.company'
+
+    std_price_neg_threshold = fields.Float(
+            string="Standard Price Negative Threshold (%)",
+            help=('Maximum percentage threshold that Standard Price is '
+                  'allowed to lower'))
