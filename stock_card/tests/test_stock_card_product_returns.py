a4082137529b0baf77d467462ebc68baf52afdaf
Osval Reyes <osval@vauxoo.com>
2016-02-01 16:32:03 -0430

    [REF] stock_card: change test in order to test order that stock card returns and values on it, add demo a dummy trx only to make some time

--- stock_card/tests/test_stock_card_product_returns.py
+++ stock_card/tests/test_stock_card_product_returns.py
@@ -32,44 +32,26 @@ class TestStockCardProductReturns(TransactionCase):
         self.move = self.env['stock.move']
         self.product_id = self.env.ref('stock_card.product02')
 
-        self.values = {
-            'po_01': {
-                'cost': 100, 'qty': 10,
-                'avg': 100, 'mov_val': 1000, 'inv_val': 1000
-            },
-            'so_01': {
-                'cost': 100, 'qty': 6,
-                'avg': 100, 'mov_val': -400, 'inv_val': 600
-            },
-            'so_02': {
-                'cost': 100, 'qty': 1,
-                'avg': 100, 'mov_val': -500, 'inv_val': 100
-            },
-            'po_02': {
-                'cost': 300, 'qty': 4,
-                'avg': 250, 'mov_val': 900, 'inv_val': 1000
-            },
-            'so_03': {
-                'cost': 250, 'qty': 1,
-                'avg': 250, 'mov_val': -750, 'inv_val': 250
-            },
-            'pick_01_so_01': {
-                'cost': 100, 'qty': 2,
-                'avg': 175, 'mov_val': 100, 'inv_val': 350
-            },
-            'po_03': {
-                'cost': 220, 'qty': 7,
-                'avg': 207.14, 'mov_val': 1100, 'inv_val': 1450
-            },
-            'po_04': {
-                'cost': 400, 'qty': 10,
-                'avg': 265, 'mov_val': 1200, 'inv_val': 2650
-            },
-            'pick_02_po_03': {
-                'cost': 220, 'qty': 8,
-                'avg': 276.25, 'mov_val': -440, 'inv_val': 2210
-            },
-        }
+        self.values = [
+            ('po_01', {'cost': 100, 'qty': 10, 'avg': 100, 'mov_val': 1000,
+                       'inv_val': 1000}),
+            ('so_01', {'cost': 100, 'qty': 6, 'avg': 100, 'mov_val': -400,
+                       'inv_val': 600}),
+            ('so_02', {'cost': 100, 'qty': 1, 'avg': 100, 'mov_val': -500,
+                       'inv_val': 100}),
+            ('po_02', {'cost': 300, 'qty': 4, 'avg': 250, 'mov_val': 900,
+                       'inv_val': 1000}),
+            ('so_03', {'cost': 250, 'qty': 1, 'avg': 250, 'mov_val': -750,
+                       'inv_val': 250}),
+            ('pick_01_so_01', {'cost': 100, 'qty': 2, 'avg': 175,
+                               'mov_val': 100, 'inv_val': 350}),
+            ('po_03', {'cost': 220, 'qty': 7, 'avg': 207.14, 'mov_val': 1100,
+                       'inv_val': 1450}),
+            ('po_04', {'cost': 400, 'qty': 10, 'avg': 265, 'mov_val': 1200,
+                       'inv_val': 2650}),
+            ('pick_02_po_03', {'cost': 220, 'qty': 8, 'avg': 276.25,
+                               'mov_val': -440, 'inv_val': 2210}),
+        ]
 
     def get_stock_valuations(self, product_id):
         sc_moves = self.sc_product._stock_card_move_get(
@@ -80,38 +62,39 @@ class TestStockCardProductReturns(TransactionCase):
         card_lines = self.get_stock_valuations(self.product_id.id)
         self.assertEqual(len(self.values), len(card_lines),
                          "Both lists should have the same length(=9)")
-        for succeded in card_lines:
+        for succeed, expected in zip(card_lines, self.values):
+            origin = self.env['stock.move'].browse(succeed['move_id']).origin
+            name, expected = expected
+            self.assertEqual(origin, name,
+                             "Transactions aren't in the same order")
 
-            origin = self.env['stock.move'].browse(succeded['move_id']).origin
-
-            expected = self.values[origin]
             self.assertEqual(0, float_compare(expected['avg'],
-                                              succeded['average'],
+                                              succeed['average'],
                                               precision_rounding=1),
                              "Average Cost current={0} expected={1} is not "
                              "the expected: {2}".
                              format(expected['avg'],
-                                    succeded['average'], expected))
+                                    succeed['average'], expected))
 
             self.assertEqual(expected['cost'],
-                             succeded['cost_unit'],
+                             succeed['cost_unit'],
                              "Unit Cost current={0} expected={1} is not "
                              "the expected: {2}".
                              format(expected['cost'],
-                                    succeded['cost_unit'], expected))
+                                    succeed['cost_unit'], expected))
 
             self.assertEqual(0, float_compare(expected['inv_val'],
-                                              succeded['inventory_valuation'],
+                                              succeed['inventory_valuation'],
                                               precision_rounding=1),
                              "Inventory Value current={0} expected={1} is not "
                              "match: {2}".
                              format(expected['inv_val'],
-                                    succeded['inventory_valuation'], expected))
+                                    succeed['inventory_valuation'], expected))
 
             self.assertEqual(0, float_compare(expected['mov_val'],
-                                              succeded['move_valuation'],
+                                              succeed['move_valuation'],
                                               precision_rounding=1),
                              "Movement Value current={0} expected={1} is not "
                              "match: {2}".
                              format(expected['mov_val'],
-                                    succeded['move_valuation'], expected))
+                                    succeed['move_valuation'], expected))
