0a90c2604535cc37011c3627e58f563109fac07f
Osval Reyes <osval@vauxoo.com>
2016-02-05 10:00:53 -0430

    [CC] product_extended_segmentation: Negative -> Bottom

--- product_extended_segmentation/model/company.py
+++ product_extended_segmentation/model/company.py
@@ -7,6 +7,6 @@ class ResCompany(models.Model):
     _inherit = 'res.company'
 
     std_price_neg_threshold = fields.Float(
-        string="Standard Price Negative Threshold (%)",
+        string="Standard Price Bottom Threshold (%)",
         help=('Maximum percentage threshold that Standard Price is '
               'allowed to lower'))
