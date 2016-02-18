c589bbf799cdbaea15bdcca3f1eacc09fcc14df2
Osval Reyes <osval@vauxoo.com>
2016-02-18 10:22:31 -0430

    [REF] stock_landed_costs_average: do returns using reverse transfer action

--- stock_landed_costs_average/tests/test_landed_cost_average.py
+++ stock_landed_costs_average/tests/test_landed_cost_average.py
@@ -2,6 +2,8 @@
 
 from openerp.tests.common import TransactionCase
 from openerp.exceptions import except_orm, Warning as UserError
+from datetime import datetime, timedelta
+from openerp.tools.safe_eval import safe_eval
 
 
 class TestLandedCostAverage(TransactionCase):
@@ -19,7 +21,17 @@ class TestLandedCostAverage(TransactionCase):
         self.customer_location = self.env.ref('stock.stock_location_customers')
         self.supplier_location = self.env.ref('stock.stock_location_suppliers')
         self.stock_location = self.env.ref('stock.stock_location_stock')
-
+        self.supplier_id = self.env.ref('base.res_partner_13')
+        self.customer_id = self.env.ref('base.res_partner_23')
+        self.purchase_order = self.env['purchase.order']
+        self.sale_order = self.env['sale.order']
+        self.product_id = self.env.ref(
+            'stock_landed_costs_average.product_mouse')
+        self.wizard = self.env['stock.transfer_details']
+        self.wizard_item = self.env['stock.transfer_details_items']
+        self.transfer_details = self.env['stock.transfer_details']
+        self.return_picking = self.env['stock.return.picking']
+        self.return_picking_line = self.env['stock.return.picking.line']
         self.transactions = [
             {  # 1 buy 8 items
                 'name': 'slca_po_01',
@@ -40,12 +52,13 @@ class TestLandedCostAverage(TransactionCase):
                 'name': 'slca_so_02',
                 'is_po': False, 'is_return': False, 'cost': 12, 'qty': 1,
                 'avg': 12, 'move_val': 12, 'inv_val': 12,
+                'origin_name': 'po_01',
             },
             {  # 5 vendor return 1 item
                 'name': 'slca_ret_po_01',
                 'is_po': True, 'is_return': True, 'cost': 12, 'qty': 1,
                 'avg': 12, 'move_val': 12, 'inv_val': 0,
-                'origin_name': 'po_01',
+                'origin_name': 'slca_po_01', 'partner_id': self.supplier_id,
             },
         ]
 
@@ -88,7 +101,7 @@ class TestLandedCostAverage(TransactionCase):
         qty = vals['qty']
         cost = vals['cost']
         purchase_order_id = self.purchase_order.create({
-            'partner_id': self.partner_id.id,
+            'partner_id': self.supplier_id.id,
             'location_id': self.ref('stock.stock_location_stock'),
             'pricelist_id': self.ref('purchase.list0'),
             'order_line': [(0, 0, {
@@ -105,12 +118,13 @@ class TestLandedCostAverage(TransactionCase):
         purchase_order_id.action_invoice_create()
         purchase_order_id.action_picking_create()
         self.do_picking(purchase_order_id.picking_ids)
+        return purchase_order_id
 
     def create_sale_order(self, vals):
         qty = vals['qty']
         price = vals['cost']
         sale_order_id = self.sale_order.create({
-            'partner_id': self.partner_id.id,
+            'partner_id': self.customer_id.id,
             'client_order_ref': "Sale Order (qty={0}, price={1})".format(
                 str(qty), str(price)),
             'order_policy': 'manual',
@@ -125,7 +139,7 @@ class TestLandedCostAverage(TransactionCase):
         self.do_picking(sale_order_id.picking_ids)
         return sale_order_id
 
-    def get_location(self, is_po, is_return):
+    def get_locations(self, is_po, is_return):
 
         if is_po and not is_return:
             return self.supplier_location.id, self.stock_location.id
@@ -136,51 +150,56 @@ class TestLandedCostAverage(TransactionCase):
         if not is_po and is_return:
             return self.customer_location.id, self.stock_location.id
 
-    def get_origin_by_name(self, origin_name):
+    def get_origin_picking(self, origin_name):
         for trx in self.transactions:
             if trx['name'] == origin_name:
-                return trx['id']
+                return trx['id'].picking_ids
         return False
 
     def create_return(self, vals):
         qty = vals['qty']
         location_id, location_dest_id = self.get_locations(vals['is_po'],
                                                            vals['is_return'])
-        picking_id = self.picking.create({
-            'name': vals['partner_id'].name + ' ' + str(qty),
-            'partner_id': vals['partner_id'].id,
-            'move_type': one,
-            'priority': 1,
-            'picking_type': self.ref('stock.picking_type_internal'),
-        })
+        for picking_id in vals['origin_id']:
+            return_id = self.return_picking.with_context({
+                'active_id': picking_id.id,
+                'active_ids': picking_id.ids,
+                'active_model': 'stock.picking',
+            }).create({})
 
-        move_id = self.move.create({
-            'origin': vals['partner_id'].name + ' ' + str(picking_id.id),
-            'name': vals['partner_id'].name + ' ' + str(picking_id.id),
-            'picking_id': picking_id.id,
-            'location_id': location_id,
-            'location_dest_id': location_dest_id,
-            'product_uom': self.ref('product.product_uom_unit'),
-            'product_uom_qty': 1,
-            'price_unit': qty,
-            'origin_returned_move_id': vals['origin_id'],
-        })
-
-        do_picking_wf(picking_id)
-        self.assertEqual(picking_id.state, 'done')
-        self.dummy_function(picking_id.move_lines)
+            return_lines = []
+            for move_id in picking_id.move_lines:
+                return_line_id = self.return_picking_line.create({
+                    'product_id': self.product_id.id,
+                    'quantity': qty,
+                    'wizard_id': return_id.id,
+                    'move_id': move_id.id
+                })
+                return_lines.append(return_line_id)
+            res = return_id.create_returns()
+            picking_id = safe_eval(res['domain'])[0][2][0]
+            picking_id = self.picking.browse(picking_id)
+            self.do_picking_wf(picking_id)
+            self.assertEqual(picking_id.state, 'done')
+            self.dummy_function(picking_id.move_lines)
+        return picking_id.id
 
     def test_01_do_transactions(self):
         for trx in self.transactions:
-
             # purchase
             if trx['is_po'] and not trx['is_return']:
-                self.create_purchase_order(trx)
+                trx['id'] = self.create_purchase_order(trx)
 
             # vendor return
             if trx['is_po'] and trx['is_return']:
-                trx['id'] = self.create_supplier_return(trx)
+                trx['origin_id'] = self.get_origin_picking(trx['origin_name'])
+                trx['id'] = self.create_return(trx)
 
             # sale
             if not trx['is_po'] and not trx['is_return']:
-                id = self.create_sale_order(trx)
+                trx['id'] = self.create_sale_order(trx)
+        card_lines = self.env['stock.card.product'].\
+            _stock_card_move_get(self.product_id.id)['res']
+        last_item = card_lines[-1]
+        self.assertEqual(last_item['product_qty'], 0)
+        self.assertEqual(last_item['inventory_valuation'], 0)
