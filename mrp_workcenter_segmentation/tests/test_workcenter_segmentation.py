07451b7617ab8879ee97007611486b75e6168f11
Osval Reyes <osval@vauxoo.com>
2016-03-14 09:24:40 -0430

    [REF] mrp_workcenter_segmentation: add test case to check journal entries

--- mrp_workcenter_segmentation/tests/test_workcenter_segmentation.py
+++ mrp_workcenter_segmentation/tests/test_workcenter_segmentation.py
@@ -25,7 +25,7 @@ class TestWorkcenterSegmentation(TransactionCase):
         self.mrp_production_e = self.env.ref(
             'mrp_workcenter_segmentation.'
             'mrp_production_segmentation_e_product')
-        self.wip_account = self.ref(
+        self.wip_account = self.env.ref(
             'mrp_workcenter_account_move.rev_work_in_process')
         self.wizard = self.env['mrp.product.produce']
         self.wizard_line = self.env['mrp.product.produce.line']
@@ -33,8 +33,8 @@ class TestWorkcenterSegmentation(TransactionCase):
         self.location = self.env['stock.location']
         location_id = self.location.search([('name', '=', 'Production')])
         location_id.write({
-            'valuation_in_account_id': self.wip_account,
-            'valuation_out_account_id': self.wip_account
+            'valuation_in_account_id': self.wip_account.id,
+            'valuation_out_account_id': self.wip_account.id
         })
 
     def produce_product(self, production_id, qties):
@@ -73,6 +73,14 @@ class TestWorkcenterSegmentation(TransactionCase):
         wz_brw.do_produce()
         return True
 
+    def get_account_values(self, account_name):
+        rec_ids = self.mrp_production_e.account_move_id.line_id.filtered(
+            lambda l: l.account_id.name == account_name)
+        res = []
+        for rec_id in rec_ids:
+            res.append((rec_id.debit, rec_id.credit))
+        return res
+
     def test_01_check_workcenters_segments(self):
         self.produce_product(self.mrp_production_d, [1, 2])
         segments_costs = self.mrp_production_d.\
@@ -83,6 +91,20 @@ class TestWorkcenterSegmentation(TransactionCase):
         self.assertEqual(segments_costs['subcontracting_cost'], 0)
 
         self.produce_product(self.mrp_production_e, [1])
+
+        production_vals = self.get_account_values('PRODUCTION_COST_ACCOUNT')
+        self.assertEqual(production_vals, [(0, 7.5), (0, 7.5), (0, 15)],
+                         'There is missing journal entries for production')
+
+        deviation_vals = self.get_account_values(
+            'INVENTORY_DEVIATION_ACCOUNT')
+        self.assertEqual(deviation_vals, [(20, 0)],
+                         'There is missing journal entries for deviation')
+
+        wip_vals = self.get_account_values('WORK_IN_PROCESS')
+        self.assertEqual(wip_vals, [(0, 20), (7.5, 0), (7.5, 0), (15, 0)],
+                         'There is missing journal entries for production')
+
         segments_costs = self.mrp_production_e.\
             get_workcenter_segmentation_amount()
         self.assertEqual(segments_costs['material_cost'], 0)
