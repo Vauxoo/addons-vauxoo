6d0abd50602c868c0cdae18e434fa85204d23e6f
Hugo Adan <hugo@vauxoo.com>
2016-03-22 16:24:08 +0000

    [REF]set more details in message warning when confirm a SO

--- partner_credit_limit/model/sale.py
+++ partner_credit_limit/model/sale.py
@@ -20,15 +20,32 @@ class SaleOrder(models.Model):
         for so in self:
             if so.payment_term.payment_type != 'credit':
                 return True
-            allowed_sale = self.env['res.partner'].with_context(
+            partner = self.env['res.partner'].with_context(
                 {'new_amount': so.amount_total,
                  'new_currency': so.company_id.currency_id.id}).browse(
-                     so.partner_id.id).allowed_sale
-            if allowed_sale:
+                     so.partner_id.id)
+            if partner.allowed_sale:
                 return True
             else:
-                msg = _('Can not confirm the Sale Order because Partner '
-                        'has late payments or has exceeded the credit limit.'
-                        '\nPlease cover the late payment or check credit limit'
-                        '\nCreadit Limit : %s') % (so.partner_id.credit_limit)
-                raise exceptions.Warning(('Warning!'), msg)
+                msg = 'The Sale order pass to state of Exception Credit.' + \
+                    '\nThe partner %s:' % (partner.name)
+                if partner.credit_overloaded:
+                    msg += ('\nHave exceeded the credit limit.'
+                            '\nThe credit available is $%s'
+                            '\nAnd the credit is being requested is $%s') % (
+                               str((partner.credit_limit - partner.credit)),
+                               str(so.amount_total))
+                if partner.overdue_credit:
+                    moveline_obj = self.env['account.move.line']
+                    movelines = moveline_obj.search(
+                        [('partner_id', '=', partner.id),
+                         ('account_id.type', '=', 'receivable'),
+                         ('state', '!=', 'draft'),
+                         ('reconcile_id', '=', False)])
+                    max_date = max(movelines.mapped('date_maturity'))
+                    msg += ('\nIt has the overdue payment period.'
+                            '\nThe expiration date was %s, '
+                            'the amount payable is: $%s') %(
+                            max_date, str(partner.credit))
+                message = _(msg)
+                raise exceptions.Warning(('Warning!'), message)
