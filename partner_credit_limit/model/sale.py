35e1a19359980981196261dce3f312471c5fb446
Hugo Adan <hugo@vauxoo.com>
2016-03-22 18:24:11 +0000

    [ADD]added new page in Sale order form to show Details credit

--- partner_credit_limit/model/sale.py
+++ partner_credit_limit/model/sale.py
@@ -8,12 +8,37 @@
 #    coded by: hugo@vauxoo.com
 #    planned by: Nhomar Hernandez <nhomar@vauxoo.com>
 ############################################################################
-from openerp import models, api, _
+from openerp import models, fields, api, _
 from openerp import exceptions
 
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
+
+    partner_credit_limit = fields.Float(related='partner_id.credit_limit',
+                                        string="Partner Credit Limit",
+                                        readonly=True)
+    partner_credit_available = fields.Float(
+        related="partner_id.credit_available",
+        string="Partner Available Credit", readonly=True)
+    requested_credit = fields.Float(related="amount_total",
+                                    string="Requested Credit")
+    partner_overdue_payments = fields.One2many(
+        comodel_name="account.move.line",
+        inverse_name='partner_id', readonly=True,
+        compute="_get_late_payments")
+    partner_overdue_amount = fields.Float(related='partner_id.overdue_amount',
+                                          string='Partner Overdue Amount',
+                                          readonly=True)
+
+    def _get_late_payments(self):
+        for so in self:
+            moveline_obj = self.env['account.move.line']
+            movelines = moveline_obj.search(
+                [('partner_id', '=', so.partner_id.id),
+                 ('account_id.type', '=', 'receivable'),
+                 ('state', '!=', 'draft'), ('reconcile_id', '=', False)])
+            so.partner_overdue_payments = movelines
 
     @api.multi
     def check_limit(self):
@@ -33,19 +58,14 @@ class SaleOrder(models.Model):
                     msg += ('\nHave exceeded the credit limit.'
                             '\nThe credit available is $%s'
                             '\nAnd the credit is being requested is $%s') % (
-                               str((partner.credit_limit - partner.credit)),
-                               str(so.amount_total))
+                            str((partner.credit_limit - partner.credit)),
+                            str(so.amount_total))
                 if partner.overdue_credit:
-                    moveline_obj = self.env['account.move.line']
-                    movelines = moveline_obj.search(
-                        [('partner_id', '=', partner.id),
-                         ('account_id.type', '=', 'receivable'),
-                         ('state', '!=', 'draft'),
-                         ('reconcile_id', '=', False)])
-                    max_date = max(movelines.mapped('date_maturity'))
+                    max_date = max(self.partner_overdue_payments.mapped(
+                        'date_maturity'))
                     msg += ('\nIt has the overdue payment period.'
                             '\nThe expiration date was %s, '
-                            'the amount payable is: $%s') %(
+                            'the amount payable is: $%s') % (
                             max_date, str(partner.credit))
                 message = _(msg)
                 raise exceptions.Warning(('Warning!'), message)
