d876057914119fe88e96f5195617cdddac0e57c0
Hugo Adan <hugo@vauxoo.com>
2016-04-04 14:50:48 +0000

    [ADD] adds sale_orders_payment. Module that allows you select 1 or more sales order from sale order tree view in order to register the payment.

---
+++ sale_orders_payment/__openerp__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+############################################################################
+#    Module Writen For Odoo, Open Source Management Solution
+#
+#    Copyright (c) 2011 Vauxoo - http://www.vauxoo.com
+#    All Rights Reserved.
+#    info Vauxoo (info@vauxoo.com)
+#    coded by: hugo@vauxoo.com
+#    planned by: Nhomar Hernandez <nhomar@vauxoo.com>
+############################################################################
+{
+    "name": "Sale Orders Payment (Bank Statements)",
+    "version": "1.0",
+    "author": "Vauxoo",
+    "category": "",
+    "website": "http://www.vauxoo.com/",
+    "license": "AGPL-3",
+    "depends": [
+        "account",
+        "sale", ],
+    "demo": [],
+    "data": [
+        "views/wizard_sale_orders_pay.xml",
+    ],
+    "installable": True,
+}
