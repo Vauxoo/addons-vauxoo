4cdae1a80a4ca2740c9a415ef4bfb058fc2dbc13
Nhomar Hernández <nhomar@vauxoo.com>
2016-04-24 13:46:08 -0500

    [FIX] sale_team_warehouse: monkey patch is a bad approach.
    Avoiding the usage of monkey patching flaky tests appear when it is
    done.
    
    With this refactor 2 things need to be considered:
    
    1. To use the feature of set the default value of warehouse in a model we
    need to inherit (ala mail.thread) the model where the field warehouse_id
    will be filled if it exists.
    
    2. If the model default.warehouse is inherited and the field is not
    setted properly as Many2one related to warehouse model, then the
    inheritance will be simply ignored.
    
    Read the test cases to understand the usage.
    
    Is closes #694

--- sale_team_warehouse/models/sales_team.py
+++ sale_team_warehouse/models/sales_team.py
@@ -1,14 +1,10 @@
 # coding: utf-8
 
 from openerp import api, fields, models
-from openerp.models import BaseModel
 
 
 class InheritedCrmSaseSection(models.Model):
 
-    '''
-        inheriting the class to add additional field
-    '''
     _inherit = "crm.case.section"
 
     default_warehouse = fields.Many2one('stock.warehouse',
@@ -18,34 +14,42 @@ class InheritedCrmSaseSection(models.Model):
                                         'the related users to the sales team.')
 
 
-@api.model
-def default_get(self, fields_list):
-    '''
-        This method modifies global default_get method
-        using _patch_method that allows Monkey-patch
-        that allows access the original method and get its
-        result. Also is possible restore the original
-        method using _revert_method
-    '''
-    defaults = default_get.origin(self, fields_list)
-    res_users_obj = self.env['res.users']
-    res_users_brw = res_users_obj.browse(self._uid)
-    warehouse_id = hasattr(res_users_brw, 'default_section_id')\
-        and res_users_brw.default_section_id and \
-        res_users_brw.default_section_id.default_warehouse.id
-    if warehouse_id:
-        model_obj = self.env['ir.model']
-        fields_obj = self.env['ir.model.fields']
-        model_id = model_obj.search([('model', '=', self._model._name)])
-        fields_data = fields_obj.search(
-            [('model_id', '=', model_id.id),
-             ('relation', '=', 'stock.warehouse'),
-             ('ttype', '=', 'many2one')])
-        names_list = []
-        [names_list.append(field.name) for field in fields_data if
-         field.name not in names_list]
-        defaults.update({name: warehouse_id for name in names_list if
-                        defaults.get(name)})
-    return defaults
+class WarehouseDefault(models.Model):
+    """If you inherit from this model and add a field called warehouse_id into
+    the model itself then the default value for such model will be the one
+    setted into the sales team.
+    """
 
-BaseModel._patch_method('default_get', default_get)
+    _auto = False
+    _name = "default.warehouse"
+
+    @api.model
+    def default_get(self, fields_list):
+        """Force that if model has a field called warehouse_id the default
+        value is the one in the sales team in the user setted
+        """
+        defaults = super(WarehouseDefault,
+                         self).default_get(fields_list)
+        res_users_obj = self.env['res.users']
+        user_brw = res_users_obj.browse(self._uid)
+        warehouse = user_brw.default_section_id.default_warehouse
+        if warehouse:
+            warehouse_id = warehouse.id
+            model_obj = self.env['ir.model']
+            fields_obj = self.env['ir.model.fields']
+            model_id = model_obj.search([('model', '=', self._model._name)])
+            fields_data = fields_obj.search(
+                [('model_id', '=', model_id.id),
+                 ('relation', '=', 'stock.warehouse'),
+                 ('ttype', '=', 'many2one')])
+            names_list = list(set([field.name for field in fields_data]))
+            defaults.update(
+                {name: warehouse_id for name in names_list
+                 if defaults.get(name)})
+        return defaults
+
+
+class SaleOrder(models.Model):
+
+    _name = "sale.order"
+    _inherit = ['sale.order', 'default.warehouse']
