993db0fd4d26d2319912e4489cd02393f651ac85
Hbto [ Vauxoo ] <hbto@users.noreply.github.com>
2016-05-09 16:04:54 -0400

    * [FIX] mrp_routing_account_journal: Getting rid of field journal_id in mrp.bom model.
    * [FIX] Getting rid of field journal_id in mrp.bom model
    * [FIX] Journal is fetch from product category when no routing is provided

--- mrp_routing_account_journal/model/mrp.py
+++ mrp_routing_account_journal/model/mrp.py
@@ -3,15 +3,6 @@
 from openerp import models, fields
 
 
-class MrpBom(models.Model):
-    _inherit = 'mrp.bom'
-    journal_id = fields.Many2one(
-        'account.journal',
-        string='Journal',
-        readonly=False,
-        )
-
-
 class MrpRouting(models.Model):
     """For specifying the routings of Work Centers.
     """
