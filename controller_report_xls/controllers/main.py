ea6a6f0d61a507b301f886a3d5f5e833eb632ee6
Moisés López <moylop260@vauxoo.com>
2016-05-18 10:19:34 -0500

    [MERGE] Improve controller report xls Vauxoo/addons-vauxoo#860 by @feketemihai  - [REF] aging_due_report: Migration to new api, small fixes  - [IMP] controller_report_xls: take in consideration thead, tbody, map some the styles from css to xfstyle

--- controller_report_xls/controllers/main.py
+++ controller_report_xls/controllers/main.py
@@ -1,45 +1,200 @@
-# coding: utf-8
+# -*- coding: utf-8 -*-
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#    Copyright (C) Vauxoo (<http://www.vauxoo.com>).
+#    All Rights Reserved
+# #############Credits######################################################
+#    Coded by: Humberto Arocha <hbto@vauxoo.com>
+#    Coded by: Humberto Arocha <hbto@vauxoo.com>
+#    Planified by: Humberto Arocha <hbto@vauxoo.com>
+#    Audited by: Humberto Arocha <hbto@vauxoo.com>
+#############################################################################
+#    This program is free software: you can redistribute it and/or modify it
+#    under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or (at your
+#    option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+###############################################################################
 
 from openerp.addons.report.controllers import main
 from openerp.addons.web.http import route, request  # pylint: disable=F0401
 from werkzeug import url_decode  # pylint: disable=E0611
-import simplejson
-import lxml.html
+from lxml import etree
 
+from ..controllers.xfstyle import css2excel
+
+import simplejson
+import xlwt
+
+import StringIO
 import logging
 _logger = logging.getLogger(__name__)
 
-import xlwt
-import StringIO
+
+def get_css_style(csstext, style):
+    cssstyle = ""
+    if csstext:
+        try:
+            import cssutils
+            from cssutils import parseString
+            cssutils.log.setLevel(logging.CRITICAL)
+        except ImportError:
+            return ""
+        cssnode = parseString(csstext)
+        stylesheet = cssnode.cssRules
+        for rule in stylesheet:
+            if rule.selectorText.replace(".", "") == style:
+                cssstyle = str(rule.style.cssText)
+    return cssstyle
+
+
+def get_odoo_style(html, style, node):
+    if node.attrib.get('class', False):
+        for class_style in node.attrib.get('class', False).split():
+            for style_element in html.xpath('//style[@type="text/css"]'):
+                styleclass = get_css_style(style_element.text,
+                                           class_style)
+                style.update(dict(item.split(":") for item in
+                             text_adapt(styleclass).split(";") if item != ''))
+    if node.attrib.get('style', False):
+        style.update(dict(item.split(":") for item in
+                     node.attrib.get('style').split(";") if item != ''))
+    return style
+
+
+def write_tables_to_excel(sheet, row, col, tables, html, table_styles):
+    for table in tables:
+        table_styles = get_odoo_style(html, table_styles, table)
+        headers = table.xpath("thead")
+        if not headers:
+            headers = table.xpath("table_header")
+        if headers:
+            for header in headers:
+                head_style = get_odoo_style(html, table_styles, header)
+                rows = header.xpath("tr")
+                if rows:
+                    row = write_rows_to_excel(sheet, row, col, rows,
+                                              html, head_style)
+        bodies = table.xpath("tbody")
+        if not bodies:
+            bodies = table.xpath("table_body")
+        if bodies:
+            for body in bodies:
+                body_style = get_odoo_style(html, table_styles, body)
+                rows = body.xpath("tr")
+                if rows:
+                    row = write_rows_to_excel(sheet, row, col, rows,
+                                              html, body_style)
+        if not headers and not bodies:
+            rows = table.xpath("tr")
+            if rows:
+                row = write_rows_to_excel(sheet, row, col, rows,
+                                          html, table_styles)
+        row += 1
+    return row
+
+
+def write_rows_to_excel(sheet, row, col, nodes, html, styles):
+    for tr in nodes:
+        new_styles = get_odoo_style(html, styles, tr)
+        rowspan = 0
+        if tr.attrib.get('rowspan', False):
+            rowspan = int(tr.attrib.get('rowspan')) - 1
+        cols = tr.xpath("td")
+        if not cols:
+            cols = tr.xpath("th")
+        if not cols:
+            continue
+        if cols:
+            row = write_cols_to_excel(sheet, row, col, rowspan, cols,
+                                      html, new_styles)
+        row += rowspan + 1
+    return row
+
+
+def write_cols_to_excel(sheet, row, col, rowspan, nodes, html, styles):
+    for td in nodes:
+        new_styles = get_odoo_style(html, styles, td)
+        # Check tables in column
+        tables = td.xpath("table")
+        if tables:
+            row = write_tables_to_excel(sheet, row, col, tables,
+                                        html, new_styles)
+        else:
+            new_text = ""
+            colspan = 0
+            if td.attrib.get('colspan', False):
+                colspan = int(td.attrib.get('colspan')) - 1
+            text = text_adapt(" ".join([x for x in td.itertext()]))
+            try:
+                new_text = float(text)
+            except ValueError:
+                new_text = text
+            cell_styles = css2excel(new_styles)
+            sheet.write_merge(row, row+rowspan,
+                              col, col+colspan,
+                              new_text, cell_styles)
+            col += colspan + 1
+    return row
+
+
+def text_adapt(text):
+    new_text = text.replace('\n', ' ').replace('\r', '')
+    new_text = new_text.replace("&nbsp;", " ").replace("  ", "")
+    return new_text.replace("; ", ";").replace(": ", ":").strip()
+
+
+def write_cell_to_excel(sheet, row, rowspan, col, colspan, node, styles):
+    # Should implement if column have many classes with different styles,
+    # but write_rich_text doesn't support write to multiple rows and columns
+    # taken from rowspan and colspan
+    cell_styles = css2excel(styles)
+    rich_text = []
+    for line in node.iter():
+        text = text_adapt(" ".join([x for x in line.itertext()]))
+        try:
+            new_text = float(text)
+        except ValueError:
+            new_text = text
+        new_style = get_odoo_style(styles, line)
+        if new_text:
+            rich_text.append(new_text)
+            text_style = css2excel(new_style)
+            rich_text.append(text_style)
+    sheet.write_rich_text(row, col, tuple(rich_text), cell_styles)
+    return True
 
 
 def get_xls(html):
-    wb = xlwt.Workbook()
+    wb = xlwt.Workbook(style_compression=2)
     ws = wb.add_sheet('Sheet 1')
-    elements = lxml.html.fromstring(html)
+    parser = etree.HTMLParser()
+    tree = etree.parse(StringIO.StringIO(html), parser)
+    root = tree.getroot()
+    html = root
     row = 0
-    for tag_id in ['table_header', 'table_body']:
-        table = elements.get_element_by_id(tag_id)
-        rows = table.findall(".//tr")
-        for tr in rows:
-            cols = tr.findall(".//td")
-            if not cols:
-                cols = tr.findall(".//th")
-            if not cols:
-                continue
-            col = 0
-            for td in cols:
-                text = "%s" % td.text_content().encode('ascii', 'ignore')
-                text = text.replace("&nbsp;", " ")
-                text = text.strip()
-                try:
-                    ws.row(row).set_cell_number(col, float(text))
-                except ValueError:
-                    ws.write(row, col, text)
-                col += 1
-            # update the row pointer AFTER a row has been printed
-            # this avoids the blank row at the top of your table
-            row += 1
+    col = 0
+    table_styles = {}
+    table_styles['background-color'] = '#FFFFFF'
+    # Check header tables
+    tables = root.xpath("//div[@class=\"header\"]/table")
+    if tables:
+        row = write_tables_to_excel(ws, row, col, tables, html, table_styles)
+    # Check page tables
+    tables = root.xpath("//div[@class=\"page\"]/table")
+    if tables:
+        row = write_tables_to_excel(ws, row, col, tables, html, table_styles)
+    # Check footer tables
+    tables = root.xpath("//div[@class=\"footer\"]/table")
+    if tables:
+        row = write_tables_to_excel(ws, row, col, tables, html, table_styles)
     stream = StringIO.StringIO()
     wb.save(stream)
     return stream.getvalue()
