80af9ea5f0b2977837ee2f5a3ca8d6b12fc1c1f2
Moisés López <moylop260@vauxoo.com>
2016-05-18 10:19:34 -0500

    [MERGE] Improve controller report xls Vauxoo/addons-vauxoo#860 by @feketemihai  - [REF] aging_due_report: Migration to new api, small fixes  - [IMP] controller_report_xls: take in consideration thead, tbody, map some the styles from css to xfstyle

--- controller_report_xls/__openerp__.py
+++ controller_report_xls/__openerp__.py
@@ -21,7 +21,7 @@
 ###############################################################################
 {
     "name": "XLS Report Controller",
-    "version": "8.0.0.1.6",
+    "version": "8.0.0.1.7",
     "author": "Vauxoo",
     "category": "Tools",
     "website": "http://www.vauxoo.com/",
@@ -29,6 +29,9 @@
     "depends": [
         "report",
     ],
+    "external_dependencies": {
+        'python': ['cssutils']
+    },
     "demo": [],
     "data": [
     ],
