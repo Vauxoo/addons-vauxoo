9aa31c5422a9962f8b63c5dcdab86452213c8c2e
Humberto Arocha <hbto@vauxoo.com>
2016-07-08 00:19:00 -0400

    [IMP] Adding a setting in company to allow skip Non-Multicurrency accounts from being include in Realization of Foreign Currency Exchange

--- account_currency_tools/wizard/exchange_realization.py
+++ account_currency_tools/wizard/exchange_realization.py
@@ -181,6 +181,9 @@ class ForeignExchangeRealization(osv.osv_memory):
                  rc_brw.journal_id.id})
 
         res['value'].update({'currency_id': cur_id})
+        res['value'].update(
+            {'check_non_multicurrency_account':
+             rc_brw.check_non_multicurrency_account})
         res['value'].update({'root_id': root_id and root_id[0]})
         return res
 
@@ -289,6 +292,14 @@ class ForeignExchangeRealization(osv.osv_memory):
             readonly=True,
             string='Company Currency',
             help="This is currency used to post Exchange Rate Difference"),
+        'check_non_multicurrency_account': fields.related(
+            'company_id', 'check_non_multicurrency_account',
+            type='boolean',
+            readonly=True,
+            string='Check Non-Multicurrency Account',
+            help="Check Accounts that were not set as multicurrency, "
+            "i.e., they were not set with a secondary currency, "
+            "but were involved in multicurrency transactions"),
         'period_ids': fields.many2many(
             'account.period', 'act_period_acc_rel',
             'period_id', 'act_id', 'Affected Periods',
@@ -478,9 +489,10 @@ class ForeignExchangeRealization(osv.osv_memory):
             ])
 
         # Searching for other accounts that could be used as multicurrency
-        args = self.get_params(cr, uid, ids, account_type, fieldname,
-                               context=context)
-        res += self.get_accounts_from_aml(cr, uid, args, context=context)
+        if wzd_brw.check_non_multicurrency_account:
+            args = self.get_params(
+                cr, uid, ids, account_type, fieldname, context=context)
+            res += self.get_accounts_from_aml(cr, uid, args, context=context)
         res = list(set(res))
 
         if res:
