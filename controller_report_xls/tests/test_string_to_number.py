6819f3f21fafd7481c524b83d280842640676cba
Humberto Arocha <hbto@vauxoo.com>
2016-07-12 13:53:27 +0000

    [IMP]Added UnitTest to string_to_number method

---
+++ controller_report_xls/tests/test_string_to_number.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+
+from openerp.tests.common import TransactionCase
+from openerp.addons.controller_report_xls.controllers.main \
+    import string_to_number
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class TestController(TransactionCase):
+
+    def setUp(self):
+        super(TestController, self).setUp()
+        # Asume lang = en_US
+        self.lang_en_US = {
+            'decimal_point': '.',
+            'thousands_sep': ',',
+        }
+        self.lang_es_ES = {
+            'decimal_point': ',',
+            'thousands_sep': '.',
+        }
+        self.value_en_US = u'-7,777,777.77'
+        self.value_es_ES = u'-7.777.777,77'
+        self.value_text = u'a-7.777.777,77'
+        self.result = -7777777.77
+
+    def test_string_to_number_en_US(self):
+        res = string_to_number(self.value_en_US, self.lang_en_US)
+        self.assertEqual(res, self.result, 'Result not expected for en_US')
+
+    def test_string_to_number_es_ES(self):
+        res = string_to_number(self.value_es_ES, self.lang_es_ES)
+        self.assertEqual(res, self.result, 'Result not expected for es_ES')
+
+    def test_string_to_number_text(self):
+        res = string_to_number(self.value_text, self.lang_es_ES)
+        self.assertEqual(res, self.result, 'Result not expected for es_ES')
+        res = string_to_number(self.value_text, self.lang_en_US)
+        self.assertEqual(res, self.result, 'Result not expected for en_US')
