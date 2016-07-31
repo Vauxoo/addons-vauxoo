4b105c84b6aa355f5c4f2d0ab1287f7e60ce8064
Katherine Zaoral <kathy@vauxoo.com>
2016-07-31 23:51:07 +0000

    [ADD] create new method used to filter the current records taking into account the current user warehouses.

--- picking_by_warehouse/models/warehouse_sales_team.py
+++ picking_by_warehouse/models/warehouse_sales_team.py
@@ -8,7 +8,7 @@
 ############################################################################
 
 
-from openerp import api, fields, models
+from openerp import api, fields, models, SUPERUSER_ID
 
 
 class InheritedCrmSaseSection(models.Model):
@@ -29,18 +29,51 @@ class WarehouseUser(models.Model):
 
     _name = "warehouse.user"
 
-    @api.multi
-    def check_access_rule(self, operation):
-        """
-        POC
-        """
-        access_rule = super(WarehouseUser, self).check_access_rule(operation)
+    @api.model
+    def get_from_my_warehouses(self):
+        """ Review current users sales teams/warehouse and return the ids
+        sub-groups that match to the warehouses
 
-        # TODO delete this
-        print ' ------ check_access_rule'
-        # print access_rule
-        import pdb
-        pdb.set_trace()
+        :return: list of ids that match with the user warehouses
+        """
+        res = []
+        user = self.env.user
+        cursor = self.env.cr
+
+        # Current user sales teams
+        cursor.execute("SELECT DISTINCT section_id FROM sale_member_rel"
+                       " WHERE member_id = '%s'" % user.id)
+        records = cursor.dictfetchall()
+        sale_teams = [record.get('section_id', False) for record in records]
+        # user_sale_teams = self.env['crm.case.section'].search([]).filtered(
+        #     lambda team: user in team.member_ids)
+        if not sale_teams:
+            return res
+
+        # Get the related Warehouses. The users could belong to a several sale
+        # teams but this teams could not have warehouse defined. Need to be
+        # careful to
+        where_clause = "IN " + str(tuple(sale_teams)) if len(sale_teams) > 1 \
+            else '= ' + str(sale_teams[0])
+        cursor.execute("SELECT DISTINCT warehouse_id FROM crm_case_section"
+                       " WHERE id " + where_clause +
+                       " AND warehouse_id IS NOT NULL")
+        records = cursor.dictfetchall()
+        warehouses = set([
+            record.get('warehouse_id', False) for record in records])
+        # warehouse_ids = user_sale_teams.mapped('warehouse_id').mapped('id')
+        if not warehouses:
+            return res
+
+        if self and warehouses:
+            where_clause = "IN " + str(tuple(warehouses)) \
+                if len(warehouses) > 1 else '= ' + str(warehouses.pop())
+            cursor.execute(
+                "SELECT id FROM " + self._table +
+                " WHERE warehouse_id " + where_clause)
+            records = cursor.dictfetchall()
+            res = [record.get('id', False) for record in records]
+        return res
 
     @api.model
     def fields_view_get(self, view_id=None, view_type='form',
