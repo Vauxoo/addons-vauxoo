87f97dd3ebe0984ee247287c054dde749ab151f0
suniagajose <josemiguel@vauxoo.com>
2016-09-05 18:16:59 -0500

    [ADD] product_available_by_warehouse: add field to show qty by warehouse. (#1035)
    This module provide a field in sale order line to show a message with
    product availability by warehouse. Also propose routes with qty > 0.0

---
+++ product_available_by_warehouse/README.rst
@@ -0,0 +1,51 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+    :alt: License: AGPL-3
+
+Product Available by Warehouse
+==============================
+
+This module provide functions in product to get availability by warehouse.
+Additional adds a field in sale order line to show a message with product
+availability by warehouse.
+
+To developers, can inherit this module as fast way to get product availability
+by warehouse.
+
+Known issues / Roadmap
+======================
+
+* Not tested yet in a multi-company environment
+
+Bug Tracker
+===========
+
+Bugs are tracked on `GitHub Issues
+<https://github.com/Vauxoo/addons-vauxoo/issues>`_. In case of trouble, please
+check there if your issue has already been reported. If you spotted it first,
+help us smashing it by providing a detailed and welcomed `feedback
+<https://github.com/vauxoo/
+addons-vauxoo/issues/new?body=module:%20
+product_available_by_warehouse%0Aversion:%20
+8.0%0A%0A**Steps%20to%20reproduce**%0A-%20...%0A%0A**Current%20behavior**%0A%0A**Expected%20behavior**>`_.
+
+Contributors
+------------
+
+* Jose Suniaga <josemiguel@vauxoo.com>
+
+Maintainer
+----------
+
+.. image:: https://www.vauxoo.com/logo.png
+   :alt: Vauxoo
+   :target: https://vauxoo.com
+
+This module is maintained by Vauxoo.
+
+a latinamerican company that provides training, coaching,
+development and implementation of enterprise management
+systems and bases its entire operation strategy in the use
+of Open Source Software and its main product is odoo.
+
+To contribute to this module, please visit http://www.vauxoo.com.
+
---
+++ product_available_by_warehouse/models/product.py
@@ -0,0 +1,42 @@
+# coding: utf-8
+############################################################################
+#    Module Writen For Odoo, Open Source Management Solution
+#
+#    Copyright (c) 2011 Vauxoo - http://www.vauxoo.com
+#    All Rights Reserved.
+#    info Vauxoo (info@vauxoo.com)
+#    coded by: Jose Suniaga <josemiguel@vauxoo.com>
+#    planned by: Julio Serna <julio@vauxoo.com>
+############################################################################
+
+from openerp import api, models
+
+
+class ProductProduct(models.Model):
+    _inherit = "product.product"
+
+    @api.multi
+    def get_product_available_by_warehouse(self):
+        ctx = dict(self._context)
+        res = {}.fromkeys(self.ids, {})
+        for product in self:
+            qty_available = []
+            virtual_available = []
+            incoming_qty = []
+            outgoing_qty = []
+            for warehouse in self.env['stock.warehouse'].search([]):
+                ctx.update({'warehouse': warehouse.id, 'location': False})
+                product_qty = product.with_context(ctx).\
+                    _product_available()[product.id]
+                qty_available += [(warehouse, product_qty['qty_available'])]
+                virtual_available += [
+                    (warehouse, product_qty['virtual_available'])]
+                incoming_qty += [(warehouse, product_qty['incoming_qty'])]
+                outgoing_qty += [(warehouse, product_qty['outgoing_qty'])]
+            res[product.id] = {
+                'qty_available': qty_available,
+                'virtual_available': virtual_available,
+                'incoming_qty': incoming_qty,
+                'outgoing_qty': outgoing_qty,
+            }
+        return res
