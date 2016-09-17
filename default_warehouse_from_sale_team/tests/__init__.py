53d2d8f671d3785cc85335c5aad346c30cfb720a
Yennifer Santiago <yennifer@vauxoo.com>
2016-09-17 05:18:01 +0000

    [ADD] defualt_warehouse_from_sale_team: Adding test to new fields default_sale_pricelist and default_purchase_pricelist.

--- default_warehouse_from_sale_team/tests/__init__.py
+++ default_warehouse_from_sale_team/tests/__init__.py
@@ -1,3 +1,4 @@
 # coding: utf-8
 from . import test_default_warehouse
 from . import test_pricelist_team
+from . import test_default_pricelist
---
+++ default_warehouse_from_sale_team/tests/test_default_pricelist.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+from openerp.tests.common import TransactionCase
+from psycopg2 import IntegrityError
+
+
+class TestDefaultPricelist(TransactionCase):
+
+    def setUp(self):
+        super(TestDefaultPricelist, self).setUp()
+        self.test_user = self.env['res.users'].create({
+            'name': 'User Test', 'login': 'user_test',
+            'password': '123456', 'email': 'user_test@email.com',
+            'sel_groups_9_45_10': 9, 'sel_groups_61_62': 61})
+        self.limited_group = self.env.ref(
+            'default_warehouse_from_sale_team.'
+            'group_limited_default_product_pricelist')
+        self.limited_group.write({'users': [(4, self.test_user.id)]})
+        self.sale_team = self.env['crm.case.section'].create({
+            'name': 'Test Sale Team',
+            'alias_contact': 'everyone',
+            'member_ids': [(4, self.test_user.id)], })
+        self.pricelist_sale = self.env['product.pricelist'].create({
+            'name': 'Pricelist Test Sale', 'type': 'sale'})
+        self.pricelist_purchase = self.env['product.pricelist'].create({
+            'name': 'Pricelist Test Purchase', 'type': 'purchase'})
+        self.pricelist_test = self.env['product.pricelist'].create({
+            'name': 'Pricelist Test', 'type': 'sale'})
+        self.partner_sale = self.env.ref('base.res_partner_9')
+        self.partner_purchase = self.env.ref('base.res_partner_4')
+        self.dict_vals_sale = {
+            'partner_id': self.partner_sale.id,
+            'partner_invoice_id': self.partner_sale.id,
+            'partner_shipping_id': self.partner_sale.id,
+        }
+
+    def test_00_sale_without_pricelist(self):
+        """In this test we are probing a sale team without default sale
+        pricelist or pricelist's available.
+        """
+        self.assertIn(self.test_user, self.limited_group.users)
+
+        # An IntegrityError raised because the sale order has not a pricelist
+        message = 'null value in column.*'
+        with self.assertRaisesRegexp(IntegrityError, message):
+            self.env['sale.order'].sudo(
+                self.test_user).create(self.dict_vals_sale)
+
+    def test_10_sale_without_pricelist_default(self):
+        """In this test we are probing that sale order is created with first
+        pricelist in pricelist's available in user sale team.
+        """
+        self.sale_team.write({
+            'pricelist_team_ids': [(4, self.pricelist_test.id)], })
+        self.test_user.write({'default_section_id': self.sale_team.id, })
+        self.assertEqual(self.test_user.default_section_id, self.sale_team)
+
+        sale_order = self.env['sale.order'].sudo(
+            self.test_user).create(self.dict_vals_sale)
+        self.assertEqual(sale_order.pricelist_id, self.pricelist_test)
+
+    def test_20_sale_with_pricelist_default(self):
+        """In this test we are probing that sale order is created with
+        default sale pricelist.
+        """
+        self.sale_team.write({
+            'pricelist_team_ids': [(4, self.pricelist_test.id)],
+            'default_sale_pricelist': self.pricelist_sale.id})
+        self.test_user.write({'default_section_id': self.sale_team.id, })
+
+        sale_order = self.env['sale.order'].sudo(
+            self.test_user).create(self.dict_vals_sale)
+        self.assertEqual(sale_order.pricelist_id, self.pricelist_sale)
+
+    def test_30_purchase_with_pricelist_default(self):
+        """In this test we are probing that purchase order is created with
+        default purchase pricelist.
+        """
+        self.sale_team.write({
+            'pricelist_team_ids': [(4, self.pricelist_test.id)],
+            'default_purchase_pricelist': self.pricelist_purchase.id})
+        self.test_user.write({'default_section_id': self.sale_team.id, })
+        dict_vals = {
+            'partner_id': self.partner_purchase.id,
+            'pricelist_id': self.env['purchase.order'].sudo(
+                self.test_user).onchange_partner_id(
+                    self.partner_purchase.id)['value']['pricelist_id'],
+            'location_id': self.env.ref('stock.stock_location_stock').id,
+        }
+
+        purchase_order = self.env['purchase.order'].sudo(
+            self.test_user).create(dict_vals)
+        self.assertEqual(purchase_order.pricelist_id, self.pricelist_purchase)
