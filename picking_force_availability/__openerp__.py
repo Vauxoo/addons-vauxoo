844ede0aff97375b9d8c1372c922a67aeacf8940
Luis Ernesto Torres Macías <luis_t@vauxoo.com>
2016-09-29 18:20:14 -0500

    [ADD]  picking_force_availability: Added group to show force (#1068)
    availability button
    
    Added new group to hide the button "Force Availability" in picking, when
    the products are unavailable.
    
    Only users with this group allow press this.

---
+++ picking_force_availability/__openerp__.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+# Copyright 2016 Vauxoo (https://www.vauxoo.com) <info@vauxoo.com>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Picking Force Availability",
+    "version": "9.0.1.0.0",
+    "author": "Vauxoo",
+    "category": "Generic/Stock",
+    "website": "http://www.vauxoo.com",
+    "license": "LGPL-3",
+    "depends": [
+        "stock"
+    ],
+    "demo": [],
+    "data": [
+        "security/res_groups.xml",
+        "views/stock_view.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
