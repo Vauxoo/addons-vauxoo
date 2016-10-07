858fd822dc15e0bdbf3233afed7eedce66246ffe
Edgar Rivero <edgar@vauxoo.com>
2016-10-07 04:27:38 +0000

    [IMP] partner_credit_limit: Test adapted to work in Odoo 9. Created common file to add function to load the demo data of account module

--- partner_credit_limit/tests/__init__.py
+++ partner_credit_limit/tests/__init__.py
@@ -1,4 +1,6 @@
 # coding: utf-8
 # Copyright 2016 Vauxoo (https://www.vauxoo.com) <info@vauxoo.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-from . import test_invoice_credit_limit, test_sale_credit_limit
+from . import common
+from . import test_invoice_credit_limit
+from . import test_sale_credit_limit
