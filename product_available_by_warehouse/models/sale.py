2fe060c35d609a29933ce235c6b248cfcdc455d1
Gabriela Quilarque <gabriela@vauxoo.com>
2016-10-16 07:48:36 +0000

    [REF] Modified way to show the product available by warehouse in sale.order.line - Added new o2m to show the product available by warehouse. - Added four pages to classify the products available by:    - On hand.    - Forecast    - Incoming    - Outgoing - Integrated the new o2m's with sale.order.view.

--- product_available_by_warehouse/models/sale.py
+++ product_available_by_warehouse/models/sale.py
@@ -24,49 +24,22 @@ class SaleOrderLine(models.Model):
         string="Show Message",
         store=False,
         search=True)
+        
+    name = fields.Text(string='Description', required=True, size=2000)
+        
+    on_hand_ids = fields.One2many('warehouse.qty','on_hand_line_id',
+                                    string='On hand quantity by warehouse',
+                                    domain=[('name','=','qty_available')])
+    forecast_ids = fields.One2many('warehouse.qty','forecast_line_id',
+                                    string='Forecast quantity by warehouse',
+                                    domain=[('name','=','virtual_available')])
+    incoming_ids = fields.One2many('warehouse.qty','incoming_line_id',
+                                    string='Incoming quantity by warehouse',
+                                    domain=[('name','=','incoming_qty')])
+    outgoing_ids = fields.One2many('warehouse.qty','outgoing_line_id',
+                                    string='Outgoing quantity by warehouse',
+                                    domain=[('name','=','outgoing_qty')])
 
-    @api.multi
-    def onchange_show_message(self, show_message, product_id):
-        if not (show_message and product_id):
-            return {}
-
-        colname = show_message
-        product = self.env['product.product'].browse(product_id)
-        titles = {
-            'qty_available': _('Product available by Warehouse'),
-            'virtual_available': _('Forecast Quantity by Warehouse'),
-            'incoming_qty': _('Incoming Quantity by Warehouse'),
-            'outgoing_qty': _('Outgoing Quantity by Warehouse'),
-        }
-        warning_msgs = "%s\n\n" % product.name
-        product_qty_by_wh = []
-        route_ids = []
-        if colname in titles:
-            product_qty_by_wh = product.\
-                get_product_available_by_warehouse()[product.id][colname]
-        for warehouse, product_qty in product_qty_by_wh:
-            # TODO: uncomment until figure out a better way to choice only
-            # the routes with availability
-            # if product_qty > 0.0 and \
-            #         colname in ['qty_available', 'virtual_available']:
-            #     route_ids += [warehouse.delivery_route_id.id]
-            warning_msgs += _("- %s . Quantity: %.2f \n" %
-                              (warehouse.name, product_qty))
-        res = {}
-        # update of warning messages
-        if product_qty_by_wh:
-            warning = {
-                'title': titles[colname],
-                'message': warning_msgs,
-            }
-            res.update({'warning': warning})
-
-        # update route domain
-        if route_ids:
-            domain = res.get('domain', {})
-            domain['route_id'] = [('id', 'in', route_ids)]
-            res.update({'domain': domain})
-        return res
 
     @api.multi
     def product_id_change_with_wh(
@@ -83,24 +56,63 @@ class SaleOrderLine(models.Model):
         if not product:
             return res
 
-        if self._context.get('show_message'):
-            colname = self._context.get('show_message')
+        product_brw = self.env['product.product'].browse(product)
+        titles = ['qty_available','virtual_available','incoming_qty',
+                    'outgoing_qty']
+        product_qty_by_wh = []
+        route_ids = []
+        on_hand_ids = []
+        forecast_ids = []
+        incoming_ids = []
+        outgoing_ids = []
+        
+        for colname in titles:
+            product_qty_by_wh = product_brw.\
+                get_product_available_by_warehouse()[product][colname]
+                
+            for warehouse, product_qty in product_qty_by_wh:
+                # TODO: uncomment until figure out a better way to choice only
+                # the routes with availability
+                # if product_qty > 0.0 and \
+                #         colname in ['qty_available', 'virtual_available']:
+                #     route_ids += [warehouse.delivery_route_id.id]
+                dict_qty = (0,0,{'warehouse_id':warehouse.id,
+                            'qty':product_qty,
+                            'sale_order_line_id':self.id,
+                            'name':colname})
+                if colname == 'qty_available':
+                    on_hand_ids.append(dict_qty)
+                elif colname == 'virtual_available':
+                    forecast_ids.append(dict_qty)
+                elif colname == 'incoming_qty':
+                    incoming_ids.append(dict_qty)
+                elif colname == 'outgoing_qty':
+                    outgoing_ids.append(dict_qty)
 
-            result = self.onchange_show_message(colname, product)
-            # replace warning
-            if 'warning' in result:
-                res.update({'warning': result['warning']})
-            # update domain
-            if 'domain' in result:
-                domain = res.get('domain', {})
-                domain['route_id'] = result['domain'].get('route_id', [])
-                res.update({'domain': domain})
+        res = {'value':{'on_hand_ids':on_hand_ids,
+                        'forecast_ids':forecast_ids,
+                        'incoming_ids':incoming_ids,
+                        'outgoing_ids':outgoing_ids,
+                        }}
         return res
 
 
 class WarehouseQty(models.Model):
     _name = 'warehouse.qty'
 
-    product_id = fields.Many2one('product.product', 'Product')
+    name = fields.Selection(
+        selection=[
+            ('qty_available', 'On hand quantity by warehouse'),
+            ('virtual_available', 'Forecast quantity by warehouse'),
+            ('incoming_qty', 'Incoming quantity by warehouse'),
+            ('outgoing_qty', 'Outgoing quantity by warehouse'),
+        ],
+        string="Quantity by Warehouse",
+        store=False,
+        search=True)
     warehouse_id = fields.Many2one('stock.warehouse', 'Warehouse')
     qty = fields.Float('Quantity')
+    on_hand_line_id = fields.Many2one('sale.order.line' ,string='On Hand');
+    forecast_line_id = fields.Many2one('sale.order.line' ,string='Forecast');
+    incoming_line_id = fields.Many2one('sale.order.line' ,string='Incoming');
+    outgoing_line_id = fields.Many2one('sale.order.line' ,string='Outgoing');
