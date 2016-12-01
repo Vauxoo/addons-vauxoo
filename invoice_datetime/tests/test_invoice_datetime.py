b5e1409a89f2bc23adfa866d39bcab2acd0a18ce
Osval Reyes <osval@vauxoo.com>
2016-12-01 01:30:58 +0000

    [REF] invoice_datetime: migrate to 10.0

--- invoice_datetime/tests/test_invoice_datetime.py
+++ invoice_datetime/tests/test_invoice_datetime.py
@@ -5,8 +5,8 @@
 import time
 import datetime
 import logging
-from openerp.tests.common import TransactionCase
-from openerp.tools import mute_logger
+from odoo.tests.common import TransactionCase
+from odoo.tools import mute_logger
 _logger = logging.getLogger(__name__)
 
 
@@ -14,13 +14,12 @@ class TestInvoiceDatetime(TransactionCase):
 
     def setUp(self):
         super(TestInvoiceDatetime, self).setUp()
-        self.user = self.registry('res.users')
-        self.data = self.registry('ir.model.data')
-        self.invoice = self.registry('account.invoice')
+        self.user = self.env.user
+        self.data = self.env['ir.model.data']
+        self.invoice = self.env['account.invoice']
 
-    @mute_logger('openerp.addons.base.ir.ir_model', 'openerp.osv.orm')
+    @mute_logger('odoo.addons.base.ir.ir_model', 'odoo.osv.orm')
     def test_get_datetime(self):
-        cr, uid = self.cr, self.uid
         # get datetime of server in string
         dt_server = time.strftime('%Y-%m-%d %H:%M:%S')
         # I generate object of type datetime
@@ -28,31 +27,27 @@ class TestInvoiceDatetime(TransactionCase):
         datetime_object = datetime.datetime.strptime(dt_server,
                                                      '%Y-%m-%d %H:%M:%S')
         # create list with tz to apply in datetime
-        self.user.write(cr, uid, uid, {'tz': 'Europe/Rome'})
+        self.user.write({'tz': 'Europe/Rome'})
         # Assign tz to user
         # call the function _get_datetime_with_user_tz
         # to get the date with tz applied
         datetime_tz_rome = self.invoice._get_datetime_with_user_tz(
-            cr, uid, datetime_object)
+            datetime_object)
         # Create an invoice with last tz of cycle America/Mexico_City
         invoice_tz_rome_id = self.invoice.create(
-            cr, uid,
             {'partner_id': 1,
              'account_id': 1,
              'invoice_datetime': dt_server})
-        self.user.write(cr, uid, uid, {'tz': 'America/Mexico_City'})
+        self.user.write({'tz': 'America/Mexico_City'})
         # Assign tz to user
         datetime_tz_mx = self.invoice._get_datetime_with_user_tz(
-            cr, uid, datetime_object)
+            datetime_object)
         invoice_tz_mx_id = self.invoice.create(
-            cr, uid,
             {'partner_id': 1,
              'account_id': 1,
              'invoice_datetime': dt_server})
-        dt_inv_tz_rome = self.invoice.read(
-            cr, uid, invoice_tz_rome_id, []).get('date_invoice_tz')
-        dt_inv_tz_mx = self.invoice.read(
-            cr, uid, invoice_tz_mx_id, []).get('date_invoice_tz')
+        dt_inv_tz_rome = invoice_tz_rome_id.date_invoice_tz
+        dt_inv_tz_mx = invoice_tz_mx_id.date_invoice_tz
         _logger.info("Validate datetime of function, for Rome-Mexico_City TZ")
         self.assertNotEqual(
             datetime_tz_rome,
