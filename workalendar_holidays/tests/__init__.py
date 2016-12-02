f3507e543285cb30a179a1d15806715c79f0647b
Jose Suniaga [Vauxoo] <josemiguel@vauxoo.com>
2016-12-02 10:41:11 -0500

    [FIX] workalendar_holidays: fixed wrong call to next_day (#1131)
    In previous_day function t was bad called next_day, what
    caused a wrong behaviour when it was used compute_working_days.
    
    Also was added unittests for resource.calendar model functions
    and holiday import wizard feature

---
+++ workalendar_holidays/tests/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+############################################################################
+#    Module Writen For Odoo, Open Source Management Solution
+#
+#    Copyright (c) 2011 Vauxoo - http://www.vauxoo.com
+#    All Rights Reserved.
+#    info Vauxoo (info@vauxoo.com)
+#    coded by: Jose Suniaga <josemiguel@vauxoo.com>
+#
+############################################################################
+from . import test_resource
+from . import test_holiday_wizard
