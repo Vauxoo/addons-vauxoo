74f6cfd87e5deaccb4f39475b9f78b30a7883d02
Osval Reyes <osval@vauxoo.com>
2016-12-13 22:21:58 +0000

    [REF] partner_validate_vat: migrate to 10.0 and enable it

---
+++ partner_validate_vat/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2013 Vauxoo - http://www.vauxoo.com/
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Partner Validate VAT",
+    "version": "10.0.1.0.0",
+    "author": "Vauxoo",
+    "category": "Addons-vauxoo",
+    "website": "http://www.vauxoo.com/",
+    "license": "AGPL-3",
+    "depends": [
+        "base"
+    ],
+    "installable": True,
+}
