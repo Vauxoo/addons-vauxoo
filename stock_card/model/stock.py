88bca89eda7409c0361d48f7e5a053903e0b19b6
JulioSerna <julio@vauxoo.com>
2016-12-14 07:42:51 +0000

    [IMP] stock_card: add method to validate return move

--- stock_card/model/stock.py
+++ stock_card/model/stock.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from openerp import fields, models
+from openerp import fields, models, api
 
 
 class StockDiscrete(models.Model):
@@ -16,3 +16,15 @@ class StockMove(models.Model):
     _inherit = 'stock.move'
 
     discrete_ids = fields.One2many('stock.discrete', 'move_id')
+
+    @api.multi
+    def _check_return_move(self):
+        """ Use this method to indicate when move is a
+        refund customer/supplier
+        """
+        for move in self:
+            if move.product_id.cost_method == 'average' and \
+                    move.product_id.valuation == 'real_time':
+                if move.location_dest_id.usage == 'internal' and \
+                        move.location_id.usage in ('supplier', 'customer'):
+                    return True
