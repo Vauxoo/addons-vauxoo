f1258f06fc53e92adb8b80629c9d6ef2922be7b7
Nhomar Hernandez <nhomar@vauxoo.com>
2017-01-15 13:49:43 -0600

    [MIG] payment_term_type and payment_term_type: Migrate to v10.0

--- partner_credit_limit/tests/common.py
+++ partner_credit_limit/tests/common.py
@@ -2,9 +2,9 @@
 # Copyright 2016 Vauxoo (https://www.vauxoo.com) <info@vauxoo.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from openerp.tests.common import TransactionCase
-from openerp import tools
-from openerp.modules.module import get_module_resource
+from odoo.tests.common import TransactionCase
+from odoo import tools
+from odoo.modules.module import get_module_resource
 
 
 class TestCommon(TransactionCase):
