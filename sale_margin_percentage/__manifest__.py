d97b2373f24482c75d4093e7fcec506b8bf32980
Jose Suniaga [Vauxoo] <josemiguel@vauxoo.com>
2017-02-07 02:12:11 +0000

    [ADD] Sale Order Gross Margin Percentage
    By this way, the sale order gross margin percentage is calculated
    by the sum all the margin lines between the inverse sum of all
    subtotals.
    
    Module features:
    - Field to configure Margin Threshold on sale settings.
    - Colors in sale order lines to point the margin threshold.
    - Fix to 100 margin percentage when purchase price is not set.
    - Fix to -100 margin percentage when price unit is not set.
    Fix to 0 margin percentage when product quatity is 0.

---
+++ sale_margin_percentage/__manifest__.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+# Copyright 2017 Vauxoo (https://www.vauxoo.com) <info@vauxoo.com>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Sale Order Gross Margin Percentage",
+    "version": "10.0.0.1.0",
+    "author": "Vauxoo",
+    "category": "",
+    "website": "http://www.vauxoo.com/",
+    "license": "LGPL-3",
+    "depends": [
+        "sale_margin",
+    ],
+    "demo": [],
+    "data": [
+        "views/sale_views.xml",
+        "views/sale_config_settings_views.xml",
+    ],
+    "installable": True,
+}
