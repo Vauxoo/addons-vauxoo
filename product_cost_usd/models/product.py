22f223f46586441b27be6999b8230428cf29bdf5
Jose Suniaga [Vauxoo] <josemiguel@vauxoo.com>
2017-02-07 02:36:21 +0000

    [ADD] product_cost_usd: handle cost in USD on products
    The approach is to have the USD as the base currency,
    so that the sale prices in company currency can be
    kept up to date through the exchange rate.
    
    Module features:
    - New Cost USD field on the Product form.
    - Validate Cost USD so that it is not less than the
      list price of the supplier.
    - Avoid save a 'Cost in USD' when product does not has
      assigned a supplier with price in USD.
    - Allowed pricelist computation based on Cost in USD.
    - Added unit tests to validate constrains and pricelists
      computation.
    - Compatibility with module sale_margin.

---
+++ product_cost_usd/models/product.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Vauxoo
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+import odoo.addons.decimal_precision as dp
+from odoo.tools import float_compare
+
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    @api.constrains('standard_price_usd', 'seller_ids')
+    def check_cost_and_price(self):
+        """ Validate 'Cost in USD' usability.
+
+        Usability conditions:
+        - Before set a 'Cost in USD' in a product at least one supplier should
+          have price in USD.
+        - The Cost in USD cannot be less than supplier price.
+        """
+        usd_currency = self.env.ref('base.USD')
+        prec = self.env['decimal.precision'].precision_get('Product Price')
+        usd_seller = self.seller_ids.filtered(
+            lambda x: x.currency_id == usd_currency)
+        list_price = usd_seller.price if usd_seller else 0.0
+        standard_price_usd = self.standard_price_usd
+        if not usd_seller and float_compare(
+                standard_price_usd, 0, precision_digits=prec) > 0:
+            raise ValidationError(
+                _('You must have at least one supplier with price in USD'
+                  ' before assign a Cost in USD'))
+        if float_compare(
+                list_price, standard_price_usd, precision_digits=prec) > 0:
+            raise ValidationError(
+                _('You cannot create or modify a product if the cost in USD'
+                  ' is less than the supplier list price.\n\n'
+                  '- Supplier list price = %s\n'
+                  '- Cost in USD = %s') % (list_price, standard_price_usd))
+
+    standard_price_usd = fields.Float(
+        'Cost in USD',
+        digits=dp.get_precision('Product Price'),
+        help="Price cost of the product in USD currency")
