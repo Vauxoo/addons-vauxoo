68f348de72bae1ed7643f5e85b850174f40e9f07
Jose Suniaga [Vauxoo] <josemiguel@vauxoo.com>
2017-02-07 02:12:11 +0000

    [ADD] Sale Order Gross Margin Percentage
    By this way, the sale order gross margin percentage is calculated
    by the sum all the margin lines between the inverse sum of all
    subtotals.
    
    Module features:
    - Field to configure Margin Threshold on sale settings.
    - Colors in sale order lines to point the margin threshold.
    - Fix to 100 margin percentage when purchase price is not set.
    - Fix to -100 margin percentage when price unit is not set.
    Fix to 0 margin percentage when product quatity is 0.

---
+++ sale_margin_percentage/models/sale_order.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Vauxoo
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from __future__ import division
+from odoo import models, fields, api
+import odoo.addons.decimal_precision as dp
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    margin_percentage = fields.Float(
+        compute='_compute_margin_percentage',
+        digits=dp.get_precision('Product Price'),
+        store=True,
+        string="Margin percentage",
+        help="Margin percentage compute based on price unit")
+
+    @api.depends('order_line.margin_percentage')
+    def _compute_margin_percentage(self):
+        for order in self:
+            lines = order.order_line.filtered(lambda r: r.state != 'cancel')
+            margin_sum = sum(lines.mapped('margin'))
+            subtotal = order.amount_untaxed
+            if not subtotal or not margin_sum:
+                order.margin_percentage = 0.0
+                continue
+            order.margin_percentage = margin_sum * (100.0 / subtotal)
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    @api.model
+    def _default_margin_threshold(self):
+        return self.env.user.company_id.margin_threshold
+
+    margin_threshold = fields.Float(default=_default_margin_threshold)
+    margin_percentage = fields.Float(
+        compute='_compute_margin_percentage',
+        digits=dp.get_precision('Product Price'),
+        store=True,
+        string="Margin Percentage",
+        help="Margin percentage compute based on price unit ")
+    purchase_price = fields.Float(readonly=True)
+
+    @api.depends('product_id', 'purchase_price', 'product_uom_qty',
+                 'price_unit')
+    def _compute_margin_percentage(self):
+        """ Same margin but we return a percentage from the purchase price.
+        """
+        for line in self:
+            currency = line.order_id.pricelist_id.currency_id
+
+            if not line.product_uom_qty:
+                line.margin_percentage = 0.0
+                continue
+
+            if not line.price_unit:
+                line.margin_percentage = -100.0
+                continue
+
+            purchase_price = \
+                line.purchase_price or line.product_id.standard_price
+            if not purchase_price:
+                line.margin_percentage = 100.0
+                continue
+
+            line.margin_percentage = currency.round((
+                line.price_subtotal - purchase_price * line.product_uom_qty) *
+                100.0 / line.price_subtotal)
