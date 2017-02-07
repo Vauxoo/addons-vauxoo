c1e739e8758d0e19d27ed1217bf83b212ac59b8a
Jose Suniaga [Vauxoo] <josemiguel@vauxoo.com>
2017-02-07 02:36:21 +0000

    [ADD] product_cost_usd: handle cost in USD on products
    The approach is to have the USD as the base currency,
    so that the sale prices in company currency can be
    kept up to date through the exchange rate.
    
    Module features:
    - New Cost USD field on the Product form.
    - Validate Cost USD so that it is not less than the
      list price of the supplier.
    - Avoid save a 'Cost in USD' when product does not has
      assigned a supplier with price in USD.
    - Allowed pricelist computation based on Cost in USD.
    - Added unit tests to validate constrains and pricelists
      computation.
    - Compatibility with module sale_margin.

---
+++ product_cost_usd/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Vauxoo
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from . import product
+from . import pricelist
+from . import sale_order
---
+++ product_cost_usd/tests/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Vauxoo
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from . import test_standard_price_usd
