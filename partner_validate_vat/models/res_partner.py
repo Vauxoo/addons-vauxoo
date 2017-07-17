7594fd4723cf328208f2ebd39e4caf15569ccfea
Deivis Laya <deivis@vauxoo.com>
2017-07-17 20:18:03 +0000

    [FIX] partner_validate_vat: Optimizing method to validate vat

--- partner_validate_vat/models/res_partner.py
+++ partner_validate_vat/models/res_partner.py
@@ -1,28 +1,6 @@
 # coding: utf-8
-#
-#    Module Writen to OpenERP, Open Source Management Solution
-#
-#    Copyright (c) 2017 Vauxoo - http://www.vauxoo.com/
-#    All Rights Reserved.
-#    info Vauxoo (info@vauxoo.com)
-#
-#    Coded by: jorge_nr (jorge_nr@vauxoo.com)
-#
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#
+# Copyright 2017 Vauxoo (https://www.vauxoo.com) <info@vauxoo.com>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from openerp import models, api, fields, _
 from openerp.exceptions import ValidationError
@@ -33,8 +11,7 @@ class ResPartner(models.Model):
 
     vat = fields.Char(copy=False)
 
-    @api.multi
-    @api.constrains('vat')
+    @api.constrains('vat', 'country_id')
     def _check_vat_uniqueness(self):
         """ Check that the vat is unique in the level
             where the partner in the tree
@@ -44,12 +21,11 @@ class ResPartner(models.Model):
 
         for partner in self:
             current_vat = partner.vat
-
-            if all([user_company.country_id == partner.country_id, current_vat,
-                    partner.search([
-                        ('vat', '=', current_vat),
-                        ('id', 'not in', [partner.parent_id.id, partner.id]),
-                        ('parent_id', '!=', partner.parent_id.id)
-                    ], limit=1)]):
+            current_commercial_partner = partner.commercial_partner_id.id
+            if (user_company.country_id == partner.country_id and
+                current_vat and current_commercial_partner and partner.search([
+                    ('vat', '=', current_vat),
+                    ('commercial_partner_id', '!=',
+                     partner.commercial_partner_id.id)], limit=1)):
                 raise ValidationError(_("Partner's VAT must be a unique "
                                         "value or empty"))
