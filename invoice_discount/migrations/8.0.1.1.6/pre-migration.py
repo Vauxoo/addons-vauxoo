03ab591da668a9ab3a4b00fc0dee9d6715acb331
Luis González <lgonzalez@vauxoo.com>
2017-08-12 04:38:36 +0000

    [REF] invoice_discount: Change SQL on migration to be consistent with native sentences from Odoo

--- invoice_discount/migrations/8.0.1.1.6/pre-migration.py
+++ invoice_discount/migrations/8.0.1.1.6/pre-migration.py
@@ -3,12 +3,16 @@
 
 def migrate(cr, version):
     cr.execute("ALTER TABLE account_invoice_line "
-               "ADD COLUMN discount_amount NUMERIC;")
+               "ADD COLUMN discount_amount DOUBLE PRECISION;")
+    cr.execute("COMMENT ON COLUMN account_invoice_line.discount_amount "
+               "IS 'Discount Amount';")
     cr.execute("UPDATE account_invoice_line "
                "SET discount_amount = discount * subtotal_wo_discount / 100")
 
     cr.execute("ALTER TABLE account_invoice "
-               "ADD COLUMN discount_amount NUMERIC;")
+               "ADD COLUMN discount_amount DOUBLE PRECISION;")
+    cr.execute("COMMENT ON COLUMN account_invoice.discount_amount "
+               "IS 'Discount';")
     cr.execute("UPDATE account_invoice inv "
                "SET discount_amount = view.da FROM( "
                "SELECT SUM(discount_amount) AS da, invoice_id FROM "
