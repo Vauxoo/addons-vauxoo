c9f2260baa45f6bfcd108a86b907d3a91007c850
Humberto Arocha <hbto@vauxoo.com>
2017-08-23 06:58:04 +0000

    [FIX] Copying latest changes from Sale to Purchase

--- account_anglo_saxon_stock_move_purchase/model/purchase.py
+++ account_anglo_saxon_stock_move_purchase/model/purchase.py
@@ -21,6 +21,10 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+import logging
+import operator as py_operator
+import time
+
 from openerp.osv import orm
 from openerp import fields, models, api, _
 from openerp.exceptions import ValidationError
@@ -29,47 +33,103 @@ import logging
 
 _logger = logging.getLogger(__name__)
 
+OPERATORS = {
+    '<': py_operator.lt,
+    '>': py_operator.gt,
+    '<=': py_operator.le,
+    '>=': py_operator.ge,
+    '=': py_operator.eq,
+    '!=': py_operator.ne
+}
+
 
 class PurchaseOrder(models.Model):
 
     _inherit = "purchase.order"
+    query = '''
+        SELECT purchase_id, COUNT(qty)
+        FROM (
+            SELECT
+                aml.purchase_id AS purchase_id,
+                aml.product_id as product_id,
+                aml.account_id as account_id,
+                COUNT(aml.id) as qty
+            FROM account_move_line aml
+            INNER JOIN account_account aa ON aa.id = aml.account_id
+            WHERE
+                purchase_id IN %s
+                AND product_id IS NOT NULL
+                AND reconcile_id IS NULL
+                AND aa.reconcile = TRUE
+            GROUP BY purchase_id, product_id, account_id
+            HAVING COUNT(aml.id)  > 1
+            AND ABS(SUM(aml.debit - aml.credit)) <= %s -- Use Threashold
+            ) AS view
+        GROUP BY purchase_id
+        ;'''
 
     @api.multi
-    def _compute_accrual_reconciled(self):
-        for po_brw in self:
-            unreconciled_lines = len(po_brw.mapped('aml_ids').filtered(
-                lambda l: l.account_id.reconcile and not l.reconcile_id))
-            po_brw.accrual_reconciled = not bool(unreconciled_lines)
-            po_brw.unreconciled_lines = unreconciled_lines
+    def _compute_pending_reconciliation(self):
 
-    accrual_reconciled = fields.Boolean(
-        compute='_compute_accrual_reconciled',
-        string="Reconciled Accrual",
-        help="Indicates if All Accrual Journal Items are reconciled")
-    unreconciled_lines = fields.Integer(
-        compute='_compute_accrual_reconciled',
-        string="Unreconciled Accrual Lines",
-        help="Indicates how many Accrual Journal Items are unreconciled")
+        company_id = self.env['res.users'].browse(self._uid).company_id
+        accrual_offset = company_id.accrual_offset
+
+        self._cr.execute(self.query, (tuple(self._ids), accrual_offset))
+        res = dict(self._cr.fetchall())
+
+        for brw in self:
+            brw.reconciliation_pending = res.get(brw.id, 0)
+
+        return
+
+    def _search_pending_reconciliation(self, operator, value):
+        ids = self.search([])._ids
+        res = {}.fromkeys(ids, 0)
+        company_id = self.env['res.users'].browse(self._uid).company_id
+        accrual_offset = company_id.accrual_offset
+        self._cr.execute(self.query, (tuple(ids), accrual_offset))
+
+        res.update(dict(self._cr.fetchall()))
+
+        ids = [purchase_id
+               for (purchase_id, computed_value) in res.items()
+               if OPERATORS[operator](computed_value, value)]
+
+        return [('id', 'in', ids)]
+
+    reconciliation_pending = fields.Integer(
+        compute='_compute_pending_reconciliation',
+        search='_search_pending_reconciliation',
+        string="Reconciliation Pending",
+        help="Indicates how many possible reconciliation are pending")
     aml_ids = fields.One2many(
         'account.move.line', 'purchase_id', 'Account Move Lines',
         help='Journal Entry Lines related to this Purchase Order')
 
     def cron_purchase_accrual_reconciliation(self, cr, uid, context=None):
+        company_id = self.pool['res.users'].browse(cr, uid, uid).company_id
         cr.execute('''
             SELECT
-                aml.purchase_id AS id
-                , COUNT(aml.id) as count
+                aml.purchase_id AS id,
+                aml.product_id as product_id,
+                aml.account_id as account_id,
+                COUNT(aml.id) as count,
+                SUM(aml.debit - aml.credit)
             FROM account_move_line aml
             INNER JOIN account_account aa ON aa.id = aml.account_id
             WHERE
                 purchase_id IS NOT NULL
+                AND product_id IS NOT NULL
                 AND reconcile_id IS NULL
                 AND aa.reconcile = TRUE
-            GROUP BY purchase_id
+            GROUP BY purchase_id, product_id, account_id
+            HAVING COUNT(aml.id)  > 1
+            AND ABS(SUM(aml.debit - aml.credit)) <= %s -- Use Threashold
+            ORDER BY count DESC, id DESC, product_id DESC
             ;
-            ''')
+            ''', (company_id.accrual_offset,))
 
-        ids = [x[0] for x in cr.fetchall()]
+        ids = list(set(x[0] for x in cr.fetchall()))
         self.browse(cr, uid, ids, context=context).reconcile_stock_accrual()
 
         return
@@ -79,17 +139,44 @@ class PurchaseOrder(models.Model):
         aml_obj = self.env['account.move.line']
         ap_obj = self.env['account.period']
         date = fields.Date.context_today(self)
-        period_id = ap_obj.with_context(self._context).find(date)[:1].id,
-        for po_brw in self:
+        period_id = ap_obj.with_context(self._context).find(date)[:1].id
+
+        total = len(self)
+        count = 0
+
+        company_id = self.env['res.users'].browse(self._uid).company_id
+        writeoff = company_id.writeoff
+        accrual_offset = company_id.accrual_offset
+        do_partial = company_id.do_partial
+
+        for brw in self:
+            count += 1
 
             # In order to keep every single line reconciled we will look for
             # all the lines related to a purchase/sale order
-            all_aml_ids = po_brw.aml_ids.filtered(lambda l: not l.reconcile_id)
+            self._cr.execute('''
+                SELECT
+                    aml.id
+                FROM account_move_line aml
+                INNER JOIN account_account aa ON aa.id = aml.account_id
+                WHERE
+                    purchase_id =%s
+                    AND reconcile_id IS NULL
+                    AND product_id IS NOT NULL
+                    AND aa.reconcile = TRUE
+                ;
+                ''', (brw.id,))
+
+            ids = [x[0] for x in self._cr.fetchall()]
+
+            if len(ids) < 2:
+                continue
+
+            all_aml_ids = aml_obj.browse(ids)
 
             # /!\ NOTE: This does not return Product Categories
             categ_ids = all_aml_ids.filtered(
                 lambda m:
-                m.product_id and
                 not m.product_id.categ_id.property_stock_journal).mapped(
                     'product_id.categ_id')
             if categ_ids:
@@ -102,26 +189,48 @@ class PurchaseOrder(models.Model):
             res = {}
             # Only stack those that are fully reconciled
             # amr_ids = all_aml_ids.mapped('reconcile_id')
-            amr_ids = all_aml_ids.mapped('reconcile_partial_id')
-            amr_ids.unlink()
+            all_aml_ids.mapped('reconcile_partial_id').unlink()
 
             # Let's group all the Accrual lines by Purchase/Sale Order, Product
             # and Account
-            for aml_brw in all_aml_ids.filtered('account_id.reconcile'):
-                doc_brw = aml_brw.purchase_id or aml_brw.sale_id
+            for aml_brw in all_aml_ids:
+                doc_brw = aml_brw.purchase_id
                 account_id = aml_brw.account_id.id
                 product_id = aml_brw.product_id
                 res.setdefault((doc_brw, account_id, product_id), aml_obj)
                 res[(doc_brw, account_id, product_id)] |= aml_brw
 
+            do_commit = False
             for (doc_brw, account_id, product_id), aml_ids in res.items():
-                if not len(aml_ids) > 1:
+                if len(aml_ids) < 2:
                     continue
                 journal_id = product_id.categ_id.property_stock_journal.id
+                writeoff_amount = sum(l.debit - l.credit for l in aml_ids)
                 try:
-                    aml_ids.reconcile_partial(
-                        writeoff_period_id=period_id,
-                        writeoff_journal_id=journal_id)
+                    # /!\ NOTE: Reconcile with write off
+                    if writeoff and abs(writeoff_amount) <= accrual_offset:
+                        aml_ids.reconcile(
+                            type='manual',
+                            writeoff_period_id=period_id,
+                            writeoff_journal_id=journal_id)
+                        do_commit = True
+                        continue
+                    elif abs(writeoff_amount) <= accrual_offset:
+                        aml_ids.reconcile_partial(
+                            writeoff_period_id=period_id,
+                            writeoff_journal_id=journal_id)
+                        do_commit = True
+                        continue
+                    # /!\ NOTE: I @hbto advise you to neglect the use of this
+                    # option. AS it is resource wasteful and provide little
+                    # value. Use only if you really find it Useful to
+                    # partially reconcile loose lines
+                    if do_partial:
+                        aml_ids.reconcile_partial(
+                            writeoff_period_id=period_id,
+                            writeoff_journal_id=journal_id)
+                        do_commit = True
+                        continue
 
                 except orm.except_orm:
                     message = (
@@ -130,17 +239,24 @@ class PurchaseOrder(models.Model):
                             values=", ".join([str(idx) for idx in aml_ids])))
                     _logger.exception(message)
 
-            po_brw._cr.commit()
+            if do_commit:
+                brw.message_post(
+                    subject='Accruals Reconciled at %s' % time.ctime(),
+                    body='Applying reconciliation on Order')
+                brw._cr.commit()
+                _logger.info(
+                    'Reconciling Purchase Order id:%s - %s/%s' % (
+                        brw.id, count, total))
 
         return True
 
     def view_accrual(self, cr, uid, ids, context=None):
-        ids = isinstance(ids, (int, long)) and [ids] or ids
+        ids = [ids] if isinstance(ids, (int, long)) else ids
         context = context or {}
         res = []
-        for purchase_brw in self.browse(cr, uid, ids, context=context):
+        for brw in self.browse(cr, uid, ids, context=context):
             res += [aml_brw.id
-                    for aml_brw in purchase_brw.aml_ids
+                    for aml_brw in brw.aml_ids
                     # This shall be taken away when fixing domain in aml_ids
                     if aml_brw.account_id.reconcile
                     ]
