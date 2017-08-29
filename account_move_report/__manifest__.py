c8a6b07bbe4a6ef2b5f06e6b1d7e348013d5dd2a
Yennifer Santiago <yennifer@vauxoo.com>
2017-08-29 18:41:29 +0000

    [REF] account_move_report: Migrate module with qweb

---
+++ account_move_report/__manifest__.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+# Copyright 2016 Vauxoo (https://www.vauxoo.com) <info@vauxoo.com>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Journal Entries Report",
+    "version": "saas~17.0.0.1",
+    "author": "Vauxoo",
+    "category": "Accouting",
+    "website": "http://www.vauxoo.com/",
+    "license": "LGPL-3",
+    "depends": [
+        "account",
+    ],
+    "demo": [],
+    "data": [
+        "views/account_reports.xml",
+        "views/account_move_report.xml"
+    ],
+    "test": [],
+    "js": [],
+    "css": [],
+    "qweb": [],
+    "installable": True,
+    "auto_install": False,
+}
