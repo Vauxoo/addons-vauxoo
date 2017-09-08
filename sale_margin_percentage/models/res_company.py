efe17877c72e7b1da9e1b6ff090d384e1266a426
Deivis Laya <deivis@vauxoo.com>
2017-09-08 14:00:11 -0400

    [FIX] sale_margin_percentage: Migrated attribute in element of list view (#1226)

--- sale_margin_percentage/models/res_company.py
+++ sale_margin_percentage/models/res_company.py
@@ -8,4 +8,6 @@ from odoo import fields, models
 class ResCompany(models.Model):
     _inherit = "res.company"
 
-    margin_threshold = fields.Float(default=0, digits=(16, 2))
+    margin_threshold = fields.Float(
+        default=0, digits=(16, 2),
+        help="Margin Threshold for product in order lines")
