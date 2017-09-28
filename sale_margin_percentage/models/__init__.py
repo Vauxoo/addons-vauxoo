a36e7831f2b8f07a74e8ff788f95ba885bc54cd4
Deivis Laya <deivis@vauxoo.com>
2017-09-28 17:49:00 -0400

    [FIX] sale_margin_percentage: Migrated to new configuration models (#1229)

--- sale_margin_percentage/models/__init__.py
+++ sale_margin_percentage/models/__init__.py
@@ -3,5 +3,5 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from . import res_company
-from . import sale_config_settings
+from . import res_config_settings
 from . import sale_order
