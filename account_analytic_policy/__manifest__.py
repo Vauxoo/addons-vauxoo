f738fdbab63d4b98678f5d2aaddeff8e3c2a3826
Nhomar Hernandez <nhomar@vauxoo.com>
2017-10-22 18:11:49 -0500

    [REF] account_analytic_required -> account_analytic_policy.
    Complete refactor on this module.
    
    1. Renamed.
    2. Moved configuration to account itself in order to not give access to
    account user types to end user due to this model is considered a fixed
    technical model on V11.0 for reporting.
    3. Migrated to New API.
    4. Only check create and write on account.move intentionally to check
       consistently from any place the creation of account move line.

---
+++ account_analytic_policy/__manifest__.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+############################################################################
+#    Module Writen For Odoo, Open Source Management Solution
+#
+#    Copyright (c) 2010 Vauxoo - http://www.vauxoo.com
+#    All Rights Reserved.
+#    info Vauxoo (info@vauxoo.com)
+#    coded by: nhomar <nhomar@vauxoo.com>
+############################################################################
+{
+    "name": "Account Analytic Policy",
+    "version": "11.0.0.1.7",
+    "author": "Vauxoo",
+    "category": "Accounting",
+    "website": "http://www.vauxoo.com",
+    "license": "LGPL-3",
+    "depends": [
+        "account",
+    ],
+    "demo": [
+
+    ],
+    "data": [
+        "view/account_view.xml",
+    ],
+    "installable": True,
+    "price": 50,
+    "auto_install": False,
+}
