3df2f713c37da7a30e0e086d67755ed61b3367cf
Hugo Adan <hugo@vauxoo.com>
2018-01-25 23:00:37 +0000

    [ADD] Account Invoice Change Currency This replace original odoo wizard for changing currency on an invoice with serveral improovements:
    - Log the currency change on the chatter
    - Add this functionality to supplier invoices

---
+++ account_invoice_change_currency/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# © 2017 Vauxoo, S.A. de C.V.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    'author': 'ADHOC SA,Odoo Community Association (OCA), Vauxoo',
+    'category': 'Accounting & Finance',
+    'demo_xml': [],
+    'depends': ['account'],
+    'installable': True,
+    'name': 'Account Invoice Change Currency',
+    'test': [],
+    'data': [
+        'security/security.xml',
+        'wizard/account_change_currency_view.xml',
+        'views/invoice_view.xml',
+    ],
+    'version': '11.0.0.0.0',
+    'website': 'www.adhoc.com.ar, www.vauxoo.com',
+    'license': 'AGPL-3'
+}
