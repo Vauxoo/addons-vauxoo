eeb72df4b97ffb92d74bc54df224f23b491679c0
Erick Birbe <erick@vauxoo.com>
2018-04-04 10:11:21 -0400

    [IMP] invoice_discount: Migrating to 11.0 (#1268)
    Migrating module to Odoo v11.0
    
    * Using new API on models
    * Removing deprecated sale_config_settigs configuration
    * Using standard file and folder names
    * Removing dependency to `sale.group_discount_per_so_line`, now using `base.group_no_one` as Odoo
    does for `discount` field in views.

---
+++ invoice_discount/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2018 Vauxoo
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Invoice Discount",
+    "version": "11.0.0.0.0",
+    "author": "Vauxoo",
+    "category": "Generic Modules",
+    "website": "http://www.vauxoo.com/",
+    "license": "AGPL-3",
+    "depends": [
+        "account",
+    ],
+    "demo": [],
+    "data": [
+        "views/invoice_view.xml",
+    ],
+    "test": [],
+    "js": [],
+    "css": [],
+    "qweb": [],
+    "installable": True,
+    "auto_install": False,
+}
