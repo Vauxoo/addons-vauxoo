c6d123ccc40585935a0d5e4eabb1f616ba55d6a1
Jose Antonio Morales Ponce [Vauxoo] <jose@vauxoo.com>
2018-07-10 14:20:07 -0500

    [ADD] Added new module to allow multi-company users see which company their accounting records belong. (#1288)

---
+++ account_multicompany_code/models/res_company.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+from odoo import _, api, models, fields
+from odoo.exceptions import ValidationError
+
+
+class Company(models.Model):
+    _inherit = 'res.company'
+
+    code = fields.Char(help="Internal code name of the company")
+
+    @api.multi
+    @api.constrains('code')
+    def unique_code(self):
+        for record in self:
+            if record.code and record.search(
+                    [('code', '=', record.code), ('id', '!=', record.id)]):
+                raise ValidationError(_('Code must be unique'))
