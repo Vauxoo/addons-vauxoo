0a8c027d07b428c197c473cb309cecb5e8fe4c0f
Arturo Flores <arturo@vauxoo.com>
2018-07-10 09:22:28 -0500

    [MIG] Migration of account_asset_move_check 8.0 --> 11.0 (#1280)
    * [MIG] migration from 8.0 --> 11.0
    
    * fixup! [MIG] migration from 8.0 --> 11.0
    
    * fixup! [MIG] migration from 8.0 --> 11.0

--- account_asset_move_check/model/account_asset.py
+++ account_asset_move_check/model/account_asset.py
@@ -24,55 +24,44 @@
 #
 ##############################################################################
 
-from openerp.osv import osv, fields
-import openerp.addons.decimal_precision as dp
+from odoo import api, fields, models
+from odoo.addons import decimal_precision as dp
 
 
-class AccountAssetDepreciationLine(osv.Model):
+class AccountAssetDepreciationLine(models.Model):
     _inherit = 'account.asset.depreciation.line'
 
-    def _get_move_check(self, cr, uid, ids, name, args, context=None):
-        res = super(AccountAssetDepreciationLine, self)._get_move_check(
-            cr, uid, ids, name, args, context=context)
-        for line in self.browse(cr, uid, ids, context=context):
-            res[line.id] = bool(line.move_id or line.historical)
-        return res
+    @api.depends('move_id')
+    def _get_move_check(self):
+        for line in self:
+            line.move_check = bool(line.move_id or line.historical)
 
-    _columns = {
-        'historical': fields.boolean('Historical'),
-        'move_check': fields.function(
-            _get_move_check, method=True, type='boolean', string='Posted',
-            store=True),
-    }
+        return super(AccountAssetDepreciationLine, self)._get_move_check()
 
-    _defaults = {
-        'historical': False,
-    }
+    historical = fields.Boolean(help="Check box for the historical validation",
+                                default=False)
+    move_check = fields.Boolean(help="Compute the move status",
+                                compute="_get_move_check",
+                                string='Posted', store=True)
 
 
-class AccountAssetAsset(osv.osv):
+class AccountAssetAsset(models.Model):
     _inherit = 'account.asset.asset'
 
-    def _amount_residual(self, cr, uid, ids, name, args, context=None):
-        res = super(AccountAssetAsset, self)._amount_residual(
-            cr, uid, ids, name, args, context=context)
-        dep_line_obj = self.pool.get('account.asset.depreciation.line')
-        for asset in res:
-            dep_lines = dep_line_obj.search(
-                cr, uid, [('asset_id', '=', asset),
-                          ('move_id', '=', False),
-                          ('move_check', '=', True),
-                          ], context=context)
-            amount = 0
-            for line in dep_line_obj.browse(
-                    cr, uid, dep_lines, context=context):
-                amount += line.amount or 0.0
-            res.update({asset: res.get(asset, 0.0) - amount})
-        return res
+    value_residual = fields.Float(
+        help="Stores the value of the process",
+        digits=dp.get_precision('Account'), compute="_amount_residual",
+        string='Net Book Value')
 
-    _columns = {
-        'value_residual': fields.function(
-            _amount_residual, method=True,
-            digits_compute=dp.get_precision('Account'),
-            string='Net Book Value'),
-    }
+    def _amount_residual(self):
+        dep_line_obj = self.env['account.asset.depreciation.line']
+        for asset in self:
+            dep_lines = dep_line_obj.search(
+                [('asset_id', '=', asset.id),
+                    ('move_id', '=', False),
+                    ('move_check', '=', True)])
+            amount = 0
+            for line in dep_lines:
+                amount += line.amount or 0.0
+            asset.value_residual = amount
+        return super(AccountAssetAsset, self)._amount_residual()
