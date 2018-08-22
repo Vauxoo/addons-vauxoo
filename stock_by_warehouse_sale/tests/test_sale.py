c05c21befa58e581eab58dd5182389db4df6483f
Hugo Adan [Vauxoo] <hugo@vauxoo.com>
2018-08-22 11:00:34 -0500

    [ADD] stock_by_warehouse_sale, stock_by_warehouse_purchase t27399 (#1291)

---
+++ stock_by_warehouse_sale/tests/test_sale.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from openerp.tests.common import TransactionCase
+
+
+class TestSaleOrder(TransactionCase):
+
+    # pylint: disable=method-required-super
+    def setUp(self):
+        pass
