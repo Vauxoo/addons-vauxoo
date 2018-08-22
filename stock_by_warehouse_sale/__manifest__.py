875d745ffe6c688701d39399ed9e2436111ed38c
Hugo Adan [Vauxoo] <hugo@vauxoo.com>
2018-08-22 11:00:34 -0500

    [ADD] stock_by_warehouse_sale, stock_by_warehouse_purchase t27399 (#1291)

---
+++ stock_by_warehouse_sale/__manifest__.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+{
+    "name": "Stock by Warehouse on Sales",
+    "version": "11.0.0.1.0",
+    "summary": """
+    Know the stock in all your warehouses with a simple click
+    from the sale order line form.
+    """,
+    "author": "Vauxoo",
+    "category": "stock",
+    "website": "http://www.vauxoo.com/",
+    "license": "LGPL-3",
+    "depends": [
+        "sale_stock",
+        "stock_by_warehouse",
+    ],
+    "demo": [],
+    "data": [
+        'views/sale_view.xml',
+    ],
+    "test": [],
+    "js": [],
+    "css": [],
+    'qweb': [],
+    "installable": True,
+    "auto_install": False,
+}
