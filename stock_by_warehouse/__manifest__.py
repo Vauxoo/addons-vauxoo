2dbe9db6ce2f160f0e8a4485b48baa38c402e8ee
Hugo Adan [Vauxoo] <hugo@vauxoo.com>
2018-08-22 11:00:34 -0500

    [ADD] stock_by_warehouse t27399 (#1291)

---
+++ stock_by_warehouse/__manifest__.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+{
+    "name": "Stock by Warehouse",
+    "version": "11.0.0.1.0",
+    "summary": """
+    Know the stock in all your warehouses with a simple click
+    from the product form.
+    """,
+    "author": "Vauxoo",
+    "category": "stock",
+    "website": "http://www.vauxoo.com/",
+    "license": "LGPL-3",
+    "depends": [
+        "stock",
+    ],
+    "demo": [],
+    "data": [
+        'views/product_view.xml',
+    ],
+    "test": [],
+    "js": [],
+    "css": [],
+    'qweb': [
+        "static/src/xml/template.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
