f6b214546c2e629af7391690f28b1d6585d31147
Julio [Vauxoo] <julio@vauxoo.com>
2018-09-18 12:24:44 -0500

    [IMP] default_warehouse_from_sales_team: adapt method in order to avoid (#1294)
    error by expected singleton

--- default_warehouse_from_sale_team/models/res_users.py
+++ default_warehouse_from_sale_team/models/res_users.py
@@ -18,10 +18,10 @@ class ResUsers(models.Model):
     def _check_section_id(self):
         """ Can only set the Default Sales team if the user is part o
         """
-        if (self.sale_team_id and
-           self.sale_team_id not in self.sale_team_ids):
+        for user in self.filtered(
+                lambda dat: dat.sale_team_id not in dat.sale_team_ids):
             raise ValidationError(_(
                 'You can not set %s sale team as default because the user'
                 ' do not belongs to the sale teams.\nPlease go to Sales >'
                 ' Settings > Users > Sales Teams if you will like to add this'
-                ' user to the sales team') % (self.sale_team_id.name))
+                ' user to the sales team') % (user.sale_team_id.name))
