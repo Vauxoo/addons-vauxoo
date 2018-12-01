ae48d76369b203460a6ff44be797636826a55943
Hbto [ Vauxoo ] <hbto@vauxoo.com>
2018-12-01 11:04:22 -0600

    [ADD] account_index_based_currency:  Hold the rates of the currency involved in an invoice at validation & agreement time. (#1316)

---
+++ account_index_based_currency/README.rst
@@ -0,0 +1,60 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+============================
+Account Index Based Currency
+============================
+
+This module adds Currency field in the company to be used to control the values
+in the invoice where new fields where added to computed invoice value based in 
+that control currency and have control on the currency at which the rate in the
+invoice was agreed.
+
+Installation
+============
+
+To install this module, you need to:
+- Only install this module
+
+
+Configuration
+=============
+
+To configure this module, you need to:
+- Set an Index Based Currency in the company
+- Set your Agreement Currency and rate in the invoice.
+
+
+Usage
+=====
+
+To use this module, you need to:
+
+
+Bug Tracker
+===========
+
+Bugs are tracked on `GitHub Issues <https://github.com/vauxoo/addons-vauxoo/issues>`_.
+In case of trouble, please check there if your issue has already been reported.
+If you spotted it first, help us smashing it by providing a detailed and welcomed feedback `here <https://github.com/vauxoo/addons-vauxoo/issues/new?body=module:%20payment_conekta%0Aversion:%209.0%0A%0A**Steps%20to%20reproduce**%0A-%20...%0A%0A**Current%20behavior**%0A%0A**Expected%20behavior**>`_.
+
+
+Credits
+=======
+
+This module was created by Vauxoo S.A. de C.V.
+
+Contributors
+------------
+* Humberto Arocha <hbto@vauxoo.com>
+
+
+Maintainer
+----------
+
+.. image:: http://www.vauxoo.com/logo.png
+   :alt: Vauxoo, S.A. de C.V.
+   :target: http://www.vauxoo.com
+
+This module is maintained by Vauxoo, S.A. de C.V.
---
+++ account_index_based_currency/models/account_invoice.py
@@ -0,0 +1,123 @@
+from odoo import fields, models, api, _
+from odoo.exceptions import UserError
+
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    @api.depends('date_invoice', 'index_based_currency_id',
+                 'company_currency_id', 'currency_id')
+    def _compute_currency_rate(self):
+        cur_obj = self.env['res.currency']
+        for inv in self:
+            inv.currency_rate = cur_obj._get_conversion_rate(
+                inv.currency_id, inv.index_based_currency_id, inv.company_id,
+                inv.date_invoice or fields.Date.today())
+            inv.company_currency_rate = cur_obj._get_conversion_rate(
+                inv.company_currency_id, inv.index_based_currency_id,
+                inv.company_id, inv.date_invoice or fields.Date.today())
+
+    @api.depends('amount_total_signed', 'agreement_currency_rate',
+                 'date_invoice')
+    def _compute_currency_amount(self):
+        for inv in self:
+            index_based_currency_amount = inv.amount_total_signed
+            if inv.index_based_currency_id != inv.currency_id:
+                index_based_currency_amount = inv.currency_id._convert(
+                    inv.amount_total_signed, inv.index_based_currency_id,
+                    inv.company_id, inv.date_invoice or fields.Date.today())
+            inv.index_based_currency_amount = index_based_currency_amount
+            inv.agreement_currency_amount = (
+                inv.index_based_currency_amount /
+                (inv.agreement_currency_rate or 1.0))
+
+    @api.onchange('agreement_currency_rate')
+    def onchange_agreement_currency_rate(self):
+        if self.agreement_currency_rate <= 0:
+            raise UserError(_('Invalid currency rate value'))
+
+    @api.onchange('agreement_currency_id')
+    def onchange_agreement_currency_id(self):
+        cur_obj = self.env['res.currency']
+        self.agreement_currency_rate = cur_obj._get_conversion_rate(
+            self.agreement_currency_id, self.index_based_currency_id,
+            self.company_id, self.date_invoice or fields.Date.today())
+        self.agreement_currency_amount = (
+            self.index_based_currency_amount * self.agreement_currency_rate)
+
+    @api.model
+    def default_get(self, default_fields):
+        res = super(AccountInvoice, self).default_get(default_fields)
+        if 'currency_id' not in res:
+            return res
+        currency_id = self.currency_id.browse(res.get('currency_id'))
+        company_id = self.company_id.browse(res.get('company_id'))
+        res['agreement_currency_id'] = currency_id.id
+        res['agreement_currency_rate'] = currency_id._get_conversion_rate(
+            currency_id, company_id.index_based_currency_id, company_id,
+            fields.Date.today())
+        return res
+
+    agreement_currency_id = fields.Many2one(
+        'res.currency',
+        copy=True,
+        help="Currency at which the agreement is to be/was settled",
+        readonly=True, states={'draft': [('readonly', False)]},
+    )
+    index_based_currency_id = fields.Many2one(
+        'res.currency',
+        related='company_id.index_based_currency_id',
+        help="Currency used for reporting purposes",
+        copy=False)
+    index_based_currency_amount = fields.Monetary(
+        currency_field='index_based_currency_id',
+        store=True, compute='_compute_currency_amount',
+        copy=False,
+        help="Total amount in the currency of the company, negative for "
+        "credit notes.")
+    agreement_currency_amount = fields.Monetary(
+        currency_field='agreement_currency_id',
+        store=True, compute='_compute_currency_amount',
+        copy=False,
+        help="Total amount in the currency of the company, negative for "
+        "credit notes.")
+    currency_rate = fields.Float(
+        help="Document currency rate at date of document creation or "
+        "document approval against Index Based Currency",
+        store=True, compute='_compute_currency_rate',
+        copy=False, digits=(12, 6))
+    company_currency_rate = fields.Float(
+        help="Company currency rate at date of document creation or "
+        "document approval against Index Based Currency",
+        store=True, compute='_compute_currency_rate',
+        copy=False, digits=(12, 6))
+    agreement_currency_rate = fields.Float(
+        help="Currency rate this Document was agreed",
+        readonly=True, states={'draft': [('readonly', False)]},
+        copy=True, digits=(12, 6))
+
+    @api.multi
+    def finalize_invoice_move_lines(self, move_lines):
+        res = super(AccountInvoice, self).finalize_invoice_move_lines(
+            move_lines)
+        for line in res:
+            line[2]['currency_rate'] = self.currency_rate
+            line[2]['agreement_currency_rate'] = self.agreement_currency_rate
+        return res
+
+
+class AccountMoveLine(models.Model):
+    _inherit = 'account.move.line'
+
+    currency_rate = fields.Float(
+        help="Document currency rate at date of document creation or "
+        "document approval against Index Based Currency",
+        copy=False, digits=(12, 6))
+    agreement_currency_rate = fields.Float(
+        help="Currency rate this Document was agreed",
+        copy=True, digits=(12, 6))
+    index_based_currency_id = fields.Many2one(
+        'res.currency',
+        related='company_id.index_based_currency_id',
+        help="Currency used por reporting purposes",
+        store=True)
