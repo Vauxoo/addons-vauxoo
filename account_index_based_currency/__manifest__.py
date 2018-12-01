829dffad7fa217822cd741a123b4021288a95bd8
Hbto [ Vauxoo ] <hbto@vauxoo.com>
2018-12-01 11:04:22 -0600

    [ADD] account_index_based_currency:  Hold the rates of the currency involved in an invoice at validation & agreement time. (#1316)

---
+++ account_index_based_currency/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2018 Vauxoo, S.A. de C.V.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    'author': 'Vauxoo',
+    'category': 'Accounting & Finance',
+    'demo_xml': [],
+    'depends': ['account'],
+    'installable': True,
+    'name': 'Account Index Based Currency',
+    'test': [],
+    'data': [
+        'views/invoice_view.xml',
+        'views/res_company_view.xml',
+    ],
+    'version': '12.0.1.0.0',
+    'website': 'http://www.vauxoo.com',
+    'license': 'AGPL-3'
+}
