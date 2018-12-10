1bc35c1d7c00d6282b7927b901f4fd68a07e8c1f
keylor2906 <josemanuel@vauxoo.com>
2018-12-10 12:36:42 -0600

    [REF] stock_cost_segmentation: Maintain history of original move on the internal moves between warehouse

--- stock_cost_segmentation/models/account_invoice.py
+++ stock_cost_segmentation/models/account_invoice.py
@@ -8,23 +8,16 @@ class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
     @api.multi
-    def generate_customs_information(self):
+    def _generate_customs_information(self):
         """Search custom information in move type out"""
-        if self.env.context('origin_move'):
-            return super(AccountInvoice, self).generate_customs_information()
+        if self.env.context.get('origin_move'):
+            return super(AccountInvoice, self)._generate_customs_information()
         landed_obj = self.env['stock.landed.cost']
-        for line in self.filtered(
-                lambda r:
-                r.l10n_mx_edi_get_pac_version() == '3.3').mapped(
-                    'invoice_line_ids').filtered('sale_line_ids'):
+        for line in self.mapped('invoice_line_ids').filtered('sale_line_ids'):
             moves = line.mapped('sale_line_ids.move_ids').filtered(
                 lambda r: r.state == 'done' and not r.scrapped)
-            if not moves:
+            landed = moves._get_landed_information()
+            if not moves or not landed:
                 continue
-            landed = landed_obj.search(
-                [('picking_ids', 'in',
-                  moves.mapped(
-                      'move_orig_logistic_ids.move_id.picking_id.id')),
-                    ('l10n_mx_edi_customs_number', '!=', False)])
             line.l10n_mx_edi_customs_number = ','.join(
                 list(set(landed.mapped('l10n_mx_edi_customs_number'))))
