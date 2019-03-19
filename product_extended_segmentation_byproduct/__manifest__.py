b363a270c27fae5be2673af4ea5772bc6c4d6f07
José Manuel Robles <josemanuel@vauxoo.com>
2019-03-19 10:13:03 -0600

    [ADD] product_extended_segmentation_byproduct: Module that extends from product_extended_segmentation adding option to calculate the cost considering the sub products of the BoM. (#1341)

---
+++ product_extended_segmentation_byproduct/__manifest__.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "Product Extension to track Segmentation Cost with Sub products",
+    'summary': '''
+    ''',
+    'version': '11.0.1.0.0',
+    'author': 'Vauxoo',
+    'category': 'Accounting',
+    'license': 'OEEL-1',
+    'depends': [
+        'product_extended_segmentation',
+        'mrp_byproduct',
+    ],
+    'data': [
+    ],
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'installable': True,
+}
