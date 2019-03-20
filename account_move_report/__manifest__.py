a4d3e39f8fd836f60d42432d63bf315b800f1924
Carmen Miranda <carmen@vauxoo.com>
2019-03-19 18:52:08 -0600

    [MIG] account_move_report: from v11 to v12

--- account_move_report/__manifest__.py
+++ account_move_report/__manifest__.py
@@ -1,9 +1,8 @@
-# coding: utf-8
-# Copyright 2016 Vauxoo (https://www.vauxoo.com) <info@vauxoo.com>
+# Copyright 2019 Vauxoo (https://www.vauxoo.com) <info@vauxoo.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Journal Entries Report",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "Vauxoo",
     "category": "Accouting",
     "website": "http://www.vauxoo.com/",
@@ -16,10 +15,6 @@
         "views/account_reports.xml",
         "views/account_move_report.xml"
     ],
-    "test": [],
-    "js": [],
-    "css": [],
-    "qweb": [],
     "installable": True,
     "auto_install": False,
 }
