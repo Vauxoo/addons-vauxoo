09feef51a5d53d9453af927a372ccfb9957fbbab
Moises Lopez - https://www.vauxoo.com/ <moylop260@vauxoo.com>
2019-08-02 17:13:22 -0500

    [FIX] stock_by_warehouse_sale: Using directly warehouse from order_id
    In order to get the real value, since that this field is not set when onchange method is used

--- stock_by_warehouse_sale/models/sale.py
+++ stock_by_warehouse_sale/models/sale.py
@@ -25,5 +25,6 @@ class SaleOrderLine(models.Model):
         if not self.warehouses_stock_recompute:
             self.warehouses_stock_recompute = True
             return
+        self.warehouse_id = self.order_id.warehouse_id
         self._compute_get_warehouses_stock()
         self.warehouses_stock_recompute = True
