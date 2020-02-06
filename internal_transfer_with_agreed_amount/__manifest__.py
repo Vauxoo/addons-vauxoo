3ab2d23c5c120f61a61aff64c43905bca9b1c722
Edilianny Sanchez - https://www.vauxoo.com/ <esanchez@vauxoo.com>
2020-02-06 12:51:49 -0600

    [ADD] internal_transfer_with_agreed_amount: New module (#1393)
    - When foreign currencies are bought, in some cases an amount is agreed
    for the transaction and it is not exactly that resulting from the
    official exchange rate.
    
    - When payments are made or received in foreign currency and the customer
    or payment service uses its own exchange rate.
    
    For these cases this module allows an internal transfer between
    different currencies, setting an agreed amount.
    
    This agreed amount will be used to set the amount currency in the
    journal entries.
    
    A new module is created to avoid duplication of code in different
    clients that require this process.

---
+++ internal_transfer_with_agreed_amount/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2020 Vauxoo
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Internal transfers with an agreed amount',
+    'version': '12.0.1.0.0',
+    'author': 'Vauxoo',
+    'website': 'http://www.vauxoo.com/',
+    'license': 'LGPL-3',
+    'category': 'account',
+    'depends': [
+        'account_accountant',
+        'l10n_mx_edi',
+    ],
+    'data': [
+        # Security
+        'security/security.xml',
+        # Wizards
+        'wizards/internal_transfer_multicurrency_view.xml',
+        # Views
+        'views/account_payment.xml',
+    ],
+    "installable": True,
+}
