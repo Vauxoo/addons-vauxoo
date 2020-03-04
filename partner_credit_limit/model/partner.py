93869eb8a5279c06a0b481959eed95d59a9f82d3
Yennifer Santiago <yennifer@vauxoo.com>
2020-03-04 02:22:33 +0000

    [REF] partner_credit_limit: Migrate module partner_credit_limit to saas-14

--- partner_credit_limit/model/partner.py
+++ partner_credit_limit/model/partner.py
@@ -8,9 +8,10 @@ from odoo import models, fields, api
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
+    credit_limit = fields.Float(company_dependent=True)
     grace_payment_days = fields.Float(
         'Days grace payment',
-        help='Days grace payment')
+        help='Days grace payment', company_dependent=True)
 
     credit_overloaded = fields.Boolean(
         compute='_get_credit_overloaded',
@@ -26,53 +27,57 @@ class ResPartner(models.Model):
 
     @api.multi
     def _get_credit_overloaded(self):
+        company = self.env.user.company_id
         for partner in self:
-            context = self.env.context or {}
-            currency_obj = self.env['res.currency']
-            res_company = self.env['res.company']
-            imd_obj = self.env['ir.model.data']
-            company_id = imd_obj.get_object_reference(
-                'base', 'main_company')[1]
-            company = res_company.browse(company_id)
-            new_amount = context.get('new_amount', 0.0)
-            new_currency = context.get('new_currency', False)
-            if new_currency:
-                from_currency = currency_obj.browse(new_currency)
-            else:
-                from_currency = company.currency_id
-            new_amount_currency = from_currency.compute(
-                new_amount, company.currency_id)
+            new_amount = self.env.context.get('new_amount', 0.0)
+            new_currency = self.env.context.get('new_currency', False)
+            new_amount_currency = new_amount
+            if new_currency and company.currency_id != new_currency:
+                new_amount_currency = new_currency.compute(
+                    new_amount, company.currency_id)
 
             new_credit = partner.credit + new_amount_currency
             partner.credit_overloaded = new_credit > partner.credit_limit
 
+    @api.model
+    def movelines_domain(self, partner):
+        """Return the domain for search the
+        account.move.line for the user's company."""
+        domain = [('partner_id', '=', partner.id),
+                  ('company_id', '=', self.env.user.company_id.id),
+                  ('account_id.internal_type', '=', 'receivable'),
+                  ('move_id.state', '!=', 'draft'),
+                  ('reconciled', '=', False)]
+        return domain
+
+    @api.model
+    def debit_credit_maturity(self, movelines):
+        debit_maturity, credit_maturity = 0.0, 0.0
+        for line in movelines:
+            limit_day = line.date_maturity
+            if line.partner_id.grace_payment_days:
+                maturity = fields.Datetime.from_string(
+                    line.date_maturity)
+                grace_payment_days = timedelta(
+                    days=line.partner_id.grace_payment_days)
+                limit_day = maturity + grace_payment_days
+                limit_day = limit_day.strftime("%Y-%m-%d")
+
+            if limit_day <= fields.Date.today():
+                # credit and debit maturity sums all aml
+                # with late payments
+                debit_maturity += line.debit
+            credit_maturity += line.credit
+        return debit_maturity, credit_maturity
+
     @api.multi
     def _get_overdue_credit(self):
+        moveline_obj = self.env['account.move.line']
         for partner in self:
-            moveline_obj = self.env['account.move.line']
-            movelines = moveline_obj.search(
-                [('partner_id', '=', partner.id),
-                 ('account_id.internal_type', '=', 'receivable'),
-                 ('move_id.state', '!=', 'draft'),
-                 ('reconciled', '=', False)])
-            # credit = 0.0
-            debit_maturity, credit_maturity = 0.0, 0.0
-            for line in movelines:
-                limit_day = line.date_maturity
-                if line.partner_id.grace_payment_days:
-                    maturity = fields.Datetime.from_string(
-                        line.date_maturity)
-                    grace_payment_days = timedelta(
-                        days=line.partner_id.grace_payment_days)
-                    limit_day = maturity + grace_payment_days
-                    limit_day = limit_day.strftime("%Y-%m-%d")
-
-                if limit_day <= fields.Date.today():
-                    # credit and debit maturity sums all aml
-                    # with late payments
-                    debit_maturity += line.debit
-                credit_maturity += line.credit
-                # credit += line.credit
+            domain = self.movelines_domain(partner)
+            movelines = moveline_obj.search(domain)
+            debit_maturity, credit_maturity = self.debit_credit_maturity(
+                movelines)
             balance_maturity = debit_maturity - credit_maturity
             partner.overdue_credit = balance_maturity > 0.0
 
--- partner_credit_limit/tests/test_sale_credit_limit.py
+++ partner_credit_limit/tests/test_sale_credit_limit.py
@@ -18,12 +18,15 @@ class TestSalesCreditLimits(common.TestCommon):
         self.sale_order_line = self.env['sale.order.line']
         self.payment_term_credit = self.env.ref(
             'account.account_payment_term_advance')
+        self.pricelist = self.env.ref('product.list0')
 
         self.partner_china = self.env.ref("base.res_partner_3")
         self.journal_id = self.env.ref("account.bank_journal")
         self.account_id = self.env.ref("account.a_recv")
         self.account_sale_id = self.env.ref("account.a_sale")
         self.product_id = self.env.ref("product.product_product_6")
+        self.mxn_currency = self.env.ref('base.MXN')
+        self.usd_currency = self.env.ref('base.USD')
 
     def test_credit_limit_overloaded(self):
         """This test validate the partner has credit overloaded
@@ -106,5 +109,38 @@ class TestSalesCreditLimits(common.TestCommon):
         # should not confirm sale order should fail,
         # couse there are late payments
         # since the invoice 1 was validate with curent day minus 2 days
+        with self.assertRaises(exceptions.Warning):
+            sale_id.action_confirm()
+
+    def test_credit_limit_overloaded_with_diferent_currency(self):
+        """This test validate when sale exceeds the credit limit because it is
+        in a different currency of sale company currency, therefore, conversion
+        must be done
+        """
+        # Set credit limit in 600
+        self.partner_china.credit_limit = 600.00
+
+        # Sale order with amount total of 600.00, the same of partner credit
+        # limit, but when conversion is done, this amount must exceed partner
+        # credit limit
+        self.pricelist.currency_id = self.usd_currency.id
+        sale_id = self.sale_order.create(
+            {'partner_id': self.partner_china.id,
+                'pricelist_id': self.pricelist.id,
+                'payment_term_id': self.payment_term_credit.id})
+        self.sale_order_line.create(
+            {'product_id': self.product_id.id,
+                'product_uom_qty': 1,
+                'price_unit': 600,
+                'order_id': sale_id.id,
+                'name': 'product that cost 100', })
+
+        sale_id.company_id.write({'currency_id': self.mxn_currency.id})
+        self.mxn_currency.rate_ids.write({'rate': 18.0})
+        # Sale currency must be USD and company currency must be MXN
+        self.assertNotEqual(sale_id.currency_id,
+                            sale_id.company_id.currency_id)
+        # Should not confirm sale order
+        # Credit limit exceded when conversion is done from USD to MXN
         with self.assertRaises(exceptions.Warning):
             sale_id.action_confirm()
