a2a509425e911aaba1823f583f8d2b1e96ac57da
Edilianny Sánchez <esanchez@vauxoo.com>
2020-04-01 01:59:36 +0000

    [FIX] partner_credit_limit: fix error `OverflowError: date value out of range`
    Since the grace_payment_days field is a float, in some cases the user
    can set really high values (> 999999) which causes the traceback
    `OverflowError: date value out of range`.
    
    Since that field is used for date operations in the
    `debit_credit_maturity` method of the` res.partner` model.

--- partner_credit_limit/__manifest__.py
+++ partner_credit_limit/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Partner Credit Limit",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.0.1",
     "author": "Vauxoo",
     "category": "",
     "website": "http://www.vauxoo.com/",
