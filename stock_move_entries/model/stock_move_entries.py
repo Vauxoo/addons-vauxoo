8504374a6a010c132be1ae2aaabd4bae0d9d7706
Luis Escobar <lescobar@vauxoo.com>
2020-04-24 14:19:55 +0000

    [MIG] stock_move_sntries: Migrated to v12.0

---
+++ stock_move_entries/model/stock_move_entries.py
@@ -0,0 +1,51 @@
+# coding: utf-8
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#
+#    Copyright (c) 2013 Vauxoo - http://www.vauxoo.com
+#    All Rights Reserved.
+#    info@vauxoo.com
+############################################################################
+#    Coded by: julio (julio@vauxoo.com)
+#              Luis Ernesto García Medina (ernesto_gm@vauxoo.com)
+############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from odoo import fields, models
+
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    sm_id = fields.Many2one('stock.move', 'Stock move', index=True)
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    aml_ids = fields.One2many(
+            'account.move.line', 'sm_id', 'Account move Lines',
+            domain=[('account_id.reconcile', '=', True)])
+    aml_all_ids = fields.One2many(
+            'account.move.line', 'sm_id', 'All Account move Lines')
+
+    def _prepare_account_move_line(self, qty, cost,
+                                   credit_account_id, debit_account_id):
+        res = super(StockMove, self)._prepare_account_move_line(
+            qty, cost, credit_account_id, debit_account_id)
+        for line in res:
+            line[2]['sm_id'] = self.id
+        return res
