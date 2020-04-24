f7f86a46e6351a3a79fc44a8f55890c3923dc95e
Luis Escobar <lescobar@vauxoo.com>
2020-04-24 22:55:13 +0000

    [MIG] account_asset_date: Migrated to v12.0

---
+++ account_asset_date/__manifest__.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+{
+    "name": "Account Asset Date",
+    "version": "12.0.1.0.0",
+    "author": "Vauxoo",
+    "category": "account asset",
+    "website": "http://www.vauxoo.com/",
+    "license": "AGPL-3",
+    "depends": [
+        "account_asset"
+    ],
+    "demo": [],
+    "data": [
+        "views/account_asset_view.xml"
+    ],
+    "test": [],
+    "js": [],
+    "css": [],
+    "qweb": [],
+    "installable": True,
+    "auto_install": False,
+}
