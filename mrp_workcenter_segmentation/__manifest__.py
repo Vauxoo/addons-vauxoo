db5da76ebf5738e780d4485248e1f88ce88228ee
Luis Escobar <lescobar@vauxoo.com>
2020-04-26 18:56:49 +0000

    [MIG] mrp_workcenter_segmentation: Migrated to v12.0

---
+++ mrp_workcenter_segmentation/__manifest__.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'MRP Cost Segmentation',
+    'summary': '''
+    ''',
+    'version': '12.0.1.0.0',
+    'author': 'Vauxoo',
+    'category': 'Accounting',
+    'license': 'OEEL-1',
+    'depends': [
+        'stock_landed_segmentation',
+        'mrp_workcenter_account_move',
+        #####
+    ],
+    'data': [
+        'views/mrp_view.xml',
+    ],
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'installable': True,
+}
