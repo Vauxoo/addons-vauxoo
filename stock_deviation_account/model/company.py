ecf6222f00dc01f143fe093bbccd259946ef7ecd
Luis Escobar <lescobar@vauxoo.com>
2020-04-26 20:02:39 +0000

    [MIG] stock_deviation_account: Migrated to v12.0

---
+++ stock_deviation_account/model/company.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+from odoo import models, fields
+
+
+class ResCompany(models.Model):
+    _inherit = 'res.company'
+    gain_inventory_deviation_account_id = fields.Many2one(
+        'account.account',
+        string='Gain Inventory Deviation Account',
+    )
+    loss_inventory_deviation_account_id = fields.Many2one(
+        'account.account',
+        string='Loss Inventory Deviation Account',
+    )
