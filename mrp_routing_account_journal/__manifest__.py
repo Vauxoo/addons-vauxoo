cc8a4c7c379974685ebced8ed09327f1d83b8876
Luis Escobar <lescobar@vauxoo.com>
2020-04-30 19:17:21 +0000

    [MIG] mrp_routing_account_journal: Migration to 12.0

--- mrp_routing_account_journal/__manifest__.py
+++ mrp_routing_account_journal/__manifest__.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 {
     "name": "MRP Routing Account Journal",
-    "version": "11.0.0.0.0",
+    "version": "12.0.1.0.0",
     "author": "Vauxoo",
     "category": "Tools",
     "website": "http://www.vauxoo.com/",
