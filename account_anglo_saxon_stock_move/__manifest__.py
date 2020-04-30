5b17bf951f8c110e26ebc04472bfadf99650430f
Luis Escobar <lescobar@vauxoo.com>
2020-04-30 20:48:57 +0000

    [MIG] account_anglo_saxon_stock_move: Migration to 12.0

--- account_anglo_saxon_stock_move/__manifest__.py
+++ account_anglo_saxon_stock_move/__manifest__.py
@@ -23,7 +23,7 @@
 ###############################################################################
 {
     "name": "Account Anglo-Saxon Stock Move",
-    "version": "11.0.0.1.0",
+    "version": "12.0.1.0.0",
     "author": "Vauxoo",
     "category": "",
     "website": "http://www.vauxoo.com/",
