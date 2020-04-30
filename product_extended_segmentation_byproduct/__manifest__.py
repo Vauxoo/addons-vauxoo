273f2fed3f625e65e0b786432d5f5ca9b9811a70
Luis Escobar <lescobar@vauxoo.com>
2020-04-30 19:32:51 +0000

    [MIG] product_extended_segmentation_byproduct: Migration to 12.0

--- product_extended_segmentation_byproduct/__manifest__.py
+++ product_extended_segmentation_byproduct/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Product Extension to track Segmentation Cost with Sub products",
     'summary': '''
     ''',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'author': 'Vauxoo',
     'category': 'Accounting',
     'license': 'OEEL-1',
