994a3fd3d24f9c62e0435cb02c9d29246ae18c51
Luis González <lgonzalez@vauxoo.com>
2020-06-16 03:38:05 +0000

    [FIX] stock_cost_segmentation: Lint & flake8 issues

--- stock_cost_segmentation/models/account_invoice.py
+++ stock_cost_segmentation/models/account_invoice.py
@@ -1,6 +1,3 @@
-# coding: utf-8
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
 from odoo import api, models
 
 
@@ -12,7 +9,6 @@ class AccountInvoice(models.Model):
         """Search custom information in move type out"""
         if self.env.context.get('origin_move'):
             return super(AccountInvoice, self)._generate_customs_information()
-        landed_obj = self.env['stock.landed.cost']
         for line in self.mapped('invoice_line_ids').filtered('sale_line_ids'):
             moves = line.mapped('sale_line_ids.move_ids').filtered(
                 lambda r: r.state == 'done' and not r.scrapped)
