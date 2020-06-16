19d681dfd489edede82dde6b3aff0a377007ed48
Luis González <lgonzalez@vauxoo.com>
2020-06-16 03:38:05 +0000

    [FIX] purchase_third_validation: Lint & flake8 issues

--- purchase_third_validation/models/res_config_settings.py
+++ purchase_third_validation/models/res_config_settings.py
@@ -8,10 +8,12 @@ from odoo.exceptions import Warning as WarningUser
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    po_order_approval = fields.Boolean("Order Approval",
+    po_order_approval = fields.Boolean(
+        "Order Approval",
         default=lambda self: self.env.user.company_id.po_double_validation in
         ['two_step', 'three_step'])
-    po_third_order_approval = fields.Boolean("Third Order Approval",
+    po_third_order_approval = fields.Boolean(
+        "Third Order Approval",
         default=lambda self: self.env.user.company_id.po_double_validation
         == 'three_step')
     po_third_validation_amount = fields.Monetary(
@@ -19,10 +21,11 @@ class ResConfigSettings(models.TransientModel):
         string="Third Validation Minimum Amount", currency_field='company_currency_id', readonly=False)
 
     def set_values(self):
-        super(ResConfigSettings, self).set_values()
+        res = super(ResConfigSettings, self).set_values()
         self.po_double_validation = 'three_step' if self.\
             po_third_order_approval else 'two_step' if self.\
             po_order_approval else 'one_step'
+        return res
 
     @api.multi
     @api.constrains(
