75655cd3eee6746c42f6f67705e1ca23382d13d5
Luis González <lgonzalez@vauxoo.com>
2020-06-16 05:29:53 +0000

    [FIX] account_asset_move_check: Lint & flake8 issues

--- account_asset_move_check/wizard/wizard_asset_depreciation.py
+++ account_asset_move_check/wizard/wizard_asset_depreciation.py
@@ -1,29 +1,3 @@
-# coding: utf-8
-###########################################################################
-#    Module Writen to OpenERP, Open Source Management Solution
-# Ch
-#    Copyright (c) 2015 Vauxoo - http://www.vauxoo.com/
-#    All Rights Reserved.
-#    info Vauxoo (info@vauxoo.com)
-############################################################################
-#    Coded by: Luis Torres (luis_t@vauxoo.com)
-############################################################################
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
 from odoo import fields, models
 
 
@@ -38,10 +12,10 @@ class WizardAssetDepreciation(models.TransientModel):
         help='Select date stop to depreciation lines that '
         'will write that the lines are historical', required=True)
 
-    def write_historical_true(self, asset):
+    def write_historical_true(self):
         date_start = self.date_start
         date_stop = self.date_stop
-        for asset in asset['active_ids']:
+        for asset in self.env.context['active_ids']:
             asset_lines = self.env['account.asset.depreciation.line'].search([
                 ('asset_id', '=', asset),
                 ('depreciation_date', '>=', date_start),
