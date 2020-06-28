377c53bd41360ec2e8758f8bbbd8d66761cfd98b
Luis González <lgonzalez@vauxoo.com>
2020-06-28 21:06:20 +0000

    [FIX] account_anglo_saxon_stock_move_sale: Lint & flake8 issues

--- account_anglo_saxon_stock_move_sale/model/sale.py
+++ account_anglo_saxon_stock_move_sale/model/sale.py
@@ -1,32 +1,36 @@
-# coding: utf-8
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C)
-#    2004-2010 Tiny SPRL (<http://tiny.be>).
-#    2009-2010 Veritos (http://veritos.nl).
-#    All Rights Reserved
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
 from odoo import fields, models, api
 
 
 class SaleOrder(models.Model):
 
     _inherit = "sale.order"
+
+    unreconciled_lines = fields.Integer(
+        compute='_compute_unreconciled_lines',
+        search='_search_unreconciled_lines',
+        help="Indicates how many unreconciled lines are still standing for this sale order")
+    to_be_reconciled = fields.Integer(
+        compute='_compute_to_be_reconciled',
+        search='_search_to_be_reconciled',
+        help="Indicates how many possible reconciliations are pending for this sale order")
+    aml_ids = fields.One2many(
+        'account.move.line', 'sale_id', 'Account Move Lines',
+        help='Journal Entry Lines related to this Sale Order')
+
+    @api.model
+    def cron_sale_accrual_reconciliation(self, do_commit=False):
+        self.env['account.invoice'].cron_accrual_reconciliation(
+            'sale_id', do_commit=do_commit)
+
+    @api.multi
+    def reconcile_stock_accrual(self):
+        self.env['account.invoice'].reconcile_stock_accrual(
+            self._ids, 'sale_id')
+
+    @api.multi
+    def view_accrual(self):
+        return self.env['account.invoice'].view_accrual(
+            self._ids, 'sale.order')
 
     # /!\ NOTE: Following two methods can be drawn from a TransientModel
     # instead of account.invoice
@@ -53,30 +57,3 @@ class SaleOrder(models.Model):
     def _search_unreconciled_lines(self, operator, value):
         return self._compute_search(
             'unreconciled_lines', 'query2', operator, value)
-
-    unreconciled_lines = fields.Integer(
-        compute='_compute_unreconciled_lines',
-        search='_search_unreconciled_lines',
-        help="Indicates how many unreconciled lines are still standing")
-    to_be_reconciled = fields.Integer(
-        compute='_compute_to_be_reconciled',
-        search='_search_to_be_reconciled',
-        help="Indicates how many possible reconciliations are pending")
-    aml_ids = fields.One2many(
-        'account.move.line', 'sale_id', 'Account Move Lines',
-        help='Journal Entry Lines related to this Sale Order')
-
-    @api.model
-    def cron_sale_accrual_reconciliation(self, do_commit=False):
-        self.env['account.invoice'].cron_accrual_reconciliation(
-            'sale_id', do_commit=do_commit)
-
-    @api.multi
-    def reconcile_stock_accrual(self):
-        self.env['account.invoice'].reconcile_stock_accrual(
-            self._ids, 'sale_id')
-
-    @api.multi
-    def view_accrual(self):
-        return self.env['account.invoice'].view_accrual(
-            self._ids, 'sale.order')
