5376f48ead8379829efcdad73c6c93de3bb0fb86
CarmenMiranda <carmen@vauxoo.com>
2020-07-15 19:11:11 +0000

    [I18N] default_warehouse_from_sale_team: Update translations terms
    By merging the two Spanish language translation files we are
    solving the issue of duplicated terms on the translations of this
    module.
    Also changing some of the messages and translations of this module.
    And increasing the module version for this major change.

--- default_warehouse_from_sale_team/models/sales_team.py
+++ default_warehouse_from_sale_team/models/sales_team.py
@@ -13,11 +13,11 @@ class InheritedCrmSaseSection(models.Model):
                                         'the related users to the sales team.')
     journal_team_ids = fields.One2many(
         'account.journal', 'section_id', string="Journal's sales teams",
-        help="Choose the Journals that user with this sale team can see")
+        help="Specify what journals a member of this sales team can see")
     journal_stock_id = fields.Many2one(
         'account.journal', 'Journal stock valuation',
-        help='It indicates journal to be used when move line is created with'
-        'the warehouse of this sale team')
+        help='It indicates the journal to be used when a move line is created with '
+        'the warehouse of this sales team')
 
 
 class WarehouseDefault(models.Model):
