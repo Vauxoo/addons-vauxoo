f8be83325e99c8d38d3a519eae3877e84ca7f5e2
Luis González <lgonzalez@vauxoo.com>
2020-08-03 04:31:15 +0000

    [FIX] product_pricelist_report_qweb: Post-migration fixes
    The following is performed:
    - Removed headers from Python files
    - Directories are now in plural (reports, wizards)
    - Bumped to correct version (11.0.1.0.0)
    - Migrated and updated translations
    - Deleted tests, as they have no effect anymore
    - And other small changes

--- product_pricelist_report_qweb/README.rst
+++ product_pricelist_report_qweb/README.rst
@@ -1,6 +1,6 @@
 Product Pricelist Report QWeb
 =============================
 
-This module inherit the original xml_id report on product
+This module inherits the original xml_id report on product
 `action_report_pricelist` and overrides it with a qweb report plus three
 fields.
--- product_pricelist_report_qweb/__manifest__.py
+++ product_pricelist_report_qweb/__manifest__.py
@@ -1,43 +1,21 @@
-# coding: utf-8
-###########################################################################
-#    Module Writen to OpenERP, Open Source Management Solution
-#    Copyright (C) Vauxoo (<http://vauxoo.com>).
-#    All Rights Reserved
-# #############Credits#########################################################
-#    Coded by: Humberto Arocha <hbto@vauxoo.com>
-###############################################################################
-#    This program is free software: you can redistribute it and/or modify it
-#    under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or (at your
-#    option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-###############################################################################
 {
     "name": "Product Pricelist Report QWeb",
-    "version": "11.0.0.1.0",
+    "version": "11.0.1.0.0",
     "author": "Vauxoo",
-    "category": "Accouting",
+    "category": "Accounting",
     "website": "http://www.vauxoo.com/",
     "license": "AGPL-3",
     "depends": [
         "product",
     ],
-    "demo": [],
     "data": [
-        "wizard/product_pricelist_views.xml",
-        "report/product_pricelist_templates.xml",
+        "wizards/product_pricelist_views.xml",
+        "reports/product_pricelist_templates.xml",
     ],
-    "test": [],
-    "js": [],
-    "css": [],
-    "qweb": [],
+    "demo": [
+    ],
+    "qweb": [
+    ],
     "installable": True,
     "auto_install": False,
 }
