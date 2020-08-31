6868bc035269de372ceddf19150077df54104a24
Nhomar Hernández <nhomar@vauxoo.com>
2020-08-30 20:15:23 -0500

    [FIX] account_multicompany_code: Categorizing

--- account_multicompany_code/__manifest__.py
+++ account_multicompany_code/__manifest__.py
@@ -5,8 +5,8 @@
     "name": "Account Multicompany Code",
     "version": "11.0.0.1.0",
     "author": "Vauxoo",
-    "category": "",
-    "website": "http://www.vauxoo.com/",
+    "category": "Accounting/Accounting",
+    "website": "https://www.vauxoo.com/",
     "license": "LGPL-3",
     "depends": [
         "account",
