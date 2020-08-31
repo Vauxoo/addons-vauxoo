55390dbdf112c402fa038ef4f447bd58eda6e851
Nhomar Hernández <nhomar@vauxoo.com>
2020-08-30 22:33:33 -0500

    [REF] account_move_report: Refactor PDF

--- account_move_report/__manifest__.py
+++ account_move_report/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Journal Entries Report",
     "version": "12.0.1.0.0",
     "author": "Vauxoo",
-    "category": "Accouting",
+    "category": "Accounting/Accounting",
     "website": "http://www.vauxoo.com/",
     "license": "LGPL-3",
     "depends": [
