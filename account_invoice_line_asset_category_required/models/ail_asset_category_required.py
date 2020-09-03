6734e75e6d9a4908e9f89f8783c128800d2221c7
Luis González <lgonzalez@vauxoo.com>
2020-09-03 18:06:20 +0000

    [FIX] account_invoice_line_asset_category_required: Remove unnecessary headers
    This removes:
    - Hencoding headers, fixing lint `utf8-coding-comment`
    - Headers like `# Part of Odoo`
    - And other ones

--- account_invoice_line_asset_category_required/models/ail_asset_category_required.py
+++ account_invoice_line_asset_category_required/models/ail_asset_category_required.py
@@ -1,14 +1,3 @@
-# coding: utf-8
-############################################################################
-#    Module Writen For Odoo, Open Source Management Solution
-#
-#    Copyright (c) 2015 Vauxoo - http://www.vauxoo.com
-#    All Rights Reserved.
-#    info Vauxoo (info@vauxoo.com)
-#    coded by: jorge_nr@vauxoo.com
-#    planned by: Humberto Arocha <hbto@vauxoo.com>
-############################################################################
-
 from odoo import models, api, _, fields
 from odoo.exceptions import ValidationError
 
