40d04015ee2d44785fdc371bb668c3e8387a93da
Luis González <lgonzalez@vauxoo.com>
2020-09-03 18:06:21 +0000

    [FIX] partner_credit_limit: Remove unnecessary headers
    This removes:
    - Hencoding headers, fixing lint `utf8-coding-comment`
    - Headers like `# Part of Odoo`
    - And other ones

--- partner_credit_limit/hooks.py
+++ partner_credit_limit/hooks.py
@@ -1,6 +1,3 @@
-# Copyright 2020 Vauxoo (https://www.vauxoo.com) <info@vauxoo.com>
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-
 from odoo import api, SUPERUSER_ID
 from odoo.tools import column_exists
 
