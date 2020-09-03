10373365fd083467f771267d67085548e26429c1
Luis González <lgonzalez@vauxoo.com>
2020-09-03 18:06:20 +0000

    [FIX] account_analytic_policy: Remove unnecessary headers
    This removes:
    - Hencoding headers, fixing lint `utf8-coding-comment`
    - Headers like `# Part of Odoo`
    - And other ones

--- account_analytic_policy/__manifest__.py
+++ account_analytic_policy/__manifest__.py
@@ -1,12 +1,3 @@
-# coding: utf-8
-############################################################################
-#    Module Writen For Odoo, Open Source Management Solution
-#
-#    Copyright (c) 2010 Vauxoo - http://www.vauxoo.com
-#    All Rights Reserved.
-#    info Vauxoo (info@vauxoo.com)
-#    coded by: nhomar <nhomar@vauxoo.com>
-############################################################################
 {
     "name": "Account Analytic Policy",
     "version": "12.0.1.0.0",
--- account_analytic_policy/model/account_move_line.py
+++ account_analytic_policy/model/account_move_line.py
@@ -1,13 +1,3 @@
-# coding: utf-8
-############################################################################
-#    Module Writen For Odoo, Open Source Management Solution
-#
-#    Copyright (c) 2010 Vauxoo - http://www.vauxoo.com
-#    All Rights Reserved.
-#    info Vauxoo (info@vauxoo.com)
-#    coded by: nhomar <nhomar@vauxoo.com>
-############################################################################
-
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError
 
