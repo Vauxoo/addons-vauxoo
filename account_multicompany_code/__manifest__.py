58432166dd76f64353a9d9d768a0b89b5c75e364
Luis González <lgonzalez@vauxoo.com>
2020-09-03 18:06:21 +0000

    [FIX] account_multicompany_code: Remove unnecessary headers
    This removes:
    - Hencoding headers, fixing lint `utf8-coding-comment`
    - Headers like `# Part of Odoo`
    - And other ones

--- account_multicompany_code/__manifest__.py
+++ account_multicompany_code/__manifest__.py
@@ -1,6 +1,3 @@
-# coding: utf-8
-# Copyright 2017 Vauxoo (https://www.vauxoo.com) <info@vauxoo.com>
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Account Multicompany Code",
     "version": "11.0.0.1.0",
