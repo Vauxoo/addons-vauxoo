ef788af77a905cb176ba51a508ce24a25d946776
Luis González <lgonzalez@vauxoo.com>
2020-09-03 18:06:21 +0000

    [FIX] mrp_partial_production: Remove unnecessary headers
    This removes:
    - Hencoding headers, fixing lint `utf8-coding-comment`
    - Headers like `# Part of Odoo`
    - And other ones

--- mrp_partial_production/wizard/__init__.py
+++ mrp_partial_production/wizard/__init__.py
@@ -1,5 +1,1 @@
-# coding: utf-8
-# Copyright 2017 Vauxoo (https://www.vauxoo.com) <info@vauxoo.com>
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-
 from . import mrp_product_produce
