5576a5bc3ca85fa0e907189579b240e29849cd30
Luis González <lgonzalez@vauxoo.com>
2020-09-03 18:06:21 +0000

    [FIX] purchase_third_validation: Remove unnecessary headers
    This removes:
    - Hencoding headers, fixing lint `utf8-coding-comment`
    - Headers like `# Part of Odoo`
    - And other ones

--- purchase_third_validation/__init__.py
+++ purchase_third_validation/__init__.py
@@ -1,11 +1,1 @@
-# coding: utf-8
-############################################################################
-#    Module Writen For Odoo, Open Source Management Solution
-#
-#    Copyright (c) 2015 Vauxoo - http://www.vauxoo.com
-#    All Rights Reserved.
-#    info Vauxoo (info@vauxoo.com)
-#    coded by: aguzman@vauxoo.com
-#    planned by: Sabrina Romero <sabrina@vauxoo.com>
-############################################################################
 from . import models
--- purchase_third_validation/__manifest__.py
+++ purchase_third_validation/__manifest__.py
@@ -1,13 +1,3 @@
-# coding: utf-8
-############################################################################
-#    Module Writen For Odoo, Open Source Management Solution
-#
-#    Copyright (c) 2015 Vauxoo - http://www.vauxoo.com
-#    All Rights Reserved.
-#    info Vauxoo (info@vauxoo.com)
-#    coded by: aguzman@vauxoo.com
-#    planned by: Sabrina Romero<sabrina@vauxoo.com>
-############################################################################
 {
     "name": "Purchase Third Validation",
     "version": "12.0.1.0.0",
