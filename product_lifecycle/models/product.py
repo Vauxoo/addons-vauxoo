a8d8a8a2e8c9b9bcbd81d37e8e5341e9c3ffad06
Luis González <lgonzalez@vauxoo.com>
2020-09-03 18:06:21 +0000

    [FIX] product_lifecycle: Remove unnecessary headers
    This removes:
    - Hencoding headers, fixing lint `utf8-coding-comment`
    - Headers like `# Part of Odoo`
    - And other ones

--- product_lifecycle/models/product.py
+++ product_lifecycle/models/product.py
@@ -1,5 +1,3 @@
-# Copyright 2019 Vauxoo (https://www.vauxoo.com) <info@vauxoo.com>
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 from odoo import models, fields, api
 
 
--- product_lifecycle/models/sale.py
+++ product_lifecycle/models/sale.py
@@ -1,5 +1,3 @@
-# Copyright 2019 Vauxoo (https://www.vauxoo.com) <info@vauxoo.com>
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 import json
 from odoo import api, fields, models
 
