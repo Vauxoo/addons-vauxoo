4ed7318b28089b4433df568bea7f11137ace3e95
Luis González <lgonzalez@vauxoo.com>
2020-09-03 18:06:21 +0000

    [FIX] base_vat_country: Remove unnecessary headers
    This removes:
    - Hencoding headers, fixing lint `utf8-coding-comment`
    - Headers like `# Part of Odoo`
    - And other ones

--- base_vat_country/__manifest__.py
+++ base_vat_country/__manifest__.py
@@ -1,6 +1,3 @@
-# Copyright 2019 Vauxoo (https://www.vauxoo.com) <info@vauxoo.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-
 {
     "name": "Base VAT Country",
     "version": "12.0.1.0.0",
