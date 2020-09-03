825c4c3c5ac4354008b477a0310407b94eae7884
Luis González <lgonzalez@vauxoo.com>
2020-09-03 18:06:21 +0000

    [FIX] product_extended_segmentation_byproduct: Remove unnecessary headers
    This removes:
    - Hencoding headers, fixing lint `utf8-coding-comment`
    - Headers like `# Part of Odoo`
    - And other ones

--- product_extended_segmentation_byproduct/__manifest__.py
+++ product_extended_segmentation_byproduct/__manifest__.py
@@ -1,6 +1,3 @@
-# coding: utf-8
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
 {
     "name": "Product Extension to track Segmentation Cost with Sub products",
     'summary': '''
