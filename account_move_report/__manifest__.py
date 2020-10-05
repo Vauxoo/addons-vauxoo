1c0e27dabf84a061e139831ed6d19f61e46c17ed
Luis González <lgonzalez@vauxoo.com>
2020-10-05 02:31:58 +0000

    [MIG] account_move_report: Migrate to v14
    The following is performed:
    - Adapt tests
    - Update translation terms

--- account_move_report/__manifest__.py
+++ account_move_report/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Journal Entries Report",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "Vauxoo",
     "category": "Accounting/Accounting",
     "website": "http://www.vauxoo.com/",
