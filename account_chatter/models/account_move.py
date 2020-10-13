7b8eb66d2d700e1888bdfcc4034fb4d85ed5bcf8
Randall Castro <randall@vauxoo.com>
2020-10-13 12:38:47 -0600

    [ADD] account_chatter: Allows to record changes in CoA, journals and account moves.

---
+++ account_chatter/models/account_move.py
@@ -0,0 +1,24 @@
+# Copyright 2020 Vauxoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class AccountMove(models.Model):
+
+    _name = "account.move"
+    _inherit = ['account.move', 'mail.thread']
+    _translate = False
+
+    name = fields.Char(track_visibility=True)
+    ref = fields.Char(track_visibility=True)
+    date = fields.Date(track_visibility=True)
+    journal_id = fields.Many2one(track_visibility=True)
+    currency_id = fields.Many2one(track_visibility=True)
+    state = fields.Selection(track_visibility=True)
+    partner_id = fields.Many2one(track_visibility=True)
+    amount = fields.Monetary(track_visibility=True)
+    narration = fields.Text(track_visibility=True)
+    auto_reverse = fields.Boolean(track_visibility=True)
+    reverse_date = fields.Date(track_visibility=True)
+    reverse_entry_id = fields.Many2one(track_visibility=True)
