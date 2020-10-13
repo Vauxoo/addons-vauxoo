eb1230388bd5bb21db320a9fe9edfb7c07e29532
Randall Castro <randall@vauxoo.com>
2020-10-13 13:29:48 -0600

    [FIX] account_chatter: lint unused import

--- account_chatter/models/account_journal.py
+++ account_chatter/models/account_journal.py
@@ -1,7 +1,7 @@
 # Copyright 2020 Vauxoo
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, models, fields
+from odoo import models, fields
 
 
 class AccountJournal(models.Model):
