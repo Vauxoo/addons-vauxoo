3e92da49683692ba65e6b27f5efd072b7f90f86a
Deivis Laya <deivis@vauxoo.com>
2020-10-22 16:56:27 +0000

    [MIG] stock_by_warehouse: Migration to 13.0
    The following is performed:
    - Remove decimal precision in float fields
    - Remove all the decorators @api.multi
    - Added @api.depends_context in non-stored computer methods, which
      use context in calls and its super method also uses it

--- stock_by_warehouse/models/product_template.py
+++ stock_by_warehouse/models/product_template.py
@@ -1,8 +1,7 @@
 import json
 from odoo import api, fields, models, _
-from odoo.addons import decimal_precision as dp
 
-UNIT = dp.get_precision('Product Unit of Measure')
+UNIT = 'Product Unit of Measure'
 
 
 class ProductTemplate(models.Model):
@@ -27,8 +26,8 @@ class ProductTemplate(models.Model):
         self.warehouses_stock = self._compute_get_quantity_warehouses_json()
         self.warehouses_stock_recompute = True
 
-    @api.multi
     @api.depends('product_variant_ids.qty_available_not_res')
+    @api.depends_context('warehouse', 'force_company')
     def _compute_product_available_not_res(self):
         for tmpl in self:
             if isinstance(tmpl.id, models.NewId):
@@ -37,7 +36,6 @@ class ProductTemplate(models.Model):
                 tmpl.mapped('product_variant_ids.qty_available_not_res')
             )
 
-    @api.multi
     def _compute_get_quantity_warehouses_json(self):
         # get original from onchange
         self_origin = self._origin if hasattr(self, '_origin') else self
@@ -51,9 +49,9 @@ class ProductTemplate(models.Model):
         # itself, we enable this context management
         warehouse_id = self._context.get('warehouse_id')
 
-        for warehouse in self.env['stock.warehouse'].sudo().search([]):
-            tmpl = self_origin.sudo().with_context(
-                warehouse=warehouse.id, location=False)
+        for warehouse in self.env['stock.warehouse'].search([]):
+            tmpl = self_origin.with_context(
+                warehouse=warehouse.id, location=False, force_company=warehouse.company_id.id)
             if warehouse_id and warehouse_id.id == warehouse.id:
                 info['warehouse'] = tmpl.qty_available_not_res
             info['content'].append({
@@ -69,3 +67,11 @@ class ProductTemplate(models.Model):
                 tmpl.qty_available - tmpl.outgoing_qty
             })
         return json.dumps(info)
+
+    @api.depends_context('company_owned', 'force_company', 'warehouse')
+    def _compute_quantities(self):
+        """Inherited method to add 'warehouse' from the api.depends_context
+        of _compute_quantities() as:
+        It is needed. It is changed in a single transaction in the current code,
+        since the widget iterates over several warehouses at the same time"""
+        return super(ProductTemplate, self)._compute_quantities()
