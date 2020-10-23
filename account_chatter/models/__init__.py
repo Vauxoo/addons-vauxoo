3edaa8b2aa1fddfe22196aa5f15b231dde761dee
Randall Castro <randall@vauxoo.com>
2020-10-23 00:17:07 -0600

    [FIX] account_chatter: change license and views name
    * Adds better description and docstring to account_account methods
    
    * Add security group to show account_account notifications

--- account_chatter/models/__init__.py
+++ account_chatter/models/__init__.py
@@ -1,5 +1,5 @@
 # Copyright 2020 Vauxoo
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from . import account_account
 from . import account_journal
