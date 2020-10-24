b4a0942092bd1079fc7a51e23fdaff04c9f0bb60
angelJuaGar <angel@vauxoo.com>
2020-10-24 00:32:47 +0000

    [MIG] sale_margin_percentage: Migration to 12.0
    The following is performed:
    - Added group for managing sales percentages.
    - Updated translations and test modifications.
    - Modifications made for test execution.
    - Added the 'for' attribute inside the labels.

--- sale_margin_percentage/__manifest__.py
+++ sale_margin_percentage/__manifest__.py
@@ -1,9 +1,8 @@
-# coding: utf-8
 # Copyright 2017 Vauxoo (https://www.vauxoo.com) <info@vauxoo.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Sale Order Gross Margin Percentage",
-    "version": "saas-18.0.0.1.0",
+    "version": "12.0.1.0.0",
     "author": "Vauxoo",
     "category": "",
     "website": "http://www.vauxoo.com/",
@@ -13,6 +12,7 @@
     ],
     "demo": [],
     "data": [
+        'security/sale_margin_percentage_security.xml',
         "views/sale_views.xml",
         "views/res_config_settings_views.xml",
     ],
