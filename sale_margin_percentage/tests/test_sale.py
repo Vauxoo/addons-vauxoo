87f3881d3c801cdf59c64244b9ab86b5208d4bc5
angelJuaGar <angel@vauxoo.com>
2020-10-24 00:32:47 +0000

    [MIG] sale_margin_percentage: Migration to 12.0
    The following is performed:
    - Added group for managing sales percentages.
    - Updated translations and test modifications.
    - Modifications made for test execution.
    - Added the 'for' attribute inside the labels.

--- sale_margin_percentage/tests/test_sale.py
+++ sale_margin_percentage/tests/test_sale.py
@@ -1,10 +1,10 @@
-# -*- coding: utf-8 -*-
-
 import time
 from odoo.tests.common import TransactionCase
+from odoo.tests import tagged
 from odoo.tools import DEFAULT_SERVER_DATE_FORMAT
 
 
+@tagged("sale_order")
 class TestSaleOrder(TransactionCase):
     """Test cases for sale.order model"""
 
@@ -28,64 +28,77 @@ class TestSaleOrder(TransactionCase):
     def test_01_margin_cost_0(self):
         """ Margin and Margin Percentage when cost is 0.0
         """
-        sale = self.sale_order_id
         product = self.prod_ipadmini
-        # Need to set the product cost to 0.0 to do not let the margin take
-        # into account the real product data instead.
         product.standard_price = 0.0
-        sale.write({"order_line": [(6, 0, [])]})
-        sale.write({"order_line": [(0, 0, {
-            'product_id': product.id,
-            'product_uom': product.uom_id.id,
-            'product_uom_qty': 4,
-            'purchase_price': 0.0,
-            'price_unit': 12.0,
-        })]})
-        self.assertEqual(sale.order_line.margin, 48)
-        self.assertEqual(sale.order_line.margin_percentage, 100.0)
+        sale = self.env['sale.order'].create({
+            'partner_id': self.partner_camp.id,
+            'order_line': [(0, 0, {
+                'name': 'iPadMini',
+                'product_id': product.id,
+                'product_uom': product.uom_id.id,
+                'product_uom_qty': 4,
+                'purchase_price': 0.0,
+                'price_unit': 12.0,
+            })],
+        })
+        sale_line = sale.order_line
+        self.assertEqual(sale_line.margin, 48)
+        self.assertEqual(sale_line.margin_percentage, 100.0)
 
     def test_02_margin_sale_price_0(self):
         """ Margin and Margin Percentage when sale price is 0.0
         """
-        sale = self.sale_order_id
-        sale.write({"order_line": [(6, 0, [])]})
-        sale.write({"order_line": [(0, 0, {
-            'product_id': self.prod_ipadmini.id,
-            'product_uom': self.prod_ipadmini.uom_id.id,
-            'product_uom_qty': 4,
-            'purchase_price': 15.0,
-            'price_unit': 0.0,
-        })]})
+        product = self.prod_ipadmini
+        product.standard_price = 0.0
+        sale = self.env['sale.order'].create({
+            'partner_id': self.partner_camp.id,
+            'order_line': [(0, 0, {
+                'name': 'iPadMini',
+                'product_id': product.id,
+                'product_uom': product.uom_id.id,
+                'product_uom_qty': 4,
+                'purchase_price': 15.0,
+                'price_unit': 0.0,
+            })],
+        })
         self.assertEqual(sale.order_line.margin, -60)
         self.assertEqual(sale.order_line.margin_percentage, -100)
 
     def test_03_margin_sale_qty_0(self):
         """ Margin and Margin Percentage when product qty is 0.0
         """
-        sale = self.sale_order_id
-        sale.write({"order_line": [(6, 0, [])]})
-        sale.write({"order_line": [(0, 0, {
-            'product_id': self.prod_ipadmini.id,
-            'product_uom': self.prod_ipadmini.uom_id.id,
-            'product_uom_qty': 0,
-            'purchase_price': 10.0,
-            'price_unit': 15.0,
-        })]})
+        product = self.prod_ipadmini
+        product.standard_price = 0.0
+        sale = self.env['sale.order'].create({
+            'partner_id': self.partner_camp.id,
+            'order_line': [(0, 0, {
+                'name': 'iPadMini',
+                'product_id': product.id,
+                'product_uom': product.uom_id.id,
+                'product_uom_qty': 0,
+                'purchase_price': 10.0,
+                'price_unit': 15.0,
+            })],
+        })
         self.assertEqual(sale.order_line.margin, 0.0)
         self.assertEqual(sale.order_line.margin_percentage, 0.0)
 
     def test_04_margin_positive(self):
         """ Margin and Margin Percentage when purchase price < sale cost
         """
-        sale = self.sale_order_id
-        sale.write({"order_line": [(6, 0, [])]})
-        sale.write({"order_line": [(0, 0, {
-            'product_id': self.prod_ipadmini.id,
-            'product_uom': self.prod_ipadmini.uom_id.id,
-            'product_uom_qty': 4,
-            'purchase_price': 10.0,
-            'price_unit': 12.0,
-        })]})
+        product = self.prod_ipadmini
+        product.standard_price = 0.0
+        sale = self.env['sale.order'].create({
+            'partner_id': self.partner_camp.id,
+            'order_line': [(0, 0, {
+                'name': 'iPadMini',
+                'product_id': product.id,
+                'product_uom': product.uom_id.id,
+                'product_uom_qty': 4,
+                'purchase_price': 10.0,
+                'price_unit': 12.0,
+            })],
+        })
         self.assertEqual(sale.order_line.margin, 8.0)
         self.assertAlmostEqual(sale.order_line.margin_percentage, 16.66,
                                places=1)
@@ -93,15 +106,19 @@ class TestSaleOrder(TransactionCase):
     def test_05_margin_negative(self):
         """ Margin and Margin Percentage when purchase price > sale cost
         """
-        sale = self.sale_order_id
-        sale.write({"order_line": [(6, 0, [])]})
-        sale.write({"order_line": [(0, 0, {
-            'product_id': self.prod_ipadmini.id,
-            'product_uom': self.prod_ipadmini.uom_id.id,
-            'product_uom_qty': 4,
-            'purchase_price': 15.0,
-            'price_unit': 12.0,
-        })]})
+        product = self.prod_ipadmini
+        product.standard_price = 0.0
+        sale = self.env['sale.order'].create({
+            'partner_id': self.partner_camp.id,
+            'order_line': [(0, 0, {
+                'name': 'iPadMini',
+                'product_id': product.id,
+                'product_uom': product.uom_id.id,
+                'product_uom_qty': 4,
+                'purchase_price': 15.0,
+                'price_unit': 12.0,
+            })],
+        })
         self.assertEqual(sale.order_line.margin, -12)
         self.assertAlmostEqual(sale.order_line.margin_percentage, -25,
                                places=1)
@@ -109,15 +126,19 @@ class TestSaleOrder(TransactionCase):
     def test_06_margin_balanced(self):
         """ Margin and Margin Percentage when purchase price = sale cost
         """
-        sale = self.sale_order_id
-        sale.write({"order_line": [(6, 0, [])]})
-        sale.write({"order_line": [(0, 0, {
-            'product_id': self.prod_ipadmini.id,
-            'product_uom': self.prod_ipadmini.uom_id.id,
-            'product_uom_qty': 4,
-            'purchase_price': 15.0,
-            'price_unit': 15.0,
-        })]})
+        product = self.prod_ipadmini
+        product.standard_price = 0.0
+        sale = self.env['sale.order'].create({
+            'partner_id': self.partner_camp.id,
+            'order_line': [(0, 0, {
+                'name': 'iPadMini',
+                'product_id': product.id,
+                'product_uom': product.uom_id.id,
+                'product_uom_qty': 4,
+                'purchase_price': 15.0,
+                'price_unit': 15.0,
+            })],
+        })
         self.assertEqual(sale.order_line.margin, 0.0)
         self.assertEqual(sale.order_line.margin_percentage, 0.0)
 
@@ -128,6 +149,10 @@ class TestSaleOrder(TransactionCase):
         # into account the real product data instead.
         product = self.prod_ipadmini
         product.standard_price = 0.0
+
+        sale = self.env['sale.order'].create({
+            'partner_id': self.partner_camp.id,
+        })
 
         lines = [
             {'product_uom_qty': 4, 'price_unit': 12.0, 'purchase_price': 0.0},
@@ -142,8 +167,6 @@ class TestSaleOrder(TransactionCase):
         for line in lines:
             line.update(product_id=product.id, product_uom=product.uom_id.id)
 
-        sale = self.sale_order_id
-        sale.write({"order_line": [(6, 0, [])]})
         sale.write({"order_line": [(0, 0, line) for line in lines]})
 
         self.assertEqual(sale.order_line.mapped("margin"),
