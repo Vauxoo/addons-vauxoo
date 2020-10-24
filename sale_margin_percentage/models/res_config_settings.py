de64731c64ad87846ad3f84965f2abdf3590a4d1
angelJuaGar <angel@vauxoo.com>
2020-10-24 00:32:47 +0000

    [MIG] sale_margin_percentage: Migration to 12.0
    The following is performed:
    - Added group for managing sales percentages.
    - Updated translations and test modifications.
    - Modifications made for test execution.
    - Added the 'for' attribute inside the labels.

--- sale_margin_percentage/models/res_config_settings.py
+++ sale_margin_percentage/models/res_config_settings.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Vauxoo
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
@@ -9,5 +8,4 @@ class ResConfiguration(models.TransientModel):
     _inherit = 'res.config.settings'
 
     margin_threshold = fields.Float(
-        related='company_id.margin_threshold',
-        help="Margin Threshold for product in order lines")
+        related='company_id.margin_threshold', readonly=False)
