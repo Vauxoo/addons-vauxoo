328a290e7dec34a7cf1b4bc93d7ede1fda0215fa
goliveirab <germana@vauxoo.com>
2020-11-24 18:18:52 +0000

    [MIG] controller_report_xls: Migration to 11.0

---
+++ controller_report_xls/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    "name": "XLS Report Controller",
+    "version": "11.0.1.0.0",
+    "author": "Vauxoo",
+    "category": "Tools",
+    "website": "http://www.vauxoo.com/",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+    ],
+    "external_dependencies": {
+        'python': ['cssutils']
+    },
+    "demo": [],
+    "data": [
+    ],
+    "test": [],
+    "js": [],
+    "css": [],
+    "qweb": [],
+    "installable": True,
+    "auto_install": False,
+}
