8c9502fcb1909785055741b52300ff982f623dba
Deivis Laya <deivis@vauxoo.com>
2020-12-11 23:16:19 +0000

    [MIG] account_chatter: Migration to 13.0
    Migration standard

--- account_chatter/models/account_move.py
+++ account_chatter/models/account_move.py
@@ -6,18 +6,11 @@ from odoo import fields, models
 
 class AccountMove(models.Model):
 
-    _name = "account.move"
-    _inherit = ['account.move', 'mail.thread']
+    _inherit = "account.move"
 
-    name = fields.Char(track_visibility=True)
-    ref = fields.Char(track_visibility=True)
-    date = fields.Date(track_visibility=True)
-    journal_id = fields.Many2one(track_visibility=True)
-    currency_id = fields.Many2one(track_visibility=True)
-    state = fields.Selection(track_visibility=True)
-    partner_id = fields.Many2one(track_visibility=True)
-    amount = fields.Monetary(track_visibility=True)
-    narration = fields.Text(track_visibility=True)
-    auto_reverse = fields.Boolean(track_visibility=True)
-    reverse_date = fields.Date(track_visibility=True)
-    reverse_entry_id = fields.Many2one(track_visibility=True)
+    name = fields.Char(tracking=True)
+    ref = fields.Char(tracking=True)
+    date = fields.Date(tracking=True)
+    journal_id = fields.Many2one(tracking=True)
+    narration = fields.Text(tracking=True)
+    reversed_entry_id = fields.Many2one(tracking=True)
