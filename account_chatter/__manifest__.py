5c45ea987a27e9c9170d2733f39554a2d9ed2f31
Deivis Laya <deivis@vauxoo.com>
2020-12-11 23:16:19 +0000

    [MIG] account_chatter: Migration to 14.0

--- account_chatter/__manifest__.py
+++ account_chatter/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Account Chatter",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "Vauxoo",
     "category": "Accounting",
     "website": "http://www.vauxoo.com",
