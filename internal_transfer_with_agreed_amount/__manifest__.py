500e872f8bde70a38267740fbe51f2ed9f39dd6c
angelJuaGar <angel@vauxoo.com>
2021-01-07 23:20:07 +0000

    [MIG] internal_transfer_with_agreed_amount: Migration to 14.0

--- internal_transfer_with_agreed_amount/__manifest__.py
+++ internal_transfer_with_agreed_amount/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Internal transfers with an agreed amount',
-    'version': '13.0.1.0.0',
+    'version': '14.0.1.0.0',
     'author': 'Vauxoo',
     'website': 'http://www.vauxoo.com/',
     'license': 'LGPL-3',
