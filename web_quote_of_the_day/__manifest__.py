4d2b99c42adfc747d03eaf9bd1dbf3a0f59a030e
Nhomar Hernández <nhomar@vauxoo.com>
2021-02-19 23:22:32 -0600

    Add odule

---
+++ web_quote_of_the_day/__manifest__.py
@@ -0,0 +1,21 @@
+{
+        'name': '',
+        'version': '1.0',
+        'description': '',
+        'summary': '',
+        'author': '',
+        'website': '',
+        'license': 'LGPL-3',
+        'category': '',
+        'depends': [
+            ''
+        ],
+        'data': [
+            ''
+        ],
+        'demo': [
+            ''
+        ],
+        'auto_install': True,
+        'application': True,
+}
