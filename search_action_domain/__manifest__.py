6221cf4a4224adc8b968c61445c8338f7fa8779b
Tomas Alvarez <tomas@vauxoo.com>
2021-02-25 17:24:50 -0600

    [ADD] search_action_domain: new module to use server actions as favorite filters

---
+++ search_action_domain/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright 2021 Vauxoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+{
+    'name': 'Search Action Domain',
+    'summary': 'Send a list of server actions via context to join the domains on the returned actions to a search.',
+    'author': 'Vauxoo',
+    'website': 'https://www.vauxoo.com',
+    'license': 'AGPL-3',
+    'category': 'Installer',
+    'version': '13.0.1.0.0',
+    'depends': [
+        'base',
+    ],
+    'test': [
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'external_dependencies': {
+    },
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
