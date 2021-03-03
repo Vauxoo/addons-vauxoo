7e07760c444fc54c8d6be9e7a5d2029a814336e8
Luis Escobar <lescobar@vauxoo.com>
2021-03-03 19:40:16 +0000

    [MIG] account_refund_early_payment: Migration to v14.0

--- account_refund_early_payment/__manifest__.py
+++ account_refund_early_payment/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Account Refund Early Payment',
-    'version': '12.0.1.0.0',
+    'version': '14.0.1.0.0',
     'author': 'Vauxoo',
     'website': 'http://www.vauxoo.com/',
     'license': 'LGPL-3',
