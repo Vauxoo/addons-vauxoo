5d540afc80724306f5bea73912bf67986e3be789
Luis González <lgonzalez@vauxoo.com>
2021-07-24 00:24:43 +0000

    [MIG] default_warehouse_from_sale_team: Migrate to 14.0
    Odoo now provides a native way to specify a default warehouse on the
    user. So this module uses the same behavior, but giving priority if a
    warehouse is set on the sales team.
    
    Other tasks are also performed:
    - Code and views are reorganized by model
    - Models that were actually mixings are renamed to use the `mixing` suffix
    - Test are improved to use the `Form` class
    - Some Typos are fixed
    - General code clean up

---
+++ default_warehouse_from_sale_team/models/default_picking_type_mixing.py
@@ -0,0 +1,31 @@
+from odoo import models, api
+
+
+class DefaultPickingTypeMixing(models.AbstractModel):
+    _name = 'default.picking.type.mixing'
+    _inherit = 'default.warehouse.mixing'
+    _description = "Default Operation Type"
+
+    @api.model
+    def default_get(self, fields_list):
+        """Fill a default picking type
+
+        Get by default the picking type depending
+        on the sales team warehouse in models where picking type is required,
+        e.g. purchase order or purchase requisition.
+        returning the first picking type that match with the warehouse
+        of the sales team.
+        """
+        res = super().default_get(fields_list)
+        default_warehouse = self.env.user._get_default_warehouse_id()
+        if default_warehouse:
+            pick_type = self.env['stock.picking.type'].search(
+                [
+                    ('code', '=', 'incoming'),
+                    ('warehouse_id', '=', default_warehouse.id),
+                ],
+                limit=1,
+            )
+            res.update({'picking_type_id': pick_type.id})
+
+        return res
