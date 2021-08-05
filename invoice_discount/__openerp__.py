kmee/project-agile

[IMP] changes in files __openerp__.py on modules

__openerp__.py
@@ -23,17 +23,12 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-
 {
-    "name" : "Portal Acess for User Story",
-    "version" : "0.1",
-    "depends" : [
-                 "user_story",
-                 "portal",
-                 "portal_project",
-                 ],
-    "author" : "Vauxoo",
-    "description" : """
+    "name": "Portal Acess for User Story", 
+    "version": "0.1", 
+    "author": "Vauxoo", 
+    "category": "Generic Modules", 
+    "description": """
 This Module only offer Portal Access to customers.
 
     - List User Stories per Project.
@@ -42,18 +37,27 @@ This Module only offer Portal Access to customers.
 
 The only objective is, easily with add Portal Access to Contacts on a Customer
 be able to let them accept user stories.
-                    """,
-    "website" : "http://vauxoo.com",
-    "category" : "Generic Modules",
-    "demo" : [
-        'demo/demo.xml',
-    ],
-    "data" : [
-        "security/ir_rules.xml",
-        "security/ir.model.access.csv",
-        'view/portal_view.xml',
-    ],
-    "active": False,
-    "images": [],
-    "installable": True,
+                    """, 
+    "website": "http://vauxoo.com", 
+    "license": "", 
+    "depends": [
+        "user_story", 
+        "portal", 
+        "portal_project"
+    ], 
+    "demo": [
+        "demo/demo.xml"
+    ], 
+    "data": [
+        "security/ir_rules.xml", 
+        "security/ir.model.access.csv", 
+        "view/portal_view.xml"
+    ], 
+    "test": [], 
+    "js": [], 
+    "css": [], 
+    "qweb": [], 
+    "installable": True, 
+    "auto_install": False
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

Vauxoo/addons-vauxoo

invoice_discount/__openerp__.py
@@ -23,28 +23,31 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-
 {
-    "name": "Invoice Discount",
-    "version": "1.0",
-    "author": "Vauxoo",
-    "category": "Generic Modules",
+    "name": "Invoice Discount", 
+    "version": "1.0", 
+    "author": "Vauxoo", 
+    "category": "Generic Modules", 
     "description": """
         Fields to get the amount without discount and discount in lines to
         invoice.
-    """,
-    "website": "http://www.vauxoo.com/",
-    "license": "AGPL-3",
+    """, 
+    "website": "http://www.vauxoo.com/", 
+    "license": "AGPL-3", 
     "depends": [
-        "account",
-        "sale",
-    ],
-    "demo": [],
+        "account", 
+        "sale"
+    ], 
+    "demo": [], 
     "data": [
-        "view/invoice_view.xml",
-        'view/config_settings_action.xml',
-    ],
-    "test": [],
-    "installable": True,
-    "active": False,
+        "view/invoice_view.xml", 
+        "view/config_settings_action.xml"
+    ], 
+    "test": [], 
+    "js": [], 
+    "css": [], 
+    "qweb": [], 
+    "installable": True, 
+    "auto_install": False
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4: