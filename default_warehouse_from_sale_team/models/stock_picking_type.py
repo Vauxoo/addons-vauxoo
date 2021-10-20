96cbbe50c383ac43250d0e9108da1fa9a9329b5c
Luis González <lgonzalez@vauxoo.com>
2021-10-20 01:23:44 +0000

    [REF] default_warehouse_from_sale_team: Sequence assignation in v14.0
    Automatic sequence numbering was not working in 14.0. For instance, now
    purchase requisitions have different sequence codes, depending on
    their types.

--- default_warehouse_from_sale_team/models/stock_picking_type.py
+++ default_warehouse_from_sale_team/models/stock_picking_type.py
@@ -3,4 +3,4 @@ from odoo import models
 
 class StockPickingType(models.Model):
     _name = "stock.picking.type"
-    _inherit = ['stock.picking.type', 'default.warehouse.mixing']
+    _inherit = ["default.warehouse.mixing", "stock.picking.type"]
