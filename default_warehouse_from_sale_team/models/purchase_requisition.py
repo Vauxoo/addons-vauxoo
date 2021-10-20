49822285c67838ad9e2af4b55800251636664bba
Luis González <lgonzalez@vauxoo.com>
2021-10-20 01:23:44 +0000

    [REF] default_warehouse_from_sale_team: Sequence assignation in v14.0
    Automatic sequence numbering was not working in 14.0. For instance, now
    purchase requisitions have different sequence codes, depending on
    their types.

--- default_warehouse_from_sale_team/models/purchase_requisition.py
+++ default_warehouse_from_sale_team/models/purchase_requisition.py
@@ -5,5 +5,11 @@ class PurchaseRequisition(models.Model):
     _name = 'purchase.requisition'
     _inherit = ['purchase.requisition', 'default.picking.type.mixing']
 
-    def _get_sequence_code(self):
-        return 'purchase.order.requisition'
+    def action_in_progress(self):
+        """Pass team by context so sequence number is computed
+
+        Since requisition name is not assigned when it's created but when confirmed,
+        we need to pass the sales team by context also here.
+        """
+        self_team = self.with_context(sequence_salesteam_id=self.warehouse_id.sale_team_ids[:1].id)
+        return super(PurchaseRequisition, self_team).action_in_progress()
