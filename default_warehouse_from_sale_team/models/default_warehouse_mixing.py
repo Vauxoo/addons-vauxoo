e536383dcd008d0923f4003b5c36630b6ba45ba5
Luis González <lgonzalez@vauxoo.com>
2021-10-20 01:23:44 +0000

    [REF] default_warehouse_from_sale_team: Sequence assignation in v14.0
    Automatic sequence numbering was not working in 14.0. For instance, now
    purchase requisitions have different sequence codes, depending on
    their types.

--- default_warehouse_from_sale_team/models/default_warehouse_mixing.py
+++ default_warehouse_from_sale_team/models/default_warehouse_mixing.py
@@ -5,6 +5,10 @@ class DefaultWarehouseMixing(models.AbstractModel):
     """If you inherit from this model and add a field called warehouse_id into
     the model, then the default value for such model will be the one
     set into the sales team.
+
+    Make sure to put this mixing at the top of inheritance (before the base model), e.g.
+
+        _inherit = ["default.warehouse.mixing", "sale.order"]
     """
     _name = "default.warehouse.mixing"
     _description = "Default Warehouse"
@@ -32,21 +36,19 @@ class DefaultWarehouseMixing(models.AbstractModel):
 
     @api.model
     def create(self, vals):
-        if vals.get('warehouse_id') and 'name' not in vals:
-            code = self._get_sequence_code()
-            pick_warehouse = self.env['stock.picking.type'].browse(vals.get('picking_type_id')).warehouse_id
-            warehouse_id = vals.get('warehouse_id', pick_warehouse.id)
-            section = self.env['crm.team'].search([('default_warehouse_id', '=', warehouse_id)], limit=1)
-            sequence = self.env['ir.sequence'].search(
-                [
-                    ('section_id', '=', section.id),
-                    ('code', '=', code),
-                ],
-                limit=1,
-            )
-            if sequence:
-                vals['name'] = sequence.next_by_id()
-        return super().create(vals)
+        """Pass sales team by context so it's taken into account when computing sequence name"""
+        salesteam = self._get_salesteam_from_vals(vals)
+        self_team = self.with_context(sequence_salesteam_id=salesteam.id)
+        return super(DefaultWarehouseMixing, self_team).create(vals)
 
-    def _get_sequence_code(self):
-        return self._name
+    def _get_salesteam_from_vals(self, vals):
+        """Determine sales team from creation values"""
+        if "name" in vals:
+            # Already has a name, so salesteam won't be used anyway
+            return self.env["crm.team"]
+        warehouse = (
+            self.env["stock.warehouse"].browse(vals.get("warehouse_id"))
+            or self.env["stock.picking.type"].browse(vals.get("picking_type_id")).warehouse_id
+        )
+        salesteam = warehouse.sale_team_ids[:1]
+        return salesteam
--- default_warehouse_from_sale_team/models/ir_sequence.py
+++ default_warehouse_from_sale_team/models/ir_sequence.py
@@ -1,7 +1,28 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class IrSequence(models.Model):
     _inherit = "ir.sequence"
 
     section_id = fields.Many2one('crm.team', string='Sales Team')
+
+    @api.model
+    def next_by_code(self, sequence_code, sequence_date=None):
+        """If a sales team is provided by context, give priority to sequences having it set"""
+        if "sequence_salesteam_id" not in self.env.context:
+            return super().next_by_code(sequence_code, sequence_date)
+        salesteam_id = self.env.context["sequence_salesteam_id"]
+        self.check_access_rights('read')
+        company_id = self.env.company.id
+        sequence = self.search(
+            [
+                ("code", "=", sequence_code),
+                ("company_id", "in", [company_id, False]),
+                ("section_id", "in", [salesteam_id, False]),
+            ],
+            limit=1,
+            order="section_id, company_id"
+        )
+        if sequence:
+            return sequence._next(sequence_date=sequence_date)
+        return super().next_by_code(sequence_code, sequence_date)
