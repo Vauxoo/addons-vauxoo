d581142ce80fb12922e96ef9952236efcad5d4a3
CarmenMiranda <carmen@vauxoo.com>
2021-12-08 00:55:28 +0000

    [MIG] pr_line_related_po_line: Migration to 14.0
    Changing the name of the manifest file, deleting the headers, using the
    new names for the imports and using the new way to get the values to
    create the purchase order lines from the purchase requisition lines to
    send the requisition line.

---
+++ pr_line_related_po_line/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "PR Line related PO Line",
+    "version": "14.0.1.0.0",
+    "author": "Vauxoo",
+    'category': 'Inventory/Purchase',
+    "website": "http://www.vauxoo.com/",
+    "license": "LGPL-3",
+    "depends": [
+        "purchase_requisition",
+    ],
+    "data": [
+        "views/purchase_order_views.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
