a42562a6d1c7282c1b5468aee0a9b009a15765fe
agarzaarvizu <agarza@vauxoo.com>
2022-01-11 21:51:32 +0000

    [ADD] stock_by_warehouse_mrp_bom: New module added for Bill of Materials
    Know the stock in all your warehouses with a simple click from the mrp bill of material's line form.

---
+++ stock_by_warehouse_mrp_bom/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    "name": "Stock by Warehouse on MRP_BoM",
+    "version": "12.0.1.0.0",
+    "summary": """
+    Know the stock in all your warehouses with a simple click
+    from the mrp bill of material's line form.
+    """,
+    "author": "Vauxoo",
+    "category": "stock",
+    "website": "http://www.vauxoo.com/",
+    "license": "LGPL-3",
+    "depends": [
+        "mrp",
+        "stock_by_warehouse",
+    ],
+    "data": [
+        'views/mrp_bom_view.xml'
+    ],
+    "demo": [
+    ],
+    "installable": True,
+    "auto_install": False,
+}
