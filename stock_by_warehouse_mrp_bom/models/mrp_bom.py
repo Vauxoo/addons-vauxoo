4723af865a321ec2e5dc743253cccfab216149f2
agarzaarvizu <agarza@vauxoo.com>
2022-01-25 23:23:52 +0000

    [MIG] stock_by_warehouse_mrp_bom: Migration to 15.0

--- stock_by_warehouse_mrp_bom/models/mrp_bom.py
+++ stock_by_warehouse_mrp_bom/models/mrp_bom.py
@@ -10,7 +10,6 @@ class MrpBomLine(models.Model):
         related='bom_id.picking_type_id.warehouse_id')
     warehouses_stock_recompute = fields.Boolean(store=False, readonly=False)
 
-    @api.multi
     def _compute_get_warehouses_stock(self):
         for line in self:
             line.warehouses_stock = line.product_id.with_context(
