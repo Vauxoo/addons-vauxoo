c84044408ca1fd6f9ade7e0e02feab9cd3255320
Luis González <lgonzalez@vauxoo.com>
2022-02-12 23:16:33 +0000

    [FIX] default_warehouse_on_sale_team: typo in model names of mixins
    mixing -> mixin

--- default_warehouse_from_sale_team/models/sale_order.py
+++ default_warehouse_from_sale_team/models/sale_order.py
@@ -3,7 +3,7 @@ from odoo import models
 
 class SaleOrder(models.Model):
     _name = "sale.order"
-    _inherit = ["default.warehouse.mixing", "sale.order"]
+    _inherit = ["default.warehouse.mixin", "sale.order"]
 
     def _prepare_invoice(self):
         """Add team by context so it's taken into account when choosing default journal"""
