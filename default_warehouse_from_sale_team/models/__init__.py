a2c754e1989ff2537faf469bb8c33882c5d2783b
Luis González <lgonzalez@vauxoo.com>
2022-02-22 19:51:39 +0000

    [FIX] default_warehouse_from_sale_team: access error when chained rule
    When a stock movement triggers an inventory rule that involves another
    warehouse on which the current user has no access, an access error is
    triggered. That's because, thanks to this module, users can only see
    warehouses allowed in their sales teams.
    
    To solve the above, access is granted to all warehouses when computing
    inventory rules to apply.

--- default_warehouse_from_sale_team/models/__init__.py
+++ default_warehouse_from_sale_team/models/__init__.py
@@ -4,6 +4,7 @@ from . import crm_team
 from . import default_warehouse_mixin
 from . import default_picking_type_mixin
 from . import ir_sequence
+from . import procurement_group
 from . import purchase_order
 from . import purchase_requisition
 from . import res_users
