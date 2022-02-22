0340b0b5f99d238d53421152adad306c28a0bdc6
Luis González <lgonzalez@vauxoo.com>
2022-02-22 19:51:39 +0000

    [FIX] default_warehouse_from_sale_team: access error when chained rule
    When a stock movement triggers an inventory rule that involves another
    warehouse on which the current user has no access, an access error is
    triggered. That's because, thanks to this module, users can only see
    warehouses allowed in their sales teams.
    
    To solve the above, access is granted to all warehouses when computing
    inventory rules to apply.

--- default_warehouse_from_sale_team/models/stock_warehouse.py
+++ default_warehouse_from_sale_team/models/stock_warehouse.py
@@ -7,3 +7,18 @@ class StockWarehouse(models.Model):
     sale_team_ids = fields.One2many(
         "crm.team", "default_warehouse_id", string="Sales teams",
     )
+
+    def _access_unallowed_current_user_salesteams(self):
+        """Check if access will be denied because warehouse is not allowed on current user's sales teams
+
+        In some cases, it's required to grant access to warehouses not allowed for the current user, e.g.
+        when an inventory rule is triggered that involves other warehouses.
+        """
+        rule_warehouse = self.env.ref("default_warehouse_from_sale_team.rule_default_warehouse_wh")
+        warehouses = self.exists()
+        return (
+            warehouses
+            and not self.env.su
+            and warehouses.check_access_rights("read", raise_exception=False)
+            and self.env["ir.rule"]._get_failing(warehouses, mode="read") == rule_warehouse
+        )
