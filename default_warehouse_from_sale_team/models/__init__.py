4bb6be1fd090b833247821cc57bcfd6f9a0c16e0
rolandojduartem <rolando@vauxoo.com>
2022-03-09 15:48:15 -0600

    [ADD] default_warehouse_from_sale_team: Allow to run procurement in warehouses not allowed on current user's sales teams
    As part of migration to 15.0 of default_warehouse_from_sale_team and
    default_warehouse_from_sale_team_jit, the dependency procurement_jit was removed
    (https://github.com/odoo/odoo/commit/a838f2af683e03cd03ba02532f002ccd5c2642f5). This change caused
    that default_warehouse_from_sale_team_jit
    (https://github.com/Vauxoo/addons-vauxoo/tree/14.0/default_warehouse_from_sale_team_jit)
    be deprecated on Odoo v15.0 and the main function of that model can be added in
    default_warehouse_from_sale_team.

--- default_warehouse_from_sale_team/models/__init__.py
+++ default_warehouse_from_sale_team/models/__init__.py
@@ -9,6 +9,7 @@ from . import purchase_order
 from . import purchase_requisition
 from . import res_users
 from . import sale_order
+from . import sale_order_line
 from . import stock_move
 from . import stock_picking
 from . import stock_picking_type
