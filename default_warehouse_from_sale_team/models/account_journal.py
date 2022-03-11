941ff0ab837703b3b685b8defdff297a9550bed9
Luis González <lgonzalez@vauxoo.com>
2022-03-11 19:14:41 +0000

    [FIX] default_warehouse_from_sale_team: access error on journal name
    Avoid access errors when user has access to a journal item but not to its
    journal, e.g. when reconciling bank statements.

--- default_warehouse_from_sale_team/models/account_journal.py
+++ default_warehouse_from_sale_team/models/account_journal.py
@@ -5,3 +5,12 @@ class AccountJournal(models.Model):
     _inherit = 'account.journal'
 
     section_id = fields.Many2one('crm.team', string='Sales Team')
+
+    def name_get(self):
+        """Avoid access errors when computing journal's display name
+
+        Avoid access errors when user has access to a journal item but not to its journal, e.g. when
+        reconciling bank statements.
+        """
+        self = self.sudo()
+        return super().name_get()
