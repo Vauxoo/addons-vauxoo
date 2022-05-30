88d71a76689b025d56f2f2d7d194068df6923254
Andy Quijada <andyjquijada@gmail.com>
2022-05-30 18:08:22 -0400

    [FIX] *: Replace AGPL licence with LGPL and remove headers on .py files t#57461
    - Replace all AGPL licence with LGPL on __manifest__ files
    - Remove all headers on .py files

--- search_action_domain/__manifest__.py
+++ search_action_domain/__manifest__.py
@@ -1,11 +1,9 @@
-# Copyright 2021 Vauxoo
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 {
     'name': 'Search Action Domain',
     'summary': 'Send a list of server actions via context to join the domains on the returned actions to a search.',
     'author': 'Vauxoo',
     'website': 'https://www.vauxoo.com',
-    'license': 'AGPL-3',
+    'license': 'LGPL-3',
     'category': 'Installer',
     'version': '13.0.1.0.0',
     'depends': [
