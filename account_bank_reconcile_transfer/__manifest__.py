20b7d622c98f8fb51fa0de8459ab94a951f24bba
Andy Quijada <andyjquijada@gmail.com>
2022-05-30 18:08:22 -0400

    [FIX] *: Replace AGPL licence with LGPL and remove headers on .py files t#57461
    - Replace all AGPL licence with LGPL on __manifest__ files
    - Remove all headers on .py files

--- account_bank_reconcile_transfer/__manifest__.py
+++ account_bank_reconcile_transfer/__manifest__.py
@@ -1,11 +1,9 @@
-# Copyright 2021 Vauxoo
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 {
     'name': 'Account Bank Reconcile Transfer',
     'summary': '''Create bank transfers directly from the bank reconciliation screen.''',
     'author': 'Vauxoo',
     'website': 'https://www.vauxoo.com',
-    'license': 'AGPL-3',
+    'license': 'LGPL-3',
     'category': 'Installer',
     'version': '13.0.1.0.0',
     'depends': [
