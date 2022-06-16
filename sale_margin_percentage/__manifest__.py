79a9c4f6460955559f8507f5455a13dfad1b8ff1
Andy Quijada <andyjquijada@gmail.com>
2022-06-15 20:54:22 -0400

    [MIG] sale_margin_percentage: Migration to v15.0
    Migration process from v12.0 to v15.0

--- sale_margin_percentage/__manifest__.py
+++ sale_margin_percentage/__manifest__.py
@@ -1,19 +1,19 @@
-# Copyright 2017 Vauxoo (https://www.vauxoo.com) <info@vauxoo.com>
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Sale Order Gross Margin Percentage",
-    "version": "12.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "Vauxoo",
-    "category": "",
-    "website": "http://www.vauxoo.com/",
+    "category": "Sales/Sales",
+    "website": "https://www.vauxoo.com/",
     "license": "LGPL-3",
     "depends": [
         "sale_margin",
     ],
-    "demo": [],
+    "demo": [
+        "demo/product_product_demo.xml",
+    ],
     "data": [
-        'security/sale_margin_percentage_security.xml',
-        "views/sale_views.xml",
+        "security/sale_margin_percentage_security.xml",
+        "views/sale_order_views.xml",
         "views/res_config_settings_views.xml",
     ],
     "installable": True,
