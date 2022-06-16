fa0f7a9ff1ecf28038195a8531dd89ccbb473e55
Andy Quijada <andyjquijada@gmail.com>
2022-06-15 20:54:22 -0400

    [MIG] sale_margin_percentage: Migration to v15.0
    Migration process from v12.0 to v15.0

--- sale_margin_percentage/README.rst
+++ sale_margin_percentage/README.rst
@@ -1,19 +1,20 @@
 .. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
-   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :target: https://www.gnu.org/licenses/agpl-3.0-standalone.html
    :alt: License: AGPL-3
 
 ==================================
 Sale Order Gross Margin Percentage
 ==================================
 
-This module change the margin calculation. Now, the sale order gross margin
-percentage is calculated by the sum all the margin lines between the inverse
+This module changes the margin calculation. Now, the sale order gross margin
+percentage is calculated by the sum of all the margin lines divided by the inverse
 sum of all subtotals.
 
 features:
 ---------
+
 - Field to configure Margin Threshold on sale settings.
-- Colors in sale order lines to point the margin threshold.
+- Colors in sale order lines to indicate the margin threshold.
 - Fix to 100 margin percentage when purchase price is not set.
 - Fix to -100 margin percentage when price unit is not set.
 - Fix to 0 margin percentage when product quatity is 0.
@@ -32,5 +33,21 @@ Contributors
 ------------
 
 * Katherine Zaoral <katherine.zaoral@vauxoo.com>
-* Nhomar Hernandez <nhomar@vauxoo.com>
-* Jose Suniaga <josemiguel@vauxoo.com>
+* Nhomar Hernández <nhomar@vauxoo.com>
+* José Suniaga <josemiguel@vauxoo.com>
+
+Maintainers
+-----------
+
+.. image:: https://www.vauxoo.com/logo.png
+   :alt: Vauxoo
+   :target: https://vauxoo.com
+
+This module is maintained by Vauxoo.
+
+a latinamerican company that provides training, coaching,
+development and implementation of enterprise management
+sytems and bases its entire operation strategy in the use
+of Open Source Software and its main product is odoo.
+
+To contribute to this module, please visit https://www.vauxoo.com.
---
+++ sale_margin_percentage/tests/test_sale_order.py
@@ -0,0 +1,98 @@
+from odoo.tests.common import Form, TransactionCase
+from odoo.tests import tagged
+
+
+@tagged("sale_order")
+class TestSaleOrder(TransactionCase):
+    """Test cases for sale.order model"""
+
+    def setUp(self):
+        super().setUp()
+        self.partner = self.env.ref('base.res_partner_12')
+        self.product_cost_0 = self.env.ref('product.product_product_4')
+        self.product_cost_5 = self.env.ref('product.product_product_5')
+        self.product_cost_10 = self.env.ref('product.product_product_6')
+        self.product_cost_12 = self.env.ref('product.product_product_7')
+        self.product_cost_15 = self.env.ref('product.product_product_8')
+
+    def create_so(self, partner=None, **line_kwargs):
+        if partner is None:
+            partner = self.partner
+        order = Form(self.env["sale.order"])
+        order.partner_id = partner
+        order = order.save()
+        self.create_so_line(order, **line_kwargs)
+        return order
+
+    def create_so_line(self, order, product=None, quantity=4, price=10):
+        if product is None:
+            product = self.product_cost_0
+        with Form(order) as order_form:
+            with order_form.order_line.new() as line:
+                line.product_id = product
+                line.product_uom_qty = quantity
+                line.price_unit = price
+
+    def test_01_margin_cost_0(self):
+        """ Margin and Margin Percentage when cost is 0
+        """
+        sale = self.create_so(quantity=4, price=12)
+        self.assertEqual(sale.order_line.margin, 48)
+        self.assertEqual(sale.order_line.margin_percentage, 100)
+
+    def test_02_margin_sale_price_0(self):
+        """ Margin and Margin Percentage when sale price is 0
+        """
+        sale = self.create_so(product=self.product_cost_15, quantity=4, price=0)
+        self.assertEqual(sale.order_line.margin, -60)
+        self.assertEqual(sale.order_line.margin_percentage, -100)
+
+    def test_03_margin_sale_qty_0(self):
+        """ Margin and Margin Percentage when product qty is 0
+        """
+        sale = self.create_so(product=self.product_cost_10, quantity=0, price=15)
+        self.assertEqual(sale.order_line.margin, 0.0)
+        self.assertEqual(sale.order_line.margin_percentage, 0.0)
+
+    def test_04_margin_positive(self):
+        """ Margin and Margin Percentage when purchase price < sale cost
+        """
+        sale = self.create_so(product=self.product_cost_10, quantity=4, price=12)
+        self.assertEqual(sale.order_line.margin, 8.0)
+        self.assertAlmostEqual(sale.order_line.margin_percentage, 16.66, places=1)
+
+    def test_05_margin_negative(self):
+        """ Margin and Margin Percentage when purchase price > sale cost
+        """
+        sale = self.create_so(product=self.product_cost_15, quantity=4, price=12)
+        self.assertEqual(sale.order_line.margin, -12)
+        self.assertAlmostEqual(sale.order_line.margin_percentage, -25, places=1)
+
+    def test_06_margin_balanced(self):
+        """ Margin and Margin Percentage when purchase price = sale cost
+        """
+        sale = self.create_so(product=self.product_cost_15, quantity=4, price=15)
+        self.assertEqual(sale.order_line.margin, 0.0)
+        self.assertEqual(sale.order_line.margin_percentage, 0.0)
+
+    def test_07_margin_total(self):
+        """ Margin Percentage of the sale order.
+        """
+        sale = self.create_so(quantity=4, price=12)
+        self.create_so_line(sale, product=self.product_cost_15, quantity=4, price=0)
+        self.create_so_line(sale, product=self.product_cost_15, quantity=4, price=12)
+        self.create_so_line(sale, product=self.product_cost_10, quantity=4, price=12)
+        self.create_so_line(sale, product=self.product_cost_12, quantity=4, price=12)
+        self.create_so_line(sale, product=self.product_cost_10, quantity=0, price=15)
+        self.create_so_line(sale, product=self.product_cost_5, quantity=4, price=13)
+        self.create_so_line(sale, product=self.product_cost_10, quantity=5, price=12)
+        self.assertEqual(
+            sale.order_line.mapped("margin"),
+            [48.0, -60.0, -12.0, 8.0, 0.0, 0.0, 32.0, 10.0]
+        )
+        self.assertEqual(
+            sale.order_line.mapped("margin_percentage"),
+            [100.0, -100.0, -25.0, 16.67, 0.0, 0.0, 61.54, 16.67]
+        )
+        self.assertEqual(sale.margin, 26)
+        self.assertAlmostEqual(sale.margin_percentage, 8.55, places=1)
