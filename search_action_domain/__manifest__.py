89310df9538fd3e6525dbcbc190255ad3a93e53e
Rolando Duarte [Vauxoo] <rolando@vauxoo.com>
2022-07-13 18:50:06 +0000

    [IMP] search_action_domain: Black lint T#59176

--- search_action_domain/__manifest__.py
+++ search_action_domain/__manifest__.py
@@ -1,20 +1,20 @@
 {
-    'name': 'Search Action Domain',
-    'summary': 'Send a list of server actions via context to join the domains on the returned actions to a search.',
-    'author': 'Vauxoo',
-    'website': 'https://www.vauxoo.com',
-    'license': 'LGPL-3',
-    'category': 'Installer',
-    'version': '13.0.1.0.0',
-    'depends': [
-        'base',
+    "name": "Search Action Domain",
+    "summary": "Send a list of server actions via context to join the domains on the returned actions to a search.",
+    "author": "Vauxoo",
+    "website": "https://www.vauxoo.com",
+    "license": "LGPL-3",
+    "category": "Installer",
+    "version": "13.0.1.0.0",
+    "depends": [
+        "base",
     ],
-    'data': [
-        'data/ir_actions_server.xml',
+    "data": [
+        "data/ir_actions_server.xml",
     ],
-    'demo': [],
-    'external_dependencies': {},
-    'installable': True,
-    'auto_install': False,
-    'application': False,
+    "demo": [],
+    "external_dependencies": {},
+    "installable": True,
+    "auto_install": False,
+    "application": False,
 }
