2d5cc58179c8d94a3d978b517ee8b909eb5a16a4
Rolando Duarte [Vauxoo] <rolando@vauxoo.com>
2022-07-21 23:00:43 +0000

    [FIX] search_action_domain: Make installable with base_automation T#59176
    At the moment to install search_action_domain with base_automation, an error
    appears to indicate the created button is not a valid action for base.automation.
    This fix avoid the validation for the created button if base_automation
    installation is in process.

--- search_action_domain/models/__init__.py
+++ search_action_domain/models/__init__.py
@@ -1,2 +1,3 @@
 from . import ir_actions_server
+from . import ir_ui_view
 from . import models
---
+++ search_action_domain/models/ir_ui_view.py
@@ -0,0 +1,18 @@
+from odoo import models
+
+
+class IrUiView(models.Model):
+    _inherit = "ir.ui.view"
+
+    def _validate_tag_button(self, node, name_manager, node_info):
+        """At the moment to install search_action_domain with base_automation, an error
+        appears to indicate the created button is not a valid action for base.automation.
+        This fix avoid the validation for the created button if base_automation
+        installation is in process.
+        """
+        if (
+            name_manager.model._name == "base.automation"
+            and node.get("name") == "create_as_ir_filter"
+        ):
+            node_info.update({"validate": 0})
+        return super()._validate_tag_button(node, name_manager, node_info)
