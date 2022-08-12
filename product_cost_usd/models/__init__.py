e3cf77f758e6d1d289639422f7d8235df32de885
agarzaarvizu <agarza@vauxoo.com>
2022-08-12 21:24:48 +0000

    [FIX] product_cost_usd: fix models, views and manifest
    - Rename model files and remove parameters in super
    - Rename views files and views IDs
    - Change category and fix version in manifest
    - Remove headers on files

--- product_cost_usd/models/__init__.py
+++ product_cost_usd/models/__init__.py
@@ -1,7 +1,4 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Vauxoo
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-
-from . import product
-from . import pricelist
-from . import sale_order
+from . import product_template
+from . import product_pricelist
+from . import product_pricelist_item
+from . import sale_order_line
---
+++ product_cost_usd/models/product_pricelist_item.py
@@ -0,0 +1,7 @@
+from odoo import fields, models
+
+
+class PricelistItem(models.Model):
+    _inherit = "product.pricelist.item"
+
+    base = fields.Selection(selection_add=[("standard_price_usd", "Cost in USD")])
