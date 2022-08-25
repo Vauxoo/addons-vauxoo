331f9d71e0c94da5e3162b94a50b17ecbb83ab1e
Alejandro Garza <agarza@vauxoo.com>
2022-08-25 22:37:07 +0000

    [MIG] product_cost_usd: module migrated to v15.0

--- product_cost_usd/models/product_pricelist_item.py
+++ product_cost_usd/models/product_pricelist_item.py
@@ -4,4 +4,6 @@ from odoo import fields, models
 class PricelistItem(models.Model):
     _inherit = "product.pricelist.item"
 
-    base = fields.Selection(selection_add=[("standard_price_usd", "Cost in USD")])
+    base = fields.Selection(
+        selection_add=[("standard_price_usd", "Cost in USD")], ondelete={"standard_price_usd": "set default"}
+    )
