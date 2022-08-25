161540509cdfb8920cd8ec04f41ce3c8af839288
Alejandro Garza <agarza@vauxoo.com>
2022-08-25 22:37:07 +0000

    [MIG] product_cost_usd: module migrated to v15.0

--- product_cost_usd/models/sale_order_line.py
+++ product_cost_usd/models/sale_order_line.py
@@ -4,38 +4,26 @@ from odoo import api, models
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    @api.model
-    def _get_purchase_price(self, pricelist, product, product_uom, date):
-        """Inherited to recalculate purchase price when pricelist item is
-        based on cost in usd.
-        """
-        res = super()._get_purchase_price(pricelist, product, product_uom, date)
-        price_rule = pricelist._compute_price_rule([(product, 1, False)])
-        price, rule = price_rule[product.id]
-        suitable_rule = pricelist.item_ids.filtered(lambda x: x.id == rule)
-        if not suitable_rule or suitable_rule.base != "standard_price_usd":
-            return res
-        frm_cur = self.env.ref("base.USD")
-        to_cur = pricelist.currency_id
-        purchase_price = product.standard_price_usd
-        if product_uom != product.uom_id:
-            purchase_price = product.uom_id._compute_price(purchase_price, product_uom)
-        price = frm_cur.with_context(date=date).compute(purchase_price, to_cur, round=False)
-        return {"purchase_price": price}
-
-    @api.model
-    def _compute_margin(self, order_id, product_id, product_uom_id):
+    @api.depends("product_id", "company_id", "currency_id", "product_uom")
+    def _compute_purchase_price(self):
         """Inherited to recalculate purchase price when pricelist item is
         based on cost in USD.
-
-        Why this inheritance?
-
-        In spite of the name this method is only used to get the purchase
-        price, calling the method get_purchase_price we reuse that logic to
-        calculate the purchase price when pricelist item is based on cost
-        in USD.
         """
-        price = super()._compute_margin(order_id, product_id, product_uom_id)
-        date = order_id.date_order
-        prices = self._get_purchase_price(order_id.pricelist_id, product_id, product_uom_id, date)
-        return prices.get("purchase_price", price)
+        res = super()._compute_purchase_price()
+        for line in self:
+            pricelist = line.order_id.pricelist_id
+            date = line.order_id.date_order
+            if not line.product_id:
+                continue
+            price_rule = pricelist._compute_price_rule([(line.product_id, 1, False)])
+            _price, rule = price_rule[line.product_id.id]
+            suitable_rule = pricelist.item_ids.filtered(lambda x: x.id == rule)
+            if not suitable_rule or suitable_rule.base != "standard_price_usd":
+                continue
+            currency_usd = self.env.ref("base.USD")
+            to_cur = pricelist.currency_id
+            purchase_price = line.product_id.standard_price_usd
+            if line.product_uom != line.product_id.uom_id:
+                purchase_price = line.product_id.uom_id._compute_price(purchase_price, line.product_uom)
+            line.purchase_price = currency_usd._convert(purchase_price, to_cur, line.company_id, date, round=False)
+        return res
--- product_cost_usd/tests/test_standard_price_usd.py
+++ product_cost_usd/tests/test_standard_price_usd.py
@@ -2,128 +2,98 @@ from __future__ import division
 
 from odoo import fields
 from odoo.exceptions import ValidationError
-from odoo.tests.common import TransactionCase
-from odoo.tools import float_compare, float_round
+from odoo.tests import Form, TransactionCase, tagged
+from odoo.tools import float_compare
 
 
+@tagged("post_install", "-at_install", "sale")
 class TestStandardPriceUsd(TransactionCase):
     def setUp(self):
         super().setUp()
         self.mxn = self.env.ref("base.MXN")
         self.usd = self.env.ref("base.USD")
-        self.sale_order = self.env["sale.order"]
         self.partner = self.env.ref("base.res_partner_4")
-        self.product_uom = self.env.ref("product.product_uom_unit")
+        self.product_uom = self.env.ref("uom.product_uom_unit")
         self.product = self.env.ref("product.product_product_24")
         self.pricelist_15_usd = self.env.ref("product_cost_usd.pricelist_15_usd")
         self.pricelist_15_mxn = self.pricelist_15_usd.copy({"name": "Pricelist 15% MXN", "currency_id": self.mxn.id})
         self.pricelist = self.env["product.pricelist"].create({"name": "Pricelist Demo"})
+
+    def create_sale_order(self, product=None, partner=None, pricelist=None, **line_kwargs):
+        if partner is None:
+            partner = self.partner
+        order = Form(self.env["sale.order"])
+        order.date_order = fields.Datetime.now()
+        order.partner_id = partner
+        order.pricelist_id = pricelist
+        with order.order_line.new() as line:
+            line.product_id = product
+            line.product_uom = self.product_uom
+            line.product_uom_qty = 1
+        order = order.save()
+        return order
 
     def set_standard_price_usd(self, price):
         self.assertTrue(self.product.seller_ids)
         self.product.seller_ids[0].write({"currency_id": self.usd.id})
         self.product.write({"standard_price_usd": price})
 
-    def test_01(self):
+    def test_01_usd_pricelist(self):
         """Test USD pricelist based on cost in USD."""
         self.set_standard_price_usd(880)
         product = self.product.with_context(pricelist=self.pricelist_15_usd.id)
-        expected_price = float_round(product.standard_price_usd * 1.15, precision_rounding=self.usd.rounding)
+        expected_price = self.usd.round(product.standard_price_usd * 1.15)
         self.assertEqual(
             float_compare(product.price, expected_price, precision_digits=2),
             0,
             "Product price should be %s" % product.price,
         )
 
-    def test_02(self):
+    def test_02_mxn_pricelist(self):
         """Test a MXN pricelist based on cost in USD."""
         self.set_standard_price_usd(880)
         product = self.product.with_context(pricelist=self.pricelist_15_mxn.id)
         mxn_rate = self.mxn.rate / self.usd.rate
-        expected_price = float_round(
-            (product.standard_price_usd * 1.15) * mxn_rate, precision_rounding=self.mxn.rounding
-        )
+        expected_price = self.mxn.round((product.standard_price_usd * 1.15) * mxn_rate)
         self.assertEqual(
             float_compare(product.price, expected_price, precision_digits=2),
             0,
             "Product price should be %s" % product.price,
         )
 
-    def test_03(self):
+    def test_03_constraint_check_cost_no_seller(self):
         """Test constraint check_cost_and_price."""
-        with self.assertRaisesRegexp(ValidationError, "You must have at least one supplier with price in USD"):
+        self.product.seller_ids = False
+        with self.assertRaisesRegex(ValidationError, "You must have at least one supplier with price in USD"):
             self.product.write({"standard_price_usd": 880})
 
-    def test_04(self):
+    def test_04_constraint_check_cost(self):
         """Test constraint check_cost_and_price."""
-        with self.assertRaisesRegexp(ValidationError, "You cannot create or modify a product if the cost in USD"):
+        with self.assertRaisesRegex(ValidationError, "You cannot create or modify a product if the cost in USD"):
             self.set_standard_price_usd(1)
 
-    def test_sale_margin(self):
+    def test_05_sale_margin(self):
         """Test the sale margin module using a pricelist with cost in USD."""
         self.set_standard_price_usd(880)
-        product = self.product.with_context(pricelist=self.pricelist_15_mxn.id)
         # Create a sale order for product Graphics Card.
-        sale_order = self.sale_order.create(
-            {
-                "date_order": fields.Datetime.now(),
-                "name": "Test",
-                "order_line": [
-                    (
-                        0,
-                        0,
-                        {
-                            "name": "[CARD] Graphics Card",
-                            "product_uom": self.product_uom.id,
-                            "product_uom_qty": 1,
-                            "state": "draft",
-                            "product_id": product.id,
-                        },
-                    )
-                ],
-                "partner_id": self.partner.id,
-                "pricelist_id": self.pricelist_15_mxn.id,
-            }
-        )
+        sale_order = self.create_sale_order(product=self.product, pricelist=self.pricelist_15_mxn)
         # Confirm the sale order.
         sale_order.action_confirm()
         # Verify that margin field gets bind with the value.
         mxn_rate = self.mxn.rate / self.usd.rate
-        expected_price = float_round(
-            (product.standard_price_usd * 1.15) * mxn_rate, precision_rounding=self.mxn.rounding
-        )
-        expected_cost = float_round(product.standard_price_usd * mxn_rate, precision_rounding=self.mxn.rounding)
-        margin = float_round(expected_price - expected_cost, precision_rounding=self.mxn.rounding)
+        expected_price = self.mxn.round((self.product.standard_price_usd * 1.15) * mxn_rate)
+        expected_cost = self.mxn.round(self.product.standard_price_usd * mxn_rate)
+        margin = self.mxn.round(expected_price - expected_cost)
         self.assertEqual(
             float_compare(sale_order.margin, margin, precision_digits=2), 0, "Sale order margin should be %s" % margin
         )
 
-    def test_sale_margin_normal(self):
+    def test_06_sale_margin_normal(self):
         """Test the sale margin module using a pricelist without cost in
         USD.
         """
         # Create a sale order for product Graphics Card.
-        sale_order = self.sale_order.create(
-            {
-                "date_order": fields.Datetime.now(),
-                "name": "Test",
-                "order_line": [
-                    (
-                        0,
-                        0,
-                        {
-                            "name": "[CARD] Graphics Card",
-                            "product_uom": self.product_uom.id,
-                            "product_uom_qty": 1,
-                            "state": "draft",
-                            "product_id": self.product.id,
-                        },
-                    )
-                ],
-                "partner_id": self.partner.id,
-                "pricelist_id": self.pricelist.id,
-            }
-        )
+        sale_order = self.create_sale_order(product=self.product, pricelist=self.pricelist)
         # Confirm the sale order.
         sale_order.action_confirm()
         # Verify that margin field gets bind with the value.
