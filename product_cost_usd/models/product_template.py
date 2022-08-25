8a7f903975422b365e896976e725674996ac2384
Alejandro Garza <agarza@vauxoo.com>
2022-08-25 22:37:07 +0000

    [MIG] product_cost_usd: module migrated to v15.0

--- product_cost_usd/models/product_template.py
+++ product_cost_usd/models/product_template.py
@@ -2,14 +2,12 @@ from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 from odoo.tools import float_compare
 
-import odoo.addons.decimal_precision as dp
-
 
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     standard_price_usd = fields.Float(
-        "Cost in USD", digits=dp.get_precision("Product Price"), help="Price cost of the product in USD currency"
+        "Cost in USD", digits="Product Price", help="Price cost of the product in USD currency"
     )
 
     @api.constrains("standard_price_usd", "seller_ids")
@@ -23,20 +21,22 @@ class ProductTemplate(models.Model):
         """
         usd_currency = self.env.ref("base.USD")
         prec = self.env["decimal.precision"].precision_get("Product Price")
-        usd_seller = self.seller_ids.filtered(lambda x: x.currency_id == usd_currency)
-        list_price = usd_seller.price
-        standard_price_usd = self.standard_price_usd
-        if not usd_seller and float_compare(standard_price_usd, 0, precision_digits=prec) > 0:
-            raise ValidationError(
-                _("You must have at least one supplier with price in USD before assigning a Cost in USD")
-            )
-        if float_compare(list_price, standard_price_usd, precision_digits=prec) > 0:
-            raise ValidationError(
-                _(
-                    "You cannot create or modify a product if the cost in USD"
-                    " is less than the supplier list price.\n\n"
-                    "- Supplier list price = %s\n"
-                    "- Cost in USD = %s"
+        for product in self:
+            usd_seller = product.seller_ids.filtered(lambda x: x.currency_id == usd_currency)[:1]
+            list_price = usd_seller.price
+            standard_price_usd = product.standard_price_usd
+            if not usd_seller and float_compare(standard_price_usd, 0, precision_digits=prec) > 0:
+                raise ValidationError(
+                    _("You must have at least one supplier with price in USD before assigning a Cost in USD")
                 )
-                % (list_price, standard_price_usd)
-            )
+            if float_compare(list_price, standard_price_usd, precision_digits=prec) > 0:
+                raise ValidationError(
+                    _(
+                        "You cannot create or modify a product if the cost in USD"
+                        " is less than the supplier list price.\n\n"
+                        "- Supplier list price = %s\n"
+                        "- Cost in USD = %s",
+                        list_price,
+                        standard_price_usd,
+                    )
+                )
