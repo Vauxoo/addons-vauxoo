f5ee59a7f05047057bf584162a01d935227fc389
Luis González <lgonzalez@vauxoo.com>
2022-08-26 14:23:43 +0000

    [ADD] ci: enable tests using GitHub actions
    The following tests are added:
    - pre-commit:
      * mandatory
      * optional
    - tests:
      * odoo

---
+++ variables.sh
@@ -0,0 +1,12 @@
+export BASE_IMAGE="vauxoo/odoo-150-image"
+export DOCKER_IMAGE_REPO="quay.io/vauxoo/addons_vauxoo"
+export PSQL_VERSION="11"
+export VERSION="15.0"
+export ODOO_REPO="vauxoo/odoo"
+export ODOO_BRANCH="15.0"
+export TRAVIS_PYTHON_VERSION="3.7"
+export TRAVIS_REPO_SLUG="Vauxoo/"
+export COUNTRY="MX"
+export CUSTOMER="addons_vauxoo"
+export COVERAGE_MIN=70
+export COVERAGE_PRECISION=2
