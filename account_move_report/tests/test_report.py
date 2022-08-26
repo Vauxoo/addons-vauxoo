f9162743dc75b9bc0dda16aa81a31097da798167
Andy Quijada [Vauxoo] <andy@vauxoo.com>
2022-08-26 09:50:23 -0400

    [IMP] account_move_report: black, isort, prettier
    Apply pre-commit-vauxoo to reformat account_move_report the code

--- account_move_report/tests/test_report.py
+++ account_move_report/tests/test_report.py
@@ -4,14 +4,14 @@ from odoo.tests import Form, TransactionCase
 class TestReport(TransactionCase):
     def setUp(self):
         super().setUp()
-        self.customer = self.env.ref('base.res_partner_3')
-        self.product1 = self.env.ref('product.product_product_5')
-        self.move_report = self.env.ref('account_move_report.template_account_move_report')
+        self.customer = self.env.ref("base.res_partner_3")
+        self.product1 = self.env.ref("product.product_product_5")
+        self.move_report = self.env.ref("account_move_report.template_account_move_report")
 
     def create_invoice(self, partner=None, **line_kwargs):
         if partner is None:
             partner = self.customer
-        invoice = Form(self.env['account.move'].with_context(default_move_type='out_invoice'))
+        invoice = Form(self.env["account.move"].with_context(default_move_type="out_invoice"))
         invoice.partner_id = partner
         invoice = invoice.save()
         self.create_inv_line(invoice, **line_kwargs)
