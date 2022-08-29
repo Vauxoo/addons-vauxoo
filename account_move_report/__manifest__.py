03d7114f69e1df09d257e56a9892af7504624808
Andy Quijada [Vauxoo] <andy@vauxoo.com>
2022-08-29 14:03:40 -0400

    [MIG] account_move_report: Migrate account_move_report to 15.0 t#54296

--- account_move_report/__manifest__.py
+++ account_move_report/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Journal Entries Report",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "Vauxoo",
     "category": "Accounting/Accounting",
     "website": "http://www.vauxoo.com/",
@@ -9,10 +9,7 @@
         "account",
     ],
     "demo": [],
-    "data": [
-        "views/account_reports.xml",
-        "views/account_move_report.xml"
-    ],
+    "data": ["views/account_reports.xml", "views/account_move_report.xml"],
     "installable": True,
     "auto_install": False,
 }
