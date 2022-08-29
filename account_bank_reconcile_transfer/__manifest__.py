e3b2674e40d0d984b00cd2961ad34f7c967bab98
Andy Quijada [Vauxoo] <andy@vauxoo.com>
2022-08-29 16:58:28 -0400

    [IMP] *: black, isort, prettier t#57461
    The pre-commit-vauxoo command is executed with the format option active,
    additional the variable `PRECOMMIT_AUTOFIX="1"` is included in variable.sh
    file

--- account_bank_reconcile_transfer/__manifest__.py
+++ account_bank_reconcile_transfer/__manifest__.py
@@ -1,23 +1,23 @@
 {
-    'name': 'Account Bank Reconcile Transfer',
-    'summary': '''Create bank transfers directly from the bank reconciliation screen.''',
-    'author': 'Vauxoo',
-    'website': 'https://www.vauxoo.com',
-    'license': 'LGPL-3',
-    'category': 'Installer',
-    'version': '13.0.1.0.0',
-    'depends': [
-        'account',
+    "name": "Account Bank Reconcile Transfer",
+    "summary": """Create bank transfers directly from the bank reconciliation screen.""",
+    "author": "Vauxoo",
+    "website": "https://www.vauxoo.com",
+    "license": "LGPL-3",
+    "category": "Installer",
+    "version": "13.0.1.0.0",
+    "depends": [
+        "account",
     ],
-    'data': [
-        'views/assets.xml',
+    "data": [
+        "views/assets.xml",
     ],
-    'demo': [],
-    'qweb': [
-        'static/src/xml/bank_reconciliation.xml',
+    "demo": [],
+    "qweb": [
+        "static/src/xml/bank_reconciliation.xml",
     ],
-    'external_dependencies': {},
-    'installable': False,
-    'auto_install': False,
-    'application': False,
+    "external_dependencies": {},
+    "installable": True,
+    "auto_install": False,
+    "application": False,
 }
--- account_bank_reconcile_transfer/static/src/js/bank_reconciliation.js
+++ account_bank_reconcile_transfer/static/src/js/bank_reconciliation.js
@@ -1,35 +1,32 @@
-odoo.define('account_bank_reconcile_transfer.bank_reconciliation', function(require) {
+odoo.define("account_bank_reconcile_transfer.bank_reconciliation", function (require) {
     "use strict";
 
-    var LineRenderer = require('account.ReconciliationRenderer').LineRenderer;
-    var StatementModel = require('account.ReconciliationModel').StatementModel;
-    var relationalFields = require('web.relational_fields');
+    var LineRenderer = require("account.ReconciliationRenderer").LineRenderer;
+    var StatementModel = require("account.ReconciliationModel").StatementModel;
+    var relationalFields = require("web.relational_fields");
 
     StatementModel.include({
-
-        init: function() {
+        init: function () {
             this._super.apply(this, arguments);
-            this.quickCreateFields = [...this.quickCreateFields, 'transfer_journal_id'];
+            this.quickCreateFields = [...this.quickCreateFields, "transfer_journal_id"];
         },
 
-        updateProposition: function(handle, values) {
-            if ('transfer_journal_id' in values && values.transfer_journal_id) {
+        updateProposition: function (handle, values) {
+            if ("transfer_journal_id" in values && values.transfer_journal_id) {
                 var self = this;
                 var superCall = self._super;
-                return new Promise(function(resolve) {
+                return new Promise(function (resolve) {
                     self._rpc({
-                        model: 'res.company',
-                        method: 'read',
-                        args: [
-                            self.getLine(handle).st_line.company_id, ['transfer_account_id'],
-                        ],
-                    }).then(function(result) {
-                        var account = result[0]['transfer_account_id'];
+                        model: "res.company",
+                        method: "read",
+                        args: [self.getLine(handle).st_line.company_id, ["transfer_account_id"]],
+                    }).then(function (result) {
+                        var account = result[0]["transfer_account_id"];
                         values.account_id = {
                             id: account[0],
                             display_name: account[1],
                         };
-                        superCall.apply(self, [handle, values]).then(function() {
+                        superCall.apply(self, [handle, values]).then(function () {
                             resolve();
                         });
                     });
@@ -38,40 +35,39 @@ odoo.define('account_bank_reconcile_transfer.bank_reconciliation', function(requ
             return this._super(handle, values);
         },
 
-        _formatToProcessReconciliation: function(line, prop) {
+        _formatToProcessReconciliation: function (line, prop) {
             var result = this._super(line, prop);
 
-            if (prop.transfer_journal_id)
-                result.transfer_journal_id = prop.transfer_journal_id.id;
+            if (prop.transfer_journal_id) result.transfer_journal_id = prop.transfer_journal_id.id;
 
             return result;
         },
-
     });
 
     LineRenderer.include({
-
-        _renderCreate: function(state) {
+        _renderCreate: function (state) {
             var self = this;
-            return self._super(state).then(function() {
+            return self._super(state).then(function () {
                 var record = self.model.get(self.handleCreateRecord);
 
                 record.fields.transfer_journal_id = {
-                    relation: 'account.journal',
-                    type: 'many2one',
+                    relation: "account.journal",
+                    type: "many2one",
                     domain: [
-                        ['company_id', '=', state.st_line.company_id],
-                        ['id', '!=', state.st_line.journal_id],
-                        ['type', 'in', ['bank', 'cash']],
+                        ["company_id", "=", state.st_line.company_id],
+                        ["id", "!=", state.st_line.journal_id],
+                        ["type", "in", ["bank", "cash"]],
                     ],
                 };
 
                 self.fields.transfer_journal_id = new relationalFields.FieldMany2One(
-                    self, 'transfer_journal_id', record, { mode: 'edit' });
-                self.fields.transfer_journal_id.appendTo(self.$('.create .transfer_journal_id .o_td_field'));
+                    self,
+                    "transfer_journal_id",
+                    record,
+                    {mode: "edit"}
+                );
+                self.fields.transfer_journal_id.appendTo(self.$(".create .transfer_journal_id .o_td_field"));
             });
         },
-
     });
-
 });
