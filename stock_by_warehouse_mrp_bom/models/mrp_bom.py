29fe238b2c173b082192c654e15109d2a43c5cde
Andy Quijada [Vauxoo] <andy@vauxoo.com>
2022-08-29 21:46:58 -0400

    [IMP] stock_by_warehouse_mrp_bom: black, isort, prettier

--- stock_by_warehouse_mrp_bom/models/mrp_bom.py
+++ stock_by_warehouse_mrp_bom/models/mrp_bom.py
@@ -5,9 +5,7 @@ class MrpBomLine(models.Model):
     _inherit = "mrp.bom.line"
 
     warehouses_stock = fields.Text(store=False, readonly=True)
-    warehouse_id = fields.Many2one(
-        string="Warehouse",
-        related='bom_id.picking_type_id.warehouse_id')
+    warehouse_id = fields.Many2one(string="Warehouse", related="bom_id.picking_type_id.warehouse_id")
     warehouses_stock_recompute = fields.Boolean(store=False, readonly=False)
 
     def _compute_get_warehouses_stock(self):
@@ -16,7 +14,7 @@ class MrpBomLine(models.Model):
                 warehouse_id=line.warehouse_id
             )._compute_get_quantity_warehouses_json()
 
-    @api.onchange('warehouses_stock_recompute', 'product_id')
+    @api.onchange("warehouses_stock_recompute", "product_id")
     def _warehouses_stock_recompute_onchange(self):
         if not self.warehouses_stock_recompute:
             self.warehouses_stock_recompute = True
