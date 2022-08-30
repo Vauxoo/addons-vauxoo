afa2ef5d5cf7fd12477b7d9b5fbaff27409edec5
Andy Quijada [Vauxoo] <andy@vauxoo.com>
2022-08-29 21:35:22 -0400

    [IMP] default_warehouse_from_sale_team: black, isort, prettier

--- default_warehouse_from_sale_team/models/stock_picking.py
+++ default_warehouse_from_sale_team/models/stock_picking.py
@@ -3,6 +3,6 @@ from odoo import fields, models
 
 class StockPicking(models.Model):
     _name = "stock.picking"
-    _inherit = ['default.warehouse.mixin', 'stock.picking']
+    _inherit = ["default.warehouse.mixin", "stock.picking"]
 
-    warehouse_id = fields.Many2one(related='picking_type_id.warehouse_id')
+    warehouse_id = fields.Many2one(related="picking_type_id.warehouse_id")
