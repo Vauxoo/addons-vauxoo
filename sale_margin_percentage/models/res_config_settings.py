99d9f0ecdf43f82d76e5d60063b1d511790c73ca
Andy Quijada [Vauxoo] <andy@vauxoo.com>
2022-08-29 21:38:03 -0400

    [IMP] sale_margin_percentage: black, isort, prettier

--- sale_margin_percentage/models/res_config_settings.py
+++ sale_margin_percentage/models/res_config_settings.py
@@ -2,9 +2,6 @@ from odoo import fields, models
 
 
 class ResConfiguration(models.TransientModel):
-    _inherit = 'res.config.settings'
+    _inherit = "res.config.settings"
 
-    margin_threshold = fields.Float(
-        related='company_id.margin_threshold',
-        readonly=False
-    )
+    margin_threshold = fields.Float(related="company_id.margin_threshold", readonly=False)
--- sale_margin_percentage/models/sale_order.py
+++ sale_margin_percentage/models/sale_order.py
@@ -1,20 +1,21 @@
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     margin_percentage = fields.Float(
-        compute='_compute_margin_percentage',
-        digits='Product Price',
+        compute="_compute_margin_percentage",
+        digits="Product Price",
         store=True,
-        help="Margin percentage compute based on price unit")
+        help="Margin percentage compute based on price unit",
+    )
 
-    @api.depends('order_line.margin_percentage')
+    @api.depends("order_line.margin_percentage")
     def _compute_margin_percentage(self):
         for order in self:
-            lines = order.order_line.filtered(lambda r: r.state != 'cancel')
-            margin_sum = sum(lines.mapped('margin'))
+            lines = order.order_line.filtered(lambda r: r.state != "cancel")
+            margin_sum = sum(lines.mapped("margin"))
             subtotal = order.amount_untaxed
             if not subtotal or not margin_sum:
                 order.margin_percentage = 0.0
@@ -26,21 +27,19 @@ class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
     margin_threshold = fields.Float(
-        default=lambda self: self.env.user.company_id.margin_threshold,
-        help="Limit margin set in sales configuration")
+        default=lambda self: self.env.user.company_id.margin_threshold, help="Limit margin set in sales configuration"
+    )
     margin_percentage = fields.Float(
-        compute='_compute_margin_percentage',
-        digits='Product Price',
+        compute="_compute_margin_percentage",
+        digits="Product Price",
         store=True,
-        help="Margin percentage compute based on price unit")
-    purchase_price = fields.Float(
-        readonly=True, help="Price purchase of product")
+        help="Margin percentage compute based on price unit",
+    )
+    purchase_price = fields.Float(readonly=True, help="Price purchase of product")
 
-    @api.depends('product_id', 'purchase_price', 'product_uom_qty',
-                 'price_unit', 'price_subtotal')
+    @api.depends("product_id", "purchase_price", "product_uom_qty", "price_unit", "price_subtotal")
     def _compute_margin_percentage(self):
-        """ Same margin but we return a percentage from the purchase price.
-        """
+        """Same margin but we return a percentage from the purchase price."""
         for line in self:
             currency = line.order_id.pricelist_id.currency_id
 
@@ -58,7 +57,5 @@ class SaleOrderLine(models.Model):
                 continue
 
             line.margin_percentage = currency.round(
-                (
-                    line.price_subtotal - purchase_price * line.product_uom_qty
-                ) * 100.0 / line.price_subtotal
+                (line.price_subtotal - purchase_price * line.product_uom_qty) * 100.0 / line.price_subtotal
             )
