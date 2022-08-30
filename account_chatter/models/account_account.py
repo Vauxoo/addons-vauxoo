c527f77bdf60dc6548624c26e2af8d74a5a3e6a2
Andy Quijada [Vauxoo] <andy@vauxoo.com>
2022-08-29 21:24:52 -0400

    [IMP] account_chatter: black, isort, prettier

--- account_chatter/models/account_account.py
+++ account_chatter/models/account_account.py
@@ -1,13 +1,13 @@
 # Copyright 2020 Vauxoo
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class AccountAccount(models.Model):
 
     _name = "account.account"
-    _inherit = ['account.account', 'mail.thread', 'mail.activity.mixin']
+    _inherit = ["account.account", "mail.thread", "mail.activity.mixin"]
 
     tax_ids = fields.Many2many(tracking=True)
     tag_ids = fields.Many2many(tracking=True)
