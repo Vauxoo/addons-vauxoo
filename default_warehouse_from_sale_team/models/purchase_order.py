f42c800b27a1e290bc2f353ed2b82c837ab9bae2
Andy Quijada [Vauxoo] <andy@vauxoo.com>
2022-08-29 21:35:22 -0400

    [IMP] default_warehouse_from_sale_team: black, isort, prettier

--- default_warehouse_from_sale_team/models/purchase_order.py
+++ default_warehouse_from_sale_team/models/purchase_order.py
@@ -2,5 +2,5 @@ from odoo import models
 
 
 class PurchaseOrder(models.Model):
-    _name = 'purchase.order'
+    _name = "purchase.order"
     _inherit = ["default.picking.type.mixin", "purchase.order"]
