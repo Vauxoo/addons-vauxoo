7ba16617bdc5f4e9032d0587b7e9810e76ea4827
❖ | Nhomar <nhomar@vauxoo.com>
2022-10-04 23:51:41 -0500

    [IMP] account_analytic_business_unit: 3 improvements.
    1.- Sequence to force order on accounts to see your cost center better ordered.
    2.- Better name, before this change the name when BU used was pointless

--- account_analytic_business_unit/models/account_analytic_account.py
+++ account_analytic_business_unit/models/account_analytic_account.py
@@ -3,17 +3,21 @@ from odoo import fields, models
 
 class AccountAnalytic(models.Model):
     _inherit = "account.analytic.account"
+    _order = "business_unit_id desc, sequence asc, code, name asc"
 
     business_unit_id = fields.Many2one("account.analytic.business.unit")
+    sequence = fields.Integer()
 
     def name_get(self):
         res = []
         for analytic in self:
             name = analytic.name
+            bu_prefix = ""
             if analytic.business_unit_id:
-                name = "BU: " + analytic.business_unit_id.code
+                bu_prefix = analytic.business_unit_id.code + ":"
+                name = bu_prefix + analytic.name
             if analytic.code:
-                name = "[" + analytic.code + "] " + name
+                name = bu_prefix + ":" + analytic.code + " - " + analytic.name
             if analytic.partner_id.commercial_partner_id.name:
                 name = name + " - " + analytic.partner_id.commercial_partner_id.name
             res.append((analytic.id, name))
