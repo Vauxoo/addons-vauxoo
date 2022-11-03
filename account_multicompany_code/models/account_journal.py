680166e9ca9fac4df7795f895601a415d0b0717d
Luis Manuel [Vauxoo] <luis.manuel@vauxoo.com>
2022-11-03 10:01:55 -0600

    [IMP] account_multicompany_code: fixes pending from migration
    - Split the models in individual files
    - Add the code field to tree and kanban view
    - Define a setUpClass method for tests
    - Set a SQL constraint "code_unique" for efficiency
    - Rename file: company_view -> res_company_views.xml

--- account_multicompany_code/models/account_journal.py
+++ account_multicompany_code/models/account_journal.py
@@ -1,17 +1,6 @@
 from odoo import models
 
 
-class AccountAccount(models.Model):
-    _inherit = "account.account"
-
-    def name_get(self):
-        result = dict(super().name_get())
-        for record in self:
-            if record.company_id.code:
-                result[record.id] += " (%s)" % record.company_id.code
-        return list(result.items())
-
-
 class AccountJournal(models.Model):
     _inherit = "account.journal"
 
