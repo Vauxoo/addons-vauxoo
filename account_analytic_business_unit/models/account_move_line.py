ecb93101c0e0bc4080ab9165e932e43d163d0cd1
Emilio Serna <emilio@vauxoo.com>
2023-01-05 21:19:06 +0000

    [IMP] account_analytic_business_unit: Added related field business_unit_id

--- account_analytic_business_unit/models/account_move_line.py
+++ account_analytic_business_unit/models/account_move_line.py
@@ -5,6 +5,10 @@ class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
     analytic_line_count = fields.Integer(compute="_compute_total_analytic_lines")
+    business_unit_id = fields.Many2one(
+        related="analytic_account_id.business_unit_id",
+        store=True,
+    )
 
     def action_open_analytic_lines(self):
         action = self.env["ir.actions.actions"]._for_xml_id("analytic.account_analytic_line_action_entries")
