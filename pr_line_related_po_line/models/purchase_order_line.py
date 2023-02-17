e79747f3b068a84a2564ca17cbc4bb0cd7dcf52d
Vicente Escaname <vicente@vauxoo.com>
2023-02-17 09:08:27 -0600

    [REF] pr_line_related_po_line: General module cleanup
    - In purchase_order_views model attribute was set after id
    - In the same view encoding was added to the header
    - Changed README text to be clearer and simpler

--- pr_line_related_po_line/models/purchase_order_line.py
+++ pr_line_related_po_line/models/purchase_order_line.py
@@ -2,7 +2,6 @@ from odoo import fields, models
 
 
 class PurchaseOrderLine(models.Model):
-
     _inherit = "purchase.order.line"
 
     purchase_requisition_line_id = fields.Many2one("purchase.requisition.line")
