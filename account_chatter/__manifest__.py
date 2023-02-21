448f16edd3ecc37163a7a9400d56263f56b84948
Vicente Escaname <vicente@vauxoo.com>
2023-02-21 20:05:49 +0000

    [MIG] account_chatter: Migration to v16.0
    Due to the inability to add groups to inherited views in v16, the views
    in this module were refactored so they would still function as intended

--- account_chatter/__manifest__.py
+++ account_chatter/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Account Chatter",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Vauxoo",
     "category": "Accounting",
     "website": "http://www.vauxoo.com",
