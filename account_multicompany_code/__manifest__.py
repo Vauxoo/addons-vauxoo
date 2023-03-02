65e5554b2de9cfe09f0e95d099f79e9453541976
Luis Manuel [Vauxoo] <luis.manuel@vauxoo.com>
2023-03-02 13:55:53 -0600

    [MIG] account_multicompany_code: Migration to 16.0

--- account_multicompany_code/__manifest__.py
+++ account_multicompany_code/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Account Multicompany Code",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Vauxoo",
     "category": "Accounting/Accounting",
     "website": "https://www.vauxoo.com/",
