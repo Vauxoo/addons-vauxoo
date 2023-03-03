2ac7e17f23e99fd4ffe70d92c6df5fee32feb25e
Luis González <lgonzalez@vauxoo.com>
2023-03-03 16:15:27 +0000

    [IMP] account_multicompany_code: don't depend on any CoA in tests
    Test account and journal are created in tests, so tests may run even if
    no chart of account is installed yet.

--- account_multicompany_code/tests/test_multicompany_code.py
+++ account_multicompany_code/tests/test_multicompany_code.py
@@ -8,27 +8,30 @@ class TestMulticompanyCode(TransactionCase):
 
         cls.company = cls.env.ref("base.main_company")
         cls.company.code = "TEST-CODE"
+        cls.account_type_expenses = cls.env.ref("account.data_account_type_expenses")
 
     def test_01_check_code_included_account_name(self):
         """Check that the code is shown in the account display names"""
-        account = self.env["account.account"].search(
-            [
-                ("company_id", "=", self.company.id),
-                ("deprecated", "=", False),
-            ],
-            limit=1,
+        account = self.env["account.account"].create(
+            {
+                "name": "Account Name",
+                "code": "999.01.05",
+                "user_type_id": self.account_type_expenses.id,
+                "company_id": self.company.id,
+            }
         )
-        expected_account_name = "%s %s (TEST-CODE)" % (account.code, account.name)
+        expected_account_name = "999.01.05 Account Name (TEST-CODE)"
         self.assertEqual(account.display_name, expected_account_name)
 
     def test_02_check_code_included_journal_name(self):
         """Check that the code is shown in the account display names"""
-        journal = self.env["account.journal"].search(
-            [
-                ("company_id", "=", self.company.id),
-                ("currency_id", "=", False),
-            ],
-            limit=1,
+        journal = self.env["account.journal"].create(
+            {
+                "name": "Journal Name",
+                "code": "JTEST",
+                "type": "general",
+                "company_id": self.company.id,
+            }
         )
-        expected_journal_name = "%s (TEST-CODE)" % journal.name
+        expected_journal_name = "Journal Name (TEST-CODE)"
         self.assertEqual(journal.display_name, expected_journal_name)
