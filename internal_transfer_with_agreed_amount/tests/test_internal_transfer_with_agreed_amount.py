27ca778963844147d1e18e5872e220db2dffddce
Christihan Laurel [Vauxoo] <laurel@vauxoo.com>
2023-08-08 09:30:54 +0000

    [IMP] internal_transfer_with_agreed_amount: black, isort, prettier
    Changelog:
    - Apply the automatic changes from pre-commit-vauxoo

--- internal_transfer_with_agreed_amount/tests/test_internal_transfer_with_agreed_amount.py
+++ internal_transfer_with_agreed_amount/tests/test_internal_transfer_with_agreed_amount.py
@@ -3,21 +3,26 @@ from odoo.tests import Form, TransactionCase
 
 class TestInternalTransferWithAgreedAmount(TransactionCase):
     def setUp(self):
-        super(TestInternalTransferWithAgreedAmount, self).setUp()
+        super().setUp()
         self.currency_usd = self.env.ref("base.USD")
         self.currency_eur = self.env.ref("base.EUR")
-        self.bank_journal_usd = self.env['account.journal'].create({
-            'name': 'Bank US', 'type': 'bank', 'code': 'BNK68',
-            'update_posted': True})
-        self.bank_journal_eur = self.env['account.journal'].create({
-            'name': 'Bank EUR', 'type': 'bank', 'code': 'BNK67',
-            'currency_id': self.currency_eur.id, 'update_posted': True})
-        self.payment_method_manual = self.env.ref(
-            "account.account_payment_method_manual_in")
+        self.bank_journal_usd = self.env["account.journal"].create(
+            {"name": "Bank US", "type": "bank", "code": "BNK68", "update_posted": True}
+        )
+        self.bank_journal_eur = self.env["account.journal"].create(
+            {
+                "name": "Bank EUR",
+                "type": "bank",
+                "code": "BNK67",
+                "currency_id": self.currency_eur.id,
+                "update_posted": True,
+            }
+        )
+        self.payment_method_manual = self.env.ref("account.account_payment_method_manual_in")
 
     def create_internal_transfer(self, currency, journal, destination_journal, amount):
-        transfer = Form(self.env['account.payment'])
-        transfer.payment_type = 'transfer'
+        transfer = Form(self.env["account.payment"])
+        transfer.payment_type = "transfer"
         transfer.journal_id = journal
         transfer.currency_id = currency
         transfer.destination_journal_id = destination_journal
@@ -25,8 +30,8 @@ class TestInternalTransferWithAgreedAmount(TransactionCase):
         return transfer.save()
 
     def create_multicurrency_transfer(self, payment, agreed_amount, currency):
-        ctx = {'active_model': payment._name, 'active_ids': payment.ids}
-        wizard = Form(self.env['internal.transfer.multicurrency'].with_context(**ctx))
+        ctx = {"active_model": payment._name, "active_ids": payment.ids}
+        wizard = Form(self.env["internal.transfer.multicurrency"].with_context(**ctx))
         wizard.agreed_amount = agreed_amount
         wizard.currency_id = currency
         wizard = wizard.save()
@@ -34,49 +39,69 @@ class TestInternalTransferWithAgreedAmount(TransactionCase):
         return wizard
 
     def test_01_transfer_usd_eur(self):
-        transfer = self.create_internal_transfer(
-            self.currency_usd, self.bank_journal_usd,
-            self.bank_journal_eur, 100)
+        transfer = self.create_internal_transfer(self.currency_usd, self.bank_journal_usd, self.bank_journal_eur, 100)
         self.create_multicurrency_transfer(transfer, 120, self.currency_eur)
 
         # Check journal item in USD
         usd_aml = transfer.move_line_ids.filtered(
-            lambda m: m.account_id == transfer.journal_id.default_debit_account_id)
-        self.assertRecordValues(usd_aml, [{
-            'balance': -100.0,  # payment amount
-            'amount_currency': 0.0,
-            'currency_id': False,
-        }])
+            lambda m: m.account_id == transfer.journal_id.default_debit_account_id
+        )
+        self.assertRecordValues(
+            usd_aml,
+            [
+                {
+                    "balance": -100.0,  # payment amount
+                    "amount_currency": 0.0,
+                    "currency_id": False,
+                }
+            ],
+        )
 
         # Check journal item in EUR
         eur_aml = transfer.move_line_ids.filtered(
-            lambda m: m.account_id == transfer.destination_journal_id.default_credit_account_id)
-        self.assertRecordValues(eur_aml, [{
-            'balance': 100.0,  # payment amount
-            'amount_currency': 120.0,  # Agreed amount
-            'currency_id': self.currency_eur.id,
-        }])
+            lambda m: m.account_id == transfer.destination_journal_id.default_credit_account_id
+        )
+        self.assertRecordValues(
+            eur_aml,
+            [
+                {
+                    "balance": 100.0,  # payment amount
+                    "amount_currency": 120.0,  # Agreed amount
+                    "currency_id": self.currency_eur.id,
+                }
+            ],
+        )
 
     def test_02_transfer_eur_usd(self):
-        transfer = self.create_internal_transfer(
-            self.currency_eur, self.bank_journal_eur,
-            self.bank_journal_usd, 100)
+        transfer = self.create_internal_transfer(self.currency_eur, self.bank_journal_eur, self.bank_journal_usd, 100)
         self.create_multicurrency_transfer(transfer, 80, self.currency_usd)
 
         # Check journal item in USD
         usd_aml = transfer.move_line_ids.filtered(
-            lambda m: m.account_id == transfer.destination_journal_id.default_credit_account_id)
-        self.assertRecordValues(usd_aml, [{
-            'balance': 80.0,  # Agreed amount
-            'amount_currency': 0.0,
-            'currency_id': False,
-        }])
+            lambda m: m.account_id == transfer.destination_journal_id.default_credit_account_id
+        )
+        self.assertRecordValues(
+            usd_aml,
+            [
+                {
+                    "balance": 80.0,  # Agreed amount
+                    "amount_currency": 0.0,
+                    "currency_id": False,
+                }
+            ],
+        )
 
         # Check journal item in EUR
         eur_aml = transfer.move_line_ids.filtered(
-            lambda m: m.account_id == transfer.journal_id.default_credit_account_id)
-        self.assertRecordValues(eur_aml, [{
-            'balance': -80.0,  # Agreed amount
-            'amount_currency': -100.0,  # Payment amount
-            'currency_id': self.currency_eur.id,
-        }])
+            lambda m: m.account_id == transfer.journal_id.default_credit_account_id
+        )
+        self.assertRecordValues(
+            eur_aml,
+            [
+                {
+                    "balance": -80.0,  # Agreed amount
+                    "amount_currency": -100.0,  # Payment amount
+                    "currency_id": self.currency_eur.id,
+                }
+            ],
+        )
