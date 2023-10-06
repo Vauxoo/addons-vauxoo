ebecd64eac5e32354fdc59b910bf0f7bcd75f71e
rolandojduartem <rolando@vauxoo.com>
2023-10-06 18:44:09 +0000

    [MIG] mail_channel_notify_tag: Migrate to 16.0

--- mail_channel_notify_tag/static/src/models/composer_view/composer_view.esm.js
+++ mail_channel_notify_tag/static/src/models/composer_view/composer_view.esm.js
@@ -1,15 +1,18 @@
 /** @odoo-module **/
 
-import {registerInstancePatchModel} from "@mail/model/model_core";
+import {registerPatch} from "@mail/model/model_core";
 
-registerInstancePatchModel("mail.composer_view", "mail_channel_notify_tag", {
-    /**
-     * @override
-     */
-    _getMessageData() {
-        return {
-            ...this._super(...arguments),
-            channel_ids: this.composer.mentionedChannels.map((channel) => channel.id),
-        };
+registerPatch({
+    name: "ComposerView",
+    recordMethods: {
+        /**
+         * @override
+         */
+        _getMessageData() {
+            return {
+                ...this._super(...arguments),
+                channel_ids: this.composer.mentionedChannels.map((channel) => channel.id),
+            };
+        },
     },
 });
