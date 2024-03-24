bf18fffc88bd0fb4ca135620479b8172767ec7f8
Randall Castro <randall@vauxoo.com>
2024-03-23 19:36:21 -0600

    [MIG] account_chatter: migration to 17.0

--- account_chatter/__manifest__.py
+++ account_chatter/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Account Chatter",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author": "Vauxoo",
     "category": "Accounting",
     "website": "http://www.vauxoo.com",
