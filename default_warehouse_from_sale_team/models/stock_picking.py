69c738f47275d5eb40595c0f79034a4dca20c1c9
Luis González <lgonzalez@vauxoo.com>
2024-09-05 15:42:31 +0000

    [FIX] default_warehouse_from_sale_team: usability for readonly users
    Limited users are able to see some pickings in readonly mode, but there
    were some usability issues that are fixed in this commit:
    - It was possible to mark as TODo and check availability, because those
      actions don't require write access to the pickings. This is fixed by
      hiding involved buttons.
    - Even though validating a picking does require access, it's also hidden
      because there's no point on showing a button that can't be used anyway.
    - Similarly, the Print button is also hidden when the picking is not
      done yet, because it requires access to mark the picking as printed.
    - The Return button is also hidden. Even though it doesn't require
      write access to the current picking, it will require creation access
      on the new picking.
    - The Cancel button is not hidden because it requires being inventory
      administrator, which is not compatible with being a limited user.

--- default_warehouse_from_sale_team/models/stock_picking.py
+++ default_warehouse_from_sale_team/models/stock_picking.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class StockPicking(models.Model):
@@ -6,3 +6,18 @@ class StockPicking(models.Model):
     _inherit = ["default.warehouse.mixin", "stock.picking"]
 
     warehouse_id = fields.Many2one(related="picking_type_id.warehouse_id")
+    is_editable = fields.Boolean(compute="_compute_is_editable")
+
+    @api.depends_context("uid")
+    def _compute_is_editable(self):
+        # Using intersection operator to keep original env
+        editable_records = self & self._filter_access_rules_python("write")
+        editable_records.is_editable = True
+        (self - editable_records).is_editable = False
+
+    @api.depends("is_editable")
+    def _compute_show_check_availability(self):
+        res = super()._compute_show_check_availability()
+        non_editable = self - self.filtered("is_editable")
+        non_editable.show_check_availability = False
+        return res
