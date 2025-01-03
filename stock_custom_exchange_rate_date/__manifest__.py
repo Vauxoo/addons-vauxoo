fbee30a1209fa8b2e0835293b8669cf40d030d3b
German Loredo [Vauxoo] <german@vauxoo.com>
2025-01-03 16:05:15 +0000

    [MIG] stock_custom_exchange_rate_date: Migration to 17.0
    The following actions were performed:
    
    - Update language file.
    - Migrate stock.move.line according to [1].
    - Migrate attrs in views according to [2].
    - Improve code by using new native methods.
    
    [1]: https://github.com/odoo/odoo/commit/7dda6bb9
    [2]: https://github.com/odoo/odoo/pull/104741

--- stock_custom_exchange_rate_date/__manifest__.py
+++ stock_custom_exchange_rate_date/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Customized Rate Date in Stock",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author": "Vauxoo",
     "category": "stock",
     "website": "http://www.vauxoo.com/",
--- stock_custom_exchange_rate_date/models/stock_move.py
+++ stock_custom_exchange_rate_date/models/stock_move.py
@@ -9,19 +9,12 @@ class StockMove(models.Model):
         line = self.purchase_line_id
         if (
             not self.picking_id.exchange_rate_date
-            or not line
             or self.product_id != line.product_id
             or line.currency_id == line.company_id.currency_id
-            or self.origin_returned_move_id
+            or self._should_ignore_pol_price()
         ):
             return super()._get_price_unit()
-        price_unit = line.price_unit
-        if line.taxes_id:
-            price_unit = line.taxes_id.with_context(round=False).compute_all(
-                price_unit, currency=line.currency_id, quantity=1.0
-            )["total_excluded"]
-        if line.product_uom.id != line.product_id.uom_id.id:
-            price_unit *= line.product_uom.factor / line.product_id.uom_id.factor
+        price_unit = line._get_gross_price_unit()
         price_unit = line.currency_id._convert(
             price_unit, line.company_id.currency_id, line.company_id, self.picking_id.exchange_rate_date, round=False
         )
