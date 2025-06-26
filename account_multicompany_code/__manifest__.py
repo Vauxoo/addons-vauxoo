afc63ddc88cc4fc110fbc6eee75f3c1cfb8b3aa5
Isaac López <isaac@vauxoo.com>
2025-06-25 18:45:30 -0600

    [MIG] account_multicompany_code: Migration to 18.0

--- account_multicompany_code/__manifest__.py
+++ account_multicompany_code/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Account Multicompany Code",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Vauxoo",
     "category": "Accounting/Accounting",
     "website": "https://www.vauxoo.com/",
