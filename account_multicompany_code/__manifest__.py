3d0c6d3fc1e6bfcecd226a7dc1c5afa367840dce
Isaac López <isaac@vauxoo.com>
2025-06-26 19:11:38 -0600

    [IMP] account_multicompany_code: quinta cambio

--- account_multicompany_code/__manifest__.py
+++ account_multicompany_code/__manifest__.py
@@ -1,6 +1,7 @@
 # cambio en V17 no porteado
 # otro mas
 # Cuarto cambio para portear a V18
+# quinto cambio que no ha sido porteado a V18
 {
     "name": "Account Multicompany Code",
     "version": "18.0.1.0.0",
