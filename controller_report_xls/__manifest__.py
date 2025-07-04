e772ad8fd40c68320529c08c72029d5cd1a01756
Daniel Sánchez <d.sanchez@vauxoo.com>
2025-07-04 12:06:00 -0600

    [MIG] controller_report_xls: Migration to 16.0

--- controller_report_xls/__manifest__.py
+++ controller_report_xls/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "XLS Report Controller",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Vauxoo",
     "category": "Tools",
     "website": "https://www.vauxoo.com/",
