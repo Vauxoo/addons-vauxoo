0eeda6fa7e96da1c1877c3e32a373a5ea47d2ac6
OscarOlar <oscar@vauxoo.com>
2025-10-07 17:32:22 -0600

    [IMP] scerd: description in HTML and assets for Apps store
     - Created an HTML description, cover image and icon in order to
    sell the App at Odoo's Apps Store.

--- stock_custom_exchange_rate_date/__manifest__.py
+++ stock_custom_exchange_rate_date/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "Customized Rate Date in Stock",
     "version": "18.0.1.0.0",
     "author": "Vauxoo",
-    "category": "stock",
+    "category": "Warehouse",
     "website": "http://www.vauxoo.com/",
     "license": "OPL-1",
     "depends": [
@@ -12,6 +12,9 @@
         "views/stock_picking_views.xml",
     ],
     "demo": [],
+    "images": [
+        "static/description/main_screen.png",
+    ],
     "installable": True,
     "auto_install": False,
     "price": 230,
