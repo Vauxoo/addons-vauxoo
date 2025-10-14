f2c8af5f3954e183e5c0781a5d129a4682866288
OscarOlar <oscar@vauxoo.com>
2025-10-13 19:07:52 -0600

    [IMP] account_chatter: description in HTML and assets for Apps store
     - Created an HTML description, cover image and icon in order to
    sell the App at Odoo's Apps Store.

--- account_chatter/__manifest__.py
+++ account_chatter/__manifest__.py
@@ -2,6 +2,9 @@
     "name": "Account Chatter",
     "version": "18.0.1.0.0",
     "author": "Vauxoo",
+    "summary": """
+    Use chatter in CoA, Journals and Account Moves, to create audit changes in every model.
+    """,
     "category": "Accounting",
     "website": "http://www.vauxoo.com",
     "license": "LGPL-3",
@@ -15,6 +18,11 @@
         "views/account_journal_views.xml",
         "views/account_move_views.xml",
     ],
+    "images": [
+        "static/description/main_screen.png",
+    ],
     "installable": True,
     "auto_install": False,
+    "price": 130,
+    "currency": "EUR",
 }
