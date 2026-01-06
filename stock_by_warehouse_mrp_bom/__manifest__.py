80cd8967e19b3f96389cfd3d87a667e42e5feee7
Axel Ruiz L <axel@vauxoo.com>
2026-01-06 22:08:28 +0000

    [MIG] stock_by_warehouse_mrp_bom: migrate to 19.0
    Perform the following changes for the 19.0 migration:
    
    - Add the is_storable field on mrp.bom.line instead of detailed_type.
      The detailed_type field was deprecated in version 18.0
    - Change the onchange method to a compute method on the mrp.bom.line
      model to ensure it always has the latest information.

--- stock_by_warehouse_mrp_bom/__manifest__.py
+++ stock_by_warehouse_mrp_bom/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Stock by Warehouse on MRP_BoM",
-    "version": "15.0.1.0.0",
+    "version": "19.0.1.0.0",
     "summary": """
     Know the stock in all your warehouses with a simple click
     from the mrp bill of material's line form.
