a4d76461dc9b9b6b1cb669c02301296754cc2c26
Axel Ruiz L <axel@vauxoo.com>
2026-01-06 22:08:28 +0000

    [MIG] stock_by_warehouse_mrp_bom: migrate to 19.0
    Perform the following changes for the 19.0 migration:
    
    - Add the is_storable field on mrp.bom.line instead of detailed_type.
      The detailed_type field was deprecated in version 18.0
    - Change the onchange method to a compute method on the mrp.bom.line
      model to ensure it always has the latest information.

--- stock_by_warehouse_mrp_bom/models/mrp_bom_line.py
+++ stock_by_warehouse_mrp_bom/models/mrp_bom_line.py
@@ -4,22 +4,17 @@ from odoo import api, fields, models
 class MrpBomLine(models.Model):
     _inherit = "mrp.bom.line"
 
-    warehouses_stock = fields.Text(store=False, readonly=True)
+    warehouses_stock = fields.Text(compute="_compute_get_warehouses_stock", store=False, readonly=True)
     warehouse_id = fields.Many2one(string="Warehouse", related="bom_id.picking_type_id.warehouse_id")
-    warehouses_stock_recompute = fields.Boolean(store=False, readonly=False)
-    detailed_type = fields.Selection(related="product_id.detailed_type")
+    warehouses_stock_recompute = fields.Boolean(store=False)
+    is_storable = fields.Boolean(related="product_id.is_storable")
 
+    @api.depends("product_id", "warehouse_id", "warehouses_stock_recompute")
     def _compute_get_warehouses_stock(self):
         for line in self:
-            line.warehouses_stock = line.product_id.with_context(
-                warehouse_id=line.warehouse_id
-            )._compute_get_quantity_warehouses_json()
-
-    @api.onchange("warehouses_stock_recompute", "product_id")
-    def _warehouses_stock_recompute_onchange(self):
-        if not self.warehouses_stock_recompute:
-            self.warehouses_stock_recompute = True
-            return
-        self.warehouse_id = self.bom_id.picking_type_id.warehouse_id
-        self._compute_get_warehouses_stock()
-        self.warehouses_stock_recompute = True
+            if line.product_id:
+                line.warehouses_stock = line.product_id.with_context(
+                    warehouse_id=line.warehouse_id.id
+                )._compute_get_quantity_warehouses_json()
+            else:
+                line.warehouses_stock = ""
