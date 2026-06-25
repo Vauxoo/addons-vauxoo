cdfc4fbc668a1fbf656517c785865f27fbb3ca13
Luigys Toro <luigys@vauxoo.com>
2026-06-25 14:48:45 -0500

    [MIG] pr_line_related_po_line: migrate from 16.0 to 19.0
    Migrate the purchase requisition / purchase order line relation module to Odoo
    19.0. Installs cleanly; the _prepare_purchase_order_line signature is unchanged
    in 19.0 so the override keeps working. The inherited purchase order view was
    adapted to the list/form syntax. Drop the superfluous installable/auto_install
    manifest keys.

---
+++ pr_line_related_po_line/README.rst
@@ -0,0 +1,5 @@
+PR Line related PO Line
+=======================
+
+This module adds a new field to purchase order lines to link them to the requisition lines from which the purchase
+order is created. It also links the related purchase requisition line to a purchase order line when one is created
---
+++ pr_line_related_po_line/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "PR Line related PO Line",
+    "version": "19.0.1.0.0",
+    "author": "Vauxoo",
+    "category": "Inventory/Purchase",
+    "website": "http://www.vauxoo.com/",
+    "license": "LGPL-3",
+    "depends": [
+        "purchase_requisition",
+    ],
+    "data": [
+        "views/purchase_order_views.xml",
+    ],
+}
